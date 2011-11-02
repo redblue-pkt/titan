a4b2443fe23e9ffb8b01348a1e2430352df7e962
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-02 15:20:36 +0000

    [titan] skinselector.h fix

--- titan/skinselector.h
+++ titan/skinselector.h
@@ -138,7 +138,7 @@ void screenskinselect(void)
 				changetext(tmp, _("unknown")); //skin.xml is in main dir
 			else
 			{
-				if(string_find("/var/swap/titanskins",list[i].name))
+				if(string_find("/var/swap/titanskins",list[j].name))
 				{
 					char* tmpname = NULL;
 					tmpname = ostrcat("", list[j].displayname, 0, 0);
