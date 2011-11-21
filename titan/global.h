5b650eca32ae2a6cc3ca400b55345d65d8a73e69
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-21 20:51:41 +0000

    fix

--- titan/global.h
+++ titan/global.h
@@ -30,7 +30,6 @@ int checkmenuforbox(char *name)
 		if(ostrcmp("rotorsettings", name) == 0) return 0;
 		if(ostrcmp("satconfig", name) == 0) return 0;
 		if(ostrcmp("satfinder", name) == 0) return 0;
-		if(ostrcmp("mediacenter", name) == 0) return 0;
 		if(ostrcmp("configurehdd", name) == 0) return 0;
 		if(ostrcmp("panel_settings_overclocking", name) == 0) return 0;
 		if(ostrcmp("panel_settings_fancontrol", name) == 0) return 0;
