e3e801d3873a56939b0eb9cb045104140116ae88
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-25 01:54:37 +0000

    [titan] fix panel

--- titan/plugins/panel/panel_main.h
+++ titan/plugins/panel/panel_main.h
@@ -94,9 +94,9 @@ int panel_menucall(struct skin* panel_menuentry)
 	} else if(ostrcmp("panel_system", panel_menuentry->name) == 0){
 		struct skin* screen = getscreen("panel_system_main");
 		panel_menu(screen);
-	} else if(ostrcmp("aafpanel_system_eraseswap", aafpanel_menuentry->name) == 0){
+	} else if(ostrcmp("aafpanel_system_eraseswap", panel_menuentry->name) == 0){
 		screenaafpanel_system_eraseswap();		
-	} else if(ostrcmp("aafpanel_system_aafwizard", aafpanel_menuentry->name) == 0){
+	} else if(ostrcmp("aafpanel_system_aafwizard", panel_menuentry->name) == 0){
 		screenaafpanel_system_aafwizard();
 	} else if(ostrcmp("panel_system_restore", panel_menuentry->name) == 0){
 		screenpanel_system_restore();
