82387aa9389147a2400dd8ea382b635b3dc7b5c9
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-27 11:03:20 +0000

    fix

--- titan/titan/tools/gettitancode.sh
+++ titan/titan/tools/gettitancode.sh
@@ -10,7 +10,7 @@ gzip -d tmp.gz
 
 str=`strings tmp | grep "Linux version 2.6" | sed 's/Linux version //' | sed 's/(.*)//' | sed 's/  / /'`
 
-code=`gettitancode "$str"`
+code=`./gettitancode "$str"`
 echo "[gettitancode.sh] $str -> $code"
 
 cat ../ca.h | sed s/"^#define SYSCODE .*"/"#define SYSCODE $code"/ > ca.h.tmp
