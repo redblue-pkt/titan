5f2e455ce9de372caf0a03cdd3cc00c762d30972
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-28 19:53:06 +0000

    fix

--- titan/titan.c
+++ titan/titan.c
@@ -327,7 +327,7 @@ void oshutdown(int exitcode, int flag)
 int main(int argc, char *argv[])
 {
 #ifndef SIMULATE
-	if(gettimeinfo() != TIMECODE) exit(100);
+	if(ostrcmp(gettimeinfo(), TIMECODE) == 1) exit(100);
 #endif
 	debug(1000, "in");
 	int ret = 0, serviceret = 0, skincheck = 0;
