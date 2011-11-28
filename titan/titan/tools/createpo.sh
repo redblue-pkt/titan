0d66923fa1ab12ccbb1cb138025125c632c83d40
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-28 02:55:33 +0000

    fix

--- titan/titan/tools/createpo.sh
+++ titan/titan/tools/createpo.sh
@@ -9,7 +9,7 @@ rm -rf "$HOME"/flashimg/source.titan/titan/tools/tmp
 mkdir -p "$HOME"/flashimg/source.titan/titan/tools/tmp
 
 LIST=`find "$HOME"/flashimg/source.titan -type f -name "*.h"`
-POLIST=`find "$HOME"/flashimg/source.titan/po -type f -name "*.po"`
+POLIST=`find "$HOME"/flashimg/source.titan/po -type f -name "*_auto.po"`
 
 
 
@@ -21,9 +21,9 @@ cd "$HOME"/flashimg/source.titan/titan/tools/tmp
 for ROUND in $POLIST; do
 	echo "[create.po] update $ROUND"
 	if [ "$1" == "update" ]; then
-		xgettext -j -k_ *.h -o $ROUND_auto.po
+		xgettext -j -k_ *.h -o $ROUND
 	else
-		xgettext -k_ *.h -o "$ROUND"_auto.po
+		xgettext -k_ *.h -o $ROUND
 	fi
 done
 
