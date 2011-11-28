80c5e5a42e161a842c32f822bfec8632d71e5397
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-28 17:54:10 +0000

    fix

--- titan/titan/makesh4.sh
+++ titan/titan/makesh4.sh
@@ -135,7 +135,7 @@ echo "[titan]--------------------------------------------------------"
 echo "[titan]--------------------------------------------------------"
 echo "[titan] Update Language Po files"
 echo "[titan]--------------------------------------------------------"
-"$HOME"/flashimg/source.titan/titan/tools/createpo.sh update
+"$HOME"/flashimg/source.titan/titan/tools/createpo.sh "$SVNUSER" update
 echo "[titan]--------------------------------------------------------"
 echo "[titan] Update Language Po files done"
 echo "[titan]--------------------------------------------------------"
