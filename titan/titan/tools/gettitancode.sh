0b33b7cb820a7e9335631162ef2705c20050c916
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-28 19:41:13 +0000

    fix

--- titan/titan/tools/gettitancode.sh
+++ titan/titan/tools/gettitancode.sh
@@ -14,9 +14,9 @@ str=`strings tmp | grep "Linux version 2.6" | sed 's/Linux version //' | sed 's/
 code=`./gettitancode "$str"`
 #echo "[gettitancode.sh] $str -> $code"
 
-cat ../ca.h | sed s/"^#define SYSCODE .*"/"#define SYSCODE \"$code\""/ > ca.h.tmp
+cat ../ca.h | sed s/"^#define SYSCODE .*"/"#define SYSCODE $code"/ > ca.h.tmp
 mv ca.h.tmp ../ca.h
 
 code=`cat $ROOTDIR/etc/image-version | cut -d= -f2`
-cat ../titan.c | sed s/"^#define TIMECODE .*"/"#define TIMECODE $code"/ > titan.c.tmp
+cat ../titan.c | sed s/"^#define TIMECODE .*"/"#define TIMECODE \"$code\""/ > titan.c.tmp
 mv titan.c.tmp ../titan.c
