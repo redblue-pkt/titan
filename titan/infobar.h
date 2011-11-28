2373d466832933e6f95a4221885d69290df3abd3
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-28 20:03:58 +0000

    [titan] add rcsubchannel

--- titan/infobar.h
+++ titan/infobar.h
@@ -172,6 +172,16 @@ void screeninfobar()
 			subtitlepause(0);
 			continue;
 		}
+		if(rcret == getrcconfigint("rcsubchannel", NULL))
+		{
+			subtitlepause(1);
+			status.infobar = 0;
+			clearscreen(infobar);
+			screenlinkedchannel();
+			drawscreen(skin, 0);
+			subtitlepause(0);
+			continue;
+		}
 		if(rcret == getrcconfigint("rcsubtitel", NULL))
 		{
 			subtitlepause(1);
