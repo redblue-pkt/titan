f313ce4591c33f2c8149dabfb81aff2df7bbe8ae
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-11-28 03:07:49 +0000

    fix

--- titan/titan/tools/createpo.sh
+++ titan/titan/tools/createpo.sh
@@ -21,9 +21,9 @@ cd "$HOME"/flashimg/source.titan/titan/tools/tmp
 for ROUND in $POLIST; do
 	echo "[create.po] update $ROUND"
 	if [ "$1" == "update" ]; then
-		xgettext -j -k_ *.h -o $ROUND
+		xgettext --omit-header -j -k_ *.h -o $ROUND
 	else
-		xgettext -k_ *.h -o $ROUND
+		xgettext --omit-header -k_ *.h -o $ROUND
 	fi
 done
 
