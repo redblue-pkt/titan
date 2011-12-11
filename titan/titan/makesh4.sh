ed82c103dc4c3ec3724d6b5b784f62cc76d5f2f0
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-12-11 21:06:41 +0000

    [titan] fix settings

--- titan/titan/makesh4.sh
+++ titan/titan/makesh4.sh
@@ -63,7 +63,7 @@ echo "[titan]--------------------------------------------------------"
 echo "[titan] get settings"
 echo "[titan]--------------------------------------------------------"
 rm -rf "$HOME"/flashimg/source.titan/settings.svn
-svn co --username $SVNUSER --password $SVNPASS http://sbnc.myphotos.cc/trac/browser/ipk/source.sh4/titansettings_default_sat_1_0/var/etc/titan "$HOME"/flashimg/source.titan/settings.svn
+svn co --username $SVNUSER --password $SVNPASS http://"$SVNURL"/svn/ipk/source.sh4/titansettings_default_sat_1_0/var/etc/titan "$HOME"/flashimg/source.titan/settings.svn
 mv -f "$HOME"/flashimg/source.titan/settings.svn/* "$HOME"/flashimg/source.titan/settings 
 echo "[titan]--------------------------------------------------------"
 echo "[titan] get settings done"
