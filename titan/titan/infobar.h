ca2d30bd74e0fa19e3643f6c2266fcd8b91684b7
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-12-13 16:59:01 +0000

    [titan] fix

--- titan/titan/infobar.h
+++ titan/titan/infobar.h
@@ -186,8 +186,6 @@ void screeninfobar()
 		}
 		if(rcret == getrcconfigint("rcgreen", NULL))
 		{
-			timeshiftff();
-			continue;
 			subtitlepause(1);
 			status.infobar = 0;
 			clearscreen(infobar);
