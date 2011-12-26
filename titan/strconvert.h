bfcac1f2b403a67ded09b9e61d873ad84340f8cf
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2011-12-26 22:38:13 +0000

    [titan] implement iconv but not use it in the moment

--- titan/strconvert.h
+++ titan/strconvert.h
@@ -1,6 +1,9 @@
 #ifndef STRCONVERT_H
 #define STRCONVERT_H
 
+//#define USE_ICONV
+#ifndef USE_ICONV
+
 static unsigned long c88592[96] = {
 0x00A0, 0x0104, 0x02D8, 0x0141, 0x00A4, 0x013D, 0x015A, 0x00A7,
 0x00A8, 0x0160, 0x015E, 0x0164, 0x0179, 0x00AD, 0x017D, 0x017B,
@@ -586,6 +589,120 @@ char* strutf8(char *data, int len, int table, unsigned long transponderid, int f
 	res[t] = '\0';
 	return (char*)res;
 }
+
+#else //iconv
+
+#include <iconv.h>
+
+static char buf[1024 * 6]; // UTF-8 needs up to 6 bytes
+
+//The spec says ISO-6937, but many stations get it wrong and use ISO-8859-1
+char *iso6937_encoding = "ISO6937";
+
+int encoding_default(char *t, char **s, char *d)
+{
+	strncpy(t, iso6937_encoding, 16);
+	return 0;
+}
+
+int encoding_fixed(char *t, char **s, char *d)
+{
+	strncpy(t, d, 16);
+	*s += 1;
+	return 0;
+}
+
+int encoding_variable(char *t, char **s, char *d)
+{
+	int i = ((unsigned char) * s[1] << 8) +  (unsigned char) * s[2];
+	snprintf(t, 16, d, i);
+	*s += 3;
+	return 0;
+}
+
+int encoding_reserved(char *t, char **s, char *d)
+{
+	err("Reserved encoding: %02x", *s[0]);
+	return 1;
+}
+
+struct encoding
+{
+	int (*handler)(char *t, char **s, char *d);
+	char *data;
+} encoding[256] = {
+	[0x00] = {encoding_reserved, NULL},
+	[0x01] = {encoding_fixed, "ISO-8859-5"},
+	[0x02] = {encoding_fixed, "ISO-8859-6"},
+	[0x03] = {encoding_fixed, "ISO-8859-7"},
+	[0x04] = {encoding_fixed, "ISO-8859-8"},
+	[0x05] = {encoding_fixed, "ISO-8859-9"},
+	[0x06] = {encoding_fixed, "ISO-8859-10"},
+	[0x07] = {encoding_fixed, "ISO-8859-11"},
+	[0x08] = {encoding_fixed, "ISO-8859-12"},
+	[0x09] = {encoding_fixed, "ISO-8859-13"},
+	[0x0A] = {encoding_fixed, "ISO-8859-14"},
+	[0x0B] = {encoding_fixed, "ISO-8859-15"},
+	[0x0C] = {encoding_reserved, NULL},
+	[0x0D] = {encoding_reserved, NULL},
+	[0x0E] = {encoding_reserved, NULL},
+	[0x0F] = {encoding_reserved, NULL},
+	[0x10] = {encoding_variable, "ISO-8859-%d"},
+	[0x11] = {encoding_fixed, "ISO-10646/UCS2"}, //FIXME: UCS-2 LE/BE ???
+	[0x12] = {encoding_fixed, "KSC_5601"},
+	[0x13] = {encoding_fixed, "GB_2312-80"},
+	[0x14] = {encoding_fixed, "BIG5"},
+	[0x15] = {encoding_fixed, "ISO-10646/UTF8"},
+	[0x16] = {encoding_reserved, NULL},
+	[0x17] = {encoding_reserved, NULL},
+	[0x18] = {encoding_reserved, NULL},
+	[0x19] = {encoding_reserved, NULL},
+	[0x1A] = {encoding_reserved, NULL},
+	[0x1B] = {encoding_reserved, NULL},
+	[0x1C] = {encoding_reserved, NULL},
+	[0x1D] = {encoding_reserved, NULL},
+	[0x1E] = {encoding_reserved, NULL},
+	[0x1F] = {encoding_reserved, NULL},
+	[0x20 ... 0xFF] = {encoding_default, NULL},
+};
+
+char cs_old[16];
+iconv_t cd;
+
+char* strutf8(char *data, int len, int table, unsigned long transponderid, int free1, int flag)
+{
+	char cs_new[16];
+
+	int i = (int)(unsigned char)data[0];
+	if(encoding[i].handler(cs_new, &data, encoding[i].data))
+		return data;
+	if(strncmp(cs_old, cs_new, 16))
+	{
+		if(cd)
+		{
+			iconv_close(cd);
+			cd = NULL;
+		}
+		cd = iconv_open("UTF-8", cs_new);
+		if(cd == (iconv_t)-1)
+		{
+			perr("iconv_open() failed");
+			return data;
+		}
+		strncpy(cs_old, cs_new, 16);
+	}
+
+	char *inbuf = (char*)data;
+	size_t inbytesleft = len;
+	char *outbuf = (char*)buf;
+	size_t outbytesleft = sizeof(buf);
+	size_t ret = iconv(cd, &inbuf, &inbytesleft, &outbuf, &outbytesleft);
+	// FIXME: handle errors
+
+	return buf;
+}
+
+#endif
 
 int strfreetype(char* string, FT_ULong* cret)
 {
