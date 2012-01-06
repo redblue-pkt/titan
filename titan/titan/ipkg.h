d5c52a881c57e736e5e7296157aa6dab2d5ab273
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-06 00:05:29 +0000

    [titan] save ipkg list in struct

--- titan/titan/ipkg.h
+++ titan/titan/ipkg.h
@@ -5,18 +5,143 @@
 #include "../libipkg/libipkg.h"
 #include "../libipkg/args.h"
 
+struct ipkg
+{
+        char* name;
+        char* desc;
+        char* version;
+        struct ipkg* prev;
+        struct ipkg* next;
+};
+
+struct ipkg *ipkg = NULL;
+
+void debugipkg()
+{
+	struct ipkg *node = ipkg;
+	
+	while(node != NULL)
+	{
+		if(node->name != NULL)
+			printf("pkg name: %s\n", node->name);
+		node = node->next;
+	}
+}
+
+struct ipkg* addipkg(char *name, char* desc, char* version, struct ipkg* last)
+{
+	//debug(1000, "in");
+	struct ipkg *newnode = NULL, *prev = NULL, *node = ipkg;
+
+	newnode = (struct ipkg*)malloc(sizeof(struct ipkg));	
+	if(newnode == NULL)
+	{
+		err("no memory");
+		return NULL;
+	}
+	memset(newnode, 0, sizeof(struct ipkg));
+
+	newnode->name = ostrcat(name, NULL, 0, 0);
+	newnode->desc = ostrcat(desc, NULL, 0, 0);
+	newnode->version = ostrcat(version, NULL, 0, 0);
+
+	if(last == NULL)
+	{
+		while(node != NULL)
+		{
+			prev = node;
+			node = node->next;
+		}
+	}
+	else
+	{
+		prev = last;
+		node = last->next;
+	}
+
+	if(prev == NULL)
+		ipkg = newnode;
+	else
+	{
+		prev->next = newnode;
+		newnode->prev = prev;
+	}
+	newnode->next = node;
+	if(node != NULL) node->prev = newnode;
+
+	//debug(1000, "out");
+	return newnode;
+}
+
+void delipkg(struct ipkg* ipkgnode)
+{
+	//debug(1000, "in");
+	struct ipkg *node = ipkg, *prev = ipkg;
+
+	while(node != NULL)
+	{
+		if(node == ipkgnode)
+		{
+			if(node == ipkg)
+			{
+				ipkg = node->next;
+				if(ipkg != NULL)
+					ipkg->prev = NULL;
+			}
+			else
+			{
+				prev->next = node->next;
+				if(node->next != NULL)
+					node->next->prev = prev;
+			}
+
+			free(node->name);
+			node->name = NULL;
+
+			free(node->desc);
+			node->desc = NULL;
+
+			free(node->version);
+			node->version = NULL;
+
+			free(node);
+			node = NULL;
+			break;
+		}
+
+		prev = node;
+		node = node->next;
+	}
+	//debug(1000, "out");
+}
+
+void freeipkg()
+{
+	debug(1000, "in");
+	struct ipkg *node = ipkg, *prev = ipkg;
+
+	while(node != NULL)
+	{
+		prev = node;
+		node = node->next;
+		if(prev != NULL)
+			delipkg(prev);
+	}
+	debug(1000, "out");
+}
+
 int ipkg_list_cb(char *name, char *desc, char *version, pkg_state_status_t status, void *userdata)
 {
 	if(desc)
-		printf("%s - %s - %s\n", name, version, desc);
+		addipkg(name, desc, version, NULL);
 	else
-		printf("%s - %s\n", name, version);
+		addipkg(name, NULL, version, NULL);
 	return 0;
 }
 
 int ipkg_status_cb(char *name, int istatus, char *desc, void *userdata)
 {
-	printf("%s\n", desc);
+	addipkg(name, desc, NULL, NULL);
 	return 0;
 }
 
@@ -141,3 +266,4 @@ int ipkg_search(const char* package)
 }
 
 #endif
+
