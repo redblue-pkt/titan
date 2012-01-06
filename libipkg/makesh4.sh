ce217a27562ef5d1f264ecebd991937762c12f79
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-06 00:07:03 +0000

    fix libipkg building

--- libipkg/makesh4.sh
+++ libipkg/makesh4.sh
@@ -13,13 +13,17 @@ export OBJDUMP="$HOME"/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/devkit/sh4/
 export CFLAGS="-O2"  
 export CXXFLAGS="-O2"  
 export LDFLAGS=""  
-export PKG_CONFIG_PATH="/home/obiwan/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/cdkroot/usr/lib/pkgconfig"  
+export PKG_CONFIG_PATH="$HOME/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/cdkroot/usr/lib/pkgconfig"  
 export LN_S="ln -s" 
 
+cp libipkg.pc $HOME/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/cdkroot/usr/lib/pkgconfig
 #CC=sh4-linux-gcc CXX=sh4-linux-g++ LD=sh4-linux-ld NM=sh4-linux-nm AR=sh4-linux-ar AS=sh4-linux-as RANLIB=sh4-linux-ranlib STRIP=sh4-linux-strip OBJCOPY=sh4-linux-objcopy OBJDUMP=sh4-linux-objdump LN_S="ln -s" CFLAGS="-O2" CXXFLAGS="-O2" LDFLAGS="" PKG_CONFIG_PATH="/home/obiwan/flashimg/BUILDGIT/checkout_stm24/tdt/tufsbox/cdkroot/usr/lib/pkgconfig" \
+make clean
 ./configure \
 --build=x86_64-unknown-linux-gnu \
 --host=sh4-linux \
 --prefix=/usr && \
 make
+#make install
 
+cp .libs/* $HOME/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/cdkroot/usr/lib
