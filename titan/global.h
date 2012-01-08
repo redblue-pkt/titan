5434a824a501eef3fc6b7fc90b6b0a957b3c7afc
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-08 20:48:00 +0000

    fix

--- titan/global.h
+++ titan/global.h
@@ -3789,7 +3789,7 @@ void get_ipk_update()
 		cmd = strstrip(string_newline(command(cmd)));
 		debug(60, "cmd: %s", cmd);
 		
-		if(ostrcmp(cmd, "//97.74.32.10/svn/atemio/atemio510-rev12940/sh4/titan") != 0)
+		if(ostrcmp(cmd, "//97.74.32.10/svn/ipk/sh4/titan") != 0)
 		{
 			textbox(_("Message"), _("check your Secret Feed !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 5, 0);
 			free(cmd), cmd = NULL;
