d8517bcf39998a6cada110ec6f478f078d4ba69b
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-09 22:40:12 +0000

    fix

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -3,6 +3,8 @@
 
 void checkserial(char* input)
 {
+	if(input == NULL) return;
+	
 	char* authfile = NULL;
 	authfile = gethttp("atemio.dyndns.tv", "/svn/auth/trustlist", 80, NULL, "YXRlbWlvOkZIWlZCR2huemZ2RWhGREZUR3p1aWY1Njc2emhqR1RVR0JOSGpt", NULL);
 
