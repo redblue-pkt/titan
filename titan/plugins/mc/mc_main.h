450c336d472bcebf18e25abf42ab4cffd03d9d55
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-10 13:23:47 +0000

    fix

--- titan/plugins/mc/mc_main.h
+++ titan/plugins/mc/mc_main.h
@@ -84,6 +84,7 @@ int mc_menucall(struct skin* menuentry)
 			}
 			else
 				textbox(_("Message"), _("Install netsurf Internetbrowser Ipk first !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0);
+		}
 		else
 			textbox(_("Message"), _("Registration needed, please contact Atemio !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0);
 		ret 1;
