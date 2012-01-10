c779ad6f1a88538066bc9b5e6890ae2bc1c8a9f3
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-10 10:39:23 +0000

    fix

--- titan/ipkg.h
+++ titan/ipkg.h
@@ -296,6 +296,7 @@ int ipkg_download(ipkg_conf_t *conf, const char *src, const char *filename)
 	{
 		if(ostrcmp("97.74.32.10", ip) == 0)
 		{
+			printf("path: %s\n");
 			if(ostrcmp(path, "//97.74.32.10/svn/ipk/sh4/titan") != 0) 	 	 
 			{
 				textbox(_("Message"), _("check your Secret Feed !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 5, 0); 	 	 
