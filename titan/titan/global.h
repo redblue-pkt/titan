af8ed322f794aaf1a7d2eead55d9137f4dd4e0b1
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-11 19:24:48 +0000

    fix po

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -54,7 +54,7 @@ void getserial()
 	cpu = getcpuid();
 
 	char* msg = NULL;
-	msg = ostrcat(_("For next Update please contact Atemio and send this Serial Number and your invoice !!\n\nBoard-ID SerialNr:"), " ", 0, 0);
+	msg = ostrcat(_("For next OnlineUpdate please contact Atemio and send this Serial Number and your Atemio Serial Number !!\n\nBoard-ID SerialNr:"), " ", 0, 0);
 	msg = ostrcat(msg, cpu, 1, 0);
 	msg = ostrcat(msg, "\n\n", 1, 0);
 	msg = ostrcat(msg, _("Email  		info@atemio.de"), 1, 0);
