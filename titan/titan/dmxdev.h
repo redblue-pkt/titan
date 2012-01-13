366fd7f8d68e3801fa230a60ff5330d39c7bccf1
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-13 23:49:18 +0000

    [titan] add dvb rst struct and pid

--- titan/titan/dmxdev.h
+++ titan/titan/dmxdev.h
@@ -268,6 +268,14 @@ int dmxsetfilter(struct dvbdev* node, int pid, int secnr, int flag)
 		sctflt.pid = pid;
 		sctflt.flags = DMX_IMMEDIATE_START | DMX_CHECK_CRC;
 	}
+	if(flag == 13)  //rst filter (pid 0x13)
+	{
+		sctflt.filter.filter[0] = 0x13;
+		sctflt.filter.mask[0] = 0xff;
+		sctflt.timeout = 0;
+		sctflt.pid = pid;
+		sctflt.flags = DMX_IMMEDIATE_START | DMX_CHECK_CRC;
+	}
 
 	debug(200, "DMX_SET_FILTER pid=%d, flag=%d", pid, flag);
 	if(ioctl(node->fd, DMX_SET_FILTER, &sctflt) < 0)
