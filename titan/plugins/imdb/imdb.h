e360ec18e9f825f459b25aa332e25c3e532539b2
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-01-18 16:23:54 +0000

    change imdb to german

--- titan/plugins/imdb/imdb.h
+++ titan/plugins/imdb/imdb.h
@@ -1,6 +1,40 @@
 #ifndef IMDB_H
 #define IMDB_H
 
+extern struct skin* skin;
+
+char* string_striptags(char* filename)
+{
+	debug(1000, "in");
+	int i, len_filename;
+
+	if(filename == NULL) return 0;
+	
+	len_filename = strlen(filename);
+
+	int skip =0;
+	for(i = 0; (i) < len_filename; i++)
+	{
+		if(filename[i] == '<')
+		{
+			printf("found < in string\n");
+			skip = 1;
+		}
+		else if(filename[i] == '>')	
+		{
+			printf("found > in string\n");
+			skip = 0;
+			filename[i] = ' ';
+		}
+		if(skip == 1)
+			filename[i] = ' ';
+	}	
+
+	debug(1000, "out");
+	return strstrip(string_strip_whitechars(filename));	
+//	return strstrip(filename);	
+}
+
 char* string_resub(char* str,char* str2, char* filename)
 {
 	debug(1000, "in");
@@ -57,10 +91,14 @@ char* screensearch(char* input)
 	return tmpstr;
 }
 
-int imdb()
+void imdb()
 {
+	int rcret = 0;
+	drawscreen(skin, 0);
 	setfbtransparent(255);
 	status.hangtime = 99999;
+	struct skin* blackscreen = getscreen("blackscreen");
+	drawscreen(blackscreen, 0);
 
 	// main screen
 	struct skin* imdbskin = getscreen("imdb");
@@ -101,21 +139,30 @@ int imdb()
 	char* cover = NULL;
 	char* cast = NULL;
 
-	input = screensearch("Rambo");
+
+	input = getepgakttitle();
+//	input = screensearch("Rambo");
+	printf("input: %s\n", input);
+//	stringreplacechar(input, ' ', '%20');
+	input = string_replace(" ", "%20", input, 1);
+
+
 	printf("input: %s\n", input);
 
 drawscreen(imdbskin, 0);
 
 //	input = ostrcat("", "Days+of+Summer", 0, 0);
-	url = ostrcat(url, "german.imdb.com", 1, 0);
+	url = ostrcat(url, "www.imdb.de", 1, 0);
 
 	apiSearchTV = ostrcat("", "/search/title?title=<title>&title_type=tv_series", 0, 0);
 	apiImdbLookup = ostrcat("", "/title/tt", 0, 0);
 	apiSearch = ostrcat("", "/find?s=tt;q=", 0, 0);
 	localfile = ostrcat("", "/tmp/cache.getMovieByTitle.html", 0, 0);
-
+	printf("get_ip(url): %s\n", get_ip(url));
 	search = ostrcat(apiSearch, input, 0, 0);
-	gethttp(get_ip(url), search, 80, localfile, NULL);
+	gethttp(get_ip(url), search, 80, localfile, NULL, NULL);
+//	gethttp(url, search, 80, localfile, NULL, NULL);
+
 
 	tmpstr = readfiletomem(localfile, 1);
 
@@ -137,64 +184,70 @@ drawscreen(imdbskin, 0);
 	printf("url: %s\n", get_ip(url));
 	printf("localfile: %s\n", localfile);
 
-	gethttp(get_ip(url), search, 80, localfile, NULL);
+//	gethttp(get_ip(url), search, 80, localfile, NULL, NULL);
+//	gethttp(url, search, 80, localfile, NULL, NULL);
+	unlink(localfile);
+//cache.getMoviesByImdbId.html
+	char* cmd = NULL;
+	cmd = ostrcat(cmd, "wget http://", 1, 0);
+	cmd = ostrcat(cmd, url, 1, 0);
+	cmd = ostrcat(cmd, search, 1, 0);
+	cmd = ostrcat(cmd, " -O ", 1, 0);
+	cmd = ostrcat(cmd, localfile, 1, 0);
+	printf("cmd: %s\n",cmd);
+	system(cmd);
+	free(cmd), cmd = NULL;
+//	tmpstr = string_strip_whitechars(readfiletomem(localfile, 1));
 	tmpstr = readfiletomem(localfile, 1);
+
 
 	title = ostrcat(tmpstr, "", 0, 0);
 	string_resub("<title>","</title>",title);
 	strstrip(title);
 	printf("title: %s\n", title);
-changetext(skin_title, title);
+	changetext(skin_title, title);
+
 ////////////////////
 	director = ostrcat(tmpstr, "", 0, 0);
-//	string_resub("<div id=\"director-info\" class=\"info\">","</div>",director);
-	string_resub("Director:","</div>",director);
-	char* tmpstr2 = NULL;
-	struct splitstr* ret2 = NULL;
-	int count2 = 0;	
-	tmpstr2 = ostrcat(tmpstr2, director, 1, 0);
-	ret2 = strsplit(tmpstr2, "<>", &count2);
-	printf("director1: %s\n", (&ret2[4])->part);
-	strstrip(director);
+//	string_resub("Director:","</div>",director);	
+	string_resub("Regisseur:","</div>",director);
+	string_striptags(director);
 	printf("director: %s\n", director);
-changetext(skin_director, (&ret2[4])->part);
+	changetext(skin_director, director);
+
+
 ////////////////////
 
 	writers = ostrcat(tmpstr, "", 0, 0);
-//	string_resub("<div id=\"director-info\" class=\"info\">","</div>",writers);
-	string_resub("Writers:\n  </h4>","</div>",writers);
-//	char* tmpstr2 = NULL;
-//	struct splitstr* ret2 = NULL;
-	count2 = 0;	
-	tmpstr2 = ostrcat(tmpstr2, writers, 1, 0);
-	ret2 = strsplit(tmpstr2, "<>", &count2);
-	printf("writers1: %s\n", (&ret2[2])->part);
-	printf("writers2: %s\n", (&ret2[6])->part);
-//	printf("writers3: %s\n", (&ret2[10])->part);
-	strstrip(writers);
+//	string_resub("<h5>Writers","</div>",writers);
+	string_resub("<h5>Drehbuchautor","</div>",writers);
+	printf("writers1: %s\n", writers);
+	string_resub("<div class=\"info-content\">","</div>",writers);
+	printf("writers2: %s\n", writers);
+
+	string_striptags(writers);
 	printf("writers: %s\n", writers);
-changetext(skin_writers, (&ret2[2])->part);
+	changetext(skin_writers, writers);
+	
 ////////////////////
 
 	genre = ostrcat(tmpstr, "", 0, 0);
-	string_resub("<h4 class=\"inline\">Genre","</div>",genre);
-	count2 = 0;	
-	tmpstr2 = ostrcat(tmpstr2, genre, 1, 0);
-	ret2 = strsplit(tmpstr2, "<>", &count2);
-	printf("genre1: %s\n", (&ret2[4])->part);
-	printf("genre2: %s\n", (&ret2[12])->part);
-	strstrip(genre);
+	string_resub("<h5>Genre:</h5>","</div>",genre);
+	printf("genre1: %s\n", genre);
+	string_striptags(genre);
 	printf("genre: %s\n", genre);
-changetext(skin_genre, (&ret2[4])->part);
+	changetext(skin_genre, genre);
+
 ////////////////////
+
+/*
 
 	tagline = ostrcat(tmpstr, "", 0, 0);
 	string_resub("<h4 class=\"inline\">Taglines:</h4>","<span class",tagline);
 
 	strstrip(tagline);
 	printf("tagline: %s\n", tagline);
-changetext(skin_tagline, tagline);
-////////////////////
+	changetext(skin_tagline, tagline);
 
 	releasetime = ostrcat(tmpstr, "", 0, 0);
 	string_resub("<h4 class=\"inline\">Release Date:</h4>","</time>",releasetime);
@@ -204,33 +257,50 @@ changetext(skin_tagline, tagline);
 	printf("releasetime1: %s\n", (&ret2[2])->part);
 	strstrip(releasetime);
 	printf("releasetime: %s\n", releasetime);
-changetext(skin_releasetime, (&ret2[2])->part);
+	changetext(skin_releasetime, (&ret2[2])->part);
+*/
+
+	releasetime = ostrcat(tmpstr, "", 0, 0);
+	string_resub("<h5>Premierendatum:</h5>","<a class=",releasetime);
+	printf("releasetime1: %s\n", releasetime);
+
+	string_striptags(releasetime);
+	printf("releasetime: %s\n", releasetime);
+	changetext(skin_releasetime, releasetime);
+
 ////////////////////
 
 	cast = ostrcat(tmpstr, "", 0, 0);
-	string_resub("<h4 class=\"inline\">Stars:</h4>","</div>",cast);
-	count2 = 0;	
-	tmpstr2 = ostrcat(tmpstr2, cast, 1, 0);
-	ret2 = strsplit(tmpstr2, "<>", &count2);
-	printf("cast1: %s\n", (&ret2[2])->part);
-	printf("cast2: %s\n", (&ret2[6])->part);
-	printf("cast3: %s\n", (&ret2[10])->part);
-	strstrip(cast);
+	string_resub("<h3>Besetzung</h3>&nbsp;","</td></tr></table>",cast);
+	string_resub("<div class=\"info-content block\"><table class=\"cast\">","</a></td></tr>",cast);
+
+	cast = string_replace("...", "als", cast, 1);
+
+	printf("cast1: %s\n", cast);
+
+	string_striptags(cast);
 	printf("cast: %s\n", cast);
-changetext(skin_cast, (&ret2[2])->part);
+	changetext(skin_cast, cast);
+	
 ////////////////////
 
+
 	pagebigcover = ostrcat(tmpstr, "", 0, 0);
-	string_resub("href=\"/media/rm","/",pagebigcover);
+//	string_resub("href=\"/media/rm","/",pagebigcover);
+	string_resub("/media/rm","/",pagebigcover);
 
 	strstrip(pagebigcover);
 	printf("pagebigcover: %s\n", pagebigcover);
 
-	char* tmpstr3 = NULL;
-	tmpstr3 = ostrcat("href=\"/media/rm", pagebigcover, 0, 0);
-	tmpstr3 = ostrcat(tmpstr3, "/tt", 1, 0);
-	tmpstr3 = ostrcat(tmpstr3, imdbid, 1, 0);
-	tmpstr3 = ostrcat(tmpstr3, "\"    ><img src=\"", 1, 0);
+/*
+http://ia.media-imdb.com/images/M/MV5BMjE2MzU3Nzk4M15BMl5BanBnXkFtZTcwODcxNTI5Mw@@._V1_.jpg
+<link rel="image_src" href="
+
+//	char* tmpstr3 = NULL;
+//	tmpstr3 = ostrcat("href=\"/media/rm", pagebigcover, 0, 0);
+//	tmpstr3 = ostrcat(tmpstr3, "/tt", 1, 0);
+//	tmpstr3 = ostrcat(tmpstr3, imdbid, 1, 0);
+//	tmpstr3 = ostrcat(tmpstr3, "\"    ><img src=\"", 1, 0);
 
 	cover = ostrcat(tmpstr, "", 0, 0);
 	string_resub(tmpstr3,"\"",cover);
@@ -242,9 +312,12 @@ changetext(skin_cast, (&ret2[2])->part);
 char* filename = NULL;
 filename = ostrcat("wget ", cover, 0, 0);
 filename = ostrcat(filename, " -O /tmp/cover.jpg", 1, 0);
-system(filename);
+system(filename);	
 changepic(skin_cover, "/tmp/cover.jpg");
 ////////////////////
+
+*/
+	char* filename = NULL;
 	search = ostrcat("/media/rm", pagebigcover, 0, 0);
 	search = ostrcat(search, "/tt", 1, 0);
 	search = ostrcat(search, imdbid, 1, 0);
@@ -257,15 +330,21 @@ changepic(skin_cover, "/tmp/cover.jpg");
 
 
 
-	gethttp(get_ip(url), search, 80, localfile, NULL);
+	gethttp(get_ip(url), search, 80, localfile, NULL, NULL);
+//	gethttp(url, search, 80, localfile, NULL, NULL);
 	bigcover = readfiletomem(localfile, 1);
 	string_resub("<link rel=\"image_src\" href=\"","\"",bigcover);
 	strstrip(bigcover);
 	printf("bigcover: %s\n", bigcover);
 
-filename = ostrcat("wget ", bigcover, 0, 0);
-filename = ostrcat(filename, " -O /tmp/bigcover.jpg", 1, 0);
-system(filename);
+	unlink("/tmp/bigcover.jpg");
+	filename = ostrcat("wget ", bigcover, 0, 0);
+	filename = ostrcat(filename, " -O /tmp/bigcover.jpg", 1, 0);
+	system(filename);
+
+	rcret = servicestop(status.aktservice, 1, 0);
+	if(rcret == 1) return;
+
 changepic(skin_cover, "/tmp/bigcover.jpg");
 ////////////////////
 
@@ -277,20 +356,38 @@ changepic(skin_cover, "/tmp/bigcover.jpg");
 	printf("search: %s\n", search);
 	printf("url: %s\n", get_ip(url));
 	printf("localfile: %s\n", localfile);
-	gethttp(get_ip(url), search, 80, localfile, NULL);
+//	gethttp(get_ip(url), search, 80, localfile, NULL, NULL);
+//	gethttp(url, search, 80, localfile, NULL, NULL);
 
+	unlink(localfile);
+	cmd = ostrcat(cmd, "wget http://", 1, 0);
+	cmd = ostrcat(cmd, url, 1, 0);
+	cmd = ostrcat(cmd, search, 1, 0);
+	cmd = ostrcat(cmd, " -O ", 1, 0);
+	cmd = ostrcat(cmd, localfile, 1, 0);
+	printf("cmd: %s\n",cmd);
+	system(cmd);
+	free(cmd), cmd = NULL;
+	
+	
 	plot = readfiletomem(localfile, 1);
 //htmldecode(plot, plot);
 //htmlencode(plot);
-	if(string_find("<p class=\"plotpar\">",plot))
-	{
-		string_resub("<p class=\"plotpar\">","<i>",plot);
-		strstrip(plot);
-		printf("plot: %s\n", plot);
-changetext(skin_plot, plot);
-	}
+	string_resub("<div id=\"swiki.2.1\">","</div>",plot);
+	printf("plot1: %s\n", plot);
+	string_striptags(plot);
+	printf("plot: %s\n", plot);
+	changetext(skin_plot, plot);	
+	
+//	if(string_find("<p class=\"plotpar\">",plot))
+//	{
+//		string_resub("<p class=\"plotpar\">","<i>",plot);
+//		strstrip(plot);
+//		printf("plot: %s\n", plot);
+//changetext(skin_plot, plot);
+//	}
+
 drawscreen(imdbskin, 0);
-	int rcret = 0;
 	struct skin* tmp = NULL;
 	while(1)
 	{
@@ -327,8 +424,16 @@ drawscreen(imdbskin, 0);
 	free(bigcover), bigcover = NULL;
 	free(cover), cover = NULL;
 	free(cast), cast = NULL;
+
+	delownerrc(imdbskin);
+	clearscreen(imdbskin);
+	drawscreen(skin, 0);
+
 	setosdtransparent(getskinconfigint("osdtransparent", NULL));
-	return apiSearch;
+	servicestart(status.lastservice->channel, NULL, NULL, 0);
+	
+//	return apiSearch;
+	return;
 }
 
 #endif
