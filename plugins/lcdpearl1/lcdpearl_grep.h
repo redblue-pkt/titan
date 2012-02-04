ecdd7e730a27b5fec0b31e125d16b7a19882155a
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-02-04 23:04:25 +0000

    [lcdpearl] add play screen

--- plugins/lcdpearl1/lcdpearl_grep.h
+++ plugins/lcdpearl1/lcdpearl_grep.h
@@ -1,5 +1,6 @@
 #include <sys/inotify.h>
 
+
 void lcd_raw_update()
 {
 	char* fbgrab = ostrcat(getconfig("pluginpath", NULL), "/lcdpearl1/fbgrab -f /tmp/titanlcd.raw -w 320 -h 240 -b 32 -i /tmp/.titanlcd1.png > /dev/null", 0, 0);
@@ -23,29 +24,43 @@ void lcd_raw_event()
 {
   int fd;
   int wd;
+  unsigned int len;
+  char *buf;
+  buf=malloc((sizeof(struct inotify_event)+16)*1024);
 
-	sleep(2);
+	while (draw == 0 && LCD_Pearl1thread != NULL)
+	{
+		sleep(1);
+	}
+	if(LCD_Pearl1thread == NULL)
+	{
+		free(buf);
+		return;
+	}
 	lcd_raw_update();
 	
 	fd = inotify_init();
   if ( fd < 0 ) {
    	perror( "inotify_init" );
   }
+
   wd = inotify_add_watch( fd,  "/tmp/titanlcd.raw", IN_CLOSE_WRITE);
 
 	while(1){
     if (lcd_event_check (fd) > 0)
     {
+      len=read(fd, buf,(sizeof(struct inotify_event)+16)*1024 );
       lcd_raw_update();
     }
-   	inotify_rm_watch( fd, wd );
-		close( fd );
+   	//inotify_rm_watch( fd, wd );
+		//close( fd );
 		if(LCD_Pearl1thread == NULL)
 			break;
-		fd = inotify_init();
-  	if ( fd < 0 ) {
-    	perror( "inotify_init" );
-  	}
-  	wd = inotify_add_watch( fd, "/tmp/titanlcd.raw", IN_CLOSE_WRITE);
+		//fd = inotify_init();
+  	//if ( fd < 0 ) {
+    //	perror( "inotify_init" );
+  	//}
+  	//wd = inotify_add_watch( fd, "/tmp/titanlcd.raw", IN_CLOSE_WRITE);
 	}
+	free(buf);
 }
