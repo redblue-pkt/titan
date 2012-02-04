9e2906b657ca433671c94d90ad836e3aa6ca286d
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-02-04 16:51:23 +0000

    [titan] add playback to tithek

--- titan/titan/menu.h
+++ titan/titan/menu.h
@@ -157,7 +157,7 @@ int menucall(struct skin* menunode, struct skin* menuentry, int check)
 	else if(ostrcmp("mediaplayer", menuentry->name) == 0)
 	{
 		if(check == 1) return 0;
-		screenplay(0, 0);
+		screenplay(NULL, 0, 0);
 	}
 	else if(ostrcmp("restoredefault", menuentry->name) == 0)
 	{
