ac22d0f68f1b3759e727d9a4282c49394672277c
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-02-20 00:04:37 +0000

    fix

--- titan/tools/gettitancode.sh
+++ titan/tools/gettitancode.sh
@@ -7,18 +7,19 @@ cd "$HOME"/flashimg/source.titan/titan/tools
 
 rm tmp
 rm tmp.gz
-dd if=$KERNELDIR of=tmp.gz bs=1 skip=64
 
 if [ $TYPE == "ufs910" ];then
-	lzma -d tmp.gz
+	dd if=$KERNELDIR of=tmp.lzma bs=1 skip=64
+	lzma -d tmp.lzma
 else
+	dd if=$KERNELDIR of=tmp.gz bs=1 skip=64
 	gzip -d tmp.gz
 fi
 
 str=`strings tmp | grep "Linux version 2.6" | sed 's/Linux version //' | sed 's/(.*)//' | sed 's/  / /'`
 
 code=`./gettitancode "$str"`
-#echo "[gettitancode.sh] $str -> $code"
+echo "[gettitancode.sh] $str -> $code"
 echo code1: $code
 cat ../titan.c | sed s/"^#define SYSCODE .*"/"#define SYSCODE $code"/ > titan.c.tmp
 mv titan.c.tmp ../titan.c
