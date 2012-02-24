53f31dd498e8c51b0df921da2485640420ab5be9
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-02-24 12:47:07 +0000

    [titan] add tithek parser beeg/youtube

---
+++ titan/titan/tools/parser/beeg.sh
@@ -0,0 +1,95 @@
+#! /bin/sh
+rm cache.beeg.*
+rm Beeg.*.m3u
+rm -rf _single
+rm -rf _full/beeg
+mkdir -p _full/beeg/streams
+mkdir _single
+wget http://beeg.com -O cache.beeg.main.html
+sections=`cat cache.beeg.main.html | grep '^<a href="http://beeg.com/section' | tr '"' '\n' | grep ^http:`
+
+cat cache.beeg.main.html | grep http://beeg.com | grep -v http://beeg.com/section | grep alt= | sed 's/<a href="/link=/' | sed 's/"><img src="/#pic=/' | sed 's/" title="/"\n/' | sed 's/" alt="/#title="/' | grep ^link= | tr ' ' '|'>> cache.beeg.list
+
+piccount=0
+count=0
+for ROUND in $sections; do	
+	count=`expr $count + 1`
+#	echo "sections($count)=$ROUND"
+	wget $ROUND -O cache.beeg.section."$count".html
+	cat cache.beeg.section."$count".html | grep http://beeg.com | grep -v http://beeg.com/section | grep alt= | sed 's/<a href="/link=/' | sed 's/"><img src="/#pic=/' | sed 's/" title="/"\n/' | sed 's/" alt="/#title="/' | grep ^link= | tr ' ' '|'>> cache.beeg.list
+done
+LIST=`cat cache.beeg.list`
+
+echo "# Beeg.com" > cache.beeg.playlist
+
+count=0
+for ROUND in $LIST; do
+	URL=`echo $ROUND | tr '#' '\n' | grep ^link= | sed 's/link=//' | tr '|' ' '`
+	PIC=`echo $ROUND | tr '#' '\n' | grep ^pic= | sed 's/pic=//' | tr '|' ' '`
+	TITLE=`echo $ROUND | tr '#' '\n' | grep ^title= | sed 's/title=//' | tr '|' ' ' | tr -d '"'`
+	CHECK=`echo $URL | cut -d / -f4`
+	if [ `cat cache.beeg.titanlist | grep "$CHECK" | wc -l` -eq 0 ];then
+		count=`expr $count + 1`
+		echo "add($count)=$ROUND"
+		wget $URL -O cache.beeg.stream."$count".html
+		STREAMURL=`cat cache.beeg.stream."$count".html | grep "'file':" | cut -d "'" -f4`
+		piccount=`expr $piccount + 1`
+		LINE="$TITLE#$STREAMURL#$PIC#beeg_$piccount.jpg#Beeg#2"
+		echo $LINE >> cache.beeg.titanlist
+		echo $STREAMURL >> cache.beeg.playlist
+		echo "$STREAMURL" >> _single/Beeg.`echo "$TITLE" | tr '_' '.' | tr ' ' '.' | tr '...' '.' | tr '..' '.' | tr '..' '.' | tr '..' '.'`."$count".m3u
+	fi
+done
+
+################
+cp cache.beeg.titanlist _full/beeg/streams/beeg.all-newfirst.list
+cat cache.beeg.titanlist | sort -u > _full/beeg/streams/beeg.all-sorted.list
+
+for ROUND in 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
+	if [ `cat cache.beeg.titanlist | grep ^"$ROUND" | wc -l` -gt 0 ];then
+		cat cache.beeg.titanlist | grep ^"$ROUND" > cache.beeg.titanlist."$ROUND"
+		cat cache.beeg.titanlist."$ROUND" | sort -u > _full/beeg/streams/beeg.`echo "$ROUND" | tr 'A-Z' 'a-z'`.list
+		echo `echo "$ROUND" | tr 'A-Z' 'a-z'`"#http://atemio.dyndns.tv/mediathek/beeg/streams/beeg."`echo "$ROUND" | tr 'A-Z' 'a-z'`".list#http://atemio.dyndns.tv/mediathek/menu/`echo "$ROUND" | tr 'A-Z' 'a-z'`.jpg#"`echo "$ROUND" | tr 'A-Z' 'a-z'`.jpg#Beeg#1 >> _full/beeg/beeg.a-z.list
+	fi
+done
+
+SECTION=`cat cache.beeg.main.html | grep "http://beeg.com/section/" | grep -v /home/ | sed 's/ target="_self">/title=/' | sed 's/      <td><a href="/link=/' | sed 's!</a></td>!!' | tr ' ' '|' | tr '"' '#'`
+for ROUND in $SECTION; do
+	echo Section $ROUND
+	URL=`echo $ROUND | tr '#' '\n' | grep ^link= | sed 's/link=//' | tr '|' ' '`
+	TITLE=`echo $ROUND | tr '#' '\n' | grep ^title= | sed 's/title=//' | tr '|' '-' | tr -d '"'`	
+	echo "$TITLE""#http://atemio.dyndns.tv/mediathek/beeg/streams/beeg."`echo "$TITLE" | tr 'A-Z' 'a-z'`".list#http://atemio.dyndns.tv/mediathek/menu/`echo "$TITLE" | tr 'A-Z' 'a-z'`.jpg#`echo "$TITLE" | tr 'A-Z' 'a-z'`.jpg"#Beeg#1 >> _full/beeg/beeg.category.list
+
+	rm cache.beeg.main.html
+	wget $URL -O cache.beeg.section.`echo "$TITLE" | tr 'A-Z' 'a-z'`.html
+	file=cache.beeg.section.`echo "$TITLE" | tr 'A-Z' 'a-z'`.html
+
+	sections=`cat $file | grep '^<a href="http://beeg.com/section' | tr '"' '\n' | grep ^http:`
+
+	cat cache.beeg.section.`echo "$TITLE" | tr 'A-Z' 'a-z'`.html | grep http://beeg.com | grep -v http://beeg.com/section | grep alt= | sed 's/<a href="/link=/' | sed 's/"><img src="/#pic=/' | sed 's/" title="/"\n/' | sed 's/" alt="/#title="/' | grep ^link= | tr ' ' '|'> cache.beeg.section.list
+
+	count=0
+	for ROUND in $sections; do
+		count=`expr $count + 1`
+		wget $ROUND -O cache.beeg.section.`echo "$TITLE" | tr 'A-Z' 'a-z'`."$count".html
+		cat cache.beeg.section.`echo "$TITLE" | tr 'A-Z' 'a-z'`."$count".html | grep http://beeg.com | grep -v http://beeg.com/section | grep alt= | sed 's/<a href="/link=/' | sed 's/"><img src="/#pic=/' | sed 's/" title="/"\n/' | sed 's/" alt="/#title="/' | grep ^link= | tr ' ' '|'>> cache.beeg.section.list
+	done
+	
+	LIST=`cat cache.beeg.section.list`
+	count=0
+	for ROUND in $LIST; do
+		count=`expr $count + 1`
+		CHECK=`echo $ROUND | cut -d"#" -f1 | sed 's!link=http://beeg.com/!!'`
+		if [ `cat cache.beeg.titanlist | grep "$CHECK" | wc -l` -eq 1 ];then
+			echo "add($count)=$ROUND"
+#			cat cache.beeg.titanlist | grep "$CHECK" >> _full/beeg/streams/beeg.`echo "$TITLE" | tr 'A-Z' 'a-z'`.list
+			cat cache.beeg.titanlist | grep "$CHECK" >> cache.beeg.section.last.`echo "$TITLE" | tr 'A-Z' 'a-z'`.list
+		fi
+	done
+	cat cache.beeg.section.last.`echo "$TITLE" | tr 'A-Z' 'a-z'`.list | sort -u > _full/beeg/streams/beeg.`echo "$TITLE" | tr 'A-Z' 'a-z'`.list	
+done
+
+################
+rm cache.beeg.*
+rm Beeg.*.m3u
+exit
---
+++ titan/titan/tools/parser/youtube.sh
@@ -0,0 +1,100 @@
+rm cache.*
+rm -rf _full/youtube
+mkdir -p _full/youtube/streams
+piccount=0
+
+SEARCHLIST="charts/trailers/most_popular?region=US&v=2 standardfeeds/DE/most_viewed_Music?v=2 standardfeeds/DE/most_viewed?v=2 standardfeedsDE/top_rated?v=2 videos/-/HD videos?q=bodyrock+tv videos?q=trailer+2012+deutsch" 
+
+for SEARCH in $SEARCHLIST; do
+	echo SEARCH=$SEARCH 
+	if [ $SEARCH == "charts/trailers/most_popular?region=US&v=2" ]; then
+		filename=trailers_most_popular_us
+	elif [ $SEARCH == "standardfeeds/DE/most_viewed_Music?v=2" ]; then
+		filename=most_viewed_Music
+	elif [ $SEARCH == "standardfeeds/DE/most_viewed?v=2" ]; then
+		filename=most_viewed
+	elif [ $SEARCH == "standardfeeds/DE/top_rated?v=2" ]; then
+		filename=top_rated
+	elif [ $SEARCH == "videos?q=bodyrock+tv" ]; then
+		filename=bodyrock_tv
+	elif [ $SEARCH == "videos?q=trailer+2012+deutsch" ]; then
+		filename=trailer_2012_deutsch
+	else
+		filename=not_found	
+	fi
+																					
+	rm cache.top_rated.list
+	wget --no-check-certificate "http://gdata.youtube.com/feeds/api/$SEARCH&max-results=50" -O cache.$filename.list
+	LIST=`./urldecode.sh cache.$filename.list | tr '><' '>\n<'| grep url | grep "http://i.ytimg.com/vi/" | grep "width='480'" | cut -d "/" -f5`
+	
+	for ROUND in $LIST; do
+		echo round=$ROUND
+		URL=http://www.youtube.com/watch?v="$ROUND"
+		PIC=http://i.ytimg.com/vi/"$ROUND"/1.jpg
+		piccount=`expr $piccount + 1`
+	
+		wget --no-check-certificate http://www.youtube.com/watch?v=$ROUND -O cache.$filename.title.list
+		TITLE=`./urldecode.sh cache.$filename.title.list | grep '<meta name="title" content="' | sed 's/      <meta name="title" content="//' | sed 's/">//' | tr '&#' '%' | tr -d ';'`
+		if [ -z "$TITLE" ]; then
+			TITLE="not found"
+		fi
+		LINE="$TITLE#$URL#$PIC#youtube_$piccount.jpg#YouTube#4"
+#		LINE="`echo $( php -r "$TITLE"; )`#$URL#$PIC#youtube_$piccount.jpg#YouTube#4"
+		echo $LINE >> cache.youtube.$filename.titanlist
+		echo $LINE >> cache.youtube.all.titanlist
+	done
+	LINE="`echo $filename | tr '_' ' '`#http://atemio.dyndns.tv/mediathek/youtube/streams/youtube.$filename.list#http://atemio.dyndns.tv/mediathek/menu/$filename.jpg#`echo "$filename" | tr 'A-Z' 'a-z'`.jpg#YouTube#1"
+
+	if [ `cat cache.youtube.$filename.titanlist | wc -l` -gt 0 ];then
+		cat cache.youtube.$filename.titanlist > _full/youtube/streams/youtube.$filename.list
+		echo $LINE >> cache.youtube.category.titanlist
+	fi
+
+done
+
+#####################
+wget http://gdata.youtube.com/schemas/2007/categories.cat -O cache.categories.list 
+SEARCHLIST=`cat cache.categories.list | tr '><' '>\n<' | grep "atom:category term=" | tr ' ' '\n' | grep term=| cut -d"'" -f2`
+
+for SEARCH in $SEARCHLIST; do
+	echo SEARCH=$SEARCH
+	filename=`echo $SEARCH | tr 'A-Z' 'a-z'`																		
+	wget --no-check-certificate "http://gdata.youtube.com/feeds/api/standardfeeds/DE/top_rated_$SEARCH?v=2&max-results=50" -O cache.$filename.list
+	LIST=`./urldecode.sh cache.$filename.list | tr '><' '>\n<'| grep url | grep "http://i.ytimg.com/vi/" | grep "width='480'" | cut -d "/" -f5`
+	
+	for ROUND in $LIST; do
+		echo round=$ROUND
+		URL=http://www.youtube.com/watch?v="$ROUND"
+		PIC=http://i.ytimg.com/vi/"$ROUND"/1.jpg
+		piccount=`expr $piccount + 1`
+	
+		wget --no-check-certificate http://www.youtube.com/watch?v=$ROUND -O cache.$filename.title.list
+		TITLE=`./urldecode.sh cache.$filename.title.list | grep '<meta name="title" content="' | sed 's/      <meta name="title" content="//' | sed 's/">//' | tr '&#' '%' | tr -d ';'`
+		if [ -z "$TITLE" ]; then
+			TITLE="not found"
+		fi
+		LINE="$TITLE#$URL#$PIC#youtube_$piccount.jpg#YouTube#4"
+		echo $LINE >> cache.youtube.$filename.titanlist
+		echo $LINE >> cache.youtube.all.titanlist
+	done
+	LINE="`echo $filename | tr '_' ' '`#http://atemio.dyndns.tv/mediathek/youtube/streams/youtube.$filename.list#http://atemio.dyndns.tv/mediathek/menu/$filename.jpg#`echo "$filename" | tr 'A-Z' 'a-z'`.jpg#YouTube#1"
+
+	if [ `cat cache.youtube.$filename.titanlist | wc -l` -gt 0 ];then
+		cat cache.youtube.$filename.titanlist > _full/youtube/streams/youtube.$filename.list
+		echo $LINE >> cache.youtube.category.titanlist
+	fi
+	echo round1111 $ROUND
+done
+
+cat cache.youtube.category.titanlist > _full/youtube/youtube.category.list
+cat cache.youtube.all.titanlist | sort -u > _full/youtube/streams/youtube.all-sorted.list	
+
+for ROUND in 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z; do
+	if [ `cat cache.youtube.all.titanlist | grep ^"$ROUND" | wc -l` -gt 0 ];then
+		cat cache.youtube.all.titanlist | grep ^"$ROUND" > cache.youtube.all.titanlist."$ROUND"
+		cat cache.youtube.all.titanlist."$ROUND" | sort -u > _full/youtube/streams/youtube.`echo "$ROUND" | tr 'A-Z' 'a-z'`.list
+		echo `echo "$ROUND" | tr 'A-Z' 'a-z'`"#http://atemio.dyndns.tv/mediathek/youtube/streams/youtube."`echo "$ROUND" | tr 'A-Z' 'a-z'`".list#http://atemio.dyndns.tv/mediathek/menu/`echo "$ROUND" | tr 'A-Z' 'a-z'`.jpg#"`echo "$ROUND" | tr 'A-Z' 'a-z'`.jpg#YouTube#1 >> _full/youtube/youtube.a-z.list
+	fi
+done
+
+rm cache.*
