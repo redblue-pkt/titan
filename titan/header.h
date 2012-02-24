327d4d198bb71cb5a693de5478746a23301dbeca
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-02-24 23:45:23 +0000

    fix header

--- titan/header.h
+++ titan/header.h
@@ -405,6 +405,7 @@ char* string_decode(char* input, int flag);
 char* string_striptags(char* filename);
 char* string_resub(char* str, char* str2, char* filename);
 int cmpfilenameext(char* filename, char* ext);
+void killnet();
 
 //rcconfig.h
 int getrcconfigint(char *key, char* ext);
