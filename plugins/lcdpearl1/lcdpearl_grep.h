b61106e177248c00a02363a8d5f079a1da547e7c
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-02-26 15:46:19 +0000

    [lcdpearl] workaround

--- plugins/lcdpearl1/lcdpearl_grep.h
+++ plugins/lcdpearl1/lcdpearl_grep.h
@@ -3,6 +3,7 @@
 
 void lcd_raw_update()
 {
+	system("touch /tmp/.imageneu");
 	//char* fbgrab = ostrcat(getconfig("pluginpath", NULL), "/lcdpearl1/fbgrab -f /tmp/titanlcd.raw -w 320 -h 240 -b 32 -i /tmp/.titanlcd1.png > /dev/null", 0, 0);
 	char* fbgrab = ostrcat(getconfig("pluginpath", NULL), "/lcdpearl1/fbgrab -f /tmp/titanlcd.raw -w 320 -h 240 -b 32 -i /tmp/titanlcd.png > /dev/null", 0, 0);
 	system(fbgrab);
@@ -12,12 +13,16 @@ void lcd_raw_update()
 
 int lcd_event_check (int fd)
 {
+  struct timeval tv;
+  tv.tv_sec = 70;
+  tv.tv_usec = 0;
+ 
   fd_set rfds;
   FD_ZERO (&rfds);
   FD_SET (fd, &rfds);
   /* Wait until an event happens or we get interrupted 
      by a signal that we catch */
-  return select (FD_SETSIZE, &rfds, NULL, NULL, NULL);
+  return select (FD_SETSIZE, &rfds, NULL, NULL, &tv);
 }
 
 
@@ -25,9 +30,11 @@ void lcd_raw_event()
 {
   int fd;
   int wd;
+  int rc;
   unsigned int len;
   char *buf;
   buf=malloc((sizeof(struct inotify_event)+16)*1024);
+  char* startlcd = ostrcat(getconfig("pluginpath", NULL), "/lcdpearl1/start.sh", 0, 0);
 
 	while (draw == 0 && LCD_Pearl1thread != NULL)
 	{
@@ -48,7 +55,21 @@ void lcd_raw_event()
   wd = inotify_add_watch( fd,  "/tmp/titanlcd.raw", IN_CLOSE_WRITE);
 
 	while(1){
-    if (lcd_event_check (fd) > 0)
+    
+    printf("new lcd image???\n");
+	
+   	rc = lcd_event_check (fd);
+   	
+   	if (system("ls /tmp/.imageneu")	== 0)
+   	{
+   		printf("kill\n");
+   		system("killall lcd4linux");
+   		sleep(1);
+   		system(startlcd);
+   	}
+   	
+   	//if (lcd_event_check (fd) > 0)
+   	if (rc > 0)
     {
       len=read(fd, buf,(sizeof(struct inotify_event)+16)*1024 );
       lcd_raw_update();
@@ -64,4 +85,5 @@ void lcd_raw_event()
   	//wd = inotify_add_watch( fd, "/tmp/titanlcd.raw", IN_CLOSE_WRITE);
 	}
 	free(buf);
+	free(startlcd);startlcd=NULL;
 }
