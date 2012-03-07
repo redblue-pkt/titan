8ecbfb42edd9210585c9062f1097915bf617b831
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-03-07 22:19:51 +0000

    [titan] WebIf - set timer at search epg

--- titan/httpdfunc.h
+++ titan/httpdfunc.h
@@ -907,7 +907,9 @@ char* webgetepg(char* param)
 		buf = ostrcat(buf, " (", 1, 0);
 		loctime = olocaltime(&epgnode->starttime);
 		if(loctime != NULL)
-			strftime(buf1, MINMALLOC, "%H:%M -", loctime);
+			%H:%M %d-%m-%Y
+			strftime(buf1, MINMALLOC, "%d-%m -> %H:%M -", loctime);
+			//strftime(buf1, MINMALLOC, "%H:%M -", loctime);
 		free(loctime); loctime = NULL;
 		loctime = olocaltime(&epgnode->endtime);
 		if(loctime != NULL)
