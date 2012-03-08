75c36c7492a3cc702be7d08f6dc1a2a920a7ea41
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-03-08 22:29:55 +0000

    [titan] aktivate dyndns

--- titan/menu.h
+++ titan/menu.h
@@ -392,6 +392,11 @@ int menucall(struct skin* menunode, struct skin* menuentry, int check)
 		if(check == 1) return 0;
 		screennetwork_wlan();
 	}
+	else if(ostrcmp("inadynsettings", menuentry->name) == 0)
+	{
+		if(check == 1) return 0;
+		screennetwork_inadyn();
+	}
 	else if(menuentry->pluginhandle != NULL)
 	{
 		if(menuentry->input != NULL)
