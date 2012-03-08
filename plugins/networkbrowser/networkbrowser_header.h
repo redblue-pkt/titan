6984d5c1a5803e3696e88d12bfe2af16ccaf0f34
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-03-08 22:13:34 +0000

    [titan] make networkbrowser multi net interface ready

--- plugins/networkbrowser/networkbrowser_header.h
+++ plugins/networkbrowser/networkbrowser_header.h
@@ -3,7 +3,7 @@
 
 char* getnetworkbrowser_cifs(char* s, char* r, char* u, char* p);
 char* getnetworkbrowser_nfs(char* s, char* r);
-char* getnetworkbrowser_dns();
+char* getnetworkbrowser_dns(struct inetwork* net);
 void screennetworkbrowser_addshare(int mode, char* sharename, char* ipaddresse, char* sharedir, char* username, char* password, char* rsize, char* wsize, char* protocol, char* options, char* hddreplacement, char* options1, char* options2, char* ssl, char* proxy, char* proxyip, char* proxyport, char* proxyuser, char* proxypass, char* ftpport, char* userauth, char* proxyauth, char* useproxy, char* usessl);
 void screennetworkbrowser_readshare(int mode, char* input_sharename, char* input_shareline);
 
