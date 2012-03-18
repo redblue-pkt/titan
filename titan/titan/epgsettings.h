1cd5c7f04668dc4482c7d90586b7ede5cd989528
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-03-18 00:17:40 +0000

    [titan] fix

--- titan/titan/epgsettings.h
+++ titan/titan/epgsettings.h
@@ -203,6 +203,12 @@ void screenepgsettings()
 			textbox(_("Message"), _("EPG resetet and now clear."), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0);
 			drawscreen(epgsettings, 0);
 		}
+		if(rcret == getrcconfigint("rcyellow", NULL))
+		{
+			clearscreen(epgsettings);
+			screenepgscanlist();
+			drawscreen(epgsettings, 0);
+		}
 	}
 
 	delownerrc(epgsettings);
