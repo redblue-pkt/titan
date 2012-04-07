1b94f116e6095f035c22011d0484d50b70cec71e
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-04-07 10:30:24 +0000

    [titan] change ipkg menu to struct

--- plugins/panel/panel_extensions.h
+++ plugins/panel/panel_extensions.h
@@ -4,111 +4,83 @@
 
 void screenpanel_extensions(int mode)
 {
-	char* section = NULL;
-	char* file = NULL;
-	char* tmpstr = NULL;
+	char* tmpstr = NULL, *tmpinfo = NULL;
+	struct menulist* mlist = NULL, *mbox = NULL;
+	struct menulist* mlist1 = NULL, *mbox1 = NULL;
 	
-	if (mode == 0)
+	if(mode == 0)
 	{
 		ipkg_update();
 		ipkg_list();
-		tmpstr = get_ipk_section();
 
-		section = ipk_listbox(NULL, tmpstr, NULL, "Ipk Install - select section", "%pluginpath%/panel/skin/", 1);
-		if(tmpstr == NULL)
-			textbox(_("Ipk Install Info"), _("No section found"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0);
+		mbox = ipkmenulist(mlist, NULL, "Ipk Install - select section", "%pluginpath%/panel/skin", 1, 0);
 
-		free(tmpstr); tmpstr = NULL;
-
-		debug(130, "section: %s", section);
-		if(section != NULL)
+		if(mbox != NULL)
 		{
-			file = ipk_listbox(NULL, section, "ipkinstall", "Ipk Install - select file", "/tmp/preview/", 2);
-			debug(130, "file: %s", file);
+			debug(130, "section: %s", mbox->name);
+			mbox1 = ipkmenulist(mlist1, "ipkinstall", "Ipk Install - select file", "/tmp/preview", 2, 1);
 			
-			if(file != NULL)
+			if(mbox1 != NULL)
 			{
-				tmpstr = ostrcat(section, "-", 0, 0);
-				tmpstr = ostrcat(tmpstr, file, 1, 0);
+				debug(130, "file: %s", mbox1->name);
+				tmpstr = ostrcat("titan-plugin-", mbox->name, 0, 0);
+				tmpstr = ostrcat(tmpstr, "-", 1, 0);
+				tmpstr = ostrcat(tmpstr, mbox1->param, 1, 0);
 
-				char* tmpinfo = NULL;
 				tmpinfo = ostrcat(tmpinfo, "Install ", 1, 0);
-				tmpinfo = ostrcat(tmpinfo, tmpstr, 1, 0);
+				tmpinfo = ostrcat(tmpinfo, mbox->name, 1, 0);
+				tmpinfo = ostrcat(tmpinfo, "-", 1, 0);
+				tmpinfo = ostrcat(tmpinfo, mbox1->name, 1, 0);
 				tmpinfo = ostrcat(tmpinfo, " ?", 1, 0);
 
-				char* installname = NULL;
-				struct splitstr* ret1 = NULL;
-				int count = 0;
-
-				ret1 = strsplit(tmpstr, " ", &count);
-				
-				if(ret1 != NULL)
+				if(textbox(_("Ipk Install Info"), _(tmpinfo), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0) == 1)
 				{
-					installname = ostrcat("titan-plugin-", (&ret1[0])->part, 0, 0);
-		
-					if(textbox(_("Ipk Install Info"), _(tmpinfo), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0) == 1)
+					char* log = NULL;
+					if(ipkg_install(tmpstr) == 0)
 					{
-//						char* ipkg_ret = get_ipk_install(installname);
-//						textbox(_("Ipk Install Info"), _(ipkg_ret), "EXIT", getrcconfigint("rcexit", NULL), "OK", getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, 1000, 600, 0, 0);
-//						free(ipkg_ret); ipkg_ret = NULL;
-						char* log = NULL;
-						if(ipkg_install(installname) == 0)
-						{
-							log = readfiletomem("/tmp/ipkg.log", 0);
-							if(log == NULL) log = ostrcat("Install success", NULL, 0, 0);
-							textbox(_("Ipk Install Info - Install OK"), _(log), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 600, 0, 0);
-						}
-						else
-						{
-							log = readfiletomem("/tmp/ipkg.log", 0);
-							if(log == NULL) log = ostrcat("Install error", NULL, 0, 0);
-							textbox(_("Ipk Install Info - Install ERROR"), _(log), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 600, 0, 0);
-						}
-						textbox(_("Message"), _("Some plugins needs restart.\nIf the plugin is not active\nreboot the box."), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 200, 0, 0);
-						free(log), log = NULL;
-						unlink("/tmp/ipkg.log");
+						log = readfiletomem("/tmp/ipkg.log", 0);
+						if(log == NULL) log = ostrcat("Install success", NULL, 0, 0);
+						textbox(_("Ipk Install Info - Install OK"), _(log), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 600, 0, 0);
 					}
+					else
+					{
+						log = readfiletomem("/tmp/ipkg.log", 0);
+						if(log == NULL) log = ostrcat("Install error", NULL, 0, 0);
+						textbox(_("Ipk Install Info - Install ERROR"), _(log), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 600, 0, 0);
+					}
+					textbox(_("Message"), _("Some plugins needs restart.\nIf the plugin is not active\nreboot the box."), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 200, 0, 0);
+					free(log), log = NULL;
+					unlink("/tmp/ipkg.log");
 				}
-				free(ret1); ret1 = NULL;
-				free(installname); installname = NULL;
-				free(tmpinfo); tmpinfo = NULL;
 			}
-			
-			free(section); section = NULL;
-			free(file); file = NULL;
-			free(tmpstr); tmpstr = NULL;
-			freeipkg();
-			screenpanel_extensions(0);
 		}
-		
-		freeipkg();
-		
-	}
-	else if (mode == 1)
-	{
-		tmpstr = get_ipk_listinstall();
-		file = ipk_listbox(NULL, tmpstr, NULL, "Ipk Remove - select file", "%pluginpath%/panel/skin/", 1);
+			
+		freemenulist(mlist1); mlist1 = NULL;
+		freemenulist(mlist); mlist = NULL;
 		free(tmpstr); tmpstr = NULL;
+		free(tmpinfo); tmpinfo = NULL;
+		freeipkg();
+		if(mbox != NULL) screenpanel_extensions(0);
+	}
+	else if(mode == 1)
+	{
+		ipkg_list_installed();
+		mbox = ipkmenulist(mlist, NULL, "Ipk Remove - select file", "%pluginpath%/panel/skin", 1, 0);
 		
-		debug(130, "file: %s", file);
-		if(file != NULL)
+		if(mbox != NULL)
 		{
-			char* tmpinfo = NULL;
-			tmpinfo = ostrcat(tmpinfo, "Remove ", 1, 0);
-			tmpinfo = ostrcat(tmpinfo, file, 1, 0);
-			tmpinfo = ostrcat(tmpinfo, " ?", 1, 0);
+			debug(130, "file: %s", mbox->name);
 
-			char* deinstallname = NULL;
-			deinstallname = ostrcat("titan-plugin-", file, 0, 0);
+			tmpstr = ostrcat("titan-plugin-", mbox->name, 0, 0);
+
+			tmpinfo = ostrcat(tmpinfo, "Remove ", 1, 0);
+			tmpinfo = ostrcat(tmpinfo, mbox->name, 1, 0);
+			tmpinfo = ostrcat(tmpinfo, " ?", 1, 0);
 
 			if(textbox(_("Remove Info"), _(tmpinfo), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0) == 1)
 			{
-//				char* ipkg_ret = get_ipk_remove(deinstallname);
-//				textbox(_("Ipk Remove Info"), _(ipkg_ret), "EXIT", getrcconfigint("rcexit", NULL), "OK", getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, 800, 600, 0, 0);
-//				free(ipkg_ret); ipkg_ret = NULL; 
-
 				char* log = NULL;
-				if(ipkg_remove(deinstallname,1) == 0)
+				if(ipkg_remove(tmpstr, 1) == 0)
 				{
 					log = readfiletomem("/tmp/ipkg.log", 0);
 					if(log == NULL) log = ostrcat("Remove success", NULL, 0, 0);
@@ -124,27 +96,24 @@ void screenpanel_extensions(int mode)
 				free(log), log = NULL;
 				unlink("/tmp/ipkg.log");
 			}
-
-			free(deinstallname); deinstallname = NULL;
-			free(tmpinfo); tmpinfo = NULL;
-			free(file); file = NULL;
-			screenpanel_extensions(1);
 		}
-		free(file); file = NULL;
+		freemenulist(mlist); mlist = NULL;
+		free(tmpstr); tmpstr = NULL;
+		free(tmpinfo); tmpinfo = NULL;
+		freeipkg();
+		if(mbox != NULL) screenpanel_extensions(1);
 	}
-	else if (mode == 2)
+	else if(mode == 2)
 	{
+/*
 		tmpstr = get_ipk_tmplistinstall();
 		file = ipk_listbox(NULL, tmpstr, NULL, "Ipk Tmp Install - select file", "%pluginpath%/panel/skin/", 1);
 		
-		if(tmpstr == NULL)
-			textbox(_("Ipk Tmp Info"), _("No IPK found in /tmp"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0);
 		free(tmpstr); tmpstr = NULL;
 		
 		debug(130, "file: %s", file);
 		if(file != NULL)
 		{
-			char* tmpinfo = NULL;
 			tmpinfo = ostrcat(tmpinfo, "Install ", 1, 0);
 			tmpinfo = ostrcat(tmpinfo, file, 1, 0);
 			tmpinfo = ostrcat(tmpinfo, " ?", 1, 0);
@@ -167,6 +136,7 @@ void screenpanel_extensions(int mode)
 			screenpanel_extensions(2);
 		}
 		free(file); file = NULL;
+*/
 	}
 }
 
