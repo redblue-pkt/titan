d06f8c6b2181e8fb38fb11512b554e2cab1522e7
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-04-07 21:10:20 +0000

    [titan] cleanup

--- plugins/panel/panel_header.h
+++ plugins/panel/panel_header.h
@@ -1,9 +1,6 @@
 #ifndef PANEL_HEADER_H
 #define PANEL_HEADER_H
 
-// panel_ipk.h
-char* ipklistbox(char* defaultstr, char* str, char* skinname, char* skintitle, char* skinpath, int showpng);
-
 void screenplugin();
 
 // panel_main.h
