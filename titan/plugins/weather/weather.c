15885c6a3c4fd96a55f67991559ea48b85937c7a
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-04-26 21:53:06 +0000

    [titan] add weather plugin

---
+++ titan/plugins/weather/weather.c
@@ -0,0 +1,37 @@
+#include "../titan/struct.h"
+#include "../titan/debug.h"
+#include "../titan/header.h"
+#include "weather.h"
+
+char pluginname[] = "Weather";
+char plugindesc[] = "Weather";
+char pluginpic[] = "%pluginpath%/weather/plugin.png";
+
+int pluginaktiv = 0;
+
+//wird beim laden ausgefuehrt
+void init(void)
+{
+	char* tmpstr = NULL;
+
+	tmpstr = ostrcat(getconfig("pluginpath", NULL), "/weather/skin.xml", 0, 0);
+	readscreen(tmpstr, 208, 1);
+	free(tmpstr); tmpstr = NULL;
+
+	pluginaktiv = 1;
+	debug(10, "Weather Plugin loadet !!!");
+}
+
+//wird beim entladen ausgefuehrt
+void deinit(void)
+{
+	delmarkedscreen(208);
+	pluginaktiv = 0;
+	debug(10, "Weather Plugin removed !!!");
+}
+
+//wird in der Pluginverwaltung bzw Menue ausfeguehrt
+void start(void)
+{
+	screenweather();
+}
