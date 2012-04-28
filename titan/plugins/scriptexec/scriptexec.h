370dde0ca6ebde7b0bf47df341f6a5a70fe84f97
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-04-28 09:15:03 +0000

    [titan] update copyright

--- titan/plugins/scriptexec/scriptexec.h
+++ titan/plugins/scriptexec/scriptexec.h
@@ -1,3 +1,10 @@
+/*****************************************************/
+/* this file is part of the tiTan / tiTanNIT Project */
+/* and allowed only for use with this.               */
+/*                                                   */
+/* copyright by NIT                                  */
+/*****************************************************/
+
 #ifndef SCRIPTEXEC_H
 #define SCRIPTEXEC_H
 
