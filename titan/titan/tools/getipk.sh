56aaa043db745972c40820406615faef14bab704
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-05-03 22:09:28 +0000

    fix

--- titan/titan/tools/getipk.sh
+++ titan/titan/tools/getipk.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 IPKDIR=$1
+. "$HOME"/flashimg/image/config/make-config
 
 cd "$HOME"/flashimg/source.titan/titan/tools
 
 cat ../../plugins/panel/panel_feed.h | sed s!/svn/ipk/sh4/titan!$IPKDIR! > panel_feed.h.tmp
 mv panel_feed.h.tmp ../../plugins/panel/panel_feed.h
 
-cat ../ipkg.h | sed s!//97.74.32.10/svn/ipk/sh4/titan!http://97.74.32.10$IPKDIR/Packages.gz! > ipkg.h.tmp
+cat ../ipkg.h | sed s!//97.74.32.10/svn/ipk/sh4/titan!http://$URLSECRET$IPKDIR/Packages.gz! > ipkg.h.tmp
 mv ipkg.h.tmp ../ipkg.h
