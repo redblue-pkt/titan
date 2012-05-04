e7af33249182c8bf6ca33dbad1826847c2c84254
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-05-04 15:04:35 +0000

    fix

--- titan/titan/header.h
+++ titan/titan/header.h
@@ -349,7 +349,7 @@ char* gettime(struct skin* node, char* format);
 char* getepgakttitle(struct skin* node);
 
 //skin.h
-unsigned char *loadjpg(char *filename, int *width, int *height, int denom);
+unsigned char *loadjpg(char *filename, unsigned long *width, unsigned long *height, unsigned long *rowbytes, int *channels, int denom);
 
 void calcautoscale(int width, int height, int mwidth, int mheight, int* scalewidth, int* scaleheight);
 void blitscale(int posx, int posy, int width, int height, int scalewidth, int scaleheight, int flag);
