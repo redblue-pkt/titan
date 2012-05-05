95547d951b037a5591dafcd2a2a36052b39569d2
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-05-05 12:53:09 +0000

    [titan] add checkpmtalways

--- titan/titan.c
+++ titan/titan.c
@@ -738,7 +738,8 @@ firstwizzardstep1:
 	//start epg scanlist
 	status.epgscanlistthread = addtimer(&epgscanlistthread, START, 1000, 1, NULL, NULL, NULL);
 	//get pmt
-	addtimer(&dvbgetpmtthread, START, 2000, -1, NULL, NULL, NULL);
+  if(getconfigint("checkpmtalways", NULL) == 1)
+    addtimer(&dvbgetpmtthread, START, 2000, -1, NULL, NULL, NULL);
 	//check hdd
 	status.addhddall = addtimer(&addhddall, START, 6000, -1, NULL, NULL, NULL);
 	//check net
