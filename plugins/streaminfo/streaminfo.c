4aa254d1b375c36a57aea4927f81be527e4e03cc
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-05-08 19:47:22 +0000

    [titan] add plugin streaminfo

---
+++ plugins/streaminfo/streaminfo.c
@@ -0,0 +1,40 @@
+#include "../titan/struct.h"
+#include "../titan/debug.h"
+#include "../titan/header.h"
+#include "streaminfo.h"
+
+char pluginname[] = "Streaminfo";
+char plugindesc[] = "Streaminfo";
+char pluginpic[] = "%pluginpath%/streaminfo/plugin.png";
+
+int pluginaktiv = 0;
+struct skin* pluginmenu = NULL;
+int pluginflag = 0;
+
+//wird beim laden ausgefuehrt
+void init(void)
+{
+	pluginaktiv = 1;
+	char* tmpstr = NULL;
+	
+	tmpstr = ostrcat(getconfig("pluginpath", NULL), "/streaminfo/skin.xml", 0, 0);
+	readscreen(tmpstr, 225, 1);
+	free(tmpstr); tmpstr = NULL;
+  
+	debug(10, "Streaminfo Plugin loadet !!!");
+}
+
+//wird beim entladen ausgefuehrt
+void deinit(void)
+{
+	delmarkedscreen(225);
+	pluginaktiv = 0;
+	debug(10, "Streaminfo Plugin removed !!!");
+}
+
+
+//wird in der Pluginverwaltung bzw Menue ausfeguehrt
+void start(void)
+{
+	screenstreaminfo();
+}
---
+++ plugins/streaminfo/streaminfo.h
@@ -0,0 +1,67 @@
+#ifndef STREAMINFO_H
+#define STREAMINFO_H
+
+void screenstreaminfo()
+{
+	int rcret = 0;
+	struct skin* streaminfo = getscreen("streaminfo");
+	struct skin* aspect = getscreennode(streaminfo, "aspect");
+	struct skin* framerate = getscreennode(streaminfo, "framerate");
+	struct skin* xres = getscreennode(streaminfo, "xres");
+	struct skin* yres = getscreennode(streaminfo, "yres");
+	char* tmpstr = NULL;
+	unsigned long tmpnr = 0;
+
+	while(1)
+	{
+		tmpstr = getdevcontent("vmpegaspectdev");
+		if(tmpstr != NULL)
+		{
+			if(ostrncmp(tmpstr, "0", 1) == 0)
+				changetext(aspect, "4:3");
+			else
+				changetext(aspect, "16:9");
+		}
+		free(tmpstr); tmpstr = NULL;
+
+		tmpstr = getdevcontent("vmpegframeratedev");
+		if(tmpstr != NULL)
+		{
+			tmpnr = strtol(tmpstr, 0, 16);
+			free(tmpstr); tmpstr = NULL;
+			tmpstr = olutoa(tmpnr);
+			changetext(framerate, tmpstr);
+		}
+		free(tmpstr); tmpstr = NULL;
+
+		tmpstr = getdevcontent("vmpegxresdev");
+		if(tmpstr != NULL)
+		{
+			tmpnr = strtol(tmpstr, 0, 16);
+			free(tmpstr); tmpstr = NULL;
+			tmpstr = olutoa(tmpnr);
+			changetext(xres, tmpstr);
+		}
+		free(tmpstr); tmpstr = NULL;
+
+		tmpstr = getdevcontent("vmpegyresdev");
+		if(tmpstr != NULL)
+		{
+			tmpnr = strtol(tmpstr, 0, 16);
+			free(tmpstr); tmpstr = NULL;
+			tmpstr = olutoa(tmpnr);
+			changetext(yres, tmpstr);
+		}
+		free(tmpstr); tmpstr = NULL;
+
+		drawscreen(streaminfo, 0);
+
+		rcret = waitrc(streaminfo, 1000, 0);
+                if(rcret == getrcconfigint("rcexit", NULL)) break;
+                if(rcret == getrcconfigint("rcok", NULL)) break;
+	}
+
+	clearscreen(streaminfo);
+}
+
+#endif
