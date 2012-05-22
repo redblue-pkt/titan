b09b5888ccaa75327654e75890f122a8e25ebe1d
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-05-22 22:34:02 +0000

    fix

--- titan/ipkg.h
+++ titan/ipkg.h
@@ -365,7 +365,10 @@ int ipkg_download(ipkg_conf_t *conf, const char *src, const char *filename)
 		checkfile = ostrcat("/tmp/Packages.", ip, 0, 0);
 
 		if(ostrcmp("97.74.32.10", ip) == 0)
-			free(ip); ip = ostrcat("atemio.dyndns.tv", NULL, 0, 0);
+		{
+			free(ip);
+			ip = ostrcat("atemio.dyndns.tv", NULL, 0, 0);
+		}
 
 		if(!file_exist(checkfile)) // +status.ipkg = date + 1day
 		{
@@ -385,7 +388,10 @@ int ipkg_download(ipkg_conf_t *conf, const char *src, const char *filename)
 	else
 	{
 		if(ostrcmp("97.74.32.10", ip) == 0)
-			free(ip); ip = ostrcat("atemio.dyndns.tv", NULL, 0, 0);
+		{
+			free(ip);
+			ip = ostrcat("atemio.dyndns.tv", NULL, 0, 0);
+		}
 	
 		err = screendownload("Download", ip, path, 80, (char*)filename, HTTPAUTH, 0);
 	}
