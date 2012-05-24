b649c8abdc2370400134da7ef448a3b038e0ced1
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-05-24 10:24:44 +0000

    [titan] fix secret feed

--- titan/tools/getipk.sh
+++ titan/tools/getipk.sh
@@ -4,9 +4,9 @@ IPKDIR=$1
 
 cd "$HOME"/flashimg/source.titan/titan/tools
 
-echo getipkg panel_feed.h patch 1
-cat ../../plugins/panel/panel_feed.h | sed s!/svn/ipk/sh4/titan!$IPKDIR! > panel_feed.h.tmp
-mv panel_feed.h.tmp ../../plugins/panel/panel_feed.h
+echo getipkg extensions.hpatch 1
+cat ../extensions.h | sed s!/svn/ipk/sh4/titan!$IPKDIR! > extensions.h.tmp
+mv extensions.h.tmp ../extensions.h
 
 echo getipkg ipkg.h patch 1
 cat ../ipkg.h | sed s!//97.74.32.10/svn/ipk/sh4/titan!http://$URLSECRET$IPKDIR/Packages.gz! > ipkg.h.tmp
