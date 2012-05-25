441d496ce5e3cf3be4aca0db4415eda92fbc91e5
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-05-25 17:25:09 +0000

    [titan] update copyrigth

--- plugins/rgui/rgui.h
+++ plugins/rgui/rgui.h
@@ -1,3 +1,10 @@
+/*****************************************************/
+/* this file is part of the tiTan / tiTanNIT Project */
+/* and allowed only for use with this.               */
+/*                                                   */
+/* copyright by NIT                                  */
+/*****************************************************/
+
 #ifndef RGUI_H
 #define RGUI_H
 
