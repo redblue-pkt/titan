ee1d12f0d63f5d2f267e9f0fb5d4e5bb43add116
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-06-03 21:32:03 +0000

    fix gmediarender

--- titan/plugins/gmediarender/gmediarender.sh
+++ titan/plugins/gmediarender/gmediarender.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
-
-gmediarender -f $1 &> /dev/null
+#
+echo start gmediarender
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/var/swap/lib
+gmediarender -f $1 --gst-debug-disable
+echo close gmediarender
