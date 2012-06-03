120ae14e53702f492703062dbbefd01f6c6213bb
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-06-03 21:32:03 +0000

    fix gmediarender

--- plugins/gmediarender/gmediarender.h
+++ plugins/gmediarender/gmediarender.h
@@ -2,6 +2,7 @@
 #define GMEDIARENDER_H
 
 extern struct skin* skin;
+struct fb *fb = NULL;
 
 void screengmediarender()
 {
@@ -9,7 +10,7 @@ void screengmediarender()
 	char* cmd = NULL;
 	char* tmpstr = NULL;
 	
-	ret = servicestop(status.aktservice, 1, 0);
+	ret = servicestop(status.aktservice, 1, 1);
 	if(ret == 1) return;
 
 	drawscreen(skin, 0);
@@ -22,9 +23,10 @@ void screengmediarender()
 	system(cmd);
 	free(cmd), cmd = NULL;
 
-	tmpstr = ostrcat(tmpstr, "--- gmediarender-", 1, 0);
+	tmpstr = ostrcat(tmpstr, _("Servername"), 1, 0);
+	tmpstr = ostrcat(tmpstr, ": gmediarender-", 1, 0);
 	tmpstr = ostrcat(tmpstr, status.boxtype, 1, 0);
-	tmpstr = ostrcat(tmpstr, " ---\n", 1, 0);
+	tmpstr = ostrcat(tmpstr, "\n\n", 1, 0);
 	tmpstr = ostrcat(tmpstr, _("Wait for connect or press EXIT"), 1, 0);
 
 	textbox(_("Message"), tmpstr, _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 7, 0);
@@ -34,13 +36,17 @@ void screengmediarender()
 	{
 		rcret = waitrc(NULL, 0, 0);
 
-		if(rcret == getrcconfigint("rcexit", NULL)) break;
+		if(rcret == getrcconfigint("rcexit", NULL))
+		{
+			printf("gui neustart\n");
+			break;
+		}
 		if(rcret == getrcconfigint("rcok", NULL)) break;
 	}
 
 	//stop renderer
 	system("killall -9 gmediarender");
-
+			
 	if(status.lastservice != NULL)
 		servicestart(status.lastservice->channel, NULL, NULL, 0);
 }
