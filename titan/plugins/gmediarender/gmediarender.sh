0d17c17210cbeed2dc26085f75e5c109f4455204
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-06-04 17:11:40 +0000

    fix links

--- titan/plugins/gmediarender/gmediarender.sh
+++ titan/plugins/gmediarender/gmediarender.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 #
+
+rm -rf /var/usr/lib/gstreamer-0.10
+ln -s /var/swap/lib/gstreamer-0.10 /var/usr/lib
+rm -rf /var/usr/lib/directfb-1.4-5
+ln -s /var/swap/lib/directfb-1.4-5 /var/usr/lib
+
 echo start gmediarender
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/var/swap/lib
 gmediarender -f $1 --gst-debug-disable
