c7d6bfb57487004861fa73b702e4964b7871dbea
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-06-04 17:12:29 +0000

    fix

--- plugins/gmediarender/gmediarender.sh
+++ plugins/gmediarender/gmediarender.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 #
 
-rm -rf /var/usr/lib/gstreamer-0.10
-ln -s /var/swap/lib/gstreamer-0.10 /var/usr/lib
-rm -rf /var/usr/lib/directfb-1.4-5
-ln -s /var/swap/lib/directfb-1.4-5 /var/usr/lib
+#rm -rf /var/usr/lib/gstreamer-0.10
+#ln -s /var/swap/lib/gstreamer-0.10 /var/usr/lib
+#rm -rf /var/usr/lib/directfb-1.4-5
+#ln -s /var/swap/lib/directfb-1.4-5 /var/usr/lib
 
 echo start gmediarender
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/var/swap/lib
