e73fd1ec8a80a9f46a47520f5627ecf897b6b0bf
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-06-14 02:17:06 +0000

    fix mc

--- plugins/mc/mc_mounter.h
+++ plugins/mc/mc_mounter.h
@@ -16,41 +16,38 @@ int imgFS  = 0;
 
 int mc_mounter_umount()
 {
-	debug(50, "[mc_mounter_umount] rarFS=%d imgFS=%d isoFS=%d", rarFS, imgFS, isoFS);
-
+	debug(50, "rarFS=%d imgFS=%d isoFS=%d", rarFS, imgFS, isoFS);
 	char* cmd = NULL;
 
-	if(isoFS == 1){
+	if(isoFS == 1)
+	{
 		cmd = ostrcat(cmd, "/bin/umount -fl `mount | grep iso9660 | cut -d ' ' -f3`", 1, 0);
+		debug(50, "cmd: %s", cmd);
 		system(cmd);
-		debug(50, "[mc_mounter_umount] cmd: %s", cmd);
-		free(isoMountDir);
-		isoMountDir = NULL;
-		free(isoMountDirSplit);
-		isoMountDirSplit = NULL;
+		free(isoMountDir), isoMountDir = NULL;
+		free(isoMountDirSplit), isoMountDirSplit = NULL;
 		isoFS = 0;
-	} else if(imgFS == 1){
+	}
+	else if(imgFS == 1)
+	{
 		cmd = ostrcat(cmd, "/bin/umount -fl `mount | grep iso9660 | cut -d ' ' -f3`", 1, 0);
+		debug(50, "cmd: %s", cmd);
 		system(cmd);
-		debug(50, "[mc_mounter_umount] cmd: %s", cmd);
-		free(imgMountDir);
-		imgMountDir = NULL;
-		free(imgMountDirSplit);
-		imgMountDirSplit = NULL;
+		free(imgMountDir), imgMountDir = NULL;
+		free(imgMountDirSplit), imgMountDirSplit = NULL;
 		imgFS = 0;
-	} else if(rarFS == 1){
+	}
+	else if(rarFS == 1)
+	{
 		cmd = ostrcat("/bin/umount -fl `mount | grep fuse | grep rarfs | grep -v '/dev/' | cut -d ' ' -f3` ", rarMountDir, 0, 0);
 		cmd = ostrcat(cmd, " ; killall -9 rarfs", 1, 0);
+		debug(50, "cmd: %s", cmd);
 		system(cmd);
-		debug(50, "[mc_mounter_umount] cmd: %s", cmd);
-		free(rarMountDir);
-		rarMountDir = NULL;
-		free(rarMountDirSplit);
-		rarMountDirSplit = NULL;
+		free(rarMountDir), rarMountDir = NULL;
+		free(rarMountDirSplit), rarMountDirSplit = NULL;
 		rarFS = 0;
 	}
-	free(cmd);
-	cmd = NULL;
+	free(cmd), cmd = NULL;
 
 	return 0;
 }
@@ -58,141 +55,137 @@ int mc_mounter_umount()
 
 char* mc_mounter_main(int mode, char* tmpstr, struct skin* filelistpath, struct skin* filelist, struct skin* mc_videoplayer, char* filemask, int tmpview, char* path)
 {
-	debug(50, "[mc_mounter_main] start");
+	debug(50, "start");
 
-	if(!strncmp(".rar",tmpstr+strlen(tmpstr)-4,4)){
-		debug(50, "[mc_mounter_main] found rar file: %s", tmpstr);
-		mkdir("/tmp/rarfs", 777);
+	if(!strncmp(".rar",tmpstr+strlen(tmpstr)-4,4))
+	{
+		debug(50, "found rar file: %s", tmpstr);
+		mkdir("/tmp/rarfs", 0777);
 
-		tmpmounter = ostrcat("/sbin/rarfs \"", tmpstr, 0, 0);
+		tmpmounter = ostrcat("rarfs \"", tmpstr, 0, 0);
 		tmpmounter = ostrcat(tmpmounter, "\" /tmp/rarfs", 1, 0);
-		debug(50, "[mc_mounter_main] mount line1: %s", tmpmounter);
+		debug(50, "mount line1: %s", tmpmounter);
 		system(tmpmounter);
 
-		tmpmounter = ostrcat("/bin/mount --bind /tmp/rarfs \"", filelistpath->text, 0, 0);
+		tmpmounter = ostrcat("mount --bind /tmp/rarfs \"", filelistpath->text, 0, 0);
 		tmpmounter = ostrcat(tmpmounter, "\"", 1, 0);
-		debug(50, "[mc_mounter_main] mount line2: %s", tmpmounter);
+		debug(50, "line2: %s", tmpmounter);
 
-		free(tmpstr);
-		tmpstr = NULL;
+		free(tmpstr),tmpstr = NULL;
 		system(tmpmounter);
 
-		debug(50, "[mc_mounter_main] save mc_videoplayerpath: %s", filelistpath->text);
-		addconfig("mc_videoplayerpath", filelistpath->text);
+		debug(50, "save mc_videoplayerpath: %s", filelistpath->text);
+		addconfig("mc_vp_path", filelistpath->text);
 		addscreenrc(mc_videoplayer, filelist);
 		getfilelist(mc_videoplayer, filelistpath, filelist, path, filemask, tmpview, NULL);
 
 		rarMountDir = ostrcat(rarMountDir, filelistpath->text, 1, 0);
-		debug(50, "[mc_mounter_main] save rarMountDir: %s", rarMountDir);
+		debug(50, "save rarMountDir: %s", rarMountDir);
 
 		rarMountDirSplit = ostrcat(rarMountDirSplit, filelistpath->text, 1, 0);
 		dirname(rarMountDirSplit);
-		debug(50, "[mc_mounter_main] save rarMountDirSplit: %s", rarMountDirSplit);
+		debug(50, "save rarMountDirSplit: %s", rarMountDirSplit);
 
 		rarFS = 1;
-	} else if(!strncmp(".img",tmpstr+strlen(tmpstr)-4,4)){
-		debug(50, "[mc_mounter_main] found img file: %s", tmpstr);
+	}
+	else if(!strncmp(".img",tmpstr+strlen(tmpstr)-4,4))
+	{
+		debug(50, "found img file: %s", tmpstr);
 		mkdir("/tmp/imgfs", 777);
 		tmpmounter = ostrcat("mount -o loop \"", tmpstr, 0, 0);
 		tmpmounter = ostrcat(tmpmounter, "\" /tmp/imgfs", 1, 0); 
 
-		debug(50, "[mc_mounter_main] mount line1: %s", tmpmounter);
+		debug(50, "mount line1: %s", tmpmounter);
 		system(tmpmounter);
 
-		tmpmounter = ostrcat("/bin/mount --bind /tmp/imgfs \"", filelistpath->text, 0, 0);
+		tmpmounter = ostrcat("mount --bind /tmp/imgfs \"", filelistpath->text, 0, 0);
 		tmpmounter = ostrcat(tmpmounter, "\"", 1, 0);
-		debug(50, "[mc_mounter_main] mount line2: %s", tmpmounter);
+		debug(50, "mount line2: %s", tmpmounter);
 
-		free(tmpstr);
-		tmpstr = NULL;
+		free(tmpstr),tmpstr = NULL;
 		system(tmpmounter);
 
-		debug(50, "[mc_mounter_main] save mc_videoplayerpath: %s", filelistpath->text);
-		addconfig("mc_videoplayerpath", filelistpath->text);
+		debug(50, "save mc_videoplayerpath: %s", filelistpath->text);
+		addconfig("mc_vp_path", filelistpath->text);
 		addscreenrc(mc_videoplayer, filelist);
 		getfilelist(mc_videoplayer, filelistpath, filelist, path, filemask, tmpview, NULL);
 
 		imgMountDir = ostrcat(imgMountDir, filelistpath->text, 1, 0);
-		debug(50, "[mc_mounter_main] save imgMountDir: %s", imgMountDir);
+		debug(50, "save imgMountDir: %s", imgMountDir);
 
 		imgMountDirSplit = ostrcat(imgMountDirSplit, filelistpath->text, 1, 0);
 		dirname(imgMountDirSplit);
-		debug(50, "[mc_mounter_main] save imgMountDirSplit: %s", imgMountDirSplit);
+		debug(50, "save imgMountDirSplit: %s", imgMountDirSplit);
 
 		imgFS = 1;
-	} else if(!strncmp(".iso",tmpstr+strlen(tmpstr)-4,4)){
-		debug(50, "[mc_mounter_main] found iso file: %s", tmpstr);
-		mkdir("/tmp/isofs", 777);
+	}
+	else if(!strncmp(".iso",tmpstr+strlen(tmpstr)-4,4))
+	{
+		debug(50, "found iso file: %s", tmpstr);
+		mkdir("/tmp/isofs", 0777);
 
 		tmpmounter = ostrcat("mount -o loop \"", tmpstr, 0, 0);
 		tmpmounter = ostrcat(tmpmounter, "\" /tmp/isofs", 1, 0); 
 
-		debug(50, "[mc_mounter_main] mount line1: %s", tmpmounter);
+		debug(50, "mount line1: %s", tmpmounter);
 		system(tmpmounter);
 
 		tmpmounter = ostrcat("/bin/mount --bind /tmp/isofs \"", filelistpath->text, 0, 0);
 		tmpmounter = ostrcat(tmpmounter, "\"", 1, 0);
-		debug(50, "[mc_mounter_main] mount line2: %s", tmpmounter);
+		debug(50, "mount line2: %s", tmpmounter);
 
-		free(tmpstr);
-		tmpstr = NULL;
+		free(tmpstr),tmpstr = NULL;
 		system(tmpmounter);
 
-		debug(50, "[mc_mounter_main] save mc_videoplayerpath: %s", filelistpath->text);
-		addconfig("mc_videoplayerpath", filelistpath->text);
+		debug(50, "save mc_videoplayerpath: %s", filelistpath->text);
+		addconfig("mc_vp_path", filelistpath->text);
 		addscreenrc(mc_videoplayer, filelist);
 		getfilelist(mc_videoplayer, filelistpath, filelist, path, filemask, tmpview, NULL);
 
 		isoMountDir = ostrcat(imgMountDir, filelistpath->text, 1, 0);
-		debug(50, "[mc_mounter_main] save isoMountDir: %s", isoMountDir);
+		debug(50, "save isoMountDir: %s", isoMountDir);
 
 		isoMountDirSplit = ostrcat(isoMountDirSplit, filelistpath->text, 1, 0);
 		dirname(isoMountDirSplit);
-		debug(50, "[mc_mounter_main] save isoMountDirSplit: %s", isoMountDirSplit);
+		debug(50, "save isoMountDirSplit: %s", isoMountDirSplit);
 
 		isoFS = 1;
 	}
-	debug(50, "[mc_mounter_main] end");
-return 0;
+	return 0;
 }
 
 char mc_mounter_chk(struct skin* filelistpath)
 {
-	debug(50, "[mc_mounter_chk] start");
-	if((rarFS == 1) && (rarMountDirSplit != NULL)){
-		debug(50, "[mc_mounter_chk] check rarFS=1");
-		debug(50, "[mc_mounter_chk] lastpath=%s", filelistpath->text);
-		debug(50, "[mc_mounter_chk] rarMountDir=%s", rarMountDir);
-		debug(50, "[mc_mounter_chk] rarMountDirSplit=%s", rarMountDirSplit);
-		if(strcmp(rarMountDirSplit, filelistpath->text) == 0){
-			debug(50, "[mc_mounter_chk] mc_mounter_umount");
+	debug(50, "start");
+	if(rarFS == 1 && rarMountDirSplit != NULL)
+	{
+		debug(50, "check rarFS=1");
+		debug(50, "lastpath=%s", filelistpath->text);
+		debug(50, "rarMountDir=%s", rarMountDir);
+		debug(50, "rarMountDirSplit=%s", rarMountDirSplit);
+		if(strcmp(rarMountDirSplit, filelistpath->text) == 0)
 			mc_mounter_umount();
-
-		}
-	} else if((imgFS == 1) && (imgMountDirSplit != NULL)){
-		debug(50, "[mc_mounter_chk] check imgFS=1");
-		debug(50, "[mc_mounter_chk] lastpath=%s", filelistpath->text);
-		debug(50, "[mc_mounter_chk] imgMountDir=%s", imgMountDir);
-		debug(50, "[mc_mounter_chk] imgMountDirSplit=%s", imgMountDirSplit);
-		if(strcmp(imgMountDirSplit, filelistpath->text) == 0){
-			debug(50, "[mc_mounter_chk] mc_mounter_umount");
-			mc_mounter_umount();
-
-		}
-	} else if((isoFS == 1) && (isoMountDirSplit != NULL)){
-		debug(50, "[mc_mounter_chk] check isoFS=1");
-		debug(50, "[mc_mounter_chk] lastpath=%s", filelistpath->text);
-		debug(50, "[mc_mounter_chk] isoMountDir=%s", isoMountDir);
-		debug(50, "[mc_mounter_chk] isoMountDirSplit=%s", isoMountDirSplit);
-		if(strcmp(isoMountDirSplit, filelistpath->text) == 0){
-			debug(50, "[mc_mounter_chk] mc_mounter_umount");
-			mc_mounter_umount();
-
-		}
 	}
-	debug(50, "[mc_mounter_chk] end");
+	else if(imgFS == 1 && imgMountDirSplit != NULL)
+	{
+		debug(50, "check imgFS=1");
+		debug(50, "lastpath=%s", filelistpath->text);
+		debug(50, "imgMountDir=%s", imgMountDir);
+		debug(50, "imgMountDirSplit=%s", imgMountDirSplit);
+		if(strcmp(imgMountDirSplit, filelistpath->text) == 0)
+			mc_mounter_umount();
+	}
+	else if(isoFS == 1 && isoMountDirSplit != NULL)
+	{
+		debug(50, "check isoFS=1");
+		debug(50, "lastpath=%s", filelistpath->text);
+		debug(50, "isoMountDir=%s", isoMountDir);
+		debug(50, "isoMountDirSplit=%s", isoMountDirSplit);
+		if(strcmp(isoMountDirSplit, filelistpath->text) == 0)
+			mc_mounter_umount();
+	}
 
-return 0;
+	return 0;
 }
 
 #endif
