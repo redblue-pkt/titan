a5872675e73305540fd1b8fb93d9c3ce4b3848ed
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-06-14 17:58:53 +0000

    fix

--- titan/plugins/mc/mc_audioplayer.h
+++ titan/plugins/mc/mc_audioplayer.h
@@ -71,7 +71,7 @@ void screenmc_audioplayer()
 		if(status.play == 1 && count <= screensaver_delay)
 			mc_audioplayer_infobar(apskin, infobar, spos, slen, sreverse, sprogress, NULL);
 
-		rcret = waitrcext(apskin, rcwait, 0, view);
+		rcret = waitrcext(apskin, rcwait, 0, tmpview);
 		debug(50, "while status play=%d", status.play);
 
 		if(rcret != RCTIMEOUT && rcret != getrcconfigint("rcvolup", NULL) && rcret != getrcconfigint("rcvoldown", NULL) && rcret != getrcconfigint("rcmute", NULL))
