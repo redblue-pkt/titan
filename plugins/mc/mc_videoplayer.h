6be53b04b111c4db7fb43a644c9af82b5a625aae
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-06-20 21:00:57 +0000

    [titan] fix mc

--- plugins/mc/mc_videoplayer.h
+++ plugins/mc/mc_videoplayer.h
@@ -651,7 +651,7 @@ void screenmc_videoplayer()
 				
 				if(getconfig("mc_vp_autoscan", NULL) != NULL)
 				{
-					mediadbscan(filelistpath->text, 0, 1);
+					mediadbscan(filelistpath->text, 1000, 1);
 					files = findfiles(filelistpath->text, (0 | 0x40000000)); //count only
 				}
 			}
