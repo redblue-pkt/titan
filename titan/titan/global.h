02b8d1c82f0f8518a5eef430da0b6912e1e940b0
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-07-07 18:32:39 +0000

    [titan] add debug für ufs910 autores segfault

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -4986,15 +4986,18 @@ void setaktres()
 	int sec = 0;
 
 	if(status.restimer == NULL) return;
+printf("1\n");
 	sec = (int)status.restimer->param1;
-
+printf("2\n");
 	if(sec > 0)
 	{
+printf("3\n");
 		while(status.restimer->aktion == START && count <= sec)
 		{
 			sleep(1);
 			if(status.restimer->aktion != START)
 			{
+printf("4\n");
 				status.restimer = NULL;
 				return;
 			}
@@ -5002,38 +5005,55 @@ void setaktres()
 		}
 	}
 
+printf("5\n");
 	if(videoreadqwidth(status.aktservice->videodev) == 0)
 	{
+printf("6\n");
 		m_width = status.videosize.w;
-		if (m_width == 720) {
+printf("7\n");
+		if(m_width == 720)
+		{
+printf("8\n");
 			res_sd = getconfig("av_videomode_autores_sd", NULL);
 			if(res_sd == NULL)
 				res = ostrcat(res, "576i50", 1, 0);
 			else
 				res = ostrcat(res, res_sd, 1, 0);
+printf("9\n");
 		}
-		else if (m_width == 1280)
+printf("10\n");
+		else if(m_width == 1280)
 			res = ostrcat(res, "720p50", 1, 0);
-		else if (m_width == 1920)
+		else if(m_width == 1920)
 			res = ostrcat(res, "1080i50", 1, 0);
 		else
 			m_width = 0;
-		if ( m_width > 0)
+printf("11\n");
+		if(m_width > 0)
 		{
+printf("12\n");
 			res_akt = getvideomode();
-			if (ostrcmp(res_akt, res) != 0)
+printf("13\n");
+			if(ostrcmp(res_akt, res) != 0)
 			{
+printf("14\n");
 				setvideomode(res, 1);
-	  		changefbresolution(res);
+printf("15\n");
+				changefbresolution(res);
+printf("16\n");
 				sleep(1);
+printf("17\n");
 				screenautores(res, 5, 0);
+printf("18\n");
 			}
 		}
 	}
 	else
 		textbox(_("Message"), _("ERROR cant read res"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 5, 0);
 
+printf("19\n");
 	free(res);
+printf("20\n");
 	res = NULL;
 	status.restimer = NULL;
 	return;
