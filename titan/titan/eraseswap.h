a70c9a466c49b998665d88c15edbbdb7828d703f
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-07-11 17:19:20 +0000

    [titan] add loading/wait screen to eraseswap

--- titan/titan/eraseswap.h
+++ titan/titan/eraseswap.h
@@ -3,11 +3,15 @@
 
 void screeneraseswap()
 {
+	struct skin* load = getscreen("loading");
+
 	if(textbox(_("Message"), _("Do you really want to erase all datas and backups from Swap?\nThe Box will reboot and the erase will be started!"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0) == 1)
 	{
 		debug(40, "Erase-Mtd");
+		drawscreen(load, 0, 0);
 		system("touch /var/etc/.erasemtd");
 		sleep(4);
+		clearscreen(load);
 		oshutdown(2, 1);
 	}       
 }
