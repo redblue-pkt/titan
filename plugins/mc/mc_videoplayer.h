74720e0fdff4113b5a3cc4de0eca9ef06ba6edb0
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-07-23 21:02:11 +0000

    fix mc autoscan bug

--- plugins/mc/mc_videoplayer.h
+++ plugins/mc/mc_videoplayer.h
@@ -681,8 +681,8 @@ void screenmc_videoplayer()
 
 				free(status.playfile); status.playfile = NULL;
 				status.playfile = ostrcat(filename, "", 0, 0);
-				
-				if(getconfig("mc_vp_autoscan", NULL) != NULL)
+
+				if(getconfigint("mc_vp_autoscan", NULL) == 1)
 				{
 					writesys("/tmp/.autoscan", "", 0);
 					mediadbscan(filelistpath->text, 1000, 1);
