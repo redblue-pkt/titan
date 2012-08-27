560213bea25cc10f419151bd011461a7823658fd
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-08-27 19:21:28 +0000

    add separate titan cfg disable fastzap and enable 2 sat

--- titan/titan/makesh4.sh
+++ titan/titan/makesh4.sh
@@ -672,6 +672,9 @@ rm -rf `find "$HOME"/flashimg/BUILD/titan -type f -name "make.sh"`
 rm -rf "$HOME"/flashimg/BUILD/titan/var/usr/local/share/titan/plugins/networkbrowser/netlib
 rm -rf "$HOME"/flashimg/BUILD/titan/var/usr/local/share/titan/plugins/tithek/menu
 
+cp -a "$HOME"/flashimg/BUILD/titan/var/etc/titan/titan.cfg."$TYPE" "$HOME"/flashimg/BUILD/titan/var/etc/titan/titan.cfg
+rm -rf "$HOME"/flashimg/BUILD/titan/var/etc/titan/titan.cfg.*
+
 cp -a "$HOME"/flashimg/BUILD/titan "$HOME"/flashimg/BUILD/titan-smal
 rm -rf "$HOME"/flashimg/BUILD/titan-smal/var/usr/local/share/titan/picons
 rm -rf "$HOME"/flashimg/BUILD/titan-smal/var/etc/titan/titan.cfg
