10ba4c4fc02cd201cc5aa12d8246945a07b5dfab
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-09-07 23:56:53 +0000

    cleanup

--- titan/plugins/kinox/putlocker-sockshare.sh
+++ titan/plugins/kinox/putlocker-sockshare.sh
@@ -12,10 +12,6 @@ HASH=`cat /tmp/kinox/cache_index.quell.html | grep hash | cut -d'"' -f 4`
 post="POST /file/$FILE HTTP/1.1\r\nContent-Length: 51\r\nAccept-Encoding: gzip\r\nConnection: close\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nHost: www.$TYPE.com\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nhash=$HASH&confirm=Continue+as+Free+User"
 echo -e $post | netcat www.$TYPE.com 80 > /tmp/kinox/cache_post.log
 
-PHPSESSID=`cat cache_post.log | grep Set-Cookie | grep PHPSESSID= | cut -d "=" -f2 | cut -d";" -f1`
-SERVERID=`cat cache_post.log | grep Set-Cookie | grep SERVERID= | cut -d "=" -f2 | cut -d";" -f1`
-usender=`cat cache_post.log | grep Set-Cookie | grep usender= | cut -d "=" -f2 | cut -d";" -f1`
-
 PHPSESSID=`strings cache_post.log | grep Set-Cookie | grep PHPSESSID= | cut -d "=" -f2 | cut -d";" -f1`
 SERVERID=`strings cache_post.log | grep Set-Cookie | grep SERVERID= | cut -d "=" -f2 | cut -d";" -f1`
 getstream="GET /file/$FILE HTTP/1.1\r\nHost: www.$TYPE.com\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nCookie: SERVERID=$SERVERID; PHPSESSID=$PHPSESSID\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n"
