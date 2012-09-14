648c2613e2dbe46d75295aad399d3aa300e34050
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-09-14 00:12:31 +0000

    fix

--- plugins/panel/panel_main.h
+++ plugins/panel/panel_main.h
@@ -69,8 +69,8 @@ int panel_menucall(struct skin* panel_menuentry)
 		screenpanel_settings_overclocking();
 	else if(ostrcmp("panel_settings_automount", panel_menuentry->name) == 0)
 		screenpanel_settings(0);
-	else if(ostrcmp("panel_settings_wizard", panel_menuentry->name) == 0)
-		screenpanel_wizard();
+//	else if(ostrcmp("panel_settings_wizard", panel_menuentry->name) == 0)
+//		screenpanel_wizard();
 	else if(ostrcmp("panel_settings_autostart", panel_menuentry->name) == 0)
 	{
 		struct skin* screen = getscreen("panel_autostart_main");
