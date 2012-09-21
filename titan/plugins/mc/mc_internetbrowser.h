976b10ac753f6088f152ceba003071b96ac0ee18
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-09-21 12:33:09 +0000

    [titan] mc update internetbrowser start. use favorite start

--- titan/plugins/mc/mc_internetbrowser.h
+++ titan/plugins/mc/mc_internetbrowser.h
@@ -6,6 +6,8 @@ void screenmc_internetbrowser()
 	if(status.security == 1)
 	{
 		int treffer = 0;
+		struct skin* pluginnode = NULL;
+		void (*startplugin)(void);
 		struct skin* plugin = getscreen("plugin");
 		struct skin* child = plugin->child;
 
@@ -21,23 +23,16 @@ void screenmc_internetbrowser()
 
 		if(treffer == 1)
 		{
-//			if(textbox(_("Message"), _("start Internet browser now ?"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0) == 1){	
-				char* tmpstr = NULL;
-				int rcret = 0;
-	
-				status.sec = 0; //deaktivate spinner
-				setfbtransparent(255);
-				rcret = servicestop(status.aktservice, 1, 0);
-				if(rcret == 1) return;
-	
-				tmpstr = ostrcat("nsfb -f linux ", getconfig("browserhome", NULL), 0, 0);
-				system(tmpstr);
-				free(tmpstr);
-				setosdtransparent(getskinconfigint("osdtransparent", NULL));
-				drawscreen(skin, 0, 0);
-				servicestart(status.lastservice->channel, NULL, NULL, 0);
-				sleep(2);
-//			}
+			pluginnode = getplugin("Internet Browser");
+		
+			if(pluginnode != NULL)
+			{
+				startplugin = dlsym(pluginnode->pluginhandle, "start");
+				if(startplugin != NULL)
+				{
+					startplugin();
+				}
+			}
 		}
 		else
 			textbox(_("Message"), _("Install netsurf Internetbrowser Ipk first !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 200, 0, 0);
