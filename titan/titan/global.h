3cc269de2bcae57049c2d3c4a79b5240b0567299
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-10-11 14:25:48 +0000

    fix

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -1,6 +1,44 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+#define MAXTOKENS       256
+
+char **str_split(char *string, char *delim) {
+	char **tokens = NULL;
+	char *working = NULL;
+	char *token = NULL;
+	int idx = 0;
+
+	tokens  = malloc(sizeof(char *) * MAXTOKENS);
+	if(tokens == NULL)
+		return NULL;
+
+	working = malloc(sizeof(char) * strlen(string) + 1);
+	if(working == NULL)
+		return NULL;
+
+	 /* to make sure, copy string to a safe place */
+	strcpy(working, string);
+	for(idx = 0; idx < MAXTOKENS; idx++)
+		tokens[idx] = NULL;
+
+	token = strtok(working, delim);
+	idx = 0;
+
+	/* always keep the last entry NULL termindated */
+	while((idx < (MAXTOKENS - 1)) && (token != NULL)) {
+		tokens[idx] = malloc(sizeof(char) * strlen(token) + 1);
+		if(tokens[idx] != NULL) {
+			strcpy(tokens[idx], token);
+			idx++;
+			token = strtok(NULL, delim);
+		}
+	}
+
+	free(working);
+	return tokens;
+}
+
 char* readfromlinetoline(char* str, int start, int end, int flag)
 {
 	if(str == NULL) return str;
