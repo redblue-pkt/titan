3fce74103e41f901f1c31ab6ce3b1c47f7777942
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-11-10 15:12:48 +0000

    [titan] fix

--- titan/cam.h
+++ titan/cam.h
@@ -373,20 +373,28 @@ start:
 	{
 		if(cadescnode->len > 0 && cadescnode->pid == 0)
 		{
+			int cadesclen = 0;
+
 			buf[pos++] = 0x09; //ca desc tag
 			if(round == 0 && status.caskipprivat == 1) //workaround for to long capmt
+			{
 				buf[pos++] = 4;
+				cadesclen = cadesclen;
+			}
 			else
-				buf[pos++] = cadescnode->len;
+			{
+				cadesclen = cadescnode->len;
+				buf[pos++] = cadesclen;
+			}
 			buf[pos++] = cadescnode->systemid >> 8;
 			buf[pos++] = cadescnode->systemid & 0xff;
 			buf[pos++] = (cadescnode->reserved << 5) | (cadescnode->capid >> 8);
 			buf[pos++] = cadescnode->capid & 0xff;
 
-			if(cadescnode->len > 4)
+			if(ccadesclen > 4)
 			{
-				memcpy(&buf[pos], cadescnode->privat, cadescnode->len - 4);
-				pos += cadescnode->len - 4;
+				memcpy(&buf[pos], cadescnode->privat, cadesclen - 4);
+				pos += cadesclen - 4;
 			}
 		}
 		cadescnode = cadescnode->next;
