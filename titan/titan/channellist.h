f68d8cc16ac92b81dcea93eafae9bf2d7a73603e
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2012-11-29 20:23:58 +0000

    fix

--- titan/titan/channellist.h
+++ titan/titan/channellist.h
@@ -1700,7 +1700,7 @@ start:
 				clearscreen(channellist);
 				resettvpic();
 				struct channel* tmpmarkedchannel = status.markedchannel;
-				screenshortepg((struct channel*)listbox->select->handle, NULL);
+				screenshortepg((struct channel*)listbox->select->handle, NULL, 0);
 				status.markedchannel = tmpmarkedchannel;
 				changebutton(listmode, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14);
 				drawscreen(channellist, 0, 0);
