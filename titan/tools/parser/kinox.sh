aa2b8c5ad2762611a734e0204b3f991905478d72
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-01-05 01:51:42 +0000

    update tithek

--- titan/tools/parser/kinox.sh
+++ titan/tools/parser/kinox.sh
@@ -1,15 +1,68 @@
 #!/bin/bash
 #
 
+LIST="/Stream/"
+rm file.test*
+rm movieliste.log
+
+for CASE in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
+	wget "http://kinox.to/aGET/List/?sEcho=1&iColumns=10&sColumns=&iDisplayStart=1&iDisplayLength=50&iSortingCols=1&iSortCol_0=5&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&bSortable_4=false&bSortable_5=false&bSortable_6=true&additional=%7B%22fType%22%3A%22movie%22%2C%22fLetter%22%3A%22$CASE%22%7D" -O file.test.$CASE
+	LINES=`cat file.test.$CASE | tr ',' '\n' | grep iTotalDisplayRecords | cut -d '"' -f4`
+	echo LINES $LINES
+
+	if [ "$LINES" == " " ] || [ -z "$LINES" ] || [ "$LINES" == 0 ]; then
+		rm file.test.$CASE
+		break
+		ls file.test.$CASE
+		exit
+	fi
+
+	if [ $LINES -gt 500 ]; then
+		LINES=500
+	fi
+
+	max=`expr $LINES - 4`
+	echo max $max
+	
+	TMPLIST=`cat file.test.$CASE | tr '],[' '\n' | grep 'Stream' | cut -d '/' -f3 | cut -d'\\' -f1`
+	LIST="$TMPLIST"
+	
+	count=0
+	echo count $count
+	echo max $max
+	
+	while [ $count -lt $max ]
+	do
+		echo "count $count"
+		count=`expr $count + 1`
+		wget "http://kinox.to/aGET/List/?sEcho=1&iColumns=10&sColumns=&iDisplayStart=$count&iDisplayLength=50&iSortingCols=1&iSortCol_0=5&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&bSortable_4=false&bSortable_5=false&bSortable_6=true&additional=%7B%22fType%22%3A%22movie%22%2C%22fLetter%22%3A%22$CASE%22%7D" -O file.test.$CASE.$count
+		TMPLIST=`cat file.test.$CASE.$count | tr '],[' '\n' | grep 'Stream' | cut -d '"' -f3 | tr '\\' ' ' | sed 's/ \+//g'`
+		echo $TMPLIST | tr ' ' '\n' >> file.test.tmpliste
+#ls file.test.$CASE.$count
+#exit
+#		if [ $count == 1 ]; then
+#			break
+#		fi
+	done
+done
+
+cat file.test.tmpliste | sort -u > movieliste.log
+
+#exit
+##############
+
 rm cache.*
 rm _liste
 rm -rf _full/kinox
 mkdir -p _full/kinox/streams
 piccount=0
-main_list="Cine-Films Popular-Movies Popular-Documentations Popular-Series Latest-Documentations Latest-Series"
+
+main_list="Cine-Films Popular-Movies Popular-Documentations Popular-Series Latest-Documentations Latest-Series Movies"
+#main_list="Movies"
+
 echo main_list $main_list
 piccount=0
-
+counttt=0
 for ROUND0 in $main_list; do
 	wget --no-check-certificate "http://kinox.to/$ROUND0.html" -O cache.main.next.list
 
@@ -18,7 +71,11 @@ for ROUND0 in $main_list; do
 	if [ $ROUND0 == "Popular-Series" ]; then
 		main_next_list="$main_next_list /Stream/Die_Gummibaerenbande.html /Stream/Touch-1.html /Stream/Alcatraz_german_subbed.html /Stream/Battlestar_Galactica.html /Stream/24.html /Stream/Heroes.html /Stream/Star_Wars_The_Clone_Wars-1.html /Stream/Star_Wars_The_Clone_Wars_german_subbed.html"
 	fi
-	
+
+	if [ $ROUND0 == "Movies" ]; then
+		main_next_list=`cat movieliste.log | tr '\n' ' '`
+	fi
+
 #	main_next_list="/Stream/Die_Simpsons.html"
 	
 	skip=0
@@ -66,7 +123,10 @@ for ROUND0 in $main_list; do
 			
 		echo cache."$filename".list
 		echo filename=$filename
-#		exit
+#		counttt=`expr $counttt + 1`
+#		if [ $counttt = 2 ]; then
+#			exit
+#		fi
 		skip1=0
 				
 		cat cache.kinox.$filename.titanlist | sort -u >> _full/kinox/streams/kinox.$ROUND0.list	
@@ -92,3 +152,4 @@ rm cache.*
 #cp -a _full/kinox/* /var/www/atemio/web/mediathek/kinox
 exit
 #done
+
