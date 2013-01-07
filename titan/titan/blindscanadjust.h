559d0c53fa03e47bc9109c1d6b0f1bb9cc98e3fd
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-01-07 22:58:11 +0000

    [titan] extend blindscanadjust for cable and terr

--- titan/titan/blindscanadjust.h
+++ titan/titan/blindscanadjust.h
@@ -1,13 +1,70 @@
 #ifndef BLINDSCANADJUST_H
 #define BLINDSCANADJUST_H
 
-//todo set defaults
+void changetunertype(struct skin* tunertype, struct skin* minfrequency, struct skin* maxfrequency, struct skin* stepfrequency, struct skin* minsignalrate, struct skin* maxsignalrate, struct skin* stepsignalrate, struct skin* usedefaultsr, struct skin* onlydvbs, struct skin* usedefaultfec, struct skin* cminfrequency, struct skin* cmaxfrequency, struct skin* cstepfrequency, struct skin* cminsignalrate, struct skin* cmaxsignalrate, struct skin* cstepsignalrate, struct skin* cusedefaultsr, struct skin* cusedefaultfec, struct skin* tminfrequency, struct skin* tmaxfrequency, struct skin* tstepfrequency)
+{
+	minfrequency->hidden = YES;
+	maxfrequency->hidden = YES;
+	stepfrequency->hidden = YES;
+	minsignalrate->hidden = YES;
+	maxsignalrate->hidden = YES;
+	stepsignalrate->hidden = YES;
+	usedefaultsr->hidden = YES;
+	onlydvbs->hidden = YES;
+	usedefaultfec->hidden = YES;
+	cminfrequency->hidden = YES;
+	cmaxfrequency->hidden = YES;
+	cstepfrequency->hidden = YES;
+	cminsignalrate->hidden = YES;
+	cmaxsignalrate->hidden = YES;
+	cstepsignalrate->hidden = YES;
+	cusedefaultsr->hidden = YES;
+	cusedefaultfec->hidden = YES;
+	tminfrequency->hidden = YES;
+	tmaxfrequency->hidden = YES;
+	tstepfrequency->hidden = YES;
+
+	if(ostrcmp(tunertype->ret, "0") == 0) //sat
+	{
+		minfrequency->hidden = NO;
+		maxfrequency->hidden = NO;
+		stepfrequency->hidden = NO;
+		minsignalrate->hidden = NO;
+		maxsignalrate->hidden = NO;
+		stepsignalrate->hidden = NO;
+		usedefaultsr->hidden = NO;
+		onlydvbs->hidden = NO;
+		usedefaultfec->hidden = NO;
+	}
+	
+	if(ostrcmp(tunertype->ret, "1") == 0) //cable
+	{
+		cminfrequency->hidden = NO;
+		cmaxfrequency->hidden = NO;
+		cstepfrequency->hidden = NO;
+		cminsignalrate->hidden = NO;
+		cmaxsignalrate->hidden = NO;
+		cstepsignalrate->hidden = NO;
+		cusedefaultsr->hidden = NO;
+		cusedefaultfec->hidden = NO;
+	}
+	
+	if(ostrcmp(tunertype->ret, "2") == 0) //terr
+	{
+		tminfrequency->hidden = NO;
+		tmaxfrequency->hidden = NO;
+		tstepfrequency->hidden = NO;
+	}
+}
 
 void screenblindscanadjust()
 {
 	int rcret = 0, i = 0;
 	struct skin* blindscan = getscreen("blindscanadjust");
 	struct skin* listbox = getscreennode(blindscan, "listbox");
+	struct skin* tunertype = getscreennode(blindscan, "tunertype");
+	
+	//sat
 	struct skin* minfrequency = getscreennode(blindscan, "minfrequency");
 	struct skin* maxfrequency = getscreennode(blindscan, "maxfrequency");
 	struct skin* stepfrequency = getscreennode(blindscan, "stepfrequency");
@@ -17,8 +74,30 @@ void screenblindscanadjust()
 	struct skin* usedefaultsr = getscreennode(blindscan, "usedefaultsr");
 	struct skin* onlydvbs = getscreennode(blindscan, "onlydvbs");
 	struct skin* usedefaultfec = getscreennode(blindscan, "usedefaultfec");
+	
+	//cable
+	struct skin* cminfrequency = getscreennode(blindscan, "cminfrequency");
+	struct skin* cmaxfrequency = getscreennode(blindscan, "cmaxfrequency");
+	struct skin* cstepfrequency = getscreennode(blindscan, "cstepfrequency");
+	struct skin* cminsignalrate = getscreennode(blindscan, "cminsignalrate");
+	struct skin* cmaxsignalrate = getscreennode(blindscan, "cmaxsignalrate");
+	struct skin* cstepsignalrate = getscreennode(blindscan, "cstepsignalrate");
+	struct skin* cusedefaultsr = getscreennode(blindscan, "cusedefaultsr");
+	struct skin* cusedefaultfec = getscreennode(blindscan, "cusedefaultfec");
+	
+	//terr
+	struct skin* tminfrequency = getscreennode(blindscan, "tminfrequency");
+	struct skin* tmaxfrequency = getscreennode(blindscan, "tmaxfrequency");
+	struct skin* tstepfrequency = getscreennode(blindscan, "tstepfrequency");
+	
 	struct skin* tmp = NULL;
+	
+	addchoicebox(tunertype, "0", _("Sat"));
+	addchoicebox(tunertype, "1", _("Cable"));
+	addchoicebox(tunertype, "2", _("Terrestrial "));
+	setchoiceboxselection(tunertype, "0");
 
+	//sat
 	changemask(minfrequency, "00000");
 	changeinput(minfrequency, getconfig("blindminfrequency", NULL));
 	if(minfrequency->input != NULL)
@@ -84,7 +163,100 @@ void screenblindscanadjust()
 	addchoicebox(usedefaultfec, "0", _("no"));
 	addchoicebox(usedefaultfec, "1", _("yes"));
 	setchoiceboxselection(usedefaultfec, getconfig("blindusedefaultfec", NULL));
+	
+	//cable
+	changemask(cminfrequency, "00000");
+	changeinput(cminfrequency, getconfig("cblindminfrequency", NULL));
+	if(cminfrequency->input != NULL)
+	{
+		int len = strlen(cminfrequency->input);
+		for(i = 0; i < 5 - len; i++)
+			cminfrequency->input = ostrcat("0", cminfrequency->input, 0, 1);
+	}
+  
+	changemask(cmaxfrequency, "00000");
+	changeinput(cmaxfrequency, getconfig("cblindmaxfrequency", NULL));
+	if(cmaxfrequency->input != NULL)
+	{
+		int len = strlen(cmaxfrequency->input);
+		for(i = 0; i < 5 - len; i++)
+			cmaxfrequency->input = ostrcat("0", cmaxfrequency->input, 0, 1);
+	}
+  
+	changeinput(cstepfrequency, getconfig("cblindstepfrequency", NULL));
+	changemask(cstepfrequency, "00");
+	if(cstepfrequency->input != NULL)
+	{
+		int len = strlen(cstepfrequency->input);
+		for(i = 0; i < 2 - len; i++)
+			cstepfrequency->input = ostrcat("0", cstepfrequency->input, 0, 1);
+	}
+  
+	changeinput(cminsignalrate, getconfig("cblindminsignalrate", NULL));
+	changemask(cminsignalrate, "00000");
+	if(cminsignalrate->input != NULL)
+	{
+		int len = strlen(cminsignalrate->input);
+		for(i = 0; i < 5 - len; i++)
+			cminsignalrate->input = ostrcat("0", cminsignalrate->input, 0, 1);
+	}
+  
+	changeinput(cmaxsignalrate, getconfig("cblindmaxsignalrate", NULL));
+	changemask(cmaxsignalrate, "00000");
+	if(cmaxsignalrate->input != NULL)
+	{
+		int len = strlen(cmaxsignalrate->input);
+		for(i = 0; i < 5 - len; i++)
+			cmaxsignalrate->input = ostrcat("0", cmaxsignalrate->input, 0, 1);
+	}
+  
+	changeinput(cstepsignalrate, getconfig("cblindstepsignalrate", NULL));
+	changemask(cstepsignalrate, "00");
+	if(cstepsignalrate->input != NULL)
+	{
+		int len = strlen(cstepsignalrate->input);
+		for(i = 0; i < 2 - len; i++)
+			cstepsignalrate->input = ostrcat("0", cstepsignalrate->input, 0, 1);
+	}
 
+	addchoicebox(cusedefaultsr, "0", _("no"));
+	addchoicebox(cusedefaultsr, "1", _("yes"));
+	setchoiceboxselection(cusedefaultsr, getconfig("cblindusedefaultsr", NULL));
+
+	addchoicebox(cusedefaultfec, "0", _("no"));
+	addchoicebox(cusedefaultfec, "1", _("yes"));
+	setchoiceboxselection(cusedefaultfec, getconfig("cblindusedefaultfec", NULL));
+	
+	//terr
+	changemask(tminfrequency, "00000");
+	changeinput(tminfrequency, getconfig("tblindminfrequency", NULL));
+	if(tminfrequency->input != NULL)
+	{
+		int len = strlen(tminfrequency->input);
+		for(i = 0; i < 5 - len; i++)
+			tminfrequency->input = ostrcat("0", tminfrequency->input, 0, 1);
+	}
+  
+	changemask(tmaxfrequency, "00000");
+	changeinput(tmaxfrequency, getconfig("tblindmaxfrequency", NULL));
+	if(tmaxfrequency->input != NULL)
+	{
+		int len = strlen(tmaxfrequency->input);
+		for(i = 0; i < 5 - len; i++)
+			tmaxfrequency->input = ostrcat("0", tmaxfrequency->input, 0, 1);
+	}
+  
+	changeinput(tstepfrequency, getconfig("tblindstepfrequency", NULL));
+	changemask(tstepfrequency, "00");
+	if(tstepfrequency->input != NULL)
+	{
+		int len = strlen(tstepfrequency->input);
+		for(i = 0; i < 2 - len; i++)
+			tstepfrequency->input = ostrcat("0", tstepfrequency->input, 0, 1);
+	}
+
+	drawscreen(blindscan, 2, 0);
+	changetunertype(tunertype, minfrequency, maxfrequency, stepfrequency, minsignalrate, maxsignalrate, stepsignalrate, usedefaultsr, onlydvbs, usedefaultfec, cminfrequency, cmaxfrequency, cstepfrequency, cminsignalrate, cmaxsignalrate, cstepsignalrate, cusedefaultsr, cusedefaultfec, tminfrequency, tmaxfrequency, tstepfrequency);
 	drawscreen(blindscan, 0, 0);
 	addscreenrc(blindscan, listbox);
 
@@ -94,11 +266,18 @@ void screenblindscanadjust()
 		addscreenrc(blindscan, tmp);
 		rcret = waitrc(blindscan, 0, 0);
 		tmp = listbox->select;
+		
+		if(listbox->select != NULL && ostrcmp(listbox->select->name, "tunertype") == 0)
+		{
+			changetunertype(tunertype, minfrequency, maxfrequency, stepfrequency, minsignalrate, maxsignalrate, stepsignalrate, usedefaultsr, onlydvbs, usedefaultfec, cminfrequency, cmaxfrequency, cstepfrequency, cminsignalrate, cmaxsignalrate, cstepsignalrate, cusedefaultsr, cusedefaultfec, tminfrequency, tmaxfrequency, tstepfrequency);
+			drawscreen(blindscan, 0, 0);
+		}
 
 		if(rcret == getrcconfigint("rcexit", NULL)) break;
 
 		if(rcret == getrcconfigint("rcok", NULL))
 		{
+			//sat
 			if(minfrequency->ret != NULL)
 			{
 				int i = atoi(minfrequency->ret);
@@ -145,6 +324,76 @@ void screenblindscanadjust()
 			addconfigscreencheck("blindusedefaultsr", usedefaultsr, "0");
 			addconfigscreencheck("blindonlydvbs", onlydvbs, "0");
 			addconfigscreencheck("blindusedefaultfec", usedefaultfec, "0");
+			
+			//cable
+			if(cminfrequency->ret != NULL)
+			{
+				int i = atoi(cminfrequency->ret);
+				addconfigint("cblindminfrequency", i);
+			}
+			if(cmaxfrequency->ret != NULL)
+			{
+				int i = atoi(cmaxfrequency->ret);
+				addconfigint("cblindmaxfrequency", i);
+			}
+			if(cstepfrequency->ret != NULL)
+			{
+				int i = atoi(cstepfrequency->ret);
+				addconfigint("cblindstepfrequency", i);
+			}
+			if(cminsignalrate->ret != NULL)
+			{
+				int i = atoi(cminsignalrate->ret);
+				addconfigint("cblindminsignalrate", i);
+			}
+			if(cmaxsignalrate->ret != NULL)
+			{
+				int i = atoi(cmaxsignalrate->ret);
+				addconfigint("cblindmaxsignalrate", i);
+			}
+			if(cstepsignalrate->ret != NULL)
+			{
+				int i = atoi(cstepsignalrate->ret);
+				addconfigint("cblindstepsignalrate", i);
+			}
+
+			if(getconfigint("cblindminfrequency", NULL) > getconfigint("cblindmaxfrequency", NULL))
+				addconfigint("cblindminfrequency", getconfigint("cblindmaxfrequency", NULL));
+
+			if(getconfigint("cblindminsignalrate", NULL) > getconfigint("cblindmaxsignalrate", NULL))
+				addconfigint("cblindminfrequency", getconfigint("cblindmaxsignalrate", NULL));
+
+			if(getconfigint("cblindstepfrequency", NULL) < 1)
+				addconfigint("cblindstepfrequency", 20);
+
+			if(getconfigint("cblindstepsignalrate", NULL) < 1)
+				addconfigint("cblindstepsignalrate", 20);
+
+			addconfigscreencheck("cblindusedefaultsr", cusedefaultsr, "0");
+			addconfigscreencheck("cblindusedefaultfec", cusedefaultfec, "0");
+			
+			//terr
+			if(tminfrequency->ret != NULL)
+			{
+				int i = atoi(tminfrequency->ret);
+				addconfigint("tblindminfrequency", i);
+			}
+			if(tmaxfrequency->ret != NULL)
+			{
+				int i = atoi(tmaxfrequency->ret);
+				addconfigint("tblindmaxfrequency", i);
+			}
+			if(tstepfrequency->ret != NULL)
+			{
+				int i = atoi(tstepfrequency->ret);
+				addconfigint("tblindstepfrequency", i);
+			}
+
+			if(getconfigint("tblindminfrequency", NULL) > getconfigint("tblindmaxfrequency", NULL))
+				addconfigint("tblindminfrequency", getconfigint("tblindmaxfrequency", NULL));
+
+			if(getconfigint("tblindstepfrequency", NULL) < 1)
+				addconfigint("tblindstepfrequency", 20);
 
 			break;
 		}
