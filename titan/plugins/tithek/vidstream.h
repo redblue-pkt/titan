568e239f3fc4bf1658cfc66cd86776bc866ec361
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-01-17 01:19:09 +0000

    fix

---
+++ titan/plugins/tithek/vidstream.h
@@ -0,0 +1,170 @@
+#ifndef VIDSTREAM_H
+#define VIDSTREAM_H
+
+unsigned char* vidstreamreceive(int* sock)
+{
+	int ret = 0;
+	unsigned char* buf = NULL;
+
+	buf = calloc(1, MINMALLOC);
+	if(buf == NULL)
+	{
+		err("no mem");
+		return NULL;
+	}
+
+	//read one line
+	unsigned char* pbuf = buf;
+
+	while(pbuf - buf < MINMALLOC)
+	{
+		unsigned char c;
+
+		ret = sockreceive(sock, &c, 1, 5000 * 1000);
+		if(ret != 0)
+		{
+			err("no client data in buffer");
+			break;
+		}
+
+		*pbuf = c;
+		if(buf != NULL && (ostrstr((char*)buf, "\n\n") != NULL || ostrstr((char*)buf, "\r\n\r\n") != NULL))
+			break;
+		pbuf++;
+	}
+
+	return buf;
+}
+
+char* vidstream(char* host, char* file)
+{
+	debug(99, "in host: %s file: %s", host, file);
+	char* tmphost = NULL;
+	char* tmpfile = NULL;
+	char* tmpstr = NULL;
+	char* send = NULL;
+	char* ip = NULL;
+	char* streamlink = NULL;
+
+	char* id = NULL;
+	char* fname = NULL;
+	char* op = NULL;
+	char* hash = NULL;
+	char* hashline = NULL;
+	char* hashlen = NULL;
+	char* referer = NULL;
+	char* usr_login = NULL;
+	char* imhuman = NULL;
+
+	if(host == NULL || file == NULL) return NULL;
+
+	tmphost = ostrcat("www.", host, 0, 0);
+	tmpfile = ostrcat("/file/", file, 0, 0);
+	debug(99, "tmphost: %s", tmphost);
+	ip = get_ip(tmphost);
+	debug(99, "ip: %s", ip);
+	debug(99, "tmpfile: %s", tmpfile);
+	debug(99, "file: %s", file);
+
+
+//'GET /1cr2nsh9pdjw HTTP/1.1\r\nHost: vidstream.in\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n'
+
+	send = ostrcat(send, "GET /", 1, 0);
+	send = ostrcat(send, file, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);	
+	send = ostrcat(send, host, 1, 0);
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
+	debug(99, "tmphost: %s", tmphost);
+	debug(99, "tmpfile: %s", tmpfile);
+	debug(99, "send: %s", send);
+	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 1);
+	writesys("/var/usr/local/share/titan/plugins/tithek/vidstream1", tmpstr, 0);
+
+	sleep(1);
+///////////////////
+
+	//get hash from tmpstr
+	char* pos1 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"fname\" value=");
+	fname = getxmlentry(pos1, "value=");
+	debug(99, "fname: %s", fname);
+	if(fname == NULL) goto end;
+
+	char* pos2 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"id\" value=");
+	id = getxmlentry(pos2, "value=");
+	debug(99, "id: %s", id);
+	if(id == NULL) goto end;
+
+	char* pos3 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"op\" value=");
+	op = getxmlentry(pos3, "value=");
+
+	char* pos4 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"hash\" value=");
+	hashline = getxmlentry(pos4, "value=");
+
+	char* pos5 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"referer\" value=");
+	referer = getxmlentry(pos5, "value=");
+
+	char* pos6 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"imhuman\" value=");
+	imhuman = getxmlentry(pos6, "value=");
+
+	char* pos7 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"usr_login\" value=");
+	usr_login = getxmlentry(pos7, "value=");
+	
+	free(tmpstr); tmpstr = NULL;
+	debug(99, "op: %s", op);
+	if(op == NULL) goto end;
+
+	hash = ostrcat(hash, "hash=", 1, 0);
+	hash = ostrcat(hash, hashline, 1, 0);
+	hash = ostrcat(hash, "&imhuman=Proceed+to+video%22+id%3D%22btn_download&k=Search...%22+size%3D16+onfocus%3D%22%24%28this%29.val%28%27%27%29.css%28%27color%27%2C%27%23333%27%29%3B%22+onblur%3D%22if%28%24%28this%29.val%28%29%3D%3D%27%27%29%24%28this%29.val%28%27Search...%27%29.css%28%27color%27%2C%27%27%29%22+id%3D%22search_top", 1, 0);
+	hash = ostrcat(hash, "&usr_login=", 1, 0);	
+	hash = ostrcat(hash, usr_login, 1, 0);
+	hash = ostrcat(hash, "&referer=", 1, 0);
+	hash = ostrcat(hash, referer, 1, 0);
+	hash = ostrcat(hash, "&fname=", 1, 0);
+	hash = ostrcat(hash, fname, 1, 0);
+	hash = ostrcat(hash, "&id=", 1, 0);	
+	hash = ostrcat(hash, id, 1, 0);
+	hash = ostrcat(hash, "&op=download1", 1, 0);
+//	hash = ostrcat(hash, op, 1, 0);
+	debug(99, "hash: %s", hash);
+	hashlen = oitoa(strlen(hash));
+
+	//create send string
+	free(send), send = NULL;
+
+	send = ostrcat(send, "POST /", 1, 0);
+	send = ostrcat(send, file, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nContent-Length: ", 1, 0);
+	send = ostrcat(send, hashlen, 1, 0);
+	send = ostrcat(send, "\r\nAccept-Encoding: gzip\r\nConnection: close\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nHost: ", 1, 0);
+	send = ostrcat(send, host, 1, 0);
+	send = ostrcat(send, "\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n", 1, 0);
+	send = ostrcat(send, hash, 1, 0);
+	debug(99, "send: %s", send);
+
+	free(tmpstr), tmpstr = NULL;
+	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 1);
+	
+	writesys("/var/usr/local/share/titan/plugins/tithek/vidstream2", tmpstr, 0);
+
+	sleep(1);
+	streamlink = string_resub("file: \"", "\",", tmpstr, 0);
+
+end:
+
+	free(hash), hash = NULL;
+	free(hashlen), hashlen = NULL;
+	free(op), op = NULL;
+	free(id), id = NULL;
+	free(fname), fname = NULL;
+		
+	free(tmphost); tmphost = NULL;
+	free(tmpfile); tmpfile = NULL;
+	free(tmpstr); tmpstr = NULL;
+	free(send); send = NULL;
+	free(ip); ip = NULL;
+
+	return streamlink;
+}
+
+#endif
