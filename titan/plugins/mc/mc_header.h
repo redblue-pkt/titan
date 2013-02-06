41f8d189b712087118a02292459f59e969598f23
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-02-06 23:22:50 +0000

    fix

--- titan/plugins/mc/mc_header.h
+++ titan/plugins/mc/mc_header.h
@@ -1,20 +1,11 @@
 #ifndef MC_HEADER_H
 #define MC_HEADER_H
 
+//menu.h 
+struct skin* menu(struct skin* menu);
+
+// mc_global.h
 void picplayer(struct skin* picscreen, struct skin* picture, struct skin* picname, char* filename, int flag);
-
-//void screenmc_audioplayer();
-void mc_audioplayer_infobar(struct skin* apskin, struct skin* infobar, struct skin* spos, struct skin* slen, struct skin* sreverse, struct skin* sprogress, struct skin* b12, struct skin* b13, char* filename);
-
-//void mc_videoplayer_infobar(struct skin* apskin, struct skin* infobar, struct skin* spos, struct skin* slen, struct skin* sreverse, struct skin* sprogress, char* filename);
-void screenplayinfobar(char* file, char* showname, int mode, int playertype, int flag);
-void playrcblue(char* file, char* showname, int playinfobarstatus, int playertype, int flag);
-void playrcyellow(char* file, char* showname, int playinfobarstatus, int playertype, int flag);
-void playrcred(char* file, char* showname, int playinfobarstatus, int playertype, int flag);
-void playrcgreen(char* file, char* showname, int playinfobarstatus, int playertype, int flag);
-void playrctext(char* file, char* showname, int playinfobarstatus, int playertype, int flag);
-void playrcok(char* file, char* showname, int playinfobarstatus, int playertype, int flag);
-void playrcplay(char* file, char* showname, int* playinfobarstatus, int* playinfobarcount, int playertype, int flag);
 
 // mc_settings.h
 void screenmc_settings();
@@ -24,6 +15,7 @@ int screenmc_view();
 
 // mc_audioplayer_settings.h
 void screenmc_audioplayer_settings();
+void mc_audioplayer_infobar(struct skin* apskin, struct skin* infobar, struct skin* spos, struct skin* slen, struct skin* sreverse, struct skin* sprogress, struct skin* b12, struct skin* b13, char* filename);
 
 // mc_videoplayer_settings.h
 void screenmc_videoplayer_settings();
