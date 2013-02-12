526631dbce891ff974a3adf02fc398e19eff4fbf
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-02-12 04:04:49 +0000

    fix

--- titan/tools/parser/solarmovie.sh
+++ titan/tools/parser/solarmovie.sh
@@ -102,66 +102,171 @@ watch-movies-of-1933.html
 watch-movies-of-1932.html
 watch-movies-of-1931.html
 watch-movies-of-1930.html
+
+tv/watch-action-tv-shows.html
+tv/watch-adventure-tv-shows.html
+tv/watch-animation-tv-shows.html
+tv/watch-comedy-tv-shows.html
+tv/watch-drama-tv-shows.html
+tv/watch-horror-tv-shows.html
+tv/watch-romance-tv-shows.html
+tv/watch-sci-fi-tv-shows.html
+tv/watch-thriller-tv-shows.html
+tv/watch-tv-shows-2013.html
+tv/watch-tv-shows-2012.html
+tv/watch-tv-shows-2011.html
+tv/watch-tv-shows-2010.html
+tv/watch-tv-shows-2009.html
+tv/watch-tv-shows-2008.html
+tv/watch-tv-shows-2007.html
+tv/watch-tv-shows-2006.html
+tv/watch-tv-shows-2005.html
+tv/watch-tv-shows-2004.html
+tv/watch-tv-shows-2003.html
+tv/watch-tv-shows-2002.html
+tv/watch-tv-shows-2001.html
+tv/watch-tv-shows-2000.html
+tv/watch-tv-shows-1999.html
+tv/watch-tv-shows-1998.html
+tv/watch-tv-shows-1997.html
+tv/watch-tv-shows-1996.html
+tv/watch-tv-shows-1995.html
+tv/watch-tv-shows-1994.html
+tv/watch-tv-shows-1993.html
+tv/watch-tv-shows-1992.html
+tv/watch-tv-shows-1991.html
+tv/watch-tv-shows-1990.html
+tv/watch-tv-shows-1989.html
+tv/watch-tv-shows-1988.html
+tv/watch-tv-shows-1987.html
+tv/watch-tv-shows-1986.html
+tv/watch-tv-shows-1985.html
+tv/watch-tv-shows-1984.html
+tv/watch-tv-shows-1983.html
+tv/watch-tv-shows-1982.html
+tv/watch-tv-shows-1981.html
+tv/watch-tv-shows-1980.html
+tv/watch-tv-shows-1979.html
+tv/watch-tv-shows-1978.html
+tv/watch-tv-shows-1977.html
+tv/watch-tv-shows-1976.html
+tv/watch-tv-shows-1975.html
+tv/watch-tv-shows-1974.html
+tv/watch-tv-shows-1973.html
+tv/watch-tv-shows-1972.html
+tv/watch-tv-shows-1971.html
+tv/watch-tv-shows-1970.html
+tv/watch-tv-shows-1969.html
+tv/watch-tv-shows-1968.html
+tv/watch-tv-shows-1967.html
+tv/watch-tv-shows-1966.html
+tv/watch-tv-shows-1965.html
+tv/watch-tv-shows-1964.html
+tv/watch-tv-shows-1963.html
+tv/watch-tv-shows-1962.html
+tv/watch-tv-shows-1961.html
+tv/watch-tv-shows-1960.html
+tv/watch-tv-shows-1959.html
+tv/watch-tv-shows-1958.html
+tv/watch-tv-shows-1957.html
+tv/watch-tv-shows-1956.html
+tv/watch-tv-shows-1955.html
+tv/watch-tv-shows-1954.html
+tv/watch-tv-shows-1953.html
+tv/watch-tv-shows-1952.html
+tv/watch-tv-shows-1951.html
+tv/watch-tv-shows-1950.html
+tv/watch-tv-shows-1949.html
+tv/watch-tv-shows-1948.html
+tv/watch-tv-shows-1947.html
+tv/watch-tv-shows-1946.html
+tv/watch-tv-shows-1945.html
+tv/watch-tv-shows-1944.html
+tv/watch-tv-shows-1943.html
+tv/watch-tv-shows-1942.html
+tv/watch-tv-shows-1941.html
+tv/watch-tv-shows-1940.html
+tv/watch-tv-shows-1939.html
+tv/watch-tv-shows-1938.html
+tv/watch-tv-shows-1937.html
+tv/watch-tv-shows-1936.html
+tv/watch-tv-shows-1935.html
+tv/watch-tv-shows-1934.html
+tv/watch-tv-shows-1933.html
+tv/watch-tv-shows-1932.html
+tv/watch-tv-shows-1931.html
+tv/watch-tv-shows-1930.html
 "
-#watchlist="tv/watch-action-tv-shows.html watch-movies-of-2013.html watch-action-movies.html"
+#watchlist="tv/watch-tv-shows-1930.html"
 
 piccount=0
 count=0
 for ROUND0 in $watchlist; do
 	count=`expr $count + 1`
-	echo file."$count"."$ROUND0"
-	wget "http://www.solarmovie.so/$ROUND0" -O file."$count"."$ROUND0"
-	if [ `cat file."$count"."$ROUND0" | grep "DOCTYPE html PUBLIC" | wc -l` -eq 0 ]; then
-		pages=`cat file."$count"."$ROUND0" | zcat | grep ".html?page=" | grep "</a></li>" | grep -v 'class="next">' | sed 's!page=!\npage="!' | grep ^"page=" | cut -d'"' -f2`
-		input=`cat file."$count"."$ROUND0" | zcat | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' |tr ' ' '~'`
-	else
-		pages=`cat file."$count"."$ROUND0" | grep ".html?page=" | grep "</a></li>" | grep -v 'class="next">' | sed 's!page=!\npage="!' | grep ^"page=" | cut -d'"' -f2`
-		input=`cat file."$count"."$ROUND0" | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' |tr ' ' '~'`
-	fi
-	
-	piccount=`expr $piccount + 1`
-	URL="http://atemio.dyndns.tv/mediathek/solarmovie/streams/solarmovie.$ROUND0.list"
-	PIC="http://atemio.dyndns.tv/mediathek/menu/$ROUND0.jpg"
-	TITLE=`echo $ROUND0 | sed 's/.html//' | sed 's/watch-//' | tr '-' ' '`
 
-	LINE="$TITLE#$URL#$PIC#solarmovie_$piccount.jpg#Solarmovie#0"
-	if [ ! -z "$TITLE" ]; then
-		echo $LINE >> cache.solarmovie.category.titanlist	
-	fi
-
-	pcount=0
-	while [ "$pcount" -lt "$pages" ]
-	do
-		echo "pcount $pcount"
-		pcount=`expr $pcount + 1`
-		wget "http://www.solarmovie.so/$ROUND0.html?page=$pcount" -O file."$count".page"$pcount"."$ROUND0"
-		if [ `cat file."$count".page"$pcount"."$ROUND0" | grep "DOCTYPE html PUBLIC" | wc -l` -eq 0 ]; then
-			input=`cat file."$count".page"$pcount"."$ROUND0" | zcat | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' | tr ' ' '~'`
+	filename0=`echo $ROUND0 | tr '/' '-'`
+	wget "http://www.solarmovie.so/$ROUND0" -O cache."$count"."$filename0"
+	if [ `cat cache."$count"."$filename0" | grep "<h4>Nothing was found</h4>" | wc -l` -eq 0 ];then
+		if [ `cat cache."$count"."$filename0" | grep "DOCTYPE html PUBLIC" | wc -l` -eq 0 ]; then
+			pages=`cat cache."$count"."$filename0" | zcat | grep ".html?page=" | grep "</a></li>" | grep -v 'class="next">' | sed 's!page=!\npage="!' | grep ^"page=" | cut -d'"' -f2`
+			input=`cat cache."$count"."$filename0" | zcat | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' |tr ' ' '~'`
 		else
-			input=`cat file."$count".page"$pcount"."$ROUND0"  | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' | tr ' ' '~'`
+			pages=`cat cache."$count"."$filename0" | grep ".html?page=" | grep "</a></li>" | grep -v 'class="next">' | sed 's!page=!\npage="!' | grep ^"page=" | cut -d'"' -f2`
+			input=`cat cache."$count"."$filename0" | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' |tr ' ' '~'`
 		fi
-
-		if [ ! -z "$input" ]; then
-			for ROUND1 in $input; do
-				piccount=`expr $piccount + 1`
-				TITLE=`echo $ROUND1 | cut -d '"' -f2 | tr '~' ' '`
-				URL=http://www.solarmovie.so/`echo $ROUND1 | cut -d '"' -f4 | tr '~' ' '`
-				PIC=`echo $ROUND1 | cut -d '"' -f8 | tr '~' ' '`
-				LINE="$TITLE#$URL#$PIC#solarmovie_$piccount.jpg#Solarmovie#28"
+		piccount=`expr $piccount + 1`
+		URL="http://atemio.dyndns.tv/mediathek/solarmovie/streams/solarmovie.$filename0.list"
+		PIC="http://atemio.dyndns.tv/mediathek/menu/$filename0.jpg"
+		TITLE=`echo $ROUND0 | sed 's/.html//' | sed 's/watch-//' | tr '-' ' ' | tr '/' ' '`
 	
-				if [ ! -z "$TITLE" ] && [ `cat cache.solarmovie.$ROUND0.titanlist | grep ^"$TITLE#" | wc -l` -gt 0 ];then
-					echo $LINE >> cache.solarmovie.$ROUND0.titanlist	
-				fi
-				if [ ! -z "$TITLE" ] && [ `cat cache.solarmovie.titanlist | grep ^"$TITLE#" | wc -l` -gt 0 ];then
-					echo $LINE >> cache.solarmovie.titanlist
+		LINE="$TITLE#$URL#$PIC#solarmovie_$piccount.jpg#Solarmovie#0"
+		if [ ! -z "$TITLE" ]; then
+			echo $LINE >> cache.solarmovie.category.titanlist	
+		fi
+	
+		pcount=0
+		echo pages $pages
+		if [ ! -z $pages ]; then
+			while [ "$pcount" -lt "$pages" ]
+			do
+				echo "pcount $pcount"
+				pcount=`expr $pcount + 1`
+		#		wget "http://www.solarmovie.so/$ROUND0.html?page=$pcount" -O cache."$count".page"$pcount"."$filename0"
+				wget "http://www.solarmovie.so/$ROUND0?page=$pcount" -O cache."$count".page"$pcount"."$filename0"
+
+				if [ `cat cache."$count".page"$pcount"."$filename0" | grep "<h4>Nothing was found</h4>" | wc -l` -eq 0 ];then
+					if [ `cat cache."$count".page"$pcount"."$filename0" | grep "DOCTYPE html PUBLIC" | wc -l` -eq 0 ]; then
+						input=`cat cache."$count".page"$pcount"."$filename0" | zcat | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' | tr ' ' '~'`
+					else
+						input=`cat cache."$count".page"$pcount"."$filename0"  | tr '\n' ' '| sed 's/<div class="typicalGrey coverGroup">\+/\n/g'| sed 's!</a>\+!\n!g'| sed 's/ \+/ /g'| grep ^' <a class="coverImage"' | sed 's! <a class="coverImage" !!' | tr ' ' '~'`
+					fi
+			
+					if [ ! -z "$input" ]; then
+						for ROUND1 in $input; do
+							piccount=`expr $piccount + 1`
+							TITLE=`echo $ROUND1 | cut -d '"' -f2 | tr '~' ' ' | sed 's/#/%/'`
+							URL=http://www.solarmovie.so`echo $ROUND1 | sed s'!href=!\nhref=!' | grep ^"href=" | cut -d '"' -f2 | tr '~' ' '`
+							PIC=`echo $ROUND1 | sed s'!<img~src=!\n<img~src=!' | grep ^"<img~src=" | cut -d '"' -f2 | tr '~' ' '`
+							LINE="$TITLE#$URL#$PIC#solarmovie_$piccount.jpg#Solarmovie#28"
+			
+			#				if [ ! -z "$TITLE" ] && [ `cat cache.solarmovie."$filename0".titanlist | grep ^"$TITLE#" | wc -l` -gt 0 ];then
+							if [ ! -z "$TITLE" ] && [ ! -z "$URL" ];then
+								echo $LINE >> cache.solarmovie."$filename0".titanlist	
+							fi
+			
+							if [ ! -z "$TITLE" ] && [ ! -z "$URL" ] && [ `cat cache.solarmovie.titanlist | grep ^"$TITLE" | wc -l` -eq 0 ];then
+								echo $LINE >> cache.solarmovie.titanlist
+							fi
+						done
+					fi
 				fi
 			done
+			
+			if [ `cat cache.solarmovie.$filename0.titanlist | wc -l` -gt 0 ];then
+				cat cache.solarmovie.$filename0.titanlist | sort -um > _full/solarmovie/streams/solarmovie.$filename0.list
+			fi	
 		fi
-	done
-	
-	if [ `cat cache.solarmovie.$ROUND0.titanlist | wc -l` -gt 0 ];then
-		cat cache.solarmovie.$ROUND0.titanlist | sort -um > _full/solarmovie/streams/solarmovie.$ROUND0.list
-	fi	
+	fi
 done
 
 cat cache.solarmovie.titanlist | sort -u > _full/solarmovie/streams/solarmovie.all-sorted.list
@@ -180,8 +285,9 @@ for ROUND in 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X
 		echo `echo "$ROUND" | tr 'A-Z' 'a-z'`"#http://atemio.dyndns.tv/mediathek/solarmovie/streams/solarmovie."`echo "$ROUND" | tr 'A-Z' 'a-z'`".list#http://atemio.dyndns.tv/mediathek/menu/`echo "$ROUND" | tr 'A-Z' 'a-z'`.jpg#"`echo "$ROUND" | tr 'A-Z' 'a-z'`.jpg#Solarmovie#0 >> _full/solarmovie/solarmovie.a-z.list
 	fi
 done
-rm cache.*
+rm file.*
+#rm cache.*
 #mkdir /var/www/atemio/web/mediathek/solarmovie
-#cp -a _full/solarmovie/* /var/www/atemio/web/mediathek/solarmovie
-#cp -a mainmenu.list /var/www/atemio/web/mediathek
-#cp -a mainmenu-solarmovie.list /var/www/atemio/web/mediathek
+cp -a _full/solarmovie/* /var/www/atemio/web/mediathek/solarmovie
+cp -a mainmenu.list /var/www/atemio/web/mediathek
+cp -a mainmenu-solarmovie.list /var/www/atemio/web/mediathek
