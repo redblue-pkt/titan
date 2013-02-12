d465d885908ad53908913a2384641ec4e38cf954
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-02-12 04:05:36 +0000

    fix

--- titan/titan/tools/parser/solarmovie.sh
+++ titan/titan/tools/parser/solarmovie.sh
@@ -286,8 +286,8 @@ for ROUND in 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X
 	fi
 done
 rm file.*
-#rm cache.*
+rm cache.*
 #mkdir /var/www/atemio/web/mediathek/solarmovie
-cp -a _full/solarmovie/* /var/www/atemio/web/mediathek/solarmovie
-cp -a mainmenu.list /var/www/atemio/web/mediathek
-cp -a mainmenu-solarmovie.list /var/www/atemio/web/mediathek
+#cp -a _full/solarmovie/* /var/www/atemio/web/mediathek/solarmovie
+#cp -a mainmenu.list /var/www/atemio/web/mediathek
+#cp -a mainmenu-solarmovie.list /var/www/atemio/web/mediathek
