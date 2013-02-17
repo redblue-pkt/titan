1a010b29cc813b8651317a2be6337322b155d054
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-02-17 10:22:04 +0000

    [titan] fix possible segfault in textinputhist

--- titan/textinputhist.h
+++ titan/textinputhist.h
@@ -155,7 +155,7 @@ char* textinputhist(char* title, char* text, char* histname)
 		rcret = waitrc(textinputhist, 0, 0);
 		if(rcret == getrcconfigint("rcexit", NULL)) break;
 
-		if(text == NULL)
+		if(text == NULL && listbox->select != NULL)
 		{
 			changeinput(input, listbox->select->name);
 
