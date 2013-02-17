e1a0af446daef9a79e57cef76c66dc6ad7e422a9
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-02-17 12:36:48 +0000

    [titan] don't show epgtimer in webif

--- titan/titan/httpdfunc.h
+++ titan/titan/httpdfunc.h
@@ -2055,7 +2055,7 @@ char* webgetrectimer(char* param, int flag, int fmt)
 
 	while(node != NULL)
 	{
-		if((flag == 0 && (node->status == 2 || node->status == 3)) || (flag == 1 && (node->status == 0 || node->status == 1)))
+		if((flag == 0 && (node->status == 2 || node->status == 3)) || (flag == 1 && (node->status == 0 || node->status == 1)) || rectimernode->status == 4 || rectimernode->status == 5)
 		{
 			node = node->next;
 			continue;
