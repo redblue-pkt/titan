dc0bba69201c2a9e3b871e2aa8bffb1c103cce15
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-02-28 22:58:14 +0000

    add load skin to restore

--- titan/titan/restoredefault.h
+++ titan/titan/restoredefault.h
@@ -5,11 +5,13 @@ void screenrestoredefault()
 {
 	int ret = 0;
 	char* tmpstr = NULL;
+	struct skin* load = getscreen("loading");
 
 	ret = textbox(_("Message"), _("Realy restore default settings ?"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0);
 
 	if(ret == 1)
 	{
+		drawscreen(load, 0, 0);
 		tmpstr = getconfig("restorecmd", NULL);
 		if(tmpstr != NULL)
 		{
@@ -20,6 +22,7 @@ void screenrestoredefault()
 		else
 			textbox(_("Message"), _("Can't restore settings"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 7, 0);
 	}
+	clearscreen(load);
 }
 
 #endif
