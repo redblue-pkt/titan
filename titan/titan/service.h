5e2d2c7f4db961274d96a5d838d667ab5dce89a4
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-03-14 22:58:52 +0000

    [titan] add settings for ask stopping timeshift on channel switch

--- titan/titan/service.h
+++ titan/titan/service.h
@@ -689,7 +689,8 @@ int servicestop(struct service *node, int clear, int flag)
 	{
 		if(status.timeshift == 1 && flag != 2)
 		{
-			rcret = textbox(_("Message"), _("Timeshift is running !!!\nStop it and switch ?"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 400, 10, 0);
+			if(status.asktimeshift == 0)
+				rcret = textbox(_("Message"), _("Timeshift is running !!!\nStop it and switch ?"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 400, 10, 0);
 
 			if(rcret == 2) return 1;
 			timeshiftstop(1);
