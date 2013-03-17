34461fa4555651b358e29bf267476b015c0dbded
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-03-17 00:18:32 +0000

    disable gcc 6.4.3 warnings -Wno-unused-but-set-variable

--- plugins/makesh4.sh
+++ plugins/makesh4.sh
@@ -40,7 +40,7 @@ compile()
 	echo "[titan] Make Plugin $1"
 	echo "[titan]--------------------------------------------------------"
 
-	$HOME/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/devkit/sh4/bin/sh4-linux-gcc -D$eplayer -Os -fPIC -Wall $4 \
+	$HOME/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/devkit/sh4/bin/sh4-linux-gcc -D$eplayer -Os -fPIC -Wall -Wno-unused-but-set-variable $4 \
 	-I "$HOME"/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/cdkroot/usr/include/freetype2 \
 	-I $eplayerinclude \
 	-I "$HOME"/flashimg/BUILDGIT/checkout_"$STM"/tdt/cvs/driver/bpamem \
