7c8d90c1b6365eed1a608215f1156e2a0e8f1d15
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-03-17 00:18:32 +0000

    disable gcc 6.4.3 warnings -Wno-unused-but-set-variable

--- titan/titan/makesh4.sh
+++ titan/titan/makesh4.sh
@@ -453,7 +453,7 @@ else
 	eplayerlib=gstreamer-0.10
 fi
 
-"$HOME"/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/devkit/sh4/bin/sh4-linux-gcc -DSH4 -D$eplayer -DDVDPLAYER -Os -export-dynamic -Wall \
+"$HOME"/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/devkit/sh4/bin/sh4-linux-gcc -DSH4 -D$eplayer -DDVDPLAYER -Os -export-dynamic -Wall -Wno-unused-but-set-variable \
 	-I "$HOME"/flashimg/BUILDGIT/checkout_"$STM"/tdt/tufsbox/cdkroot/usr/include/freetype2 \
 	-I $eplayerinclude \
 	-I "$HOME"/flashimg/source.titan/libdreamdvd \
