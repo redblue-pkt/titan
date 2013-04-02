722f3c2277b4dfab3efa332fa12833fddcba7694
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-04-02 19:19:05 +0000

    [titan] make hddbar ajustable

--- titan/titan/defaults.h
+++ titan/titan/defaults.h
@@ -115,6 +115,7 @@ void setdefaults()
 	status.crosscontrol = getconfigint("crosscontrol", NULL);
 	status.timeshifttype = getconfigint("timeshifttype", NULL);
 	status.autochangechannelname = getconfigint("autochangechannelname", NULL);
+	status.showrecfreesize = getconfigint("showrecfreesize", NULL);
 
 	if(status.filelistselectcol == 0) status.filelistselectcol = status.listboxselectcol;
 	
