26fbf9c311267ba70462a22704ce7ba1370b753b
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-05-15 19:16:28 +0000

    fix

--- tpk/make.sh
+++ tpk/make.sh
@@ -1,1 +1,1 @@
-gcc -D TPKCREATE "$HOME"/flashimg/source.titan/tpk/tpk.c -Wall -I "$HOME"/flashimg/source.titan/titan -o "$HOME"/flashimg/source.titan/tpk/tpk
+gcc -D TPKCREATE -D WORKDIR=\"$HOME/ipk/BUILD\" "$HOME"/flashimg/source.titan/tpk/tpk.c -Wall -I "$HOME"/flashimg/source.titan/titan -o "$HOME"/flashimg/source.titan/tpk/tpk
