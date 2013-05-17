741b3b2e7da8367cebec1fc374fedd4294c71677
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-05-17 22:17:08 +0000

    fix

---
+++ titan/titan/tools/gettpk.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+TPKDIR=$1
+VERSION=$2
+
+. "$HOME"/flashimg/config/make-config
+
+cd "$HOME"/flashimg/source.titan/titan/tools
+
+echo gettpkg extensions.hpatch 1
+cat ../extensions.h | sed s!/svn/tpk/sh4/titan!$TPKDIR! > extensions.h.tmp
+mv extensions.h.tmp ../extensions.h
+
+echo gettpkg tpk.h patch 1
+cat ../tpk.h | sed s!//svn/tpk/sh4/titan!$TPKDIR! > tpk.h.tmp
+mv tpk.h.tmp ../tpk.h
+
+echo gettpk tpk.h patch 2
+cat ../tpk.h | sed s!97.74.32.10!$URLSECRET! > tpk.h.tmp
+mv tpk.h.tmp ../tpk.h
+
+echo add pluginversionsnummer to: $VERSION
+cat ../struct.h | sed s/"^#define PLUGINVERSION 0"/"#define PLUGINVERSION $VERSION"/ > struct.h.tmp
+mv struct.h.tmp ../struct.h
