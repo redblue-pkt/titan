0feabf0b8f3deabf46eb730821f39c6d4b563173
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-05-20 13:05:46 +0000

    [titan] change ipk to tpk

--- titan/plugins/mc/mc_internetbrowser.h
+++ titan/plugins/mc/mc_internetbrowser.h
@@ -35,7 +35,7 @@ void screenmc_internetbrowser()
 			}
 		}
 		else
-			textbox(_("Message"), _("Install netsurf Internetbrowser Ipk first !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 200, 0, 0);
+			textbox(_("Message"), _("Install netsurf Internetbrowser Tpk first !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 200, 0, 0);
 	}
 	else
 		textbox(_("Message"), _("Registration needed, please contact Atemio !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 200, 0, 0);
