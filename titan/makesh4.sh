ddd7989272b68ad06c904e9cdaecfef3b66f7da6
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-06-07 16:03:09 +0000

    fix

--- titan/makesh4.sh
+++ titan/makesh4.sh
@@ -108,7 +108,7 @@ echo "[titan]--------------------------------------------------------"
 echo "[titan] get settings"
 echo "[titan]--------------------------------------------------------"
 rm -rf "$HOME"/flashimg/source.titan/settings.svn
-svn co --username $SVNUSER --password $SVNPASS http://"$SVNURL"/svn/ipk/source.sh4/titansettings_default_sat_1_0/var/etc/titan "$HOME"/flashimg/source.titan/settings.svn
+svn co --username $SVNUSER --password $SVNPASS http://"$SVNURL"/svn/ipk/source.sh4/titansettings_default_sat_1_0/mnt/settings "$HOME"/flashimg/source.titan/settings.svn
 mv -f "$HOME"/flashimg/source.titan/settings.svn/* "$HOME"/flashimg/source.titan/settings
 sort -u "$HOME"/flashimg/source.titan/settings/channel > "$HOME"/flashimg/source.titan/settings/channel.sort
 mv "$HOME"/flashimg/source.titan/settings/channel.sort "$HOME"/flashimg/source.titan/settings/channel
