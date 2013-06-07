50b5d3649ba3d5abc2e175dcfc19b58a9221a032
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-06-07 20:16:56 +0000

    fix

--- titan/plugins/panel/panel_settings_wizard.h
+++ titan/plugins/panel/panel_settings_wizard.h
@@ -6,7 +6,6 @@ void screenpanel_wizard()
 	if(textbox(_("Message"), _("Do you really want to start the Wizard?\nThe Box will reboot and the Wizard will be started!"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0) == 1)
 	{
 		debug(40, "Wizard");
-		system("touch /var/etc/.firstszap");
 		system("touch /var/etc/.firstwizard");
 		sleep(4);
 		oshutdown(2, 1);
