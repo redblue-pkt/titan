03d47544dde35520591ca496b94ca8045b2731fa
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-08-02 09:31:52 +0000

    fix

--- titan/tools/parser/kinox.sh
+++ titan/tools/parser/kinox.sh
@@ -67,80 +67,83 @@ if [ "$buildtype" = "full" ];then
 	PIC="http://atemio.dyndns.tv/mediathek/menu/Movies.update.jpg"
 	LINE="Filme (letzten uploads)#$URL#$PIC#kinox_$piccount.jpg#KinoX#3"
 	echo $LINE >> cache.kinox.category.titanlist
-	
-	main_list="Cine-Films Popular-Movies Popular-Documentations Popular-Series Latest-Documentations Latest-Series Movies"
-	#main_list="Movies"
-	
-	echo main_list $main_list
-	piccount=0
-	counttt=0
-	for ROUND0 in $main_list; do
-		$wgetbin --no-check-certificate "http://kinox.to/$ROUND0.html" -O cache.main.next.list
-		main_next_list=`cat cache.main.next.list | grep /Stream/ | sed 's!/Stream/!\n/Stream/!' | grep ^/Stream/ | cut -d '"' -f1  | cut -d "'" -f1 | sort -um`
-		
-		if [ $ROUND0 == "Popular-Series" ]; then
-			main_next_list="$main_next_list /Stream/Die_Gummibaerenbande.html /Stream/Touch-1.html /Stream/Alcatraz_german_subbed.html /Stream/Battlestar_Galactica.html /Stream/24.html /Stream/Heroes.html /Stream/Star_Wars_The_Clone_Wars-1.html /Stream/Star_Wars_The_Clone_Wars_german_subbed.html /Stream/TRON_Der_Aufstand.html /Stream/TRON_Uprising.html"
-		fi
-	
-		if [ $ROUND0 == "Movies" ]; then
-			main_next_list=`cat movieliste.log | tr '\n' ' '`
-		fi
-		
-		skip=0
-	
-		echo main_next_list $main_next_list
-		lokaltest=0
-		if [ $lokaltest == 1 ];then
-			main_next_list="/Stream/Touch-1.html"
-		fi
-		
-		TITLE=`echo $ROUND0 | tr "_" " " | tr "-" " "`
-	
-		piccount=`expr $piccount + 1`
-		URL="http://atemio.dyndns.tv/mediathek/kinox/streams/kinox.$ROUND0.list"
-		PIC="http://atemio.dyndns.tv/mediathek/menu/$ROUND0.jpg"
-		LINE="$TITLE#$URL#$PIC#kinox_$piccount.jpg#KinoX#3"
-		if [ ! -z "$TITLE" ]; then
-			echo $LINE >> cache.kinox.category.titanlist	
-		fi
-			
-		for ROUND1 in $main_next_list; do
-			piccount=`expr $piccount + 1`
-			filename=`echo $ROUND1 | sed 's!/Stream/!!'`
-			picname=`echo $filename | sed 's!.html!.jpg!'`
-			searchname=`echo $filename | sed 's!.html!!'`	
-			
-			TITLE=`echo $picname | sed 's!.jpg!!' | tr "_" " "`
-		
-			$wgetbin --no-check-certificate "http://kinox.to/$ROUND1" -O cache."$filename".list
-			PIC=`cat cache."$filename".list | tr '><' '>\n<' | grep $picname | cut -d '"' -f2 | sort -um`
-			LANG=`cat cache."$filename".list | grep 'alt="language" src="/gr/sys/lng' | sed 's!alt="language" src="/gr/sys/lng/!\n!' | tail -n1 |cut -d"." -f1`
-		 	LANGTXT=" (??)"
-	
-			if [ ! -z $LANG ];then
-				if [ $LANG = 1 ];then
-				 	LANGTXT=" (de)"
-				elif [ $LANG = 2 ];then
-				 	LANGTXT=" (en)"
-				else
-				 	LANGTXT=" (??)"
-				fi
-			fi
-	
-			URL="http://kinox.to/$ROUND1"
-		
-			LINE="$TITLE$LANGTXT#$URL#$PIC#kinox_$piccount.jpg#KinoX#22"
-			if [ ! -z "$TITLE" ]; then
-				echo $LINE >> cache.kinox.$filename.titanlist
-				echo $LINE >> cache.kinox.titanlist			
-			fi
-
-			skip1=0
-					
-			cat cache.kinox.$filename.titanlist | sort -u >> _full/kinox/streams/kinox.$ROUND0.list	
-		done
-	done
 fi
+
+if [ "$buildtype" = "full" ];then
+	main_list="Cine-Films Popular-Movies Popular-Documentations Popular-Series Latest-Documentations Latest-Series Movies"
+else
+	main_list="Cine-Films Popular-Movies Popular-Documentations Latest-Documentations Latest-Series"
+fi
+	
+echo main_list $main_list
+piccount=0
+counttt=0
+for ROUND0 in $main_list; do
+	$wgetbin --no-check-certificate "http://kinox.to/$ROUND0.html" -O cache.main.next.list
+	main_next_list=`cat cache.main.next.list | grep /Stream/ | sed 's!/Stream/!\n/Stream/!' | grep ^/Stream/ | cut -d '"' -f1  | cut -d "'" -f1 | sort -um`
+	
+	if [ $ROUND0 == "Popular-Series" ]; then
+		main_next_list="$main_next_list /Stream/Die_Gummibaerenbande.html /Stream/Touch-1.html /Stream/Alcatraz_german_subbed.html /Stream/Battlestar_Galactica.html /Stream/24.html /Stream/Heroes.html /Stream/Star_Wars_The_Clone_Wars-1.html /Stream/Star_Wars_The_Clone_Wars_german_subbed.html /Stream/TRON_Der_Aufstand.html /Stream/TRON_Uprising.html"
+	fi
+
+	if [ $ROUND0 == "Movies" ]; then
+		main_next_list=`cat movieliste.log | tr '\n' ' '`
+	fi
+	
+	skip=0
+
+	echo main_next_list $main_next_list
+	lokaltest=0
+	if [ $lokaltest == 1 ];then
+		main_next_list="/Stream/Touch-1.html"
+	fi
+	
+	TITLE=`echo $ROUND0 | tr "_" " " | tr "-" " "`
+
+	piccount=`expr $piccount + 1`
+	URL="http://atemio.dyndns.tv/mediathek/kinox/streams/kinox.$ROUND0.list"
+	PIC="http://atemio.dyndns.tv/mediathek/menu/$ROUND0.jpg"
+	LINE="$TITLE#$URL#$PIC#kinox_$piccount.jpg#KinoX#3"
+	if [ ! -z "$TITLE" ]; then
+		echo $LINE >> cache.kinox.category.titanlist	
+	fi
+		
+	for ROUND1 in $main_next_list; do
+		piccount=`expr $piccount + 1`
+		filename=`echo $ROUND1 | sed 's!/Stream/!!'`
+		picname=`echo $filename | sed 's!.html!.jpg!'`
+		searchname=`echo $filename | sed 's!.html!!'`	
+		
+		TITLE=`echo $picname | sed 's!.jpg!!' | tr "_" " "`
+	
+		$wgetbin --no-check-certificate "http://kinox.to/$ROUND1" -O cache."$filename".list
+		PIC=`cat cache."$filename".list | tr '><' '>\n<' | grep $picname | cut -d '"' -f2 | sort -um`
+		LANG=`cat cache."$filename".list | grep 'alt="language" src="/gr/sys/lng' | sed 's!alt="language" src="/gr/sys/lng/!\n!' | tail -n1 |cut -d"." -f1`
+	 	LANGTXT=" (??)"
+
+		if [ ! -z $LANG ];then
+			if [ $LANG = 1 ];then
+			 	LANGTXT=" (de)"
+			elif [ $LANG = 2 ];then
+			 	LANGTXT=" (en)"
+			else
+			 	LANGTXT=" (??)"
+			fi
+		fi
+
+		URL="http://kinox.to/$ROUND1"
+	
+		LINE="$TITLE$LANGTXT#$URL#$PIC#kinox_$piccount.jpg#KinoX#22"
+		if [ ! -z "$TITLE" ]; then
+			echo $LINE >> cache.kinox.$filename.titanlist
+			echo $LINE >> cache.kinox.titanlist			
+		fi
+
+		skip1=0
+				
+		cat cache.kinox.$filename.titanlist | sort -u >> _full/kinox/streams/kinox.$ROUND0.list	
+	done
+done
 	
 $wgetbin http://kinox.to -O cache.main.update.list
 
