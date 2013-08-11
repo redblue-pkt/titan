c9ab9ee85f790ef31e60a406501c66cbadb809a3
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-08-11 08:00:13 +0000

    [titan] optimize titan size (7%-8%)

--- titan/titan/debug.h
+++ titan/titan/debug.h
@@ -13,15 +13,66 @@ short debug_level = 10;
 // mc debug_level = 50;
 // panel debug_level = 60;
 
+//debug
 //first line shows greater/same debuglevel
 //second line shows only same debuglevel
 //#define debug(level, fmt, args...) if(debug_level >= level) { do { printf("[%s] " fmt, PROGNAME, ##args); } while (0); printf(", file=%s, func=%s, line=%d\n", __FILE__, __FUNCTION__, __LINE__); }
-#define debug(level, fmt, args...) if(debug_level == level) { do { printf("[%s] " fmt, PROGNAME, ##args); } while (0); printf(", file=%s, func=%s, line=%d\n", __FILE__, __FUNCTION__, __LINE__); }
+//#define debug(level, fmt, args...) if(debug_level == level) { do { printf("[%s] " fmt, PROGNAME, ##args); } while (0); printf(", file=%s, func=%s, line=%d\n", __FILE__, __FUNCTION__, __LINE__); }
+void debugfunc(int level, char* file, const char* function, int line, char* msg, ...)
+{
+	if(debug_level == level)
+	{
+		va_list ap;
+		va_start(ap, msg);
+		printf("[%s] ", PROGNAME);
+		vfprintf(stdout, msg, ap);
+		va_end(ap);
+		printf(", file=%s, func=%s, line=%d\n", file, function, line);
+	}
+}
+#define debug(level, msg...) debugfunc(level, __FILE__, __FUNCTION__, __LINE__, msg);
 
-#define err(fmt, args...) { do { fprintf(stderr, "[%s] error: " fmt, PROGNAME, ##args); } while (0); fprintf(stderr, ", file=%s, func=%s, line=%d\n", __FILE__, __FUNCTION__, __LINE__); }
+//err
+//#define err(fmt, args...) { do { fprintf(stderr, "[%s] error: " fmt, PROGNAME, ##args); } while (0); fprintf(stderr, ", file=%s, func=%s, line=%d\n", __FILE__, __FUNCTION__, __LINE__); }
+void errfunc(char* file, const char* function, int line, char* msg, ...)
+{
+	va_list ap;
+	va_start(ap, msg);
+	fprintf(stderr, "[%s] error: ", PROGNAME);
+	vfprintf(stderr, msg, ap);
+	va_end(ap);
+	fprintf(stderr, ", file=%s, func=%s, line=%d\n", file, function, line);
+}
+#define err(msg...) errfunc(__FILE__, __FUNCTION__, __LINE__, msg);
 
-#define perr(fmt, args...) { do { fprintf(stderr, "[%s] error: " fmt, PROGNAME, ##args); } while (0); fprintf(stderr, ", err=%m, file=%s, func=%s, line=%d\n", __FILE__, __FUNCTION__, __LINE__); }
+//perr
+//#define perr(fmt, args...) { do { fprintf(stderr, "[%s] error: " fmt, PROGNAME, ##args); } while (0); fprintf(stderr, ", err=%m, file=%s, func=%s, line=%d\n", __FILE__, __FUNCTION__, __LINE__); }
+void perrfunc(char* file, const char* function, int line, char* msg, ...)
+{
+	va_list ap;
+	va_start(ap, msg);
+	fprintf(stderr, "[%s] error: ", PROGNAME);
+	vfprintf(stderr, msg, ap);
+	va_end(ap);
+	fprintf(stderr, ", err=%m, file=%s, func=%s, line=%d\n", file, function, line);
+}
+#define perr(msg...) perrfunc(__FILE__, __FUNCTION__, __LINE__, msg);
 
-#define filedebug(file, fmt, args...) { FILE* fd = fopen(file, "a"); if(fd != NULL) { do { fprintf(fd, "" fmt, ##args); } while (0); fprintf(fd, "\n"); fclose(fd); }}
+//filedebug
+//#define filedebug(file, fmt, args...) { FILE* fd = fopen(file, "a"); if(fd != NULL) { do { fprintf(fd, "" fmt, ##args); } while (0); fprintf(fd, "\n"); }}
+void filedebugfunc(char* file, char* msg, ...)
+{
+	FILE* fd = fopen(file, "a"); 
+	if(fd != NULL)
+	{
+		va_list ap;
+		va_start(ap, msg);
+		vfprintf(fd, msg, ap);
+		va_end(ap);
+		fprintf(fd, "\n");
+		fclose(fd);
+	}
+}
+#define filedebug(file, msg...) filedebugfunc(file, msg);
 
 #endif
