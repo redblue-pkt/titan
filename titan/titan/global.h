d1977520fe27fbfec906a2b39d8648e39a812528
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-08-20 22:19:26 +0000

    fix

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -900,19 +900,20 @@ int checkflash()
 	char* dev = NULL;
 	char* dir = NULL;
 	
-	if((checkbox("ATEMIO510") == 1) || (checkbox("ATEVIO700") == 1) || (checkbox("ATEVIO7000") == 1) || (checkbox("UFS910") == 1) || (checkbox("UFS922") == 1))
+	if((checkbox("ATEMIO7600") == 1) || (checkbox("ATEMIO510") == 1) || (checkbox("ATEVIO700") == 1) || (checkbox("ATEVIO7000") == 1) || (checkbox("UFS910") == 1) || (checkbox("UFS922") == 1) || (checkbox("ATEMIO520") == 1))
 	{
 		dev = ostrcat(dev, "3", 1, 0);
 		dir = ostrcat(dir, "var", 1, 0);		
 	}
-	else if((checkbox("ATEMIO7600") == 1) || (checkbox("IPBOX91") == 1) || (checkbox("IPBOX900") == 1) || (checkbox("IPBOX910") == 1) || (checkbox("IPBOX9000") == 1) || (checkbox("WHITEBOX") == 1) || (checkbox("ATEMIO520") == 1))
+	else if((checkbox("IPBOX91") == 1) || (checkbox("IPBOX900") == 1) || (checkbox("IPBOX910") == 1) || (checkbox("IPBOX9000") == 1) || (checkbox("WHITEBOX") == 1))
 	{
 		dev = ostrcat(dev, "4", 1, 0);
 		dir = ostrcat(dir, "var", 1, 0);
 	}
 	else if(checkbox("UFS912") == 1)
 	{
-		dev = ostrcat(dev, "5", 1, 0);
+//		dev = ostrcat(dev, "5", 1, 0);
+		dev = ostrcat(dev, "4", 1, 0);
 		dir = ostrcat(dir, "var", 1, 0);
 	}
 	else
