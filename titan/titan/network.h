2b84c9d4686ca186e330f3f50ec53152ce243d05
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-08-21 23:54:16 +0000

    fix

--- titan/titan/network.h
+++ titan/titan/network.h
@@ -781,8 +781,11 @@ void screennetwork_wlan()
 						if(ret == 0)
 						{
 							net = getinetworkfirstwlan();
-							net->type = 1; //dhcp
-							if(net != NULL) screennetwork_adapterext(0, net->device);
+							if(net != NULL) 
+							{
+								net->type = 1; //dhcp
+								screennetwork_adapterext(0, net->device);
+							}	
 						}
 					}
 					break;
