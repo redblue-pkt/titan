5b3fd775120887daba4e5402dd7af2028cfc168f
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-08-22 13:18:27 +0000

    [titan] update usb reset

--- titan/plugins/usbreset/usbreset.h
+++ titan/plugins/usbreset/usbreset.h
@@ -1,9 +1,77 @@
 #ifndef USBRESET_H
 #define USBRESET_H
 
-#define USBRESETPATH "/sys/bus/usb/drivers/usb/unbind"
+#define USBRESETPATH "/sys/bus/usb/drivers/usb"
 #define USBRESETUNBIND USBRESETPATH"/unbind"
 #define USBRESETBIND USBRESETPATH"/bind"
+
+int addusbreset(char* dirname, struct menulist** mlist)
+{
+	int ret = 0;
+	DIR *d;
+	char* tmpstr = NULL;
+
+	//Open the directory specified by dirname
+	d = opendir(dirname);
+
+	//Check it was opened
+	if(! d)
+	{
+		perr("Cannot open directory %s", dirname);
+		return 1;
+	}
+
+	while(1)
+	{
+		struct dirent* entry;
+		int path_length;
+		char path[PATH_MAX];
+
+		snprintf(path, PATH_MAX, "%s", dirname);
+		//Readdir gets subsequent entries from d
+		entry = readdir(d);
+
+		if(!entry) //There are no more entries in this directory, so break out of the while loop
+			break;
+
+		//See if entry is a subdirectory of d
+		if(entry->d_type == DT_LNK)
+		{
+			//Check that the directory is not d or d's parent
+			if(entry->d_name != NULL && ostrcmp(entry->d_name, ".") != 0 && ostrcmp(entry->d_name, "..") != 0)
+			{
+				path_length = snprintf(path, PATH_MAX, "%s/%s/product", dirname, entry->d_name);
+				if(path_length >= PATH_MAX)
+				{
+					err("path length has got too long");
+					ret = 1;
+					break;
+				}
+				
+				tmpstr = readsys(path, 1);
+				if(tmpstr != NULL && path[1] == '-')
+				{
+					tmpstr = ostract(tmpstr, " (", 1, 0);
+					tmpstr = ostract(tmpstr, entry->d_name, 1, 0);
+					tmpstr = ostract(tmpstr, ")", 1, 0);
+					struct menulist* m = addmenulist(mlist, tmpstr, NULL, NULL, 0, 0);
+					changemenulistparam(m, entry->d_name, NULL);
+				}
+				free(tmpstr); tmpstr = NULL;
+				
+			}
+		}
+	}
+
+	//After going through all the entries, close the directory
+	if(d && closedir(d))
+	{
+		perr("Could not close %s", dirname);
+		ret = 1;
+	}
+
+	return ret;
+}
 
 void screenusbreset()
 {
@@ -11,25 +79,25 @@ void screenusbreset()
 	struct menulist* mlist = NULL, *mbox = NULL;	
 	struct skin* load = getscreen("loading");
 	
-	// /sys/bus/usb/drivers/usb/unbind
-	// /sys/bus/usb/drivers/usb/1-1/product
-	
-	tmpstr = readsys("", 1);
-	addmenulist(&mlist, tmpstr, NULL, NULL, 0, 0);
-	free(tmpstr); tmpstr = NULL;
+	addusbreset(USBRESETPATH, &mlist);
 	
 	mbox = menulistbox(mlist, NULL, "USB Reset", NULL, NULL, 1, 0);
 	
 	if(mbox != NULL)
 	{
-		drawscreen(load, 0, 0);
-	
-		writesys(USBRESETUNBIND, value, 0); //unbind
-		sleep(1);
-		writesys(USBRESETBIND, value, 0); //bind
+		if(mbox->param != NULL)
+		{
+			drawscreen(load, 0, 0);
 		
-		clearscreen(load);
+			writesys(USBRESETUNBIND, mbox->param, 0); //unbind
+			sleep(2);
+			writesys(USBRESETBIND, mbox->param, 0); //bind
+			
+			clearscreen(load);
+		}
 	}
+	
+	freemenulist(mlist, 1); mlist = NULL;
 }
 
 #endif
