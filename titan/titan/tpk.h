464b072ce23bf776d174171a653a5a434a05b92e
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-08-25 17:45:49 +0000

    [titan] add sort to tpk

--- titan/titan/tpk.h
+++ titan/titan/tpk.h
@@ -108,12 +108,12 @@ struct tpk* addtpk(char *name, char* desc, char* section, char* showname, char* 
 
 	if(last == NULL)
 	{
-		while(node != NULL && strcasecmp(newnode->section, node->section) >= 0)
+		while(node != NULL && strcasecmp(_(newnode->section), _(node->section)) >= 0)
 		{
 			prev = node;
 			node = node->next;
 		}
-		while(node != NULL && strcasecmp(newnode->section, node->section) == 0 && strcasecmp(newnode->showname, node->showname) > 0)
+		while(node != NULL && strcasecmp(_(newnode->section), _(node->section)) == 0 && strcasecmp(_(newnode->showname), _(node->showname)) > 0)
 		{
 			prev = node;
 			node = node->next;
