4ada9dfc5510148f6675be2132a66ce7340917ce
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-09-20 00:23:31 +0000

    update internetradio genres

--- titan/tools/parser/internetradio.sh
+++ titan/tools/parser/internetradio.sh
@@ -80,12 +80,75 @@ if [ "$buildtype" == "full" ];then
 		for ROUND2 in $filename; do
 #			echo "$filename ($ROUND2)"
 			back=$ROUND2
-			ROUND2=`echo $ROUND2 | sed -e 's/.*00s.*/2000/' -e 's/.*100.*/100/' -e 's/.*alt.*/alternative/' -e 's/.*blue.*/blues/' -e 's/.*bala.*/balladen/' -e 's/.*ball.*/balladen/' -e 's/.*bas.*/bass/' -e 's/.*classi.*/classical/' -e 's/.*colo.*/colombia/' -e 's/.*count.*/country/' -e 's/.*dance.*/dance/' -e 's/.*latin.*/latin/' -e 's/.*liv.*/live/' -e 's/.*love.*/love/' -e 's/.*oldi.*/oldies/' -e 's/.*pop.*/pop/' -e 's/.*reg.*/reggae/' -e 's/.*rela.*/relaxing/' -e 's/.*rock.*/rock/' -e 's/.*soul.*/soul/' -e 's/.*sound.*/soundtracks/' -e 's/.*talk.*/talk/' -e 's/.*easy.*/easylistning/' -e 's/.*elect.*/electronic/' -e 's/.*folk.*/folk/' -e 's/.*inspi.*/inspirational/' -e 's/.*intern.*/international/' -e 's/.*jazz.*/jazz/' -e 's/.*metal.*/metal/' -e 's/.*misc.*/misc/' -e 's/.*new.*/newage/' -e 's/.*pop.*/pop/' -e 's/.*public.*/publicradio/' -e 's/.*urban.*/urban/' -e 's/.*rnb.*/rnb/' -e 's/.*rap.*/rap/' -e 's/.*holi.*/holiday/' -e 's/.*them.*/themes/' -e 's/.*deca.*/decades/'`
+			#-e 's/.*00s.*/2000/' -e 's/.*100.*/100/' 
+			
+			ROUND2=`echo $ROUND2 | sed -e 's/.*alter.*/Alternative/' -e 's/.*adult.*/Alternative-(Adult)/' -e 's/.*brit.*/Alternative-(Britpop)/' -e 's/.*classic.*/Alternative-(Classic)/' -e 's/.*coll.*/Alternative-(College)/' -e 's/.*dancepunk.*/Alternative-(Dancepunk)/' -e 's/.*dream.*/Alternative-(Dream-Pop)/' -e 's/.*emo.*/Alternative-(Emo)/' -e 's/.*goth.*/Alternative-(Goth)/' -e 's/.*grunge.*/Alternative-(Grunge)/' -e 's/.*hardcore.*/Alternative-(Hardcore)/' -e 's/.*indie.*/Alternative-(Indie-Pop)/' -e 's/.*industrial.*/Alternative-(Industrial)/' -e 's/.*lofi.*/Alternative-(LoFi)/' -e 's/.*modern.*/Alternative-(Modern-Rock)/' -e 's/.*wave.*/Alternative-(New-Wave)/' -e 's/.*noise.*/Alternative-(Noise-Pop)/' -e 's/.*post.*/Alternative-(Post-Punk)/' -e 's/.*power.*/Alternative-(Power-Pop)/' -e 's/.*punk.*/Alternative-(Punk)/' -e 's/.*ska.*/Alternative-(Ska)/' -e 's/.*xtreme.*/Alternative-(Xtreme)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*blues.*/Blues/' -e 's/.*acoustic.*/Blues-(Acoustic)/' -e 's/.*cajun.*/Blues-(Cajun-and-Zydeco)/' -e 's/.*chicago.*/Blues-(Chicago)/' -e 's/.*contemporary.*/Blues-(Contemporary)/' -e 's/.*country.*/Blues-(Country)/' -e 's/.*delta.*/Blues-(Delta)/' -e 's/.*electric.*/Blues-(Electric)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*classical.*/Classical/' -e 's/.*baroque.*/Classical-(Baroque)/' -e 's/.*chamber.*/Classical-(Chamber)/' -e 's/.*choral.*/Classical-(Choral)/' -e 's/.*period.*/Classical-(Period)/' -e 's/.*early.*/Classical-(Early)/' -e 's/.*impressionist.*/Classical-(Impressionist)/' -e 's/.*modern.*/Classical-(Modern)/' -e 's/.*opera.*/Classical-(Opera)/' -e 's/.*piano.*/Classical-(Piano)/' -e 's/.*romantic.*/Classical-(Romantic)/' -e 's/.*symphony.*/Classical-(Symphony)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*country.*/Country/' -e 's/.*country.*/Country-(Alt)/' -e 's/.*americana.*/Country-(Americana)/' -e 's/.*bluegrass.*/Country-(Bluegrass)/' -e 's/.*classic.*/Country-(Classic)/' -e 's/.*contemporary.*/Country-(Contemporary)/' -e 's/.*honky.*/Country-(Honky-Tonk)/' -e 's/.*hot.*/Country-(Hot-Country-Hits)/' -e 's/.*western.*/Country-(Western)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*listening.*/Easy-Listening/' -e 's/.*exotica.*/Easy-Listening-(Exotica)/' -e 's/.*light.*/Easy-Listening-(Light-Rock)/' -e 's/.*lounge.*/Easy-Listening-(Lounge)/' -e 's/.*orchestral.*/Easy-Listening-(Orchestral-Pop)/' -e 's/.*polka.*/Easy-Listening-(Polka)/' -e 's/.*space.*/Easy-Listening-(Space-Age-Pop)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*electronic.*/Electronic/' -e 's/.*acid.*/Electronic-(Acid-House)/' -e 's/.*ambient.*/Electronic-(Ambient)/'  -e 's/.*breakbeat.*/Electronic-(Big-Beat)/' -e 's/.*dance.*/Electronic-(Dance)/' -e 's/.*demo.*/Electronic-(Demo)/' -e 's/.*disco.*/Electronic-(Disco)/' -e 's/.*downtempo.*/Electronic-(Downtempo)/' -e 's/.*drum.*/Electronic-(Drum-and-Bass)/' -e 's/.*electro.*/Electronic-(Electro)/' -e 's/.*garage.*/Electronic-(Garage)/' -e 's/.*hard.*/Electronic-(Hard-House)/' -e 's/.*house.*/Electronic-(House)/' -e 's/.*idm.*/Electronic-(IDM)/' -e 's/.*jungle.*/Electronic-(Jungle)/' -e 's/.*jungle.*/Electronic-(Jungle)/' -e 's/.*progressive.*/Electronic-(Progressive)/' -e 's/.*techno.*/Electronic-(Techno)/' -e 's/.*trance.*/Electronic-(Trance)/' -e 's/.*tribal.*/Electronic-(Tribal)/' -e 's/.*trip.*/Electronic-(Trip-Hop)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*folk.*/Folk/' -e 's/.*alterna.*/Folk-(Alternative)/' -e 's/.*contemp.*/Folk-(Contemporary)/' -e 's/.*rock.*/Folk-(Rock)/' -e 's/.*acoustic.*/Folk-(New-Acoustic)/' -e 's/.*time.*/Folk-(Old-Time)/' -e 's/.*traditional.*/Folk-(Traditional)/' -e 's/.*world.*/Folk-(World)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*inspirational.*/Inspirational/' -e 's/.*christian.*/Inspirational-(Christian)/' -e 's/.*contemp.*/Inspirational-(Contemporary)/' -e 's/.*gospel.*/Inspirational-(Gospel)/' -e 's/.*praise.*/Inspirational-(Praise-and-Worship)/' -e 's/.*sermons.*/Inspirational-(Sermons-and-Services)/' -e 's/.*southern.*/Inspirational-(Southern-Gospel)/' -e 's/.*traditional.*/Inspirational-(Traditional-Gospel)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*international.*/International/' -e 's/.*european.*/International-(European)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*jazz.*/Jazz/' -e 's/.*avant.*/Jazz-(Avant)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*latin.*/Latin/' -e 's/.*bachata.*/Latin-(Bachata)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*metal.*/Metal/' -e 's/.*black.*/Metal-(Black)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*misc.*/Misc/' -e 's/.*age.*/Misc-(New-Age)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*Pop.*/Pop/' -e 's/.*oldies.*/Pop-(Oldies)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*public.*/Public-Radio/' -e 's/.*sports.*/Public-Radio-(Sports)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*Rap.*/Rap/' -e 's/.*Hip.*/Rap-(Hip-Hop)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*reggae.*/Reggae/' -e 's/.*dancehall.*/Reggae-(Ragga)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*rock.*/Rock/' -e 's/.*glam.*/Rock-(Glam)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*soundtracks.*/Soundtracks/' -e 's/.*original.*/Soundtracks-(Original)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*talk.*/Talk/' -e 's/.*political.*/Talk-(Political)/'`
+			ROUND2=`echo $ROUND2 | sed -e 's/.*themes.*/Themes/' -e 's/.*best.*/Themes-(Best)/' -e 's/.*party.*/Themes-(Party)/' -e 's/.*chill.*/Themes-(Chill)/'`
+
 			if [ "$back" != "$ROUND2" ];then
 				echo "genre: $back > $ROUND2"
 			fi
 
-			if [ "$ROUND2" = "alternative" ] || [ "$ROUND2" = "blues" ] || [ "$ROUND2" = "balladen" ] || [ "$ROUND2" = "bass" ] || [ "$ROUND2" = "classical" ] || [ "$ROUND2" = "colombia" ] || [ "$ROUND2" = "country" ] || [ "$ROUND2" = "dance" ] || [ "$ROUND2" = "easylistning" ] || [ "$ROUND2" = "electronic" ] || [ "$ROUND2" = "folk" ] || [ "$ROUND2" = "inspirational" ] || [ "$ROUND2" = "international" ] || [ "$ROUND2" = "jazz" ] || [ "$ROUND2" = "metal" ] || [ "$ROUND2" = "misc" ] || [ "$ROUND2" = "newage" ] || [ "$ROUND2" = "pop" ] || [ "$ROUND2" = "latin" ] || [ "$ROUND2" = "live" ] || [ "$ROUND2" = "love" ] || [ "$ROUND2" = "misc" ] || [ "$ROUND2" = "oldies" ] || [ "$ROUND2" = "publicradio" ] || [ "$ROUND2" = "urban" ] || [ "$ROUND2" = "rnb" ] || [ "$ROUND2" = "rap" ] || [ "$ROUND2" = "reggae" ] || [ "$ROUND2" = "rock" ] || [ "$ROUND2" = "relax" ] || [ "$ROUND2" = "holiday" ] || [ "$ROUND2" = "soundtracks" ] || [ "$ROUND2" = "talk" ] || [ "$ROUND2" = "themes" ] || [ "$ROUND2" = "decades" ] || [ "$ROUND2" = "metal" ] || [ "$ROUND2" = "jazz" ]; then
+			if [ "$ROUND2" = "Alternative" ] || [ "$ROUND2" = "Alternative-(Adult)" ] || [ "$ROUND2" = "Alternative-(Britpop)" ] || [ "$ROUND2" = "Alternative-(Classic)" ] || [ "$ROUND2" = "Alternative-(College)" ] || [ "$ROUND2" = "Alternative-(Dancepunk)" ] || [ "$ROUND2" = "Alternative-(Dream-Pop)" ] || [ "$ROUND2" = "Alternative-(Emo)" ] || [ "$ROUND2" = "Alternative-(Goth)" ] || [ "$ROUND2" = "Alternative-(Grunge)" ] || [ "$ROUND2" = "Alternative-(Hardcore)" ] || [ "$ROUND2" = "Alternative-(Indie-Pop)" ] || [ "$ROUND2" = "Alternative-(Industrial)" ] || [ "$ROUND2" = "Alternative-(LoFi)" ] || [ "$ROUND2" = "Alternative-(Modern-Rock)" ] || [ "$ROUND2" = "Alternative-(New-Wave)" ] || [ "$ROUND2" = "Alternative-(Noise-Pop)" ] || [ "$ROUND2" = "Alternative-(Post-Punk)" ] || [ "$ROUND2" = "Alternative-(Power-Pop)" ] || [ "$ROUND2" = "Alternative-(Punk)" ] || [ "$ROUND2" = "Alternative-(Ska)" ] || [ "$ROUND2" = "Alternative-(Xtreme)" ];then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Blues" ] || [ "$ROUND2" = "Blues-(Acoustic)" ] || [ "$ROUND2" = "Blues-(Cajun-and-Zydeco)" ] || [ "$ROUND2" = "Blues-(Chicago)" ] || [ "$ROUND2" = "Blues-(Contemporary)" ] || [ "$ROUND2" = "Blues-(Country)" ] || [ "$ROUND2" = "Blues-(Delta)" ] || [ "$ROUND2" = "Blues-(Electric)" ];then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Classical" ] || [ "$ROUND2" = "Classical-(Baroque)" ] || [ "$ROUND2" = "Classical-(Chamber)" ] || [ "$ROUND2" = "Classical-(Choral)" ] || [ "$ROUND2" = "Classical-(Period)" ] || [ "$ROUND2" = "Classical-(Early)" ] || [ "$ROUND2" = "Classical-(Impressionist)" ] || [ "$ROUND2" = "Classical-(Modern)" ] || [ "$ROUND2" = "Classical-(Opera)" ] || [ "$ROUND2" = "Classical-(Piano)" ] || [ "$ROUND2" = "Classical-(Romantic)" ] || [ "$ROUND2" = "Classical-(Symphony)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Country" ] || [ "$ROUND2" = "Country-(Alt)" ] || [ "$ROUND2" = "Country-(Americana)" ] || [ "$ROUND2" = "Country-(Bluegrass)" ] || [ "$ROUND2" = "Country-(Classic)" ] || [ "$ROUND2" = "Country-(Contemporary)" ] || [ "$ROUND2" = "Country-(Honky-Tonk)" ] || [ "$ROUND2" = "Country-(Hot-Country-Hits)" ] || [ "$ROUND2" = "Country-(Western)" ];then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Easy-Listening" ] || [ "$ROUND2" = "Easy-Listening-(Exotica)" ] || [ "$ROUND2" = "Easy-Listening-(Light-Rock)" ] || [ "$ROUND2" = "Easy-Listening-(Lounge)" ] || [ "$ROUND2" = "Easy-Listening-(Orchestral-Pop)" ] || [ "$ROUND2" = "Easy-Listening-(Polka)" ] || [ "$ROUND2" = "Easy-Listening-(Space-Age-Pop)" ];then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Electronic" ] || [ "$ROUND2" = "Electronic-(Acid-House)" ] || [ "$ROUND2" = "Electronic-(Ambient)" ] || [ "$ROUND2" = "Electronic-(Big-Beat)" ] || [ "$ROUND2" = "Electronic-(Dance)" ] || [ "$ROUND2" = "Electronic-(Demo)" ] || [ "$ROUND2" = "Electronic-(Disco)" ] || [ "$ROUND2" = "Electronic-(Downtempo)" ] || [ "$ROUND2" = "Electronic-(Drum-and-Bass)" ] || [ "$ROUND2" = "Electronic-(Electro)" ] || [ "$ROUND2" = "Electronic-(Garage)" ] || [ "$ROUND2" = "Electronic-(Hard-House)" ] || [ "$ROUND2" = "Electronic-(House)" ] || [ "$ROUND2" = "Electronic-(IDM)" ] || [ "$ROUND2" = "Electronic-(Jungle)" ] || [ "$ROUND2" = "Electronic-(Progressive)" ] || [ "$ROUND2" = "Electronic-(Techno)" ] || [ "$ROUND2" = "Electronic-(Trance)" ] || [ "$ROUND2" = "Electronic-(Tribal)" ] || [ "$ROUND2" = "Electronic-(Trip-Hop)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Folk" ] || [ "$ROUND2" = "Folk-(Alternative)" ] || [ "$ROUND2" = "Folk-(Contemporary)" ] || [ "$ROUND2" = "Folk-(Rock)" ] || [ "$ROUND2" = "Folk-(New-Acoustic)" ] || [ "$ROUND2" = "Folk-(Old-Time)" ] || [ "$ROUND2" = "Folk-(Alternative)" ] || [ "$ROUND2" = "Folk-(Traditional)" ] || [ "$ROUND2" = "Folk-(World)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Inspirational" ] || [ "$ROUND2" = "Inspirational-(Christian)" ] || [ "$ROUND2" = "Inspirational-(Contemporary)" ] || [ "$ROUND2" = "Inspirational-(Gospel)" ] || [ "$ROUND2" = "Inspirational-(Praise-and-Worship)" ] || [ "$ROUND2" = "Inspirational-(Sermons-and-Services)" ] || [ "$ROUND2" = "Inspirational-(Southern-Gospel)" ] || [ "$ROUND2" = "Inspirational-(Traditional-Gospel)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "International" ] || [ "$ROUND2" = "International-(European)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Jazz" ] || [ "$ROUND2" = "Jazz-(Avant)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Latin" ] || [ "$ROUND2" = "Latin-(Bachata)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Metal" ] || [ "$ROUND2" = "Metal-(Black)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Misc" ] || [ "$ROUND2" = "Misc-(New-Age)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Pop" ] || [ "$ROUND2" = "Pop-(Oldies)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Public-Radio" ] || [ "$ROUND2" = "Public-Radio-(Sports)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Rap" ] || [ "$ROUND2" = "Rap-(Hip-Hop)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Reggae" ] || [ "$ROUND2" = "Reggae-(Ragga)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Rock" ] || [ "$ROUND2" = "Rock-(Glam)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Soundtracks" ] || [ "$ROUND2" = "Soundtracks-(Glam)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Talk" ] || [ "$ROUND2" = "Talk-(Political)" ]; then
+				GENRE="$ROUND2"
+			elif [ "$ROUND2" = "Themes" ] || [ "$ROUND2" = "Themes-(Best)" ] || [ "$ROUND2" = "Themes-(Party)" ] || [ "$ROUND2" = "Themes-(Chill)" ]; then
 				GENRE="$ROUND2"
 			else
 				GENRE="unknown"
