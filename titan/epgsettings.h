0f2475c6bcd3186ead1fcf5d9698f7939262fad5
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-09-20 14:19:11 +0000

    [titan] fix

--- titan/epgsettings.h
+++ titan/epgsettings.h
@@ -249,12 +249,15 @@ void screenepgsettings()
 				epgscandeltimer();
 			else
 				epgscancreatetimer();
-				
-			if(getconfig("epg_listmode", NULL) == 1 || getconfig("epg_listmode", NULL) == 3)
-			{
-				textbox(_("Message"), _("Deavtivate Refresh Time and After EPG in scan-mode"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 15, 0);
-				delconfig("epg_refreshtime");
-				delconfig("epg_afterevent");		
+			
+			if(getconfig("epg_refreshtime", NULL) != NULL || getconfigint("epg_afterevent", NULL) != 0)
+			{   	
+				if(getconfigint("epg_listmode", NULL) == 1 || getconfigint("epg_listmode", NULL) == 3)
+				{
+					textbox(_("Message"), _("Deavtivate Refresh Time and After EPG in scan-mode"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 15, 0);
+					delconfig("epg_refreshtime");
+					delconfig("epg_afterevent");		
+				}
 			}
 
 			break;
