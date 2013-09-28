dac01c4bbee69b28eacc2a7e7f1ddda069002a8a
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-09-28 08:10:16 +0000

    [titan] extend epgsearch with genre search and only aktual epg search

--- titan/titan/epgsearch.h
+++ titan/titan/epgsearch.h
@@ -1,14 +1,16 @@
 #ifndef EPGSEARCH_H
 #define EPGSEARCH_H
 
-int searchepg(char* search, int searchtype, struct skin* epgsearch, struct skin* listbox)
+int searchepg(char* search, char* search1, char* search2, int searchtype, struct skin* epgsearch, struct skin* listbox)
 {
+	int allepg = 1, count = 0;
 	char* result = NULL, *epgdesc = NULL, *tmpstr = NULL;
 	char* buf = NULL;
 	struct channel* chnode = channel;
 	struct epg* epgnode = NULL;
 	struct skin* tmp = NULL;
 	struct tm *loctime = NULL;
+	time_t akttime = time(NULL) + 10800;
   
 	if(search == NULL || strlen(search) < 2) return 1;
 
@@ -21,19 +23,32 @@ int searchepg(char* search, int searchtype, struct skin* epgsearch, struct skin*
 		err("no mem");
 		return 1;
 	}
+	
+	if(textbox(_("Message"), _("Search only aktual epg ?"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 0, 0) == 1)
+		allepg = 0;
 
 	while(chnode != NULL)
 	{
 		epgnode = getepgakt(chnode);
 		while(epgnode != NULL)
 		{
+			if(allepg == 0 && epgnode->starttime > akttime) break;
+		
 			result = NULL;
-			if(searchtype == 0 || searchtype == 2)
+			count = 0;
+			
+start:
+			if(searchtype == 0 || searchtype == 2 || searchtype == 3)
 			{
 				if(epgnode->title != NULL)
 					result = ostrstrcase(epgnode->title, search);
 			}
-			if((searchtype == 1 || searchtype == 2) && result == NULL)
+			if(searchtype == 3 && result == NULL)
+			{
+				if(epgnode->subtitle != NULL)
+					result = ostrstrcase(epgnode->subtitle, search);
+			}
+			if((searchtype == 1 || searchtype == 2 || searchtype == 3) && result == NULL)
 			{
 				epgdesc = epgdescunzip(epgnode);
 				if(epgdesc != NULL)
@@ -42,6 +57,21 @@ int searchepg(char* search, int searchtype, struct skin* epgsearch, struct skin*
 					free(epgdesc); epgdesc = NULL;
 				}
 			}
+			
+			if(result == NULL && count == 0 && search1 != NULL && ostrcmp(search, search1) != 0)
+			{
+				count = 1;
+				search = search1;
+				goto start;
+			}
+			
+			if(result == NULL && count == 1 && search2 != NULL)
+			{
+				count = 2;
+				search = search2;
+				goto start;
+			}
+			
 			if(result != NULL)
 			{
 				tmp = addlistbox(epgsearch, listbox, tmp, 1);
@@ -84,15 +114,16 @@ int searchepg(char* search, int searchtype, struct skin* epgsearch, struct skin*
 
 void screenepgsearch(char* text)
 {
-	int rcret = -1, ret = 0;
+	int rcret = -1, ret = 0, genre = 0;
 	struct skin* epgsearch = getscreen("epgsearch");
 	struct skin* listbox = getscreennode(epgsearch, "listbox");
 	char* search = NULL;
+	struct skin* tmp = NULL;
 
 	if(text != NULL)
 	{
 		delmarkedscreennodes(epgsearch, 1);
-		searchepg(text, 0, epgsearch, listbox);
+		searchepg(text, NULL, NULL, 0, epgsearch, listbox);
 	}
 
 	listbox->aktline = 1;
@@ -110,14 +141,21 @@ void screenepgsearch(char* text)
 		{
 			if(listbox->select != NULL)
 			{
-				servicecheckret(servicestart((struct channel*)listbox->select->handle, NULL, NULL, 0), 0);
+				if(genre == 0)
+					servicecheckret(servicestart((struct channel*)listbox->select->handle, NULL, NULL, 0), 0);
+				else
+				{
+					genre = 0;
+				  delmarkedscreennodes(epgsearch, 1);
+				  searchepg(listbox->select->text, listbox->select->handle, listbox->select->handle1, 3, epgsearch, listbox);
+				}
 			}
 			break;
 		}
     
 		if(rcret == getrcconfigint("rcred", NULL))
 		{
-			if(listbox->select != NULL)
+			if(listbox->select != NULL && genre == 0)
 			{
 				ret = addrecepg((struct channel*)listbox->select->handle, (struct epg*)listbox->select->handle1, NULL);
 				drawscreen(epgsearch, 0, 0);
@@ -130,8 +168,9 @@ void screenepgsearch(char* text)
 			search = textinput("Search", NULL);
 			if(search != NULL)
 			{
+				genre = 0;
 				delmarkedscreennodes(epgsearch, 1);
-				searchepg(search, 0, epgsearch, listbox);
+				searchepg(search, NULL, NULL, 0, epgsearch, listbox);
 				free(search); search = NULL;
 			}
 			drawscreen(epgsearch, 0, 0);
@@ -143,8 +182,9 @@ void screenepgsearch(char* text)
 			search = textinput("Search", NULL);
 			if(search != NULL)
 			{
+				genre = 0;
 				delmarkedscreennodes(epgsearch, 1);
-				searchepg(search, 1, epgsearch, listbox);
+				searchepg(search, NULL, NULL, 1, epgsearch, listbox);
 				free(search); search = NULL;
 			}
 			drawscreen(epgsearch, 0, 0);
@@ -156,10 +196,140 @@ void screenepgsearch(char* text)
 			search = textinput("Search", NULL);
 			if(search != NULL)
 			{
+				genre = 0;
 				delmarkedscreennodes(epgsearch, 1);
-				searchepg(search, 2, epgsearch, listbox);
+				searchepg(search, NULL, NULL, 2, epgsearch, listbox);
 				free(search); search = NULL;
 			}
+			drawscreen(epgsearch, 0, 0);
+			continue;
+		}
+		
+		if(rcret == getrcconfigint("rcmenu", NULL))
+		{
+			genre = 1;
+			delmarkedscreennodes(epgsearch, 1);
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Action"));
+				tmp->handle = "Action";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{ 
+				changetext(tmp, _("Animation"));
+				tmp->handle = "Animation";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("History"));
+				tmp->handle = "History";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Doku"));
+				tmp->handle = "Doku";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Thriller"));
+				tmp->handle = "Thriller";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Child"));
+				tmp->handle = "Child";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Fantasy"));
+				tmp->handle = "Fantasy";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Horror"));
+				tmp->handle = "Horror";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Comedy"));
+				tmp->handle = "Comedy";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("War"));
+				tmp->handle = "War";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Scifi"));
+				tmp->handle = "Scifi";
+				tmp->handle1 = "Fiction";
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Sport"));
+				tmp->handle = "Sport";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Western"));
+				tmp->handle = "Western";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Eastern"));
+				tmp->handle = "Eastern";
+				tmp->handle1 = NULL;
+			}
+			
+			tmp = addlistbox(epgsearch, listbox, tmp, 1);
+			if(tmp != NULL)
+			{
+				changetext(tmp, _("Music"));
+				tmp->handle = "Music";
+				tmp->handle1 = NULL;
+			}
+			
 			drawscreen(epgsearch, 0, 0);
 			continue;
 		}
