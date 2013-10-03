a4ab07ba7c7d1f01403a0470786eb37d51bc6d0b
nit <nit@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-10-03 20:05:06 +0000

    [titan] fix build

--- titan/titan/skinadjust.h
+++ titan/titan/skinadjust.h
@@ -16,7 +16,6 @@ void screenskinadjust()
 	struct skin* showrecfreesize = getscreennode(skinadjust, "showrecfreesize");
 	struct skin* listboxselect = getscreennode(skinadjust, "listboxselect");
 	struct skin* tmp = NULL;
-	char* ret = NULL;
 
 	changeinput(fontsizeadjust, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n-15\n-14\n-13\n-12\n-11\n-10\n-9\n-8\n-7\n-6\n-5\n-4\n-3\n-2\n-1");
 	setchoiceboxselection(fontsizeadjust, getskinconfig("fontsizeadjust", NULL));
