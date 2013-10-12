1e755bd8da4af6752e6c5a6fc001055b8fb95196
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-10-12 23:07:23 +0000

    [tithek] fix flashx hoster

--- plugins/tithek/flashx.h
+++ plugins/tithek/flashx.h
@@ -1,88 +1,220 @@
 #ifndef FLASHX_H
 #define FLASHX_H
 
-char* flashx(char* host, char* file)
+char* flashx(char* host, char* file, char* hosterurl)
 {
-	debug(99, "in host: %s file: %s", host, file);
-	int debuglevel = getconfigint("debuglevel", NULL);
+	debug(99, "host: %s file: %s", host, file);
+	debug(99, "hosterurl: %s", hosterurl);
 	char* tmphost = NULL;
-	char* tmpfile = NULL;
+	char* tmppath = NULL;
 	char* tmpstr = NULL;
 	char* send = NULL;
-	char* ip = NULL;
 	char* streamlink = NULL;
-
-	unlink("/tmp/tithek/x9");
+	char* ip = NULL, *pos = NULL, *path = NULL;
 
 	if(host == NULL || file == NULL) return NULL;
 
-	tmphost = ostrcat("www.", host, 0, 0);
-	tmpfile = ostrcat("/file/", file, 0, 0);
-	debug(99, "tmphost: %s", tmphost);
-	ip = get_ip(tmphost);
-	debug(99, "ip: %s", ip);
-	debug(99, "tmpfile: %s", tmpfile);
-	debug(99, "file: %s", file);
+	ip = string_replace("http://", "", (char*)hosterurl, 0);
 
-	send = ostrcat(send, "GET /player/embed_player.php?vid=", 1, 0);
-	send = ostrcat(send, file, 1, 0);
-	send = ostrcat(send, " HTTP/1.1\r\nHost: www.", 1, 0);	
-	send = ostrcat(send, host, 1, 0);
-	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
-	debug(99, "tmphost: %s", tmphost);
-	debug(99, "tmpfile: %s", tmpfile);
-	debug(99, "send: %s", send);
-	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
-
-	titheklog(debuglevel, "/tmp/flashx1_tmpstr_get1", NULL, tmpstr);
-
-	free(tmpstr); tmpstr = NULL;
-	free(send); send = NULL;
-	sleep(1);
-
-	send = ostrcat(send, "GET /player/embed_player.php?vid=", 1, 0);
-	send = ostrcat(send, file, 1, 0);
-	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);	
-	send = ostrcat(send, host, 1, 0);
-	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
-	debug(99, "tmphost: %s", tmphost);
-	debug(99, "tmpfile: %s", tmpfile);
-	debug(99, "send: %s", send);
-	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
-
-	titheklog(debuglevel, "/tmp/flashx2_tmpstr_get2", NULL, tmpstr);
-
-	free(tmpstr); tmpstr = NULL;
-	free(send); send = NULL;
-	sleep(1);
-
-	send = ostrcat(send, "GET /nuevo/player/cst.php?hash=", 1, 0);
-	send = ostrcat(send, file, 1, 0);
-	send = ostrcat(send, " HTTP/1.1\r\nHost: play.flashx.tv\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
-	debug(99, "tmphost: %s", tmphost);
-	debug(99, "tmpfile: %s", tmpfile);
-	debug(99, "send: %s", send);
-	gethttpreal(tmphost, tmpfile, 80, "/tmp/tithek/x9", NULL, NULL, 0, send, NULL, 5000, 0);
-
-	if(!file_exist("/tmp/tithek/x9"))
+	if(ip != NULL)
+		pos = strchr(ip, '/');
+	if(pos != NULL)
 	{
-		textbox(_("Message"), _("This file doesn't exist, or has been removed") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
-		goto end;
+		pos[0] = '\0';
+		path = pos + 1;
 	}
+	
+	tmphost = ostrcat(ip, NULL, 0, 0);
+	tmppath = ostrcat("/", path, 0, 0);
+	
+//printf("\n#######################################################\n");
+//printf("GET /video/UX98AKXU2ARB/RiddickUeberlebenistseineRacheTSLDGermanX264-AOE\n");
+//printf("tmphost: %s\n",tmphost);
+//printf("tmppath: %s\n",tmppath);
+//printf("\n#######################################################\n");
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmppath, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);	
+	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
+	debug(99, "send: %s", send);
+	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+	if(getconfigint("debuglevel", NULL) == 99)
+		writesys("/tmp/flashx_tmpstr_get1", tmpstr, 0);
+//	writesys("/var/usr/local/share/titan/plugins/tithek//flashx_tmpstr_get1", tmpstr, 0);
+//printf("\n#######################################################\n");
 
-	if(debuglevel == 99) system("cp -a /tmp/tithek/x9 /tmp/flashx3_tmpstr_get3");
+	char* cookie1 = string_resub("Set-Cookie: pageredir=", ";", tmpstr, 0);
+	debug(99, "cookie1: %s", cookie1);
 
+	char* cookie2 = string_resub("Set-Cookie: PHPSESSID=", ";", tmpstr, 0);
+	debug(99, "cookie2: %s", cookie2);
+
+	char* cookie3 = string_resub("Set-Cookie: video_", "=", tmpstr, 0);
+	debug(99, "cookie3: %s", cookie3);
+	
+	free(tmppath), tmppath = NULL;
+	tmppath = ostrcat("/player/embed.php?vid=", cookie3, 0, 0);
+	free(tmphost), tmphost = NULL;
+	char* tmphost1 = oregex(".*<iframe width=.*src=\"(http://.*)&width.*", tmpstr);
+	tmphost = string_resub("http://", "/", tmphost1, 0);
+	free(tmphost1), tmphost1 = NULL;
+	free(tmpstr); tmpstr = NULL;
 	free(send); send = NULL;
-	tmpstr = command("cat /tmp/tithek/x9 | sed '1,1d' | zcat");
+//	sleep(1);
+//printf("\n#######################################################\n");
+//printf("GET /player/embed.php?vid=1008580\n");
+//printf("tmphost: %s\n",tmphost);
+//printf("tmppath: %s\n",tmppath);
+//printf("\n#######################################################\n");
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmppath, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);	
+	send = ostrcat(send, tmphost, 1, 0);	
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\n", 1, 0);
 
-	titheklog(debuglevel, "/tmp/flashx4_tmpstr_get3_zcat", NULL, tmpstr);
+	send = ostrcat(send, "Cookie: pageredir=", 1, 0);	
+	send = ostrcat(send, cookie1, 1, 0);
+	send = ostrcat(send, "; PHPSESSID=", 1, 0);	
+	send = ostrcat(send, cookie2, 1, 0);	
+	send = ostrcat(send, "\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
+	debug(99, "send: %s", send);
+	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+
+	if(getconfigint("debuglevel", NULL) == 99)
+		writesys("/tmp/flashx_tmpstr_get2", tmpstr, 0);
+//	writesys("/var/usr/local/share/titan/plugins/tithek//flashx_tmpstr_get2", tmpstr, 0);
+//printf("\n#######################################################\n");
+
+	char* hash1tmp = string_resub("<input type=\"hidden\" name=\"sec\" value=\"", "\"", tmpstr, 0);
+	char* hash2tmp = string_resub("<input type=\"hidden\" name=\"id\" value=\"", "\"", tmpstr, 0);
+	
+	char* hash1 = htmlencode(hash1tmp);
+	char* hash2 = htmlencode(hash2tmp);
+	
+	// htmldecod cant / to %2f
+//	hash1 = string_replace_all("/", "%2F", hash1, 1);
+//	hash2 = string_replace_all("/", "%2F", hash2, 1);
+
+	char* hash = ostrcat("sec=", hash1, 0, 0);
+	hash = ostrcat(hash, "&id=", 0, 0);
+	hash = ostrcat(hash, hash2, 0, 0);
+	char* hashlen = NULL;
+	hashlen = oitoa(strlen(hash));
+	debug(99, "hashlen: %s", hashlen);
+
+	free(tmpstr); tmpstr = NULL;
+	free(send); send = NULL;
+//	sleep(1);
+
+//printf("\n#######################################################\n");
+//printf("POST /player/show.php\n");
+//printf("\n#######################################################\n");	
+
+	send = ostrcat(send, "POST /player/show.php HTTP/1.1\r\nContent-Length: ", 1, 0);
+	send = ostrcat(send, hashlen, 1, 0);
+	send = ostrcat(send, "\r\nAccept-Encoding: gzip\r\nConnection: close\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nHost: ", 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "\r\nCookie: pageredir=", 1, 0);
+	send = ostrcat(send, cookie1, 1, 0);
+	send = ostrcat(send, "; PHPSESSID=", 1, 0);	
+	send = ostrcat(send, cookie2, 1, 0);
+	send = ostrcat(send, "\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n", 1, 0);	
+	send = ostrcat(send, hash, 1, 0);
+
+	debug(99, "send: %s", send);
+	tmpstr = gethttpreal(tmphost, "/player/show.php", 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+
+	if(getconfigint("debuglevel", NULL) == 99)
+		writesys("/tmp/flashx_tmpstr_get3", tmpstr, 0);
+//	writesys("/var/usr/local/share/titan/plugins/tithek//flashx_tmpstr_get3", tmpstr, 0);
+//printf("\n#######################################################\n");
+//var code ='<object id="nuevoplayer" width="'+ww+'" height="'+hh+'" data="http://play.flashx.tv/nuevo/player/player.swf?config=http://play.flashx.tv/nuevo/player/play.php?str=4MfrzrW4qrPKnM2dw5/Fvcs=" type="application/x-shockwave-flash">';
+	char* playurl = string_resub("?config=", "\"", tmpstr, 0);
+	free(tmppath), tmppath = NULL;
+	tmppath = ostrcat("/nuevo/player/player.swf?config=", playurl, 0, 0);
+//	free(playurl), playurl = NULL;
+
+	free(tmpstr); tmpstr = NULL;
+	free(send); send = NULL;
+//	sleep(1);
+
+//printf("\n#######################################################\n");
+//printf("GET /nuevo/player/player.swf?config=http://play.flashx.tv/nuevo/player/play.php?str=4MfrzrW70J.wp8O.zZi5uLo=\n");
+//printf("tmphost: %s\n",tmphost);
+//printf("tmppath: %s\n",tmppath);
+//printf("\n#######################################################\n");	
+
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmppath, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);	
+	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\n", 1, 0);
+	send = ostrcat(send, "Cookie: pageredir=", 1, 0);	
+	send = ostrcat(send, cookie1, 1, 0);
+	send = ostrcat(send, "; PHPSESSID=", 1, 0);	
+	send = ostrcat(send, cookie2, 1, 0);	
+	send = ostrcat(send, "\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
+
+
+	debug(99, "send: %s", send);
+	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+
+	if(getconfigint("debuglevel", NULL) == 99)
+		writesys("/tmp/flashx_tmpstr_get4", tmpstr, 0);
+//	writesys("/var/usr/local/share/titan/plugins/tithek//flashx_tmpstr_get4", tmpstr, 0);
+//printf("\n#######################################################\n");
+
+
+
+//printf("tmppath: %s\n",tmppath);
+//printf("playurl: %s\n",playurl);
+	free(tmppath), tmppath = NULL;
+//	tmppath = ostrcat("/nuevo/player/play.php?str=4MfrzrW4qrPKnM2dw5/Fvcs=", NULL, 0, 0);
+//	tmppath = string_resub("/play.php?", "\0", tmpstr, 0);
+	char* tmpstr2 = ostrcat("http://", tmphost, 0, 0);
+	tmppath = string_replace(tmpstr2, "", playurl, 0);
+
+	free(tmpstr2); tmpstr2 = NULL;		
+	free(playurl), playurl = NULL;
+	free(tmpstr); tmpstr = NULL;
+	free(send); send = NULL;
+//	sleep(1);
+	
+//printf("\n#######################################################\n");
+//printf("GET /nuevo/player/play.php?str=4MfrzrW70J.wp8O.zZi5uLo=\n");
+//printf("tmphost: %s\n",tmphost);
+//printf("tmppath: %s\n",tmppath);
+
+//printf("\n#######################################################\n");	
+
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmppath, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);	
+	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\n", 1, 0);
+	send = ostrcat(send, "Cookie: pageredir=", 1, 0);	
+	send = ostrcat(send, cookie1, 1, 0);
+	send = ostrcat(send, "; PHPSESSID=", 1, 0);	
+	send = ostrcat(send, cookie2, 1, 0);	
+	send = ostrcat(send, "\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
+
+
+	debug(99, "send: %s", send);
+	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+
+	if(getconfigint("debuglevel", NULL) == 99)
+		writesys("/tmp/flashx_tmpstr_get5", tmpstr, 0);
+//	writesys("/var/usr/local/share/titan/plugins/tithek//flashx_tmpstr_get5", tmpstr, 0);
+//printf("\n#######################################################\n");
 
 	streamlink = string_resub("<file>", "</file>", tmpstr, 0);
-	titheklog(debuglevel, "/tmp/flashx5_streamlink", NULL, streamlink);
+	if(getconfigint("debuglevel", NULL) == 99)
+		writesys("/tmp/flashx5_streamlink", streamlink, 0);
 
-end:
+//end:
 	free(tmphost); tmphost = NULL;
-	free(tmpfile); tmpfile = NULL;
+	free(tmphost); tmphost = NULL;
 	free(tmpstr); tmpstr = NULL;
 	free(send); send = NULL;
 	free(ip); ip = NULL;
