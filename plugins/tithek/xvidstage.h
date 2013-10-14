b26f7674a729bd00e9572f85307e9b9374ad1e3f
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-10-14 19:47:04 +0000

    fix

--- plugins/tithek/xvidstage.h
+++ plugins/tithek/xvidstage.h
@@ -32,80 +32,51 @@ char* xvidstage(char* host, char* file, char* hosterurl)
 	char* tmpstr3 = NULL;
 	char* charlist = NULL;
 
-	unlink("/tmp/tithek/get");
-	unlink("/tmp/tithek/post");
+	unlink("/tmp/xvidstage1_get");
+	unlink("/tmp/xvidstage2_post");
 
 	if(host == NULL || file == NULL) return NULL;
 
-	tmphost = ostrcat("www.", host, 0, 0);
-	tmpfile = ostrcat("/file/", file, 0, 0);
+//	tmphost = ostrcat("www.", host, 0, 0);
+	tmphost = ostrcat(host, NULL, 0, 0);
+	tmpfile = ostrcat("/", file, 0, 0);
 	debug(99, "tmphost: %s", tmphost);
 	ip = get_ip(tmphost);
-	debug(99, "ip: %s", ip);
-	debug(99, "tmpfile: %s", tmpfile);
-	debug(99, "file: %s", file);
-
-	send = ostrcat(send, "GET /", 1, 0);
-	send = ostrcat(send, file, 1, 0);
-	send = ostrcat(send, " HTTP/1.1\r\nHost: www.", 1, 0);	
-	send = ostrcat(send, host, 1, 0);
-	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
 	debug(99, "tmphost: %s", tmphost);
 	debug(99, "tmpfile: %s", tmpfile);
-	debug(99, "send: %s", send);
+	debug(99, "ip: %s", ip);
 
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmpfile, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);	
+	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);	
+	debug(99, "send: %s", send);
+	
 	char* line = NULL;
 	char* lines = NULL;
 	char* cmd = NULL;
-	gethttpreal(tmphost, tmpfile, 80, "/tmp/tithek/get", NULL, NULL, 0, send, NULL, 5000, 1);
-	sleep(1);
+
+	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+	debug(99, "tmpstr: %s", tmpstr);
+	titheklog(debuglevel, "/tmp/xvidstage1_get", NULL, tmpstr);
 	free(send); send = NULL;
-	if(!file_exist("/tmp/tithek/get"))
+
+	sleep(1);
+	if(tmpstr == NULL)
 	{
 		textbox(_("Message"), _("This file doesn't exist, or has been removed") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
 		goto end;
 	}
 
-	if(debuglevel == 99) system("cp -a /tmp/tithek/get /tmp/xvidstage1_tmpstr_get");
-
-	tmpstr = command("cat /tmp/tithek/get");
 	cookie = string_resub("Set-Cookie: afc=", ";", tmpstr, 0);	
 	debug(99, "cookie: %s", cookie);
-	
-	titheklog(debuglevel, "/tmp/xvidstage2_tmpstr", NULL, tmpstr);
-
-	free(tmpstr), tmpstr = NULL;
-
-	line = string_newline(command("cat /tmp/tithek/get | grep 'Set-Cookie:' -n | tail -n1| cut -d':' -f1"));
-	cmd = ostrcat(cmd, "cat /tmp/tithek/get | wc -l", 1, 0);
-	lines = string_newline(command(cmd));
-	free(cmd); cmd = NULL;
-	debug(99, "line: %s", line);
-	debug(99, "lines: %s", lines);
-
-	if(lines == NULL || line == NULL) goto end;	
-	
-	int sline = atoi(lines) - atoi(line) - 4;
-	debug(99, "sline: %d", sline);
-	debug(99, "sline: %s", oitoa(sline));
-
-	cmd = ostrcat(cmd, "cat /tmp/tithek/get | tail -n", 1, 0);
-	cmd = ostrcat(cmd, oitoa(sline), 1, 0);
-	cmd = ostrcat(cmd, " | sed '1,1d' | zcat", 1, 0);
-	debug(99, "cmd: %s", cmd);	
-	tmpstr = command(cmd);
-	free(cmd); cmd = NULL;
-	free(lines); lines = NULL;
-
-	titheklog(debuglevel, "/tmp/xvidstage3_tmpstr_zcat", NULL, tmpstr);
-
-	sleep(1);
 
 	//get hash from tmpstr
 	char* pos1 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"fname\" value=");
 	fname = getxmlentry(pos1, "value=");
 	debug(99, "fname: %s", fname);
-//	if(fname == NULL) goto end;
+	if(fname == NULL) goto end;
 
 	char* pos2 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"id\" value=");
 	id = getxmlentry(pos2, "value=");
@@ -114,21 +85,25 @@ char* xvidstage(char* host, char* file, char* hosterurl)
 
 	char* pos3 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"op\" value=");
 	op = getxmlentry(pos3, "value=");
+	debug(99, "op: %s", op);
 
 	char* pos4 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"rand\" value=");
 	hashline = getxmlentry(pos4, "value=");
+	debug(99, "hashline: %s", hashline);
 
 	char* pos5 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"referer\" value=");
 	referer = getxmlentry(pos5, "value=");
+	debug(99, "referer: %s", referer);
 
 	char* pos6 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"imhuman\" value=");
 	imhuman = getxmlentry(pos6, "value=");
+	debug(99, "imhuman: %s", imhuman);
 
 	char* pos7 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"usr_login\" value=");
 	usr_login = getxmlentry(pos7, "value=");
+	debug(99, "usr_login: %s", usr_login);
 	
 	free(tmpstr); tmpstr = NULL;
-	debug(99, "op: %s", op);
 	if(op == NULL) goto end;
 
 	hash = ostrcat(hash, "hash=", 1, 0);
@@ -143,64 +118,50 @@ char* xvidstage(char* host, char* file, char* hosterurl)
 	hash = ostrcat(hash, "&id=", 1, 0);	
 	hash = ostrcat(hash, id, 1, 0);
 	hash = ostrcat(hash, "&op=download1", 1, 0);
-//	hash = ostrcat(hash, op, 1, 0);
+	hash = ostrcat(hash, op, 1, 0);
 	debug(99, "hash: %s", hash);
 	hashlen = oitoa(strlen(hash));
 
-	//create send string
-	free(send), send = NULL;
-
-	send = ostrcat(send, "POST /", 1, 0);
-	send = ostrcat(send, file, 1, 0);
+	send = ostrcat(send, "POST ", 1, 0);
+	send = ostrcat(send, tmpfile, 1, 0);
 	send = ostrcat(send, " HTTP/1.1\r\nContent-Length: ", 1, 0);
 	send = ostrcat(send, hashlen, 1, 0);
 	send = ostrcat(send, "\r\nAccept-Encoding: gzip\r\nConnection: close\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nHost: ", 1, 0);
-	send = ostrcat(send, host, 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
 	send = ostrcat(send, "\r\nCookie: afc=", 1, 0);
 	send = ostrcat(send, cookie, 1, 0);
 	send = ostrcat(send, "\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n", 1, 0);
 	send = ostrcat(send, hash, 1, 0);
 	debug(99, "send: %s", send);
 
-	free(tmpstr), tmpstr = NULL;
-
 	//send and receive answer
-//	post = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 0);
-//	writesys("/var/usr/local/share/titan/plugins/tithek/xvidstage_post1", post, 0);
-///
-	gethttpreal(tmphost, tmpfile, 80, "/tmp/tithek/post", NULL, NULL, 0, send, NULL, 5000, 0);
+	post = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+	debug(99, "post: %s", post);
+	titheklog(debuglevel, "/tmp/xvidstage2_post", NULL, post);
 
-	if(!file_exist("/tmp/tithek/post"))
+// hier wird nun ein captcha bild auth verlangt geht also erstmal nicht mehr
+	if(post == NULL)
 	{
 		textbox(_("Message"), _("This file doesn't exist, or has been removed") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
 		goto end;
 	}
-
-	if(debuglevel == 99) system("cp -a /tmp/tithek/post /tmp/xvidstage4_tmpstr_post");	
-
-	cmd = ostrcat(cmd, "cat /tmp/tithek/post | zcat", 1, 0);
-	debug(99, "cmd: %s", cmd);
-	post = command(cmd);
-	titheklog(debuglevel, "/tmp/xvidstage5_tmpstr_post1_zcat", NULL, post);
-	free(cmd); cmd = NULL;
-
-	free(tmpstr),tmpstr = NULL;
+	
 	tmpstr = string_resub(";return p}('", ");'", post, 0);
-	titheklog(debuglevel, "/tmp/xvidstage6_tmpstr_post1_zcat_resub", NULL, tmpstr);
+	titheklog(debuglevel, "/tmp/xvidstage3_tmpstr", NULL, tmpstr);
 	
 	post = string_replace_all(tmpstr, "", post, 1);
 	post = string_replace_all(";return p}(');'", "", post, 1);
-	titheklog(debuglevel, "/tmp/xvidstage7_tmpstr_post2", NULL, post);
+	titheklog(debuglevel, "/tmp/xvidstage4_tmpstr", NULL, post);
 
 	free(tmpstr),tmpstr = NULL;
 	free(b36code),b36code = NULL;
 	tmpstr = string_resub(";return p}('", ");'", post, 0);
-	titheklog(debuglevel, "/tmp/xvidstage8_tmpstr_post2_resub", NULL, tmpstr);
+	titheklog(debuglevel, "/tmp/xvidstage5_tmpstr", NULL, tmpstr);
 
 	b36code = oregex(".*;',[0-9]{2,2},[0-9]{2,2},'(.*)'.split.*", post);
 	
 	b36code = string_replace_all("||", "| |", b36code, 1);
-	titheklog(debuglevel, "/tmp/xvidstage9_tmpstr_post2_resub_b36code2", NULL, b36code);
+	titheklog(debuglevel, "/tmp/xvidstage6_tmpstr", NULL, b36code);
 	
 	struct splitstr* ret1 = NULL;
 	int count = 0;
@@ -279,7 +240,7 @@ char* xvidstage(char* host, char* file, char* hosterurl)
 	free(post), post = NULL;
 	free(charlist), charlist = NULL;
 
-	titheklog(debuglevel, "/tmp/xvidstage10_tmpstr_last", NULL, tmpstr);
+	titheklog(debuglevel, "/tmp/xvidstage7_tmpstr_last", NULL, tmpstr);
 
 	streamlink = oregex(".*file.*(http:.*video.flv).*image.*", tmpstr);
 	if(streamlink == NULL)
@@ -309,7 +270,7 @@ char* xvidstage(char* host, char* file, char* hosterurl)
 	if(streamlink == NULL)
 		streamlink = oregex(".*value=.*(http:.*video.avi).*\".*", tmpstr);			
 
-	titheklog(debuglevel, "/tmp/xvidstage11_streamlink", NULL, streamlink);
+	titheklog(debuglevel, "/tmp/xvidstage8_streamlink", NULL, streamlink);
 
 	free(tmpstr); tmpstr = NULL;
 
