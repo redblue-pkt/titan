cf9d8e15979f47557db7bb9de8d4ceb667854f92
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-10-14 16:36:57 +0000

    fix

--- titan/plugins/tithek/putlocker.h
+++ titan/plugins/tithek/putlocker.h
@@ -5,23 +5,13 @@ char* putlocker(char* host, char* file, char* hosterurl)
 {
 	debug(99, "in host: %s file: %s", host, file);
 	int debuglevel = getconfigint("debuglevel", NULL);
-	char* tmphost = NULL;
-	char* tmpfile = NULL;
-	char* tmpstr = NULL;
-	char* send = NULL;
-	char* hash = NULL;
-	char* hashstr = NULL;
-	char* hashlen = NULL;
-	char* ip = NULL;
-	char* phpsessid = NULL;
-	char* serverid = NULL;
-	char* usender = NULL;
-	char* streamlink = NULL;
-	char* streamlink1 = NULL;
-	char* tmpstr2 = NULL;
+	char* tmphost = NULL, *tmpfile = NULL, *tmpstr = NULL, *send = NULL, *hash = NULL, *hashstr = NULL, *hashlen = NULL;
+	char* ip = NULL, *phpsessid = NULL, *serverid = NULL, *usender = NULL, *streamlink = NULL, *streamlink1 = NULL, *tmpstr2 = NULL;
 
-	unlink("/tmp/tithek/get");
-	unlink("/tmp/tithek/x9");
+	unlink("/tmp/putlocker1_tmpstr_get");
+	unlink("/tmp/putlocker2_tmpstr_post");
+	unlink("/tmp/putlocker3_tmpstr_get");
+	unlink("/tmp/putlocker4_streamlink");
 
 	if(host == NULL || file == NULL) return NULL;
 
@@ -41,6 +31,9 @@ char* putlocker(char* host, char* file, char* hosterurl)
 
 	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
 	free(send); send = NULL;
+	debug(99, "tmpstr: %s", tmpstr);
+	titheklog(debuglevel, "/tmp/putlocker1_tmpstr_get", NULL, tmpstr);
+	
 	phpsessid = getxmlentry(tmpstr, "PHPSESSID=");
 	debug(99, "phpsessid: %s", phpsessid);
 	serverid = getxmlentry(tmpstr, "SERVERID=");
@@ -50,7 +43,6 @@ char* putlocker(char* host, char* file, char* hosterurl)
 
 	//get hash from tmpstr
 	char* pos = ostrstr(tmpstr, "<input type=\"hidden\" value=");
-	debug(99, "tmpstr: %s", tmpstr);
 	hashstr = getxmlentry(pos, "value=");
 // free(tmpstr) after getxml or segfault...
 	free(tmpstr); tmpstr = NULL;
@@ -70,8 +62,8 @@ char* putlocker(char* host, char* file, char* hosterurl)
 	send = ostrcat(send, file, 1, 0);
 	send = ostrcat(send, " HTTP/1.1\r\nContent-Length: ", 1, 0);
 	send = ostrcat(send, hashlen, 1, 0);
-	send = ostrcat(send, "\r\nAccept-Encoding: gzip\r\nConnection: close\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nHost: www.", 1, 0);
-	send = ostrcat(send, host, 1, 0);
+	send = ostrcat(send, "\r\nAccept-Encoding: gzip\r\nConnection: close\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nHost: ", 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
 	send = ostrcat(send, "\r\nCookie: usender=", 1, 0);
 	send = ostrcat(send, usender, 1, 0);
 	send = ostrcat(send, " SERVERID=", 1, 0);
@@ -86,7 +78,7 @@ char* putlocker(char* host, char* file, char* hosterurl)
 	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
 	free(send); send = NULL;
 	debug(99, "tmpstr: %s", tmpstr);
-	titheklog(debuglevel, "/tmp/putlocker4_tmpstr_post_zcat", NULL, tmpstr);
+	titheklog(debuglevel, "/tmp/putlocker2_tmpstr_post", NULL, tmpstr);
 	
 	if(ostrstr(tmpstr, "warning_message") != NULL)
 	{
@@ -126,8 +118,8 @@ char* putlocker(char* host, char* file, char* hosterurl)
 	//create send string
 	send = ostrcat(send, "GET ", 1, 0);
 	send = ostrcat(send, streamlink, 1, 0);
-	send = ostrcat(send, " HTTP/1.1\r\nAccept-Encoding: identity\r\nHost: www.", 1, 0);
-	send = ostrcat(send, host, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\nAccept-Encoding: identity\r\nHost: ", 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
 	send = ostrcat(send, "\r\nCookie: SERVERID=", 1, 0);
 	send = ostrcat(send, serverid, 1, 0);
 	send = ostrcat(send, "PHPSESSID=", 1, 0);
@@ -138,11 +130,12 @@ char* putlocker(char* host, char* file, char* hosterurl)
 	
 	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 0);
 	free(send); send = NULL;
-	titheklog(debuglevel, "/tmp/putlocker5_tmpstr_get", NULL, tmpstr);
+	debug(99, "tmpstr: %s", tmpstr);
+	titheklog(debuglevel, "/tmp/putlocker3_tmpstr_get", NULL, tmpstr);
 
 	//get streamlink1
 	streamlink1 = getxmlentry(tmpstr, "url=");
-	titheklog(debuglevel, "/tmp/putlocker5_streamlink", NULL, streamlink1);
+	titheklog(debuglevel, "/tmp/putlocker4_streamlink", NULL, streamlink1);
 
 	free(tmpstr); tmpstr = NULL;
 
--- titan/plugins/tithek/streamcloud.h
+++ titan/plugins/tithek/streamcloud.h
@@ -5,60 +5,38 @@ char* streamcloud(char* host, char* file, char* hosterurl)
 {
 	debug(99, "in host: %s file: %s", host, file);
 	int debuglevel = getconfigint("debuglevel", NULL);
-	char* tmphost = NULL;
-	char* tmpfile = NULL;
-	char* tmpstr = NULL;
-	char* send = NULL;
-	char* id = NULL;
-	char* fname = NULL;
-	char* op = NULL;
-	char* hash = NULL;
-	char* hashline = NULL;
-	char* hashlen = NULL;
-	char* referer = NULL;
-	char* usr_login = NULL;
-	char* imhuman = NULL;	
-	char* ip = NULL;
-	char* cookie = NULL;
-	char* cmd = NULL;
-	char* streamlink = NULL;
+	char* tmphost = NULL, *tmpfile = NULL, *tmpstr = NULL, *send = NULL, *id = NULL, *fname = NULL, *op = NULL, *hash = NULL, *cmd = NULL;
+	char* hashline = NULL, *hashlen = NULL, *referer = NULL, *usr_login = NULL, *imhuman = NULL, *ip = NULL, *cookie = NULL, *streamlink = NULL;
+
+	unlink("/tmp/streamcould1_get");
+	unlink("/tmp/streamcould2_post");
+	unlink("/tmp/streamcould3_streamlink");
 
 	if(host == NULL || file == NULL) return NULL;
 
-	tmphost = ostrcat(tmphost, host, 1, 0);
+	tmphost = ostrcat(host, NULL, 0, 0);
 	tmpfile = ostrcat("/", file, 0, 0);
-	
+
 	debug(99, "tmphost: %s", tmphost);
 	ip = get_ip(tmphost);
 	debug(99, "ip: %s", ip);
 	debug(99, "tmpfile: %s", tmpfile);
 
-	send = ostrcat(send, "GET /", 1, 0);
-	send = ostrcat(send, file, 1, 0);
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmpfile, 1, 0);
 	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);
-	send = ostrcat(send, host, 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
 	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);
 	debug(99, "send: %s", send);
 
 	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
-
-	if(getconfigint("debuglevel", NULL) == 99)
-		writesys("/tmp/streamcould1_tmpstr", tmpstr, 0);
-/*
-	old code	
-	cmd = ostrcat(cmd, "echo -e \"", 1, 0);
-	cmd = ostrcat(cmd, send, 1, 0);
-	cmd = ostrcat(cmd, "\" | /var/usr/local/share/titan/plugins/tithek/netcat ", 1, 0);
-	cmd = ostrcat(cmd, host, 1, 0);	
-	cmd = ostrcat(cmd, " 80", 1, 0);
-	debug(99, "cmd: %s", cmd);
-	free(tmpstr), tmpstr = NULL;
-	tmpstr = command(cmd);
-*/
+	free(send), send = NULL;
+	debug(99, "tmpstr: %s", tmpstr);
+	sleep(20);
+	titheklog(debuglevel, "/tmp/streamcould1_get", NULL, tmpstr);
+	
 	cookie = string_resub("Set-Cookie: afc=", ";", tmpstr, 0);	
 	debug(99, "cookie: %s", cookie);
- 
-	sleep(20);
 
 	//get hash from tmpstr
 	char* pos1 = ostrstr(tmpstr, "<input type=\"hidden\" name=\"fname\" value=");
@@ -106,30 +84,26 @@ char* streamcloud(char* host, char* file, char* hosterurl)
 	debug(99, "hash: %s", hash);
 	hashlen = oitoa(strlen(hash));
 
-	//create send string
-	free(send), send = NULL;
-
-	send = ostrcat(send, "POST /", 1, 0);
-	send = ostrcat(send, file, 1, 0);
+	send = ostrcat(send, "POST ", 1, 0);
+	send = ostrcat(send, tmpfile, 1, 0);
 	send = ostrcat(send, " HTTP/1.1\r\nContent-Length: ", 1, 0);
 	send = ostrcat(send, hashlen, 1, 0);
 	send = ostrcat(send, "\r\nAccept-Encoding: gzip\r\nConnection: close\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nHost: ", 1, 0);
-	send = ostrcat(send, host, 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
 	send = ostrcat(send, "\r\nCookie: afc=", 1, 0);
 	send = ostrcat(send, cookie, 1, 0);
 	send = ostrcat(send, "\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n", 1, 0);
 	send = ostrcat(send, hash, 1, 0);
 	debug(99, "send: %s", send);
 
-	free(tmpstr), tmpstr = NULL;
-	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 0);
-	titheklog(debuglevel, "/tmp/streamcould2_tmpstr_post", NULL, tmpstr);
+	tmpstr = gethttpreal(tmphost, tmpfile, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+	free(send), send = NULL;
+	debug(99, "tmpstr: %s", tmpstr);
+	titheklog(debuglevel, "/tmp/streamcould2_post", NULL, tmpstr);
 
 //	streamlink = oregex(".*file: \".*(http:.*video.mp4).*\".*", tmpstr);
 	streamlink = string_resub("file: \"", "\"", tmpstr, 0);
 	titheklog(debuglevel, "/tmp/streamcould3_streamlink", NULL, streamlink);
-
-	free(tmpstr); tmpstr = NULL;
 
 end:
 
