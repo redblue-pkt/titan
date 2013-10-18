b703cee65f281d61097e5fa6bf581b3ec16247df
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-10-18 20:48:23 +0000

    fix ????

--- titan/titan/header.h
+++ titan/titan/header.h
@@ -784,7 +784,7 @@ int getlfiletype(char* filename);
 unsigned long long getfreespace(char* dir);
 char* getfilenamepng(char* filename);
 char* htmlencode(char* from);
-void waitmsgbar(int sec, int exit, int flag);
+void waitmsgbar(int sec, int exit, char* text, int flag)
 
 //rcconfig.h
 int getrcconfigint(char *key, char* ext);
