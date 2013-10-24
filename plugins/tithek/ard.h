02b50b73eaba4c3aeee84eb75e5175b01e9ae1f6
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-10-24 15:41:25 +0000

    fix ard mediathek

--- plugins/tithek/ard.h
+++ plugins/tithek/ard.h
@@ -5,8 +5,9 @@ char* ard(char* link)
 {
 	debug(99, "link %s", link);
 	int debuglevel = getconfigint("debuglevel", NULL);
-	char* ip = NULL, *pos = NULL, *path = NULL, *streamurl = NULL, *tmpstr = NULL;
-	
+	char* ip = NULL, *pos = NULL, *path = NULL, *streamurl = NULL, *tmpstr = NULL, *nummer = NULL;
+	char* tmpstr2 = NULL, *title = NULL, *pic = NULL, *part1 = NULL, *part2 = NULL;
+
 	ip = string_replace("http://", "", (char*)link, 0);
 
 	if(ip != NULL)
@@ -18,10 +19,8 @@ char* ard(char* link)
 	}
 
 	tmpstr = gethttp(ip, path, 80, NULL, NULL, 10000, NULL, 0);
-	titheklog(debuglevel, "/tmp/ard1_tmpstr", NULL, tmpstr);
-
+	titheklog(debuglevel, "/tmp/tithek/ard1_tmpstr", NULL, tmpstr);
 //	writesys("/var/usr/local/share/titan/plugins/tithek/ard1_tmpstr", tmpstr, 0);
-	
 
 	if(tmpstr != NULL)
 	{
@@ -32,27 +31,91 @@ char* ard(char* link)
 			textbox(_("Message"), tmpstr2, _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 400, 0, 0);
 			free(tmpstr2), tmpstr2 = NULL;
 		}
-/*
-		streamurl = string_resub("'file': '","',",tmpstr,0);
 
-mediaCollection.addMediaStream(0, 0, "", "http://http-stream.rbb-online.de/rbb/abendschau/abendschau_20130923_wahl_s_16_9_256x144.mp4", "akamai");
-mediaCollection.addMediaStream(0, 1, "rtmp://ondemand.rbb-online.de/ondemand/", "mp4:rbb/abendschau/abendschau_20130923_wahl_m_16_9_512x288.mp4", "akamai");
-mediaCollection.addMediaStream(1, 0, "", "http://http-stream.rbb-online.de/rbb/abendschau/abendschau_20130923_wahl_s_16_9_256x144.mp4", "akamai");
-mediaCollection.addMediaStream(1, 1, "", "http://http-stream.rbb-online.de/rbb/abendschau/abendschau_20130923_wahl_m_16_9_512x288.mp4", "akamai");
+		tmpstr2 = string_resub("mediaCollection.setPreviewImage", "playerConfiguration = new PlayerConfiguration();", tmpstr, 0);
+		titheklog(debuglevel, "/tmp/tithek/ard2_tmpstr2", NULL, tmpstr2);
 
-*/
+//		writesys("/var/usr/local/share/titan/plugins/tithek/ard1_tmpstr2", tmpstr2, 0);
 
-		streamurl = oregex(".*mediaCollection.addMediaStream.*(http://.*.mp4).*", tmpstr);
+		int count = 0, i = 0;	
+ 		struct splitstr* ret1 = NULL;
+ 		struct menulist* mlist = NULL, *mbox = NULL;
+		ret1 = strsplit(string_decode(tmpstr2,0), "\n", &count);
+		for(i = 0; i < count; i++)
+		{
+			if(ret1[i].part != NULL && strlen(ret1[i].part) > 1)
+			{
+				debug(99, "Round (%d) %s",i, strstrip(ret1[i].part));
+				streamurl = string_resub("<url>", "</url>", ret1[i].part,0);
+
+				if(ostrstr(ret1[i].part, "mp4") != NULL)
+					pic = ostrcat("mp4.png", NULL, 0, 0);
+				else if(ostrstr(ret1[i].part, "3gp") != NULL)
+					pic = ostrcat("3gp.png", NULL, 0, 0);
+				else if(ostrstr(ret1[i].part, "flv") != NULL)
+					pic = ostrcat("flv.png", NULL, 0, 0);
+
+				if(ostrstr(ret1[i].part, "http://") != NULL)
+				{
+					streamurl = oregex(".*mediaCollection.addMediaStream.*(http://.*.mp4).*", ret1[i].part);
+					nummer = string_resub("mediaCollection.addMediaStream\(", ", \"", ret1[i].part, 0);
+
+					title = ostrcat("Http Stream (", NULL, 0, 0);
+					title = ostrcat(title, nummer, 1, 0);
+					title = ostrcat(title, ")", 1, 0);
+
+					debug(99, "(%d) title: %s streamurl: %s\n", i, title, streamurl);																									
+					addmenulist(&mlist, title, streamurl, pic, 0, 0);
+				}
+				else if(ostrstr(ret1[i].part, "rtmp://") != NULL)
+				{
+					part1 = oregex(".*mediaCollection.addMediaStream.*(rtmp://.*)\", \"mp4:.*", ret1[i].part);
+					part2 = oregex(".*mediaCollection.addMediaStream.*(mp4:.*)\", \".*", ret1[i].part);
+//					part2 = oregex(".*mediaCollection.addMediaStream.*(mp4:.*)[?].*", ret1[i].part);
+					part2 = stringreplacecharonce(part2, '?', '\0');
+
+					streamurl = ostrcat(part1, part2, 0, 0);
+
+					nummer = string_resub("mediaCollection.addMediaStream\(", ", \"", ret1[i].part, 0);
+
+					title = ostrcat("RTMP Stream (", NULL, 0, 0);
+					title = ostrcat(title, nummer, 1, 0);
+					title = ostrcat(title, ")", 1, 0);
+
+					debug(99, "(%d) title: %s streamurl: %s\n", i, title, streamurl);																									
+					addmenulist(&mlist, title, streamurl, pic, 0, 0);
+				}
+
+				free(part1), part1 = NULL;
+				free(part2), part2 = NULL;		
+				free(title), title = NULL;
+				free(pic), pic = NULL;
+				free(nummer), nummer = NULL;
+				free(streamurl), streamurl = NULL;
+			}
+		}
+		free(ret1), ret1 = NULL;
+		free(tmpstr2), tmpstr2 = NULL;
+
+		if(mlist != NULL)
+		{
+			mbox = menulistbox(mlist, NULL, NULL, NULL, NULL, 1, 0);
+			if(mbox != NULL)
+			{
+				free(streamurl), streamurl = NULL;
+
+				debug(99, "mbox->name %s", mbox->name);
+				debug(99, "mbox->text %s", mbox->text);
+				streamurl = ostrcat(mbox->text, NULL, 0, 0);
+
+			}
+		}
 	}
 
 	free(tmpstr); tmpstr = NULL;
 	free(ip), ip = NULL;
 
-	titheklog(debuglevel, "/tmp/ard2_streamurl", NULL, streamurl);
-
-// segfault munmap_chunk(): invalid pointer
-//	free(pos), pos = NULL;
-//	free(path), path = NULL;
+	titheklog(debuglevel, "/tmp/tithek/ard3_streamurl", NULL, streamurl);
 
 	debug(99, "streamurl: %s", streamurl);	
 	return streamurl;
