e6e3a95090af3e70475272b435d02e33ff26cb54
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2013-12-07 21:18:36 +0000

    [tithek] add to all theken a search local entry, here search all pre parsed entrys

--- titan/plugins/tithek/kinox.h
+++ titan/plugins/tithek/kinox.h
@@ -164,6 +164,69 @@ int kinox_search(struct skin* grid, struct skin* listbox, struct skin* countlabe
 	return ret;
 }
 
+int kinox_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag)
+{
+	char* tmpstr = NULL, *tmpstr1 = NULL, *line = NULL, *menu = NULL, *search = NULL;
+	int ret = 1, count = 0, i = 0;
+
+	if(listbox == NULL || listbox->select == NULL || listbox->select->handle == NULL)
+		return ret;
+
+	if(searchstr == NULL)
+		search = textinputhist("Search", " ", "searchhist");
+	else
+		search = textinputhist("Search", searchstr, "searchhist");
+
+	if(search != NULL)
+	{
+		drawscreen(load, 0, 0);
+
+		strstrip(search);
+		string_tolower(search);
+
+		tmpstr = gethttp("atemio.dyndns.tv", "/mediathek/kinox/streams/kinox.all-sorted.list", 80, NULL, HTTPAUTH, 5000, NULL, 0);
+
+		struct splitstr* ret1 = NULL;
+		ret1 = strsplit(tmpstr, "\n", &count);
+
+		if(ret1 != NULL)
+		{
+			int max = count;
+			for(i = 0; i < max; i++)
+			{
+			
+				tmpstr1 = ostrcat(ret1[i].part, NULL, 0, 0);
+				tmpstr1 = stringreplacecharonce(tmpstr1, '#', '\0');
+				string_tolower(tmpstr1);
+
+				if(ostrstr(tmpstr1, search) != NULL)
+				{
+					printf("found: %s\n", ret1[i].part);
+					int rcret = waitrc(NULL, 10, 0);
+					if(rcret == getrcconfigint("rcexit", NULL)) break;
+
+					line = ostrcat(line, ret1[i].part, 1, 0);
+					line = ostrcat(line, "\n", 0, 0);
+				}
+				free(tmpstr1), tmpstr1 = NULL;				
+			}
+			free(ret1), ret1 = NULL;
+
+			if(line != NULL)
+			{
+				menu = ostrcat("/tmp/tithek/kinox.search.list", NULL, 0, 0);
+				writesys(menu, line, 0);
+				struct tithek* tnode = (struct tithek*)listbox->select->handle;
+				createtithek(tnode, tnode->title, menu, tnode->pic, tnode->localname, tnode->menutitle, tnode->flag);
+				ret = 0;
+			}
+		}
+		free(tmpstr), tmpstr = NULL;
+	}
+	free(search), search = NULL;
+	return ret;
+}
+
 int kinox_hoster(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title)
 {
 	debug(99, "link: %s", link);
--- titan/plugins/tithek/tithek_header.h
+++ titan/plugins/tithek/tithek_header.h
@@ -55,5 +55,22 @@ char* faststream(char* link);
 char* played(char* link);
 char* videoweed(char* link);
 int internetradio_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int internettv_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int kinox_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int movie4k_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int solarmovie_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int myvideo_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int youtube_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int ard_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int zdf_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int drdish_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int giga_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int beeg_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+
+int rtl2now_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int rtlnow_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int superrtlnow_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+int voxnow_search_local(struct skin* grid, struct skin* listbox, struct skin* countlabel, struct skin* load, char* link, char* title, char* searchstr, int flag);
+
 
 #endif
