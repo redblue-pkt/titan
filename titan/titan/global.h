3c6d8a6c40e5f4e639d32a05ad18538f2f9ac521
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-01-09 21:51:57 +0000

    show overclocking only on ufs912

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -1688,7 +1688,7 @@ void setskinnodeslocked(int flag)
 			if(ostrcmp("Reader Config", child->name) == 0) child->locked = 1;
 
 			// show this menus
-			if(checkbox("UFS912") == 0)
+			if(checkbox("UFS912") == 1)
 			{
 				if(ostrcmp("overclocking", child->name) == 0) child->locked = 0;
 			}
