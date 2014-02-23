4347d2d1658e7b03bb83ae01af6141af24d46dd1
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-02-23 23:21:28 +0000

    fix header

--- titan/header.h
+++ titan/header.h
@@ -634,6 +634,8 @@ int changemask(struct skin* node, char* text);
 int changepicmem(struct skin* node, char* text, int timeout, int del);
 int convertxmlentry(char *value, uint8_t *proz);
 unsigned char* savejpg(char* filename, int width, int height, int channels, int newwidth, int newheight, int quality, unsigned char *buf);
+inline void drawpixelfastfb(struct fb* node, int posx, int posy, unsigned long color);
+inline void drawpixelfb(struct fb* node, int posx, int posy, unsigned long color);
 
 //volume.h
 void screenvolumeup();
