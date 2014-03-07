efcc5d77d55e0ed3b1cced832e85cc1cc4d37b87
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-03-07 13:34:14 +0000

    fix unicable

--- titan/tunerconfig.h
+++ titan/tunerconfig.h
@@ -430,30 +430,21 @@ start:
 	drawscreen(tunerreceptiondvbs, 0, 0);
 	addscreenrc(tunerreceptiondvbs, listbox);
 
-printf("11\n");
 	tmp = listbox->select;
 	while(1)
 	{
-printf("22\n");
-
 		addscreenrc(tunerreceptiondvbs, tmp);
 		rcret = waitrc(tunerreceptiondvbs, 0, 0);
 		tmp = listbox->select;
 
 		if(rcret == getrcconfigint("rcexit", NULL))
 		{
-printf("33\n");
-
 			break;
 		}
 		if(ostrcmp(listbox->select->name, "sat_type") == 0)
 		{
-printf("44\n");
-		
 			if((ostrcmp(listbox->select->ret, "0") == 0) && (maxsat != 1))
 			{
-printf("55\n");
-
 				delconfigtmpall();
 				delmarkedscreennodes(tunerreceptiondvbs, 1);
 				delmarkedscreennodes(tunerreceptiondvbs, 2);
@@ -464,8 +455,6 @@ printf("55\n");
 			}
 			else if((ostrcmp(listbox->select->ret, "1") == 0) && (maxsat != 2))
 			{
-printf("66\n");
-
 				delconfigtmpall();
 				delmarkedscreennodes(tunerreceptiondvbs, 1);
 				delmarkedscreennodes(tunerreceptiondvbs, 2);
@@ -476,8 +465,6 @@ printf("66\n");
 			}
 			else if((ostrcmp(listbox->select->ret, "2") == 0) && (maxsat != 4))
 			{
-printf("77\n");
-
 				delconfigtmpall();
 				delmarkedscreennodes(tunerreceptiondvbs, 1);
 				delmarkedscreennodes(tunerreceptiondvbs, 2);
@@ -488,8 +475,6 @@ printf("77\n");
 			}
 			else if((ostrcmp(listbox->select->ret, "3") == 0) && (maxsat != 8))
 			{
-printf("88\n");
-
 				delconfigtmpall();
 				delmarkedscreennodes(tunerreceptiondvbs, 1);
 				delmarkedscreennodes(tunerreceptiondvbs, 2);
@@ -500,8 +485,6 @@ printf("88\n");
 			}
 			else if((ostrcmp(listbox->select->ret, "4") == 0) && (maxsat != MAXSAT))
 			{
-printf("99\n");
-
 				delconfigtmpall();
 				delmarkedscreennodes(tunerreceptiondvbs, 1);
 				delmarkedscreennodes(tunerreceptiondvbs, 2);
@@ -513,20 +496,14 @@ printf("99\n");
 		}
 		if(rcret == getrcconfigint("rcred", NULL))
 		{
-printf("aa\n");
-
-			if(listbox->select != NULL && ostrcmp(listbox->select->text, "Lnb / Unicable") == 0 && listbox->select->ret != NULL && ostrcmp(listbox->select->ret, "0") != 0)
+			if(listbox->select != NULL && ostrcmp(listbox->select->text, _("Lnb / Unicable")) == 0 && listbox->select->ret != NULL && ostrcmp(listbox->select->ret, "0") != 0)
 			{
-printf("bb\n");
-
 				clearscreen(tunerreceptiondvbs);
 				screenlnb(tuner->feshortname, listbox->select->handle);
 				drawscreen(tunerreceptiondvbs, 0, 0);
 			}
-			if(listbox->select != NULL && ostrcmp(listbox->select->text, "DiSEqC") == 0 && listbox->select->ret != NULL && ostrcmp(listbox->select->ret, "0") != 0)
+			if(listbox->select != NULL && ostrcmp(listbox->select->text, _("DiSEqC")) == 0 && listbox->select->ret != NULL && ostrcmp(listbox->select->ret, "0") != 0)
 			{
-printf("cc\n");
-
 				clearscreen(tunerreceptiondvbs);
 				screendiseqc(tuner->feshortname, listbox->select->handle);
 				drawscreen(tunerreceptiondvbs, 0, 0);
@@ -534,8 +511,6 @@ printf("cc\n");
 		}
 		else if(rcret == getrcconfigint("rcok", NULL))
 		{
-printf("dd\n");
-
 			ret = 1;
 			deltranspondertunablestatus();
 			writetunerconfigsat(tuner, tunerreceptiondvbs);
@@ -545,10 +520,7 @@ printf("dd\n");
 			writeallconfig(1);
 			break;
 		}
-printf("ee\n");
-
 	}
-printf("ff\n");
 
 	delconfigtmpall();
 	delmarkedscreennodes(tunerreceptiondvbs, 1);
