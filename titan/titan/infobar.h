3911cfd0decacaf0deb59bf3adfb4fa5453eae7f
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-03-19 15:17:17 +0000

    fix

--- titan/titan/infobar.h
+++ titan/titan/infobar.h
@@ -423,7 +423,7 @@ void screeninfobar()
 			status.updatevfd = START;
 			drawscreen(skin, 0, 0);
 			subtitlepause(0);
-			textbox(_("Message"), _("Screenshoot created on /tmp/screenshoot.png !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 10, 0);
+			textbox(_("Message"), _("Shooting Background done !\nSave Screenshoot Path: /tmp/screenshot.jpg"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 800, 200, 0, 0);
 			continue;
 		}
 
