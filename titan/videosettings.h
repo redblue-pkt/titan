a2800672f53897ac1b37931fc30c892a116e860e
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-03-20 12:02:33 +0000

    fix

--- titan/videosettings.h
+++ titan/videosettings.h
@@ -4,13 +4,13 @@
 void screenvideosettings()
 {
 	int rcret = 0, ibrightness = 0, icontrast = 0, itint = 0;
-	//int isaturation = 0;
+	int isaturation = 0;
 	struct skin* videosettings = getscreen("videosettings");
 	struct skin* listbox = getscreennode(videosettings, "listbox");
 	struct skin* brightness = getscreennode(videosettings, "brightness");
 	struct skin* contrast = getscreennode(videosettings, "contrast");
 	struct skin* tint = getscreennode(videosettings, "tint");
-	//struct skin* saturation = getscreennode(videosettings, "saturation");
+	struct skin* saturation = getscreennode(videosettings, "saturation");
 	struct skin* tmp = NULL;
 
 	if(brightness != NULL)
@@ -49,7 +49,7 @@ void screenvideosettings()
 		ibrightness = (int)ceil(((float)brightness->progresssize * 2.56));
 		icontrast = (int)ceil(((float)contrast->progresssize * 2.56));
 		itint = (int)ceil(((float)tint->progresssize * 2.56));
-		//isaturation = (int)ceil(((float)saturation->progresssize * 2.56));
+		isaturation = (int)ceil(((float)saturation->progresssize * 2.56));
 
 		if(rcret == getrcconfigint("rcexit", NULL)) break;
 		if(rcret == getrcconfigint("rcok", NULL))
@@ -65,7 +65,7 @@ void screenvideosettings()
 			ibrightness = 128;
 			icontrast = 128;
 			itint = 128;
-			//isaturation = 128;
+			isaturation = 128;
 
 			brightness->progresssize = (int)ceil(((float)ibrightness / 2.56));
 			contrast->progresssize = (int)ceil(((float)icontrast / 2.56));
