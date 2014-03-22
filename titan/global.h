fc32c1be4f5b72a1f41628fb2acf9f626e7ea93c
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-03-22 15:56:18 +0000

    mipsel disable system_backup

--- titan/global.h
+++ titan/global.h
@@ -1815,6 +1815,12 @@ void setskinnodeslocked(int flag)
 			if(checkbox("ATEVIO5200") == 1)
 			{
 				if(ostrcmp("skinadjust", child->name) == 0) child->locked = 1;
+				if(ostrcmp("system_backup", child->name) == 0) child->locked = 1;
+			}
+
+			if(checkbox("ATEVIO5000") == 1)
+			{
+				if(ostrcmp("system_backup", child->name) == 0) child->locked = 1;
 			}
 
 			if(ostrcmp("savesettings", child->name) == 0) child->locked = tmpflag;
