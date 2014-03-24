8cc0e9a0b1d1142b367f65b2f3902e9ee05cb0b8
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-03-24 19:12:08 +0000

    add save webif save support

--- titan/httpdfunc.h
+++ titan/httpdfunc.h
@@ -3346,6 +3346,9 @@ char* webgetbouquetepg(char* param, int fmt)
 
 char* webadjust(char* param, int fmt)
 {
+printf("webadjust param: %s\n",param);
+printf("webadjust fmt: %d\n",fmt);
+
 	char* buf = NULL, *param1 = NULL, *param2 = NULL, *tmpstr = NULL, *tmpstr1 = NULL;
 	int i = 0, max = 0, count = 0, maxlen = 0, pos = 0;
 
@@ -3373,6 +3376,12 @@ char* webadjust(char* param, int fmt)
 
 	ostrcatbig(&buf, "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><link rel=\"stylesheet\" type=\"text/css\" href=\"titan.css\"></head>", &maxlen, &pos);
 	ostrcatbig(&buf, "<body class=body ><center>", &maxlen, &pos);
+	ostrcatbig(&buf, "<form name=F1 action=query method=get><br><br>", &maxlen, &pos);
+	ostrcatbig(&buf, "<input type=\"hidden\" name=\"adjustsend&node\" value=\"", &maxlen, &pos);
+	ostrcatbig(&buf, "0", &maxlen, &pos);
+	ostrcatbig(&buf, "\">", &maxlen, &pos);
+	ostrcatbig(&buf, "<table border=\"0\"><tr>", &maxlen, &pos);
+
 	ostrcatbig(&buf, "<table>", &maxlen, &pos);
 
 ///////////////////
@@ -4368,7 +4377,6 @@ char* webadjust(char* param, int fmt)
 	ostrcatbig(&buf, _("yes"), &maxlen, &pos);
 	ostrcatbig(&buf, "</option>", &maxlen, &pos);
 
-
 ///////////////////
 	ostrcatbig(&buf, "<tr><td><font class=\"label\">", &maxlen, &pos);
 	ostrcatbig(&buf, _("SATA Connector"), &maxlen, &pos);
@@ -4408,10 +4416,227 @@ char* webadjust(char* param, int fmt)
 
 
 	ostrcatbig(&buf, "</select></td></tr>", &maxlen, &pos);
-	ostrcatbig(&buf, "</table><br><br><input class=button type=submit name=send value=\"Send\" onClick=\"return checkdaytime(begin.value, end.value)\"></input>&nbsp;<input class=button type=reset name=reset value=\"Reset\"></input></form></center></body></html>", &maxlen, &pos);
+//	ostrcatbig(&buf, "</table><br><br><input class=button type=submit name=send value=\"Send\" onClick=\"return checkdaytime(begin.value, end.value)\"></input>&nbsp;<input class=button type=reset name=reset value=\"Reset\"></input></form></center></body></html>", &maxlen, &pos);
+	ostrcatbig(&buf, "</table><br><br><input class=button type=submit name=send value=\"Send\" onClick=\"return volbartimeout.value, infobartimeout.value, infobarsleep.value, spinnerspeed.value, spinnertime.value, hangtime.value, secondinfobar.value, nozapclear.value, fastzap.value, faststop.value, dirsort.value, poweraktion.value, virtualzap.value, fasttextrender.value, recsplitsize.value, recforerun.value, recoverrun.value, skip13.value, skip46.value, skip79.value, playertype.value, autochangechannelname.value, def_rectimer_after.value, showchanneltimeline.value, screenanim.value, screenanimspeed.value, channellistview.value, showlastpos.value, recsync.value, recordnamefmt.value, newsletter.value, showhiddenfiles.value, expertmodus.value, infobarprogram.value, emucontrol.value, choiceminitv.value, usecec.value, playerbufferseektime.value\"></input>&nbsp;<input class=button type=reset name=reset value=\"Reset\"></input></form></center></body></html>", &maxlen, &pos);
 
+//writesys("/tmp/tmpstr", buf, 1);
 	//ostrcatbig(&buf, param, &maxlen, &pos);
 	return buf;
+}
+
+char* webadjustsend(char* param, int fmt)
+{
+printf("webadjustsend param: %s\n",param);
+printf("webadjustsend fmt: %d\n",fmt);
+
+	char* buf = NULL;
+	char* anode = NULL;
+
+	char* volbartimeout = NULL;
+	char* infobartimeout = NULL;
+	char* infobarsleep = NULL;
+	char* spinnerspeed = NULL;
+	char* spinnertime = NULL;
+	char* hangtime = NULL;
+
+	char* secondinfobar = NULL;
+	char* nozapclear = NULL;
+	char* fastzap = NULL;
+	char* faststop = NULL;
+	char* dirsort = NULL;
+	char* poweraktion = NULL;
+	char* virtualzap = NULL;
+	char* fasttextrender = NULL;
+	char* recsplitsize = NULL;
+	char* recforerun = NULL;
+	char* recoverrun = NULL;
+	char* skip13 = NULL;
+	char* skip46 = NULL;
+	char* skip79 = NULL;
+	char* playertype = NULL;
+	char* autochangechannelname = NULL;
+	char* def_rectimer_after = NULL;
+	char* showchanneltimeline = NULL;
+	char* screenanim = NULL;
+	char* screenanimspeed = NULL;
+	char* channellistview = NULL;
+	char* showlastpos = NULL;
+	char* recsync = NULL;
+	char* recordnamefmt = NULL;
+	char* newsletter = NULL;
+	char* showhiddenfiles = NULL;
+	char* expertmodus = NULL;
+	char* infobarprogram = NULL;
+	char* emucontrol = NULL;
+	char* choiceminitv = NULL;
+	char* usecec = NULL;
+	char* playerbufferseektime = NULL;
+
+	anode = ostrstr(param, "node=");
+	if(anode != NULL)
+		anode = anode + 5;
+
+	volbartimeout = ostrstr(param, "volbartimeout=");
+	if(volbartimeout != NULL) volbartimeout = volbartimeout + 14;
+	if(volbartimeout != NULL) addconfigtmp("volbartimeout", volbartimeout);
+
+	infobartimeout = ostrstr(param, "infobartimeout=");
+	if(infobartimeout != NULL) infobartimeout = infobartimeout + 15;
+	if(infobartimeout != NULL) addconfigtmp("infobartimeout", infobartimeout);
+
+	infobarsleep = ostrstr(param, "infobarsleep=");
+	if(infobarsleep != NULL) infobarsleep = infobarsleep + 13;
+	if(infobarsleep != NULL) addconfigtmp("infobarsleep", infobarsleep);
+
+	spinnerspeed = ostrstr(param, "spinnerspeed=");
+	if(spinnerspeed != NULL) spinnerspeed = spinnerspeed + 13;
+	if(spinnerspeed != NULL) addconfigtmp("spinnerspeed", spinnerspeed);
+
+	spinnertime = ostrstr(param, "spinnertime=");
+	if(spinnertime != NULL) spinnertime = spinnertime + 12;
+	if(spinnertime != NULL) addconfigtmp("spinnertime", spinnertime);
+
+	hangtime = ostrstr(param, "hangtime=");
+	if(hangtime != NULL) hangtime = hangtime + 9;
+	if(hangtime != NULL) addconfigtmp("hangtime", hangtime);
+
+	secondinfobar = ostrstr(param, "secondinfobar=");
+	if(secondinfobar != NULL) secondinfobar = secondinfobar + 14;
+	if(secondinfobar != NULL) addconfigtmp("secondinfobar", secondinfobar);
+
+	nozapclear = ostrstr(param, "nozapclear=");
+	if(nozapclear != NULL) nozapclear = nozapclear + 11;
+	if(nozapclear != NULL) addconfigtmp("nozapclear", nozapclear);
+
+	fastzap = ostrstr(param, "fastzap=");
+	if(fastzap != NULL) fastzap = fastzap + 8;
+	if(fastzap != NULL) addconfigtmp("fastzap", fastzap);
+
+	faststop = ostrstr(param, "faststop=");
+	if(faststop != NULL) faststop = faststop + 9;
+	if(faststop != NULL) addconfigtmp("faststop", faststop);
+
+	dirsort = ostrstr(param, "dirsort=");
+	if(dirsort != NULL) dirsort = dirsort + 8;
+	if(dirsort != NULL) addconfigtmp("dirsort", dirsort);
+
+	poweraktion = ostrstr(param, "poweraktion=");
+	if(poweraktion != NULL) poweraktion = poweraktion + 12;
+	if(poweraktion != NULL) addconfigtmp("poweraktion", poweraktion);
+
+	virtualzap = ostrstr(param, "virtualzap=");
+	if(virtualzap != NULL) virtualzap = virtualzap + 11;
+	if(virtualzap != NULL) addconfigtmp("virtualzap", virtualzap);
+
+	fasttextrender = ostrstr(param, "fasttextrender=");
+	if(fasttextrender != NULL) fasttextrender = fasttextrender + 15;
+	if(fasttextrender != NULL) addconfigtmp("fasttextrender", fasttextrender);
+
+	recsplitsize = ostrstr(param, "recsplitsize=");
+	if(recsplitsize != NULL) recsplitsize = recsplitsize + 13;
+	if(recsplitsize != NULL) addconfigtmp("recsplitsize", recsplitsize);
+
+	recforerun = ostrstr(param, "recforerun=");
+	if(recforerun != NULL) recforerun = recforerun + 11;
+	if(recforerun != NULL) addconfigtmp("recforerun", recforerun);
+	
+	recoverrun = ostrstr(param, "recoverrun=");
+	if(recoverrun != NULL) recoverrun = recoverrun + 11;
+	if(recoverrun != NULL) addconfigtmp("recoverrun", recoverrun);
+
+	skip13 = ostrstr(param, "skip13=");
+	if(skip13 != NULL) skip13 = skip13 + 7;
+	if(skip13 != NULL) addconfigtmp("skip13", skip13);
+
+	skip46 = ostrstr(param, "skip46=");
+	if(skip46 != NULL) skip46 = skip46 + 7;
+	if(skip46 != NULL) addconfigtmp("skip46", skip46);
+
+	skip79 = ostrstr(param, "skip79=");
+	if(skip79 != NULL) skip79 = skip79 + 7;
+	if(skip79 != NULL) addconfigtmp("skip79", skip79);
+
+	playertype = ostrstr(param, "playertype=");
+	if(playertype != NULL) playertype = playertype + 11;
+	if(playertype != NULL) addconfigtmp("playertype", playertype);
+
+	autochangechannelname = ostrstr(param, "autochangechannelname=");
+	if(autochangechannelname != NULL) autochangechannelname = autochangechannelname + 22;
+	if(autochangechannelname != NULL) addconfigtmp("autochangechannelname", autochangechannelname);
+
+	def_rectimer_after = ostrstr(param, "def_rectimer_after=");
+	if(def_rectimer_after != NULL) def_rectimer_after = def_rectimer_after + 19;
+	if(def_rectimer_after != NULL) addconfigtmp("def_rectimer_after", def_rectimer_after);
+
+	showchanneltimeline = ostrstr(param, "showchanneltimeline=");
+	if(showchanneltimeline != NULL) showchanneltimeline = showchanneltimeline + 20;
+	if(showchanneltimeline != NULL) addconfigtmp("showchanneltimeline", showchanneltimeline);
+
+	screenanim = ostrstr(param, "screenanim=");
+	if(screenanim != NULL) screenanim = screenanim + 11;
+	if(screenanim != NULL) addconfigtmp("screenanim", screenanim);
+
+	screenanimspeed = ostrstr(param, "screenanimspeed=");
+	if(screenanimspeed != NULL) screenanimspeed = screenanimspeed + 16;
+	if(screenanimspeed != NULL) addconfigtmp("screenanimspeed", screenanimspeed);
+
+
+	channellistview = ostrstr(param, "channellistview=");
+	if(channellistview != NULL) channellistview = channellistview + 16;
+	if(channellistview != NULL) addconfigtmp("channellistview", channellistview);
+
+	showlastpos = ostrstr(param, "showlastpos=");
+	if(showlastpos != NULL) showlastpos = showlastpos + 12;
+	if(showlastpos != NULL) addconfigtmp("showlastpos", showlastpos);
+
+	recsync = ostrstr(param, "recsync=");
+	if(recsync != NULL) recsync = recsync + 8;
+	if(recsync != NULL) addconfigtmp("recsync", recsync);
+
+	recordnamefmt = ostrstr(param, "recordnamefmt=");
+	if(recordnamefmt != NULL) recordnamefmt = recordnamefmt + 14;
+	if(recordnamefmt != NULL) addconfigtmp("recordnamefmt", recordnamefmt);
+
+	newsletter = ostrstr(param, "newsletter=");
+	if(newsletter != NULL) newsletter = newsletter + 11;
+	if(newsletter != NULL) addconfigtmp("newsletter", newsletter);
+
+	showhiddenfiles = ostrstr(param, "showhiddenfiles=");
+	if(showhiddenfiles != NULL) showhiddenfiles = showhiddenfiles + 16;
+	if(showhiddenfiles != NULL) addconfigtmp("showhiddenfiles", showhiddenfiles);
+
+	expertmodus = ostrstr(param, "expertmodus=");
+	if(expertmodus != NULL) expertmodus = expertmodus + 12;
+	if(expertmodus != NULL) addconfigtmp("expertmodus", expertmodus);
+
+	infobarprogram = ostrstr(param, "infobarprogram=");
+	if(infobarprogram != NULL) infobarprogram = infobarprogram + 15;
+	if(infobarprogram != NULL) addconfigtmp("infobarprogram", infobarprogram);
+
+	emucontrol = ostrstr(param, "emucontrol=");
+	if(emucontrol != NULL) emucontrol = emucontrol + 10;
+	if(emucontrol != NULL) addconfigtmp("emucontrol", emucontrol);
+
+	choiceminitv = ostrstr(param, "choiceminitv=");
+	if(choiceminitv != NULL) choiceminitv = choiceminitv + 13;
+	if(choiceminitv != NULL) addconfigtmp("choiceminitv", choiceminitv);
+
+	usecec = ostrstr(param, "usecec=");
+	if(usecec != NULL) usecec = usecec + 7;
+	if(usecec != NULL) addconfigtmp("usecec", usecec);
+
+	playerbufferseektime = ostrstr(param, "playerbufferseektime=");
+	if(playerbufferseektime != NULL) playerbufferseektime = playerbufferseektime + 21;
+	if(playerbufferseektime != NULL) addconfigtmp("playerbufferseektime", playerbufferseektime);
+
+	buf = webadjust(NULL, fmt);
+
+//param: node=0&volbartimeout=<option+value=&infobartimeout=4&infobarsleep=<option+value=&spinnerspeed=12&spinnertime=5&hangtime=60&secondinfobar=1&nozapclear=0&fastzap=0&faststop=1&dirsort=4&poweraktion=0&virtualzap=0&fasttextrender=0&recsplitsize=0&recforerun=2&recoverrun=<option+value=&skip13=<option+value=&skip46=<option+value=&skip79=<option+value=&playertype=1&autochangechannelname=0&def_rectimer_after=0&showchanneltimeline=1&screenanim=0&screenanimspeed=1&channellistview=0&showlastpos=1&recsync=0&recordnamefmt=0&newsletter=1&showhiddenfiles=1&expertmodus=0&infobarprogram=0&crosscontrol=0&emucontrol=0&choiceminitv=0&usecec=0&sataswitch=0&playerbufferseektime=3&send=Send
+//playerbufferseektime.value\"></input>&nbsp;<input class=button type=reset name=reset value=\"Reset\"></input></form></center></body></html>", &maxlen, &pos);
+//writesys("/tmp/tmpstr2", buf, 1);
+	//ostrcatbig(&buf, param, &maxlen, &pos);
+	return buf;
+	
 }
 
 #endif
