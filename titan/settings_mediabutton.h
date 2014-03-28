2b8e209b68cd1ff5f18bf0406fbab7e8af71a2ec
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-03-28 03:28:30 +0000

    fix

--- titan/settings_mediabutton.h
+++ titan/settings_mediabutton.h
@@ -11,6 +11,8 @@ void screensettings_mediabutton()
 // needed for translation
 	char* tmptxt = NULL;
 	tmptxt = ostrcat(tmptxt, _("RecordPlayer"), 1, 0);
+	tmptxt = ostrcat(tmptxt, _("Media Plugins List"), 1, 0);
+	tmptxt = ostrcat(tmptxt, _("MediaPlayer"), 1, 0);
 	free(tmptxt), tmptxt = NULL;
 
 	addmenulist(&mlist, "Media Plugins List", NULL, NULL, 0, 0);
