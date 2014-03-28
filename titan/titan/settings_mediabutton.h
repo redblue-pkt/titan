0c2eb28f789fca9ced40ce25911fbbe7d8cb97e8
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-03-28 00:27:06 +0000

    add lost file

---
+++ titan/titan/settings_mediabutton.h
@@ -0,0 +1,45 @@
+#ifndef SETTINGS_MEDIABUTTON_H
+#define SETTINGS_MEDIABUTTON_H
+
+void screensettings_mediabutton()
+{
+	char* skintitle = _("Media Key Action - Setup");
+	struct menulist* mlist = NULL, *mbox = NULL;
+
+// needed for translation
+	char* tmptxt = NULL;
+	tmptxt = ostrcat(tmptxt, _("MediaCenter"), 1, 0);
+	tmptxt = ostrcat(tmptxt, _("VideoPlayer"), 1, 0);
+	tmptxt = ostrcat(tmptxt, _("AudioPlayer"), 1, 0);
+	tmptxt = ostrcat(tmptxt, _("PicturePlayer"), 1, 0);
+	tmptxt = ostrcat(tmptxt, _("MediaThek"), 1, 0);
+	tmptxt = ostrcat(tmptxt, _("MiniPlayer"), 1, 0);
+	free(tmptxt), tmptxt = NULL;
+
+	addmenulist(&mlist, "MediaCenter List", NULL, NULL, 0, 0);
+	addmenulist(&mlist, "VideoPlayer", NULL, NULL, 0, 0);
+	addmenulist(&mlist, "AudioPlayer", NULL, NULL, 0, 0);
+	addmenulist(&mlist, "PicturePlayer", NULL, NULL, 0, 0);
+	addmenulist(&mlist, "MediaThek", NULL, NULL, 0, 0);
+	addmenulist(&mlist, "MiniPlayer", NULL, NULL, 0, 0);
+	addmenulist(&mlist, "RecordPlayer", NULL, NULL, 0, 0);
+
+	char* mediakey = getconfig("mediakey", NULL);
+	debug(60, "Mediakey: %s (default)", mediakey);
+	
+	setmenulistdefault(mlist, mediakey);
+	mbox = menulistbox(mlist, NULL, skintitle, _("Choose your Media plugin item from the following list and show it so the plugin button"), NULL, NULL, 1, 0);
+
+	if(mbox == NULL)
+	{
+		free(tmpstr); tmpstr = NULL;
+		return;
+	}
+	debug(60, "(new) Mediakey=%s", mbox->name);
+
+	freemenulist(mlist, 1); mlist = NULL;
+	free(tmpstr); tmpstr = NULL;
+	return;
+}
+
+#endif
