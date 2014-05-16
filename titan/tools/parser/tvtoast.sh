7f92f9b954a51f12affc21932a299d42b715e280
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-05-16 23:42:08 +0000

    fix tvtoast server parser

--- titan/tools/parser/tvtoast.sh
+++ titan/tools/parser/tvtoast.sh
@@ -25,16 +25,15 @@ if [ "$buildtype" = "full" ];then
 	$wgetbin http://tvtoast.com -O cache.tvtoast.main.html
 
 	list=`cat cache.tvtoast.main.html | tr '\n' '\r' |  tr '\r' ' ' |  tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<a href="sub-category/!\n\n<a href="sub-category/!g' | grep ^'<a href="sub-category/' | sed 's/ /~/g'`
-	
+
 	for ROUND in $list; do
 		count=`expr $count + 1`
 		ROUND=`echo $ROUND | tr '~' ' '` 
 		url=http://tvtoast.com/`echo $ROUND | cut -d'"' -f2`
 		tagname=`echo $ROUND | sed 's/<h3>/\ntitle</g' | grep ^"title<" | cut -d"<" -f2`
 		filename1=`echo $tagname | tr ' ' '.' | tr 'A-Z' 'a-z'`
-		$wgetbin $url -O cache.$tagname.$count.list
-		
-		list1=`cat cache.$tagname.$count.list | tr '\n' '\r' |  tr '\r' ' ' |  tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<a href="http://tvtoast.com/watch/!\n\n<a href="http://tvtoast.com/watch/!g' | grep ^'<a href="http://tvtoast.com/watch/' | sed 's/ /~/g'`
+		$wgetbin "$url" -O cache.$filename1.$count.list
+		list1=`cat cache.$filename1.$count.list | tr '\n' '\r' |  tr '\r' ' ' |  tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<a href="http://tvtoast.com/watch/!\n\n<a href="http://tvtoast.com/watch/!g' | grep ^'<a href="http://tvtoast.com/watch/' | sed 's/ /~/g'`
 
 		for ROUND1 in $list1; do
 			count=`expr $count + 1`
@@ -42,10 +41,11 @@ if [ "$buildtype" = "full" ];then
 			ROUND1=`echo $ROUND1 | tr '~' ' '` 
 			URL=`echo $ROUND1 | cut -d'"' -f2`
 			TITLE=`echo $ROUND1 | sed 's/title=/\ntitle=/g' | grep ^"title=" | cut -d'"' -f2`
-			picname=`echo $ROUND1 | sed 's/channel-logo /\nchannel-logo"/g' | grep ^"channel-logo" | cut -d'"' -f2`
-			
+			picname=`echo $ROUND1 | sed 's/channel-logo color-white/\nchannel-logo-color-white"/g' | grep ^"channel-logo" | cut -d'"' -f2 | sed 's/ //g'`
 			filename=`echo $TITLE | tr ' ' '.' | tr 'A-Z' 'a-z'`
+
 			id=`echo $url | cut -d"=" -f2`
+
 			$wgetbin http://tvtoast.com/css/channel-logo-css.css?v=$id -O cache.$filename.$count.list
 			list2=`cat cache.$filename.$count.list | tr '\n' '\r' |  tr '\r' ' ' |  tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed "s!$picname!\n\n$picname!g" | grep ^"$picname" | sed 's/ /~/g'`
 			PIC=`echo $list2 | sed 's!../uploads/!\n\npiclink=)http://tvtoast.com/uploads/!g' | grep ^"piclink=)http://tvtoast.com/uploads" | head -n1 | cut -d")" -f2`
