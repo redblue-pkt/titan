bf8a7e121abe75116e1ec2100022e002ae18bf3e
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-06-02 18:38:16 +0000

    [titan] new function... resize

--- titan/header.h
+++ titan/header.h
@@ -662,6 +662,7 @@ int convertxmlentry(char *value, uint8_t *proz);
 unsigned char* savejpg(char* filename, int width, int height, int channels, int newwidth, int newheight, int quality, unsigned char *buf);
 inline void drawpixelfastfb(struct fb* node, int posx, int posy, unsigned long color);
 inline void drawpixelfb(struct fb* node, int posx, int posy, unsigned long color);
+unsigned char * resize(unsigned char *origin, int ox, int oy, int dx, int dy, int type, unsigned char * dst, int flag)
 
 //volume.h
 void screenvolumeup();
