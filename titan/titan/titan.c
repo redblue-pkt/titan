c6ae8c1b2392931a12af8b9b9c65956f312dcefd
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-06-17 20:34:46 +0000

    fix

--- titan/titan/titan.c
+++ titan/titan/titan.c
@@ -634,50 +634,50 @@ int main(int argc, char *argv[])
 		if(ostrcmp(string_newline(gettimeinfo()), TIMECODE) == 1)
 		{
 			printf("error: 1\n");
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 		if(ostrcmp(string_newline(gettimeinfovar()), TIMECODE) == 1)
 		{
 			printf("error: 2\n");		
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 		if(checkreseller() != 0)
 		{
 			printf("error: 3\n");		
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 		if(getsysinfo() != SYSCODE)
 		{
 			printf("error: 4\n");		
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 		if(file_exist("/mnt/swapextensions/etc/.vnumber") == 1)
 		{
 			printf("error: 5\n");		
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 		if(checkflash() != 0)
 		{
 			printf("error: 6\n");		
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 		if((checkbox("ATEMIO-NEMESIS") == 1 || checkbox("ATEMIO5200") == 1 || checkbox("UFS913") == 1) && checkhighflash() != 0)
 		{
 			printf("error: 7\n");		
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 		if(checkbox("UFS910") == 1 && checklowflash() != 0)
 		{
 			printf("error: 8\n");		
-//			destroy();
-//			exit(100);
+			destroy();
+			exit(100);
 		}
 	
 		char* cpuid = getcpuid();
