c03c857dcf7bc158f255769be1fe0882b17c5f28
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-09-24 12:18:12 +0000

    [webif] add translate stuff

--- titan/httpd.h
+++ titan/httpd.h
@@ -640,13 +640,47 @@ void gotdata(int* connfd)
 				return;
 			}
 
+			int translate = 0;
+			if(ostrstr(filename, ".html") != NULL)
+			{
+				translate = 1;
+				debug(250, "filename: %s", filename);
+				debug(250, "fullfilename: %s/%s", getconfig("httpdpath", NULL), filename);				
+				debug(250, "buf1: %s", buf);
+			}
+
 			//TODO:
 			int readret = 1;
 			while(readret > 0 && auth == 0)
 			{
 				readret = dvbreadfd(filefd, buf, 0, MINMALLOC, 1000, 0);
+				if(translate == 1)
+					debug(250, "buf2: %s", buf);
+
+				if(translate == 1)
+				{
+					while(ostrstr(buf, "_\(\"") != NULL)
+					{
+						char* tmpstr1 = string_resub("_(\"", "\")", buf, 0);
+						char* tmpstr2 = ostrcat("_(\"", tmpstr1, 0, 0);
+						tmpstr2 = ostrcat(tmpstr2, "\")", 1, 0);
+
+						debug(250, "######################################");
+						debug(250, "Search  string: %s", tmpstr2);
+						debug(250, "Replace string: %s", tmpstr1);
+						debug(250, "Replace %s -> %s", tmpstr2, tmpstr1);
+						debug(250, "######################################");
+	
+						buf = string_replace_all(tmpstr2, _(tmpstr1), buf, 1);
+						free(tmpstr1), tmpstr1 = NULL;
+						free(tmpstr2), tmpstr2 = NULL;
+					}
+
+					debug(250, "buf3....................: %s", buf);		
+				}
 				if(readret > 0)
 					socksend(connfd, buf, readret, 5000 * 1000);
+
 			}
 		}
 	}
