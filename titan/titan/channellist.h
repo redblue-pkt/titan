3dd887b457ba03b1fcfffd62e60913668e145124
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-09-25 12:00:39 +0000

    reset to rev 30109

--- titan/titan/channellist.h
+++ titan/titan/channellist.h
@@ -1003,7 +1003,7 @@ start:
 						listbox->aktpage = -1;
 					}
 					else
-						textbox(_("Message"), _("Can't remove active Service"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 7, 0);
+						textbox(_("Message"), _("Can't remove akive Service"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 7, 0);
 				}
 				if(list == SATLIST)
 				{
