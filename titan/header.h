b6edc0de85b8971ee149f66b7fd96e4e3dba410a
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-10-25 11:24:55 +0000

    fix

--- titan/header.h
+++ titan/header.h
@@ -576,6 +576,7 @@ void playerafterendts();
 int playerisplayingts();
 char* playergetinfo(char* tag);
 int playerstartts(char* file, int flag);
+static void analyze_streams(CustomData *data);
 
 //filelist.h
 void getfilelist(struct skin* input, struct skin* filelistpath, struct skin* filelist, char* path, char* filemask, int tmpview, char* selection);
