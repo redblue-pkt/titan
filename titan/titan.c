2f652f5541e72f045974a5b7886a2ca1ea665e63
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2014-12-16 16:53:05 +0000

    reset

--- titan/titan.c
+++ titan/titan.c
@@ -622,9 +622,9 @@ int main(int argc, char *argv[])
 
 	if(checkbox("UFS922") == 1)
 		setfanspeed(-1, 0);
-	if(checkbox("ATEMIO-NEMESIS") == 1)
-	{
-		if(getconfigint("fanmode", NULL) == 0)
+  if(checkbox("ATEMIO-NEMESIS") == 1)
+  {
+  	if(getconfigint("fanmode", NULL) == 0)
 			addconfig("fanmode", "3");
 		writesys("/proc/stb/fp/fan", getconfig("fanmode", NULL), 1);
 	}
@@ -767,13 +767,11 @@ int main(int argc, char *argv[])
 	tmpstr = NULL;
 
 #ifdef MIPSEL
-// disable... with this jpg not working
-//	status.usedirectfb = 1;
 	waitvsync();
 	setfbosd();
+	status.usedirectfb = 1;
 #endif
-// disable... with this jpg not working	
-//#else
+
 	if(status.usedirectfb != 1)
 	{
 		skinfb = addfb(SKINFB, 0, getconfigint("skinfbwidth", NULL), getconfigint("skinfbheight", NULL), 4, fb->fd, fb->fb + fb->varfbsize, fb->fixfbsize);
@@ -789,15 +787,14 @@ int main(int argc, char *argv[])
 			status.usedirectfb = 1;
 		}
 	}
-// disable... with this jpg not working	
-//#endif
-
 	if(status.usedirectfb == 1)
 	{
 		skinfb = fb;
+#ifndef MIPSEL
 		ret = getfbsize(0);
 		if(ret > 0) 
 			accelfb = addfb(ACCELFB, 0, ret / 4, 1, 4, fb->fd, skinfb->fb + skinfb->varfbsize, fb->fixfbsize);
+#endif
 	}
 
 	//if(lcdskinfb == NULL) {
@@ -851,7 +848,8 @@ int main(int argc, char *argv[])
 	ret = dvrgetdev();
 	ret = scgetdev();
 #ifdef MIPSEL
-	//ret = encodergetdev();
+	if(checkbox("ATEMIO-NEMESIS") == 1)
+		ret = encodergetdev();
 #endif
 
 	settunerstatus();
