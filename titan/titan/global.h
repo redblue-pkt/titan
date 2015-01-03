17f167c67c55882c1a553beed9375b913ebba2c1
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-01-03 23:52:04 +0000

    move int setrtctimemips to mipselport

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -6750,46 +6750,4 @@ void wakeup_record_device()
 	free(cmd), cmd = NULL;
 }
 
-#ifdef MIPSEL
-
-int setrtctimemips()
-{
-	char *rtctimedev = NULL, *tmpstr = NULL;
-	int ret = 0;
-	int value = 0;
-	
-	time_t t = time(NULL);
-  struct tm *local = localtime(&t);
- 	time_t rawlocal = mktime(local);
- 	
- 	t = time(NULL);
-  struct tm *gmt = gmtime(&t);
-  time_t rawgmt = mktime(gmt);
-  
-  int offset = difftime(rawlocal, rawgmt);
-
-	tmpstr = oitoa(offset);
-	rtctimedev = getconfig("rtctime_offsetdev", NULL);
-	if(rtctimedev != NULL)
-		ret = writesys(rtctimedev, tmpstr, 0);
-	else
-		ret = writesys("/proc/stb/fp/rtc_offset", tmpstr, 0);
-	free(tmpstr); tmpstr = NULL;
-	rtctimedev = NULL;
-	
-	if(ret == 0)
-	{
-		tmpstr = oitoa(rawlocal);
-		rtctimedev = getconfig("rtctimedev", NULL);
-		if(rtctimedev != NULL)
-			ret = writesys(rtctimedev, tmpstr, 0);
-		else
-			ret = writesys("/proc/stb/fp/rtc", tmpstr, 0);
-		free(tmpstr); tmpstr = NULL;
-		rtctimedev = NULL;
-	}
-	return ret;
-}
-#endif
-
 #endif
