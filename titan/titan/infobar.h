13f0ce8d0aa0c38eb9c688b46b17402ad6ed8e6b
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-01-07 17:50:32 +0000

    fix

--- titan/titan/infobar.h
+++ titan/titan/infobar.h
@@ -605,6 +605,7 @@ void screeninfobar()
 			subtitlepause(1);
 			status.infobar = 0;
 			clearscreen(infobar);
+			status.updatevfd = PAUSE;
 			screenkeyactions(2, 0);
 			status.updatevfd = START;
 			drawscreen(skin, 0, 0);
