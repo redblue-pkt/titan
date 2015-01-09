88ef837eeca23e7b3b7a5b18b53f3db3ba453b0b
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-01-09 00:45:48 +0000

    fix output

--- titan/titan/tpk.h
+++ titan/titan/tpk.h
@@ -3048,7 +3048,7 @@ int tpkupdate(int flag)
 			// disable settings upgrade group 100
 			if(ostrcmp(tpkinstallednode->name, tpknode->name) == 0 && tpknode->version > tpkinstallednode->version && tpkinstallednode->group != 100 && tpknode->minversion <= PLUGINVERSION)
 			{
-				debug(10, "Tpk Upgrade: %s (Install Version: %d > Installed Version: %d) (Min Version: %d <= Plugin Version: %d) (Group: %d)\n", tpknode->name, tpkinstallednode->version, tpknode->version, tpknode->minversion, PLUGINVERSION, tpkinstallednode->group);
+				debug(10, "Tpk Upgrade: %s (Install Version %d > %d Installed Version) (Min Version %d <= %d Plugin Version) (Group %d)\n", tpknode->name, tpknode->version, tpkinstallednode->version, tpknode->minversion, PLUGINVERSION, tpkinstallednode->group);
 				//TODO: remove file that never exist in new pakages
 				tmpstr = ostrcat(tmpstr, EXTRACTDIR, 1, 0);
 				tmpstr = ostrcat(tmpstr, "/", 1, 0);
