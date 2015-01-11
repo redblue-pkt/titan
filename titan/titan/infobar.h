221e55eb78b209a53fb10606afb786264530b9c9
tobayer <tobayer@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-01-11 02:43:41 +0000

    [titan] add infobar selection, step 3, fix

--- titan/titan/infobar.h
+++ titan/titan/infobar.h
@@ -13,10 +13,16 @@ void screeninfobar()
 	debug(10, "get infobar2=%s", infobar2_sel);
 
 	//if(infobar_sel == NULL) ostrcat(infobar_sel, "infobar", 0, 0); // fallback to default
-	if(ostrcmp(infobar_sel, NULL) == 0) ostrcat(infobar_sel, "infobar", 0, 0); // fallback to default
+	if(ostrcmp(infobar_sel, NULL) == 0) {
+		ostrcat(infobar_sel, "infobar", 0, 0); // fallback to default
+		debug(10, "fallback, set infobar1=%s", infobar_sel);
+	}
 
 	//if(infobar2_sel == NULL) ostrcat(infobar2_sel, "infobar2", 0, 0); // fallback to default
-	if(ostrcmp(infobar2_sel, NULL) == 0) ostrcat(infobar2_sel, "infobar", 0, 0); // fallback to default
+	if(ostrcmp(infobar2_sel, NULL) == 0) {
+		ostrcat(infobar2_sel, "infobar", 0, 0); // fallback to default
+		debug(10, "fallback, set infobar2=%s", infobar2_sel);
+	}
 
 	struct skin* infobar1 = getscreen(infobar_sel);
 	debug(10, "get infobar1=%s", infobar_sel);
