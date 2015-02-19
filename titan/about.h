3dab5c2b1b1e611327b0405506cf3a1c0b679d1d
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-02-19 11:22:08 +0000

    enable demo thread

--- titan/about.h
+++ titan/about.h
@@ -6,7 +6,7 @@ int screenabout(void)
 	char* tmpstr = NULL;
 	tmpstr = getabout();
 
-	textbox(_("System-Info"), tmpstr, _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1100, 700, 0, 0);
+	textbox(_("System-Info"), tmpstr, _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 600, 0, 0);
 	free(tmpstr);
 
 	return 0;
