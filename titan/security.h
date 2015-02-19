e28cb2fb635e06778a3824e041bc58d8355c2d8b
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-02-19 02:08:00 +0000

    fix

--- titan/security.h
+++ titan/security.h
@@ -657,7 +657,7 @@ void trialendemodethread(struct stimerthread* self)
 {
 	if(status.security >= 1) return;
 	sleep(30);
-	textbox(_("Info"), _("!!! Trial period ended !!!\n\nFor new trial period install a new nightly image via USB\nor purchase a license from Atemio !!!\n\nContact:\nTel +49 (0) 6403/97759-0\nFax +49 (0) 6403/97759-10\nEmail info@atemio.de\nWebsite www.atemio.de"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 900, 400, 30, 0);
+	textbox(_("Info"), _("!!! Trial period ended !!!\n\nFor new trial period install a new nightly image via USB\nor purchase a license from Atemio !!!\n\nContact:\nTel +49 (0) 6403/97759-0\nFax +49 (0) 6403/97759-10\nEmail info@atemio.de\nWebsite www.atemio.de"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1000, 400, 30, 0);
 }
 
 void trialcheckmodethread(struct stimerthread* self)
