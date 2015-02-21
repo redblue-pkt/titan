cb37c57fbb24fb27ba2269cd89b7fc8ced71d9d3
caliban <caliban@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-02-21 09:17:07 +0000

    next

--- titan/titan/tunerconfig.h
+++ titan/titan/tunerconfig.h
@@ -658,7 +658,7 @@ int screentunerreceptionhyprid(struct dvbdev* tuner)
 //					realname = gethypridtunername(tuner->devnr, listbox->select->ret);
 //					strcpy(tuner->feinfo->name, realname);
 //					textbox(_("Message"), _("They need to switch the tuner Hyprid restart the gui !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 800, 200, 0, 0);
-					if(checkbox("SPARK7162"") == 1)
+					if(checkbox("SPARK7162") == 1)
 						textbox(_("Message"), _("They need to switch the tuner Hyprid a Box Restart!"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 800, 200, 0, 0);
 					changedvbdev(tuner);
 				}
