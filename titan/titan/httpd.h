da57a430ae07d6b4fdb0ded20f547533a38639d3
wonderdoc <wonderdoc@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-02-22 16:14:10 +0000

    [titan] add WebIf function getbouquetm3u, next try

--- titan/titan/httpd.h
+++ titan/titan/httpd.h
@@ -440,7 +440,7 @@ void checkquery(int* connfd, char* query, int auth, int fmt)
 		}
 	}
 #endif
-	/*else if(ostrcmp(query, "getbouquetm3u") == 0)
+	else if(ostrcmp(query, "getbouquetm3u") == 0)
 	{
 		buf = webgetbouquetm3u(param, *connfd, fmt);
 		if(fmt == 0)
@@ -448,7 +448,7 @@ void checkquery(int* connfd, char* query, int auth, int fmt)
 			ext = "Content-Disposition: attachment; filename=bouquetstream.m3u";
 			mime = "audio/x-mpegurl";
 		}
-	}*/
+	}
 	else if(ostrcmp(query, "getvideo") == 0)
 		buf = webgetvideo(param, *connfd, fmt);
 	else if(ostrcmp(query, "videoplay") == 0 || ostrcmp(query, "videoplay=") == 0)
--- titan/titan/httpdfunc.h
+++ titan/titan/httpdfunc.h
@@ -284,13 +284,13 @@ void webcreatechannelbody(char** buf, int line, struct channel* chnode, char* ch
 		{
 			ostrcatbig(buf, "</td><td width=100 align=right valign=middle nowrap><img style=\"margin-left: 5\" border=0 src=img/tv.png title=\"", maxlen, pos);
 			ostrcatbig(buf, _("TV"), maxlen, pos);
-			ostrcatbig(buf, "\" width=16 height=16></a>", maxlen, pos);		
+			ostrcatbig(buf, "\" width=16 height=16>", maxlen, pos);		
 		}
 		else
 		{
 			ostrcatbig(buf, "</td><td width=100 align=right valign=middle nowrap><img style=\"margin-left: 5\" border=0 src=img/radio.png title=\"", maxlen, pos);
 			ostrcatbig(buf, _("Radio"), maxlen, pos);
-			ostrcatbig(buf, "\" width=16 height=16></a>", maxlen, pos);
+			ostrcatbig(buf, "\" width=16 height=16>", maxlen, pos);
 		}
 
 		//single epg
@@ -337,7 +337,7 @@ void webcreatechannelbody(char** buf, int line, struct channel* chnode, char* ch
 		{
 			ostrcatbig(buf, "<img style=\"margin-left: 5\" border=0 src=img/cross.png title=\"", maxlen, pos);
 			ostrcatbig(buf, _("Channel not available"), maxlen, pos);
-			ostrcatbig(buf, "\" width=16 height=16></a>", maxlen, pos);
+			ostrcatbig(buf, "\" width=16 height=16>", maxlen, pos);
 		}
 		ostrcatbig(buf, "</td></tr>", maxlen, pos);
 	}
@@ -979,6 +979,70 @@ char* webgettranscodem3u(char* param, int connfd, int fmt)
 	return buf;
 }
 #endif
+
+char* webgetbouquetm3u(char* param, int connfd, int fmt)
+{
+	int extip = 1;
+	char* buf = NULL, *ip = NULL, *streamport = NULL;
+	struct sockaddr_in sin;
+	struct mainbouquet *mbouquet = NULL;
+	struct bouquet *node = NULL;
+	struct channel* chnode = NULL;
+	socklen_t len = sizeof(sin);
+	
+	if(param == NULL) return NULL;
+
+	if(getconfigint("webifip", NULL) == 1)
+		ip = getispip();
+
+	if(ip == NULL)
+	{
+		if(getsockname(connfd, &sin, &len) < 0)
+		{
+			perr("getsockname");
+			return NULL;
+		}
+
+		extip = 0;
+		ip = inet_ntoa(sin.sin_addr);
+			if(ip == NULL) return NULL;
+	}
+	
+	streamport = getconfig("streamport", NULL);
+
+	buf = ostrcat(buf, "#EXTM3U\n", 1, 0);
+	buf = ostrcat(buf, "#EXTVLCOPT--http-reconnect=true\n", 1, 0);
+	
+	mbouquet = getmainbouquet(param);
+	if(mbouquet != NULL)
+	{
+		node = mbouquet->bouquet;
+		while(node != NULL)
+		{
+			chnode = getchannel(node->serviceid, node->transponderid);
+			if(chnode != NULL)
+			{
+				buf = ostrcat(buf, "#EXTINF:-1,", 1, 0);
+				buf = ostrcat(buf, chnode->name, 1, 0);
+				buf = ostrcat(buf, "\n", 1, 0);
+
+				buf = ostrcat(buf, "http://", 1, 0);
+				buf = ostrcat(buf, ip, 1, 0);
+				buf = ostrcat(buf, ":", 1, 0);
+				buf = ostrcat(buf, streamport, 1, 0);
+				buf = ostrcat(buf, "/", 1, 0);
+				buf = ostrcat(buf, oitoa(chnode->serviceid), 1, 0);
+				buf = ostrcat(buf, "%2c", 1, 0);
+				buf = ostrcat(buf, ollutoa(chnode->transponderid), 1, 0);
+				buf = ostrcat(buf, "\n", 1, 0);
+			}
+			node = node->next;
+		}
+	}
+	if(extip == 1) free(ip);
+	free(streamport); streamport = NULL;
+	return buf;
+}
 
 char* webgetvideo(char* param, int connfd, int fmt)
 {
