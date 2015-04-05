31a587c59e911775c49ef30f17dca1404203f3f4
tobayer <tobayer@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-04-05 17:26:12 +0000

    [titan] fix bordersize for timeline in gmultiepg

--- titan/titan/gmultiepg.h
+++ titan/titan/gmultiepg.h
@@ -62,10 +62,7 @@ void createtimeline(struct skin* gmultiepg, struct skin* timeline, time_t akttim
 			node->prozwidth = 0;
 			node->posx = posx;
 			node->width = 30 * zoom; //30 min * zoom pixel
-			if(timeline->bordersize != NULL)
-				node->bordersize = timeline->bordersize;
-			else
-				node->bordersize = 1;
+			node->bordersize = timeline->bordersize;
 			posx += node->width;
 			if(posx > timeline->iwidth)
 				node->width -= (posx - timeline->iwidth);
