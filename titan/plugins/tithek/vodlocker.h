512906936363d5c31e9836a9dc6d11d41df7870e
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-04-21 08:51:09 +0000

    [tithek] add vodlocker hoster

---
+++ titan/plugins/tithek/vodlocker.h
@@ -0,0 +1,102 @@
+#ifndef VODLOCKER_H
+#define VODLOCKER_H
+
+char* vodlocker(char* link)
+{
+	debug(99, "link: %s", link);
+	int debuglevel = getconfigint("debuglevel", NULL);
+	char* tmphost = NULL, *error = NULL, *tmppath = NULL, *tmpstr = NULL, *send = NULL, *id = NULL, *fname = NULL, *hashmsg = NULL, *hash = NULL, *hashlen = NULL, *ip = NULL, *streamlink = NULL;
+	
+	if(link == NULL) return NULL;
+
+	unlink("/tmp/vodlocker1_get");
+	unlink("/tmp/vodlocker2_post");
+	unlink("/tmp/vodlocker3_post");
+	unlink("/tmp/vodlocker4_streamlink");
+/////////////
+	char* tmplink = NULL, *pos = NULL, *path = NULL;
+
+	tmplink = ostrcat(link, NULL, 0, 0);
+
+	if(ostrstr(link, "/Out/?s=") != NULL)
+	{
+		tmplink = string_replace("/Out/?s=", "", tmplink, 1);
+		debug(99, "remove out string: %s", tmplink);
+	}
+
+
+	if(tmplink == NULL || ostrncmp("http://", tmplink, 7))
+	{
+		textbox(_("Message"), _("Hoster Url not http://") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
+		goto end;
+	}
+
+	tmphost = string_replace("http://", "", tmplink, 0);
+	free(tmplink) , tmplink = NULL;
+
+	if(tmphost != NULL)
+		pos = strchr(tmphost, '/');
+	if(pos != NULL)
+	{
+		pos[0] = '\0';
+		path = pos + 1;
+	}
+
+	tmppath = ostrcat("/embed-", path, 0, 0);
+	tmppath = ostrcat(tmppath, "-640x400.html", 1, 0);
+	
+//	free(path), path = NULL;
+
+/////////////
+
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmppath, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\n", 1, 0);
+	send = ostrcat(send, "Host: ", 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\n", 1, 0);
+	send = ostrcat(send, "Connection: close\r\n", 1, 0);
+	send = ostrcat(send, "Accept-Encoding: gzip\r\n\r\n", 1, 0);
+	debug(99, "send: %s", send);
+
+	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+	free(send), send = NULL;
+	debug(99, "tmpstr: %s", tmpstr);
+	titheklog(debuglevel, "/tmp/vodlocker1_get", NULL, NULL, NULL, tmpstr);
+
+	if(tmpstr == NULL)
+	{
+		textbox(_("Message"), _("The page is temporarily unavailable") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
+		goto end;
+	}
+
+	if(ostrstr(tmpstr, "404 Not Found") != NULL)
+	{
+		textbox(_("Message"), _("The video no longer exists") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
+		goto end;
+	}
+
+	streamlink = string_resub("file: \"", "\"", tmpstr, 0);		
+
+	titheklog(debuglevel, "/tmp/vodlocker4_streamlink", NULL, NULL, NULL, streamlink);
+
+	free(tmpstr); tmpstr = NULL;
+
+end:
+
+	free(error); error = NULL;
+	free(tmphost); tmphost = NULL;
+	free(tmppath); tmppath = NULL;
+	free(tmpstr); tmpstr = NULL;
+	free(send); send = NULL;
+	free(hash), hash = NULL;
+	free(hashlen), hashlen = NULL;
+	free(hashmsg), hashmsg = NULL;
+	free(id), id = NULL;
+	free(fname), fname = NULL;
+	free(ip); ip = NULL;
+
+	return streamlink;
+}
+
+#endif
