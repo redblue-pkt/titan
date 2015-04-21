30483b2973841c20e9e45b911ebb8ee1255660be
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-04-21 08:34:30 +0000

    [tithek] add vidbull hoster

--- titan/plugins/tithek/tithek_global.h
+++ titan/plugins/tithek/tithek_global.h
@@ -248,6 +248,8 @@ char* hoster(char* url)
 		streamurl = promptfile(url);
 	else if(ostrstr(tmplink, "letwatch") != NULL)
 		streamurl = letwatch(url);
+	else if(ostrstr(tmplink, "vidbull") != NULL)
+		streamurl = vidbull(url);
 	else
 		textbox(_("Message"), _("The hoster is not yet supported !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 5, 0);
 
---
+++ titan/plugins/tithek/vidbull.h
@@ -0,0 +1,124 @@
+#ifndef VIDBULL_H
+#define VIDBULL_H
+
+char* vidbull(char* link)
+{
+	debug(99, "link: %s", link);
+	int debuglevel = getconfigint("debuglevel", NULL);
+	char* tmphost = NULL, *error = NULL, *tmppath = NULL, *tmpstr = NULL, *send = NULL, *id = NULL, *fname = NULL, *hashmsg = NULL, *hash = NULL, *hashlen = NULL, *ip = NULL, *streamlink = NULL;
+	
+	if(link == NULL) return NULL;
+
+	unlink("/tmp/vidbull1_get");
+	unlink("/tmp/vidbull2_post");
+	unlink("/tmp/vidbull3_post");
+	unlink("/tmp/vidbull4_streamlink");
+/////////////
+	char* tmplink = NULL, *pos = NULL, *path = NULL;
+
+	tmplink = ostrcat(link, NULL, 0, 0);
+
+	if(ostrstr(link, "/Out/?s=") != NULL)
+	{
+		tmplink = string_replace("/Out/?s=", "", tmplink, 1);
+		debug(99, "remove out string: %s", tmplink);
+	}
+
+
+	if(tmplink == NULL || ostrncmp("http://", tmplink, 7))
+	{
+		textbox(_("Message"), _("Hoster Url not http://") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
+		goto end;
+	}
+
+	tmphost = string_replace("http://", "", tmplink, 0);
+	free(tmplink) , tmplink = NULL;
+
+	if(tmphost != NULL)
+		pos = strchr(tmphost, '/');
+	if(pos != NULL)
+	{
+		pos[0] = '\0';
+		path = pos + 1;
+	}
+
+	tmppath = ostrcat("/", path, 0, 0);
+	tmppath = string_replace_all(".html", "", tmppath, 1);
+
+	tmphost = ostrcat("www.", tmphost, 0 ,1);
+
+//	free(path), path = NULL;
+
+/////////////
+//17:54:59 T:2284  NOTICE: 'GET /xl0rxekh8c7b HTTP/1.1\r\nHost: www.vidbull.com\r\nUser-Agent: Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n'
+/*
+GET /xl0rxekh8c7b HTTP/1.1\r\n
+Host: www.vidbull.com\r\n
+User-Agent: Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36\r\n
+Connection: close\r\n
+Accept-Encoding: gzip\r\n\r\n'
+
+[titan] send: GET /dasoa03go9iz HTTP/1.1
+Host: www.vidbull.com
+User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1
+Connection: close
+Accept-Encoding: gzip
+
+
+[titan] send: 
+GET /dasoa03go9iz.html HTTP/1.1
+Host: vidbull.com
+User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1
+Connection: close
+Accept-Encoding: gzip
+*/
+	send = ostrcat(send, "GET ", 1, 0);
+	send = ostrcat(send, tmppath, 1, 0);
+	send = ostrcat(send, " HTTP/1.1\r\n", 1, 0);
+	send = ostrcat(send, "Host: ", 1, 0);
+	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36\r\n\r\n", 1, 0);
+	send = ostrcat(send, "Connection: close\r\n", 1, 0);
+	send = ostrcat(send, "Accept-Encoding: gzip\r\n\r\n", 1, 0);
+	debug(99, "send: %s", send);
+
+	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+	free(send), send = NULL;
+	debug(99, "tmpstr: %s", tmpstr);
+	titheklog(debuglevel, "/tmp/vidbull1_get", NULL, NULL, NULL, tmpstr);
+
+	if(tmpstr == NULL)
+	{
+		textbox(_("Message"), _("The page is temporarily unavailable") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
+		goto end;
+	}
+
+	if(ostrstr(tmpstr, "File Not Found (404)") != NULL)
+	{
+		textbox(_("Message"), _("The video no longer exists") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
+		goto end;
+	}
+
+	streamlink = string_resub("<source src=\"", "\"", tmpstr, 0);
+	titheklog(debuglevel, "/tmp/vidbull4_streamlink", NULL, NULL, NULL, streamlink);
+
+	free(tmpstr); tmpstr = NULL;
+
+end:
+
+	free(error); error = NULL;
+	free(tmphost); tmphost = NULL;
+	free(tmppath); tmppath = NULL;
+	free(tmpstr); tmpstr = NULL;
+	free(send); send = NULL;
+	free(hash), hash = NULL;
+	free(hashlen), hashlen = NULL;
+	free(hashmsg), hashmsg = NULL;
+	free(id), id = NULL;
+	free(fname), fname = NULL;
+	free(ip); ip = NULL;
+
+	return streamlink;
+}
+
+#endif
