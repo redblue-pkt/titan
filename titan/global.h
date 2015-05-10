62fda2c1387fbe321f05752720c4db981e627fac
caliban <caliban@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-05-10 12:33:00 +0000

    fix special char

--- titan/global.h
+++ titan/global.h
@@ -1487,7 +1487,7 @@ void delspezchar(char* text, int flag)
 			if(tmpstr[0] == '/') tmpstr[0] = '-';
 			if(flag == 2)
 			{
-				if(tmpstr[0] == '\A7') tmpstr[0] = '-';
+				if(tmpstr[0] == '§') tmpstr[0] = '-';
 				if(tmpstr[0] == '<') tmpstr[0] = '-';
  				if(tmpstr[0] == '>') tmpstr[0] = '-';
 				if(tmpstr[0] == ':') tmpstr[0] = '-';
