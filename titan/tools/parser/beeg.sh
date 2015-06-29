2e558eca298baedc4085f95b492db06d29f772b4
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-06-29 23:39:11 +0000

    fix beeg cat

--- titan/tools/parser/beeg.sh
+++ titan/tools/parser/beeg.sh
@@ -47,22 +47,18 @@ if [ "$buildtype" = "full" ];then
 		count0=`expr $count0 + 1`
 		echo "sections($count0)=$ROUND0"
 	
-	#	echo $wgetbin $ROUND0
 		$wgetbin $ROUND0 -O cache.beeg.section."$count0".html
 	
 		tumbid=`cat cache.beeg.section."$count0".html | grep "var tumbid" | cut -d"[" -f2 | cut -d"]" -f1 | tr ',' ' '`
 		tumbalt=`cat cache.beeg.section."$count0".html | grep "var tumbalt" | cut -d"[" -f2 | cut -d"]" -f1 | sed "s/','\+/|/g" | tr ' ' '_' | tr '|' ' '`
 		tumburl=`cat cache.beeg.section."$count0".html | grep "var IMGthumb" | cut -d"'" -f2`
-	
-	#	echo tumbid: $tumbid
-	#	echo tumbalt: $tumbalt
-	#	echo tumburl: $tumburl
-	#sleep 2
+
 		count1=0
 		for ROUND1 in $tumbid; do
-	#if [ $count1 = 3 ];then
-	#	break
-	#fi
+#			if [ $count1 = 2 ];then
+#				echo bra
+#				break
+#			fi
 			count1=`expr $count1 + 1`
 			
 			count=`expr $count + 1`
@@ -83,71 +79,27 @@ if [ "$buildtype" = "full" ];then
 			done
 			LINE="$TITLE#$STREAMURL#$PIC#beeg_$piccount.jpg#Beeg#50"
 			echo $LINE >> cache.beeg.titanlist
+
+			tagname=`echo $ROUND0 | cut -d"/" -f5`
+			
+			if [ ! -z "$LINE" ] && [ `cat cache.beeg.$tagname.titanlist | grep "$LINE" | wc -l` -eq 0 ];then
+				echo "$LINE" >> cache.beeg.$tagname.titanlist
+				cat cache.beeg.$tagname.titanlist | sort -u > _full/beeg/streams/beeg.$tagname.list
+			fi
+
+			STREAMURL="http://atemio.dyndns.tv/mediathek/beeg/streams/beeg.$tagname.list"
+			PIC="http://atemio.dyndns.tv/mediathek/menu/$tagname.jpg"
+			LINE="$tagname#$STREAMURL#$PIC#beeg_$piccount.jpg#Beeg#3"
+			
+			if [ `cat cache.beeg.category.titanlist | grep "$tagname#$STREAMURL#$PIC#beeg_" | wc -l` -eq 0 ];then
+				echo $LINE >> cache.beeg.category.titanlist
+				cat cache.beeg.category.titanlist | sort -u > _full/beeg/beeg.category.list
+			fi
+
+
 		done
 	done
 	#fi
-	
-	for ROUND3 in $sectionstags; do
-		count3=`expr $count3 + 1`
-		$wgetbin http://beeg.com$ROUND3 -O cache.beeg.tags.$count3.html
-		tagcount=`cat cache.beeg.tags.$count3.html | grep ^'<a href="/tag/' | sort -u | cut -d"/" -f4 | tail -n1`
-	
-	#if [ $count3 = 3 ];then
-	#	break
-	#fi
-	
-		if [ ! -z $tagcount ];then
-			tags=""
-			i=1
-	#		echo tagcount = $tagcount
-			until [ "$i" -gt "$tagcount" ]
-			do
-	#		echo $i
-			tags="$tags http://beeg.com$ROUND3/$i/"
-			i=$[$i+1]
-			done
-	#		echo tags $tags
-			
-			count4=0
-			for ROUND4 in $tags; do
-				count4=`expr $count4 + 1`
-				echo "tags($count4)=$ROUND4"
-				
-				piccount=`expr $piccount + 1`
-				tagname=`echo $ROUND4 | cut -d"/" -f5`
-				STREAMURL="http://atemio.dyndns.tv/mediathek/beeg/streams/beeg.$tagname.list"
-				PIC="http://atemio.dyndns.tv/mediathek/menu/$tagname.jpg"
-				LINE="$tagname#$STREAMURL#$PIC#beeg_$piccount.jpg#Beeg#3"
-				
-				if [ `cat cache.beeg.category.titanlist | grep "$tagname#$STREAMURL#$PIC#beeg_" | wc -l` -eq 0 ];then
-					echo $LINE >> cache.beeg.category.titanlist
-					cat cache.beeg.category.titanlist | sort -u > _full/beeg/beeg.category.list
-				fi
-							
-	#			echo $wgetbin $ROUND4
-				$wgetbin $ROUND4 -O cache.beeg.tags."$count4".html
-			
-				tumbid=`cat cache.beeg.tags."$count4".html | grep "var tumbid" | cut -d"[" -f2 | cut -d"]" -f1 | tr ',' ' '`
-			
-	#			echo tumbid: $tumbid
-		#	sleep 2
-			
-				count5=0
-				for ROUND5 in $tumbid; do
-					LINE=""
-					LINE=`cat cache.beeg.titanlist | grep $ROUND5`
-		
-					if [ ! -z "$LINE" ] && [ `cat cache.beeg.$tagname.titanlist | grep "$LINE" | wc -l` -eq 0 ];then
-						count=`expr $count + 1`
-						echo "tags($tagname $count4/$i) add($count) id($ROUND5)"
-						echo "$LINE" >> cache.beeg.$tagname.titanlist
-						cat cache.beeg.$tagname.titanlist | sort -u > _full/beeg/streams/beeg.$tagname.list
-	echo "$LINE"
-					fi		
-				done
-			done
-		fi
-	done
 	
 	cp cache.beeg.titanlist _full/beeg/streams/beeg.all-newfirst.list
 	cat cache.beeg.titanlist | sort -u > _full/beeg/streams/beeg.all-sorted.list
