07f22e46c719fa99ad87883ef65ca9bf7eebeef9
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-07-27 20:12:22 +0000

    workaround auf 840

--- titan/titan/record.h
+++ titan/titan/record.h
@@ -683,7 +683,7 @@ int readwritethread(struct stimerthread* stimer, struct service* servicenode, in
 								buf[i+3] = buf[i+3] & 0x3f;
 								i = i + 188;
 							}
-							else
+/*							else
 							{
 								while(i < readret-1)
 								{
@@ -695,7 +695,7 @@ int readwritethread(struct stimerthread* stimer, struct service* servicenode, in
 										break;
 									}
 								}	
-							}
+							}*/
 						}
 					}
 				}
@@ -756,7 +756,7 @@ int readwritethread(struct stimerthread* stimer, struct service* servicenode, in
 									buf[i+3] = buf[i+3] & 0x3f;
 									i = i + 188;
 								}
-								else
+/*								else
 								{
 									while(i < readret-1)
 									{
@@ -768,7 +768,7 @@ int readwritethread(struct stimerthread* stimer, struct service* servicenode, in
 											break;
 										}
 									}	
-								}
+								}*/
 							}
 						}
 #ifndef MIPSEL
