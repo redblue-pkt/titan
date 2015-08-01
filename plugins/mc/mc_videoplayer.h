ed846491541af13fa092d20bb575e722f78a29c6
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-08-01 19:51:32 +0000

    fix

--- plugins/mc/mc_videoplayer.h
+++ plugins/mc/mc_videoplayer.h
@@ -976,7 +976,8 @@ void screenmc_videoplayer()
 				playereof(apskin, filelist, listbox, filelistpath, b2, NULL, NULL, NULL, &skip, &eof, &playlist, playertype, flag);
 			}
 
-		if(videooff == 1) drawscreen(blackscreen, 0, 0);
+		if(videooff == 1)
+			drawscreen(blackscreen, 0, 0);
 	}
 
 	deinitscreensaver();
