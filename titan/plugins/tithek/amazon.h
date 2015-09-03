ff94bd5377e852a465e60b8f6a52bff404c472e6
caliban <caliban@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-09-03 17:16:30 +0000

    fix text

--- titan/plugins/tithek/amazon.h
+++ titan/plugins/tithek/amazon.h
@@ -516,7 +516,7 @@ int amazon_search(struct skin* grid, struct skin* listbox, struct skin* countlab
 	loginret = login();
 	printf("loginret=%d\n", loginret);
 	if(loginret == 0)
-		textbox(_("Message"), _("Please Add your Amazon Prime User/Pass in Tithek Settings, and try agaun later !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1100, 200, 0, 0);
+		textbox(_("Message"), _("Please add your Amazon Prime User/Pass in Tithek settings and try again!"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1100, 200, 0, 0);
 	else
 		textbox(_("Message"), _("Amazon Prime Login Successful!"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 800, 200, 0, 0);
 
