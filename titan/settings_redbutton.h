a2cbca3b8e649cda7645edf485c0fc73ea843db9
caliban <caliban@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-09-05 13:34:13 +0000

    [titan] cleanup text

--- titan/settings_redbutton.h
+++ titan/settings_redbutton.h
@@ -13,7 +13,7 @@ void screensettings_redbutton()
 	addmenulist(&mlist, "Auto Resolution", _("Auto Resolution"), NULL, 0, 0);
 	if(checkemu() == 1)
 		addmenulist(&mlist, "Softcam Panel", _("Softcam Panel"), NULL, 0, 0);
-	addmenulist(&mlist, "TV / Radio Switch", _("TV / Radio Switch"), NULL, 0, 0);
+	addmenulist(&mlist, "TV / Radio Switch", _("TV/Radio"), NULL, 0, 0);
 	addmenulist(&mlist, "Multi EPG", _("Multi EPG"), NULL, 0, 0);
 	addmenulist(&mlist, "Graphic Multi EPG", _("Graphic Multi EPG"), NULL, 0, 0);
 	addmenulist(&mlist, "Sleep Timer", _("Sleep Timer"), NULL, 0, 0);
