1d4441bacf5cb6afd139ec5873b2c9e52f99ef19
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-10-09 16:55:35 +0000

    [titan] sh4.. test fix2 backup

--- titan/global.h
+++ titan/global.h
@@ -7293,9 +7293,12 @@ char* create_backup(char* input, int flag)
 			cmd = ostrcat(cmd, " > /mnt/logs/backup_debug.log 2>&1", 1, 0);
 	}
 
-	tmpstr = command(cmd);
-	if(tmpstr == NULL)
+	//tmpstr = command(cmd);
+	if(system(cmd) == 0)
+	//if(tmpstr == NULL)
 		tmpstr = ostrcat(tmpstr, _("Backup created successfully"), 1, 0);
+	else
+		tmpstr = ostrcat(tmpstr, _("Backup ended with error"), 1, 0);
 
 	free(cmd); cmd = NULL;
 	status.sec = merksec;
