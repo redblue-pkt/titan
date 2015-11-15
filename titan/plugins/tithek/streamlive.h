a4b42d7a1089973383140cf505342e25f69b930c
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-11-15 16:06:31 +0000

    tithek add zerocast and fix some hoster

--- titan/plugins/tithek/streamlive.h
+++ titan/plugins/tithek/streamlive.h
@@ -12,18 +12,20 @@ char* streamlive(char* link, int incount)
 	char* swfurl = NULL;
 	char* app = NULL;
 	char* token = NULL;
-	char* tmpurl = NULL;
+	char* url1 = NULL;
+	char* url2 = NULL;
+	char* url3 = NULL;
 	char* host = NULL;
 	char* path = NULL;
 	char* id = NULL;
-/*
+
 	char* streamurl1 = NULL;
 	char* title = NULL;
 	char* pic = NULL;
 	char* typemsg = NULL;
 
 	typemsg = string_resub("http://", "/", link, 0);
-*/
+
 /*
 http://www.streamlive.to/embedplayer_new.php?width=640&height=480&channel=71562&autoplay=true
 http://www.streamlive.to/view/72736/BT_Sport_2-live-stream-channel
@@ -31,41 +33,48 @@ http://www.streamlive.to/view/72736/BT_Sport_2-live-stream-channel
 	if(ostrstr(link, "http://www.streamlive.to/view/") != NULL)
 	{
 		id = string_resub("/view/", "/", link, 0);
-		tmpurl = ostrcat("http://www.streamlive.to/embedplayer_new.php?width=640&height=480&channel=", id, 0, 0);
-		tmpurl = ostrcat(tmpurl, "&autoplay=true", 1, 0);
+		url1 = ostrcat("http://www.streamlive.to/embedplayer_new.php?width=640&height=480&channel=", id, 0, 0);
+		url1 = ostrcat(url1, "&autoplay=true", 1, 0);
 	}
 	else
-		tmpurl = ostrcat(link, NULL, 0, 0);
+		url1 = ostrcat(link, NULL, 0, 0);
 
-	tmpstr = gethttps(tmpurl, NULL, NULL, NULL, NULL, NULL, 1);
-	host = string_resub("http://", "/", tmpurl, 0);
-	path = string_replace_all(host, "", tmpurl, 0);
+//<script type="text/javascript" src="http://www.streamlive.to/embed/69654&width=650&height=480&autoplay=true"></script>
+	tmpstr = gethttps(url1, NULL, NULL, NULL, NULL, NULL, 1);
+	host = string_resub("http://", "/", url1, 0);
+	path = string_replace_all(host, "", url1, 0);
 	path = string_replace_all("http://", "", path, 1);
 	path = string_replace_all(" ", "%20", path, 1);	
 	titheklog(debuglevel, "/var/usr/local/share/titan/plugins/tithek/tvtoast4_tmpstr", oitoa(incount), host, path, tmpstr);
-	free(tmpurl), tmpurl = NULL;
 
-	if(ostrstr(tmpstr, "rtmp") == NULL)
-	{
-		tmpurl = string_resub("src='", "'", tmpstr, 0);
-		free(tmpstr), tmpstr = NULL;						
-		
-		tmpstr = gethttps(tmpurl, NULL, NULL, NULL, NULL, NULL, 1);
-		free(host), host = NULL;
-		free(path), path = NULL;
-		host = string_resub("http://", "/", tmpurl, 0);
-		path = string_replace_all(host, "", tmpurl, 0);
-		path = string_replace_all("http://", "", path, 1);
-		path = string_replace_all(" ", "%20", path, 1);	
-		titheklog(debuglevel, "/var/usr/local/share/titan/plugins/tithek/tvtoast5_tmpstr", oitoa(incount), host, path, tmpstr);
-	}
-/*
 	if(ostrstr(tmpstr, "'file': 'http") != NULL)
 	{
 		streamurl1 = string_resub("'file': 'http", "'", tmpstr, 0);
 		streamurl1 = ostrcat("http", streamurl1, 0, 1);
 	}
-*/
+
+	if(ostrstr(tmpstr, "rtmp") == NULL)
+	{
+//    document.write("<iframe src='http://www.streamlive.to/embedplayer_new.php?width=650&height=480&channel=69654&autoplay=true' frameborder='0' marginheight='0' marginwidth='0' scrolling='no' width='650' height='480'></iframe>");
+		url2 = string_resub("src='", "'", tmpstr, 0);
+		free(tmpstr), tmpstr = NULL;						
+		tmpstr = gethttps(url2, NULL, NULL, NULL, NULL, url1, 1);
+		free(host), host = NULL;
+		free(path), path = NULL;
+		host = string_resub("http://", "/", url2, 0);
+		path = string_replace_all(host, "", url2, 0);
+		path = string_replace_all("http://", "", path, 1);
+		path = string_replace_all(" ", "%20", path, 1);	
+		titheklog(debuglevel, "/var/usr/local/share/titan/plugins/tithek/tvtoast5_tmpstr", oitoa(incount), host, path, tmpstr);
+	}
+
+	if(ostrstr(tmpstr, "'file': 'http") != NULL)
+	{
+		streamurl1 = string_resub("'file': 'http", "'", tmpstr, 0);
+		streamurl1 = ostrcat("http", streamurl1, 0, 1);
+	}
+
+//streamer: "rtmp:\/\/163.172.8.19:1935\/edge\/_definst_\/?xs=_we_eXVtN2d4emZubWNnYXd0fDE0NDc2ODg2ODR8OTUuOTEuNi4yNXw1NjQ4YTg2Yzg5ZWEwfDVhOTkxZWIzY2JiNzA0NjYyM2Y4ODVlODY0NDhhZTJmNDM0OTc4ODI.",
 	streamer = string_resub("streamer: \"", "\"", tmpstr, 0);
 	streamer = string_replace_all("\\", "", streamer, 1);
 	debug(99, "tvtoast5_tmpstr%d streamer=%s", incount, streamer)
@@ -85,118 +94,54 @@ http://www.streamlive.to/view/72736/BT_Sport_2-live-stream-channel
 		free(tmpstr1), tmpstr1 = NULL;
 		app = string_replace_all("rtmp:///", "", app, 1);
 		debug(99, "tvtoast5_tmpstr%d app5=%s", incount, app)
-	
-		tmpurl = string_resub("getJSON(\"", "\"", tmpstr, 0);
-		tmpurl = string_replace_all("//", "http://", tmpurl, 1);
+
+//$.getJSON("//www.streamlive.to/server.php?id=1447602284", function(json){
+		url3 = string_resub("getJSON(\"", "\"", tmpstr, 0);
+		url3 = string_replace_all("//", "http://", url3, 1);
 		free(tmpstr), tmpstr = NULL;
 	
-		tmpstr = gethttps(tmpurl, NULL, NULL, NULL, NULL, NULL, 1);
+		tmpstr = gethttps(url3, NULL, NULL, NULL, NULL, url2, 1);
 		free(host), host = NULL;
 		free(path), path = NULL;
-		host = string_resub("http://", "/", tmpurl, 0);
-		path = string_replace_all(host, "", tmpurl, 0);
+		host = string_resub("http://", "/", url3, 0);
+		path = string_replace_all(host, "", url3, 0);
 		path = string_replace_all("http://", "", path, 1);
 		path = string_replace_all(" ", "%20", path, 1);
 		titheklog(debuglevel, "/var/usr/local/share/titan/plugins/tithek/tvtoast6_tmpstr", oitoa(incount), host, path, tmpstr);
 	
 		token = string_resub("{\"token\":\"", "\"", tmpstr, 0);
 		free(tmpstr), tmpstr = NULL;
-	
-		streamurl = ostrcat(streamer, " app=", 0, 0);
-		streamurl = ostrcat(streamurl, app, 1, 0);
-		streamurl = ostrcat(streamurl, " playpath=", 1, 0);
-		streamurl = ostrcat(streamurl, playpath, 1, 0);
-		streamurl = ostrcat(streamurl, " swfUrl=", 1, 0);
-		streamurl = ostrcat(streamurl, swfurl, 1, 0);
-		streamurl = ostrcat(streamurl, " live=1", 1, 0);
-		streamurl = ostrcat(streamurl, " timeout=15", 1, 0);
-		streamurl = ostrcat(streamurl, " token=", 1, 0);
-		streamurl = ostrcat(streamurl, token, 1, 0);
-		streamurl = ostrcat(streamurl, " pageUrl=", 1, 0);
-		streamurl = ostrcat(streamurl, tmpurl, 1, 0);
-	
-	/*
-		rtmp://163.172.8.19:1935/edge/_definst_/?xs=_we_Mzcxamx1aXJsamNzamVufDE0NDY5MDkxODF8OTUuOTEuNi4yNXw1NjNjYzM3ZGIxNTU1fGQzOTYwODlmYjQyOGViYjRhZGQ0YjU3NDgwODY1Zjk2Nzk5ZTE4OWU. app=edge/_definst_/?xs=_we_Mzcxamx1aXJsamNzamVufDE0NDY5MDkxODF8OTUuOTEuNi4yNXw1NjNjYzM3ZGIxNTU1fGQzOTYwODlmYjQyOGViYjRhZGQ0YjU3NDgwODY1Zjk2Nzk5ZTE4OWU. playpath=371jluirljcsjen swfUrl=http://www.streamlive.to/ads/embed/player_ilive_embed.swf live=1 timeout=15 token=Qb582083d22804a892ce37092f7f3d2d pageUrl=http://www.streamlive.to/server.php?id=1446822781
-	*/
-	
-	/*
-		http://163.172.8.19:1935/edge/_definst_/r3t8xd8ss6pdequ/playlist.m3u8?xs=_we_cjN0OHhkOHNzNnBkZXF1fDE0NDcwNzY3Mjh8OTUuOTEuNi4yNXw1NjNmNTFmODVkZTRmfGNiMDZjMWNhN2UwNTAxYmQzNThkM2I3ZGJjOTVhNmY2YmNlNDg1NGY.
-	
-		streamurl = ostrcat(streamurl, "\n", 1, 0);
-		if(streamurl1 != NULL)
+
+/*
+	rtmp://163.172.8.19:1935/edge/_definst_/?xs=_we_Mzcxamx1aXJsamNzamVufDE0NDc2ODk3ODJ8OTUuOTEuNi4yNXw1NjQ4YWNiNjQ4MDNhfGE5ZjdhNzgyZmUwZmVhZWFhYjBhYTEyMjc4MTE5NGY3NWMxMjE4YmU. app=edge/_definst_/?xs=_we_Mzcxamx1aXJsamNzamVufDE0NDc2ODk3ODJ8OTUuOTEuNi4yNXw1NjQ4YWNiNjQ4MDNhfGE5ZjdhNzgyZmUwZmVhZWFhYjBhYTEyMjc4MTE5NGY3NWMxMjE4YmU. playpath=371jluirljcsjen swfUrl=http://www.streamlive.to/ads/embed/player_ilive_embed.swf live=1 timeout=15 token=D57b0914d73be4057f69d6443745ed28 pageUrl=http://www.streamlive.to/embedplayer_new.php?width=640&height=480&channel=68969&autoplay=true
+*/
+		if(streamer != NULL)
 		{
-			streamurl = ostrcat(streamurl, streamurl1, 1, 0);
-			tmpstr = ostrcat(streamurl, NULL, 0, 0);
+// without app not working
+			streamurl = ostrcat(streamer, " app=", 0, 0);
+			streamurl = ostrcat(streamurl, app, 1, 0);
+			streamurl = ostrcat(streamurl, " playpath=", 1, 0);
+			streamurl = ostrcat(streamurl, playpath, 1, 0);
+			streamurl = ostrcat(streamurl, " swfUrl=", 1, 0);
+			streamurl = ostrcat(streamurl, swfurl, 1, 0);
+			streamurl = ostrcat(streamurl, " live=1", 1, 0);
+			streamurl = ostrcat(streamurl, " timeout=15", 1, 0);
+			streamurl = ostrcat(streamurl, " token=", 1, 0);
+			streamurl = ostrcat(streamurl, token, 1, 0);
+			streamurl = ostrcat(streamurl, " pageUrl=", 1, 0);
+			streamurl = ostrcat(streamurl, url1, 1, 0);
 		}
-	
-		if(incount == -1 && streamurl1 != NULL)
-		{
-		
-			incount = 0;
-			int count = 0, i = 0;	
-			struct splitstr* ret1 = NULL;
-			struct menulist* mlist = NULL, *mbox = NULL;
-			ret1 = strsplit(tmpstr, "\n", &count);
-			for(i = 0; i < count; i++)
-			{
-				incount++;
-	
-				free(streamurl), streamurl = NULL;
-				streamurl = ostrcat(ret1[i].part, NULL, 0, 0);
-	
-				if(ostrstr(streamurl, "rtmp") != NULL)
-					title = ostrcat("Rtmpe Stream (", NULL, 0, 0);
-				else
-					title = ostrcat("Http Stream (", NULL, 0, 0);
-		
-				title = ostrcat(title, oitoa(incount), 1, 1);
-				title = ostrcat(title, ")", 1, 0);
-				if(typemsg != NULL)
-				{
-					title = ostrcat(title, " (", 1, 0);
-					title = ostrcat(title, typemsg, 1, 0);
-					title = ostrcat(title, ")", 1, 0);
-				}
-				pic = ostrcat("flv.png", NULL, 0, 0);
-				
-				if(streamurl != NULL)
-				{
-					debug(99, "(%d) title: %s streamurl: %s\n", i, title, streamurl);																									
-					addmenulist(&mlist, title, streamurl, pic, 0, 0);
-				}
-		
-				free(title), title = NULL;
-				free(pic), pic = NULL;
-			}
-			free(ret1), ret1 = NULL;
-			if(mlist != NULL)
-			{
-				mbox = menulistbox(mlist, NULL, _("Stream Menu"), _("Choose your Streaming Format from the following list"), NULL, NULL, 1, 0);
-				if(mbox != NULL)
-				{
-					free(streamurl), streamurl = NULL;
-		
-					debug(99, "mbox->name %s", mbox->name);
-					debug(99, "mbox->text %s", mbox->text);
-					streamurl = ostrcat(mbox->text, NULL, 0, 0);
-		
-				}
-			}
-		}
-		free(streamurl1), streamurl1 = NULL;
-	*/
 	}
-	else
-	{
-		free(streamurl), streamurl = NULL;
-		if(ostrstr(tmpstr, "'file': 'http") != NULL)
-			streamurl = string_resub("'file': 'http", "'", tmpstr, 0);
-	}
+
 	free(streamer), streamer = NULL;
 	free(app), app = NULL;
 	free(playpath), playpath = NULL;
 	free(swfurl), swfurl = NULL;
 	free(token), token = NULL;
+	free(url1), url1 = NULL;
+	free(url2), url2 = NULL;
+	free(url3), url3 = NULL;
+
 	free(host), host = NULL;
 	free(path), path = NULL;
 	free(id), id = NULL;
