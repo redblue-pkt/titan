cff8d40e2130996b9500c9fbc3fecd20a7cdc6ce
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-11-21 18:07:54 +0000

    fix box parser

--- titan/plugins/tithek/parser/nbaondemand.sh
+++ titan/plugins/tithek/parser/nbaondemand.sh
@@ -45,58 +45,37 @@ category()
   	echo "/tmp/tithek/$PARSER.$INPUT.list"
 }
 
-category1()
-{
-	WATCHLIST="tv/alle-videos"
-
-	rm /tmp/tithek/$PARSER.$INPUT.list > /dev/null 2>&1
-	for ROUND0 in $WATCHLIST; do
-		tags=""
-		tagcount=60
-		i=1
-		until [ "$i" -gt "$tagcount" ]
-		do
-		tags="$tags~"$ROUND0"/page/$i"
-		TITLE="`echo ${ROUND0^} | sed 's!/! !g' | sed 's!-! !g'` Page $i"
-		echo "$TITLE#$SRC $SRC page $ROUND0/page/$i#http://atemio.dyndns.tv/mediathek/menu/page.jpg#page.jpg#$NAME#0" >> /tmp/tithek/$PARSER.$INPUT.list
-	
-		i=$[$i+1]
-		done
-	done
-
-	echo "/tmp/tithek/$PARSER.$INPUT.list"
-}
-
 listvideos()
 {
-	piccount=0
-
-	$wgetbin $URL/$PAGE -O $TMP/cache.$PARSER."$filename"."1"
-#	cat $TMP/cache.$PARSER."$filename"."1" | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<tr> <td> <table width=\"100%\" height=27!\nfound=!g' | grep '^found=' | grep -v LiveTV | tr ' ' '~' >$TMP/cache.$PARSER."$filename"."2"
-	LIST=`cat $TMP/cache.$PARSER."$filename"."1" | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<tr> <td> <table width=\"100%\" height=27!\nfound=!g' | grep '^found=' | grep -v LiveTV | tr ' ' '~'`
-
-#	while read -u 3 ROUND; do
-	for ROUND in $LIST; do
-		ROUND=`echo $ROUND | tr '~' ' '`
-		URL=http://xlivetv.sx`echo $ROUND | sed 's!href=!\nurl=!g' | grep ^url= | grep "Full match record" | cut -d'"' -f2`
-		TITLE=`echo $ROUND | sed 's!width="30%"> <b>!\ntitle=<!g' | grep ^title= | cut -d'<' -f2 | sed 's!&ndash;!-!g'`
-		PIC="http://atemio.dyndns.tv/mediathek/menu/default.jpg"
-		EXTRA=`echo $ROUND | sed 's!data-pop="ddr_!\ndate=_!g' | grep ^date= | cut -d'_' -f2`
-
-		if [ ! -z "$TITLE" ] && [ ! -z "$EXTRA" ];then
-			TITLE="$TITLE ($EXTRA)"
-		fi
-
-		if [ ! -z "$TITLE" ] && [ ! -z "$URL" ];then
-			piccount=$[$piccount+1]
-			LINE="$TITLE#$URL#$PIC#$PARSER_$piccount.jpg#$NAME#98"
-			echo "$LINE" >> /tmp/tithek/$PARSER.$INPUT.list
-		fi
-
-	done
-#	done 3<$TMP/cache.$PARSER."$filename"."2"
-#	rm $TMP/cache.* > /dev/null 2>&1
+	if [ ! -e "/tmp/tithek/$PARSER.$INPUT.list" ]; then
+		piccount=0
 	
+		$wgetbin $URL/$PAGE -O $TMP/cache.$PARSER."$filename"."1"
+#		cat $TMP/cache.$PARSER."$filename"."1" | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<tr> <td> <table width=\"100%\" height=27!\nfound=!g' | grep '^found=' | grep -v LiveTV | tr ' ' '~' >$TMP/cache.$PARSER."$filename"."2"
+		LIST=`cat $TMP/cache.$PARSER."$filename"."1" | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<tr> <td> <table width=\"100%\" height=27!\nfound=!g' | grep '^found=' | grep -v LiveTV | tr ' ' '~'`
+	
+#		while read -u 3 ROUND; do
+		for ROUND in $LIST; do
+			ROUND=`echo $ROUND | tr '~' ' '`
+			URL=http://xlivetv.sx`echo $ROUND | sed 's!href=!\nurl=!g' | grep ^url= | grep "Full match record" | cut -d'"' -f2`
+			TITLE=`echo $ROUND | sed 's!width="30%"> <b>!\ntitle=<!g' | grep ^title= | cut -d'<' -f2 | sed 's!&ndash;!-!g'`
+			PIC="http://atemio.dyndns.tv/mediathek/menu/default.jpg"
+			EXTRA=`echo $ROUND | sed 's!data-pop="ddr_!\ndate=_!g' | grep ^date= | cut -d'_' -f2`
+	
+			if [ ! -z "$TITLE" ] && [ ! -z "$EXTRA" ];then
+				TITLE="$TITLE ($EXTRA)"
+			fi
+	
+			if [ ! -z "$TITLE" ] && [ ! -z "$URL" ];then
+				piccount=$[$piccount+1]
+				LINE="$TITLE#$URL#$PIC#$PARSER_$piccount.jpg#$NAME#98"
+				echo "$LINE" >> /tmp/tithek/$PARSER.$INPUT.list
+			fi
+	
+		done
+#		done 3<$TMP/cache.$PARSER."$filename"."2"
+#		rm $TMP/cache.* > /dev/null 2>&1
+	fi
 	echo "/tmp/tithek/$PARSER.$INPUT.list"
 }
 
