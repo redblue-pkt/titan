b2fdd28523996e99788e889a0a5f09d395fb248f
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2015-11-29 18:10:49 +0000

    readd tuner config after autoscan start

--- titan/titan.c
+++ titan/titan.c
@@ -1013,6 +1013,8 @@ int main(int argc, char *argv[])
 	{
 		status.updatevfd = PAUSE;
 		resettvpic();
+		screentunerconfig();
+		resettvpic();
 		screenscanconfig(1);
 		resettvpic();
 		writevfd("");
