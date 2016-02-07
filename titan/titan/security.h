58ef17eeea1ec3e0be6b99e7643abc27d256b962
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-02-07 17:49:32 +0000

    fix

--- titan/titan/security.h
+++ titan/titan/security.h
@@ -1179,7 +1179,7 @@ void setskinnodeslocked(int flag)
 			{
 //				if(ostrcmp("systemupdatemenu", child->name) == 0) child->locked = 1;
 				if(ostrcmp("system_backup", child->name) == 0) child->locked = 1;
-				if(ostrcmp("restoredefault", child->name) == 0) child->locked = 1;
+//				if(ostrcmp("restoredefault", child->name) == 0) child->locked = 1;
 			}
 
 			// hide this menu on all boxes without ATEMIO530
