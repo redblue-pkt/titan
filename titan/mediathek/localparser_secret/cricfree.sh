1410161ad2de28f86a96c60fb555a9fd388a70c3
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-02-13 17:43:56 +0000

    fix

--- titan/mediathek/localparser_secret/cricfree.sh
+++ titan/mediathek/localparser_secret/cricfree.sh
@@ -38,7 +38,7 @@ fi
 
 init()
 {
-	if [ "$arch" = "mipsel" ];then
+	if [ "$ARCH" != "sh4" ];then
 		rm -f /mnt/network/cookies > /dev/null 2>&1
 		rm -rf $TMP > /dev/null 2>&1
 		echo "$NAME ($TYPE)#$SRC $SRC mainmenu#http://atemio.dyndns.tv/mediathek/menu/$PARSER.jpg#$PARSER.jpg#TiThek#0"
