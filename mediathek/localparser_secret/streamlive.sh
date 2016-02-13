e11302d45be9fe260cb4998a6dede25436ea8bbf
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-02-13 20:36:15 +0000

    fix streamlive

--- mediathek/localparser_secret/streamlive.sh
+++ mediathek/localparser_secret/streamlive.sh
@@ -13,7 +13,7 @@ fi
 ARCH=`cat /etc/.arch`
 URL=http://cricfree.sx/
 PARSER=`echo $SRC | tr '/' '\n' | tail -n1 | sed 's/.sh//'`
-NAME=CricFree
+NAME=StreamLive
 
 curlbin='curl -k -s -v -L --cookie /mnt/network/cookies --cookie-jar /mnt/network/cookies'
 #useragent="Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.7.3000 Chrome/30.0.1599.101 Safari/537.36"
@@ -38,7 +38,7 @@ fi
 
 init()
 {
-	if [ "$ARCH" != "sh4" ];then
+	if [ "$ARCH" = "mipsel" ];then
 		rm -f /mnt/network/cookies > /dev/null 2>&1
 		rm -rf $TMP > /dev/null 2>&1
 		echo "$NAME ($TYPE)#$SRC $SRC mainmenu#http://atemio.dyndns.tv/mediathek/menu/$PARSER.jpg#$PARSER.jpg#TiThek#0"
@@ -62,16 +62,26 @@ list()
 	if [ ! -e "$TMP/$PARSER.$INPUT.$FROM.$FILENAME.list" ]; then
 		piccount=0
 
-		$curlbin $URL/$PAGE -o $TMP/cache.$PARSER.$FROM.$FILENAME.1
-		cat $TMP/cache.$PARSER.$FROM.$FILENAME.1 | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<li class="has-sub">!\n<li class="has-sub">!g' | grep ^'<li class="has-sub">' | sed 's!href=!\n<a href=!g' | grep ^'<a href=' | grep "http://cricfree.sx" | grep -v "/watch/live/" | cut -d '"' -f2 >$TMP/cache.$PARSER.$FROM.$FILENAME.2
+#		$wgetbin $URL/$PAGE -O $TMP/cache.$PARSER.$FROM.$FILENAME.1
+		$curlbin "http://www.streamlive.to/?p=1&q=&sort=1" -o $TMP/cache.$PARSER.$FROM.$FILENAME.1
+
+#		cat $TMP/cache.$PARSER.$FROM.$FILENAME.1 | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<li class="has-sub">!\n<li class="has-sub">!g' | grep ^'<li class="has-sub">' | sed 's!href=!\n<a href=!g' | grep ^'<a href=' | grep "http://cricfree.sx" | grep -v "/watch/live/" | cut -d '"' -f2 >$TMP/cache.$PARSER.$FROM.$FILENAME.2
+		cat $TMP/cache.$PARSER.$FROM.$FILENAME.1 | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!<a href="http://www.streamlive.to/view/!\nhttp://www.streamlive.to/view/!g' | grep ^"http://www.streamlive.to/view/" | grep -v "<strong>" | grep -v "premium_only" >$TMP/cache.$PARSER.$FROM.$FILENAME.2
 
 		while read -u 3 ROUND; do
 
 			URL=`echo $ROUND | cut -d'"' -f1`
-			TITLE=`echo $ROUND | sed 's!http://cricfree.sx/!!' | sed 's!watch/live/!!' | sed 's!-live-stream-2!!' | sed 's!-live-stream!!' | sed 's!-in-streaming!!' | sed 's!-live-streaming-2!!' | sed 's!-live-streaming!!'`
-			PIC="http://atemio.dyndns.tv/mediathek/menu/default.jpg"
+			PIC=`echo $ROUND | sed 's!src="!\npic="!g' | grep ^pic= | cut -d'"' -f2 | tail -n1`
+			TITLE=`echo $ROUND | sed 's!title="!\ntitle="!g' | grep ^title= | cut -d'"' -f2 | tail -n1`
+			if [ -z "$TITLE" ];then 
+				TITLE=`echo $ROUND | cut -d"<" -f2 | cut -d">" -f2`
+			fi
 		
-			TITLE=`echo $TITLE | sed 's/-/ /g' | sed 's/_/ /g'`
+			if [ -z "$PIC" ] || [ "$PIC" = "http:" ]; then  
+				PIC="http://atemio.dyndns.tv/mediathek/menu/default.jpg"
+			fi
+		
+			TITLE=`echo $URL | tr '/' '\n' | tail -n1 | sed 's/_/ /g'`
 			TITLE=`echo $TITLE | sed -e 's/&#038;/&/g' -e 's/&amp;/und/g' -e 's/&quot;/"/g' -e 's/&lt;/\</g' -e 's/&#034;/\"/g' -e 's/&#039;/\"/g' -e 's/#034;/\"/g' -e 's/#039;/\"/g' -e 's/&szlig;/Ãx/g' -e 's/&ndash;/-/g' -e 's/&Auml;/Ã/g' -e 's/&Uuml;/ÃS/g' -e 's/&Ouml;/Ã/g' -e 's/&auml;/Ã¤/g' -e 's/&uuml;/Ã¼/g' -e 's/&ouml;/Ã¶/g' -e 's/&eacute;/Ã©/g' -e 's/&egrave;/Ã¨/g' -e 's/%F6/Ã¶/g' -e 's/%FC/Ã¼/g' -e 's/%E4/Ã¤/g' -e 's/%26/&/g' -e 's/%C4/Ã/g' -e 's/%D6/Ã/g' -e 's/%DC/ÃS/g' -e 's/%28/(/g' -e 's/%29/)/g' -e 's/%3A/:/g' -e 's/%40/@/g' -e 's/%2B/&/g' -e 's/%C3/A/g' -e 's/%B1/&/g' -e 's/%5B//g' -e 's/%5D//g' -e 's!%2F!/!g' -e 's/|/ /g' -e 's/(/ /g' -e 's/)/ /g' -e 's/+/ /g' -e 's/\//-/g' -e 's/,/ /g' -e 's/;/ /g' -e 's/:/ /g' -e 's/\.\+/./g'`
 
 			if [ ! -z "$TITLE" ] && [ ! -z "$URL" ];then
@@ -79,7 +89,7 @@ list()
 					touch $TMP/$PARSER.$INPUT.$FROM.$FILENAME.list
 				fi
 				piccount=$[$piccount+1]
-				LINE="$TITLE#$URL#$PIC#$PARSER_$piccount.jpg#$NAME#92"
+				LINE="$TITLE#$URL#$PIC#$PARSER_$piccount.jpg#$NAME#91"
 				echo "$LINE" >> $TMP/$PARSER.$INPUT.$FROM.$FILENAME.list
 			fi
 	
