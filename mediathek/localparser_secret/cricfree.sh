6f2ad79961758ab9f4474589b594fdafd2311bf2
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-02-13 22:46:59 +0000

    show on beta all local mediathek

--- mediathek/localparser_secret/cricfree.sh
+++ mediathek/localparser_secret/cricfree.sh
@@ -38,7 +38,7 @@ fi
 
 init()
 {
-	if [ "$ARCH" != "sh4" ];then
+	if [ "$ARCH" != "sh4" ] || [ -e /etc/.beta ];then
 		rm -f /mnt/network/cookies > /dev/null 2>&1
 		rm -rf $TMP > /dev/null 2>&1
 		echo "$NAME ($TYPE)#$SRC $SRC mainmenu#http://atemio.dyndns.tv/mediathek/menu/$PARSER.jpg#$PARSER.jpg#TiThek#0"
