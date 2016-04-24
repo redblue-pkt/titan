40336ec101feebd97d3a1a767a735889d76f0b5f
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-04-24 12:16:03 +0000

    fix lang

--- titan/language.h
+++ titan/language.h
@@ -6,7 +6,7 @@
 //flag 2: reload skin
 int screenlanguage(int flag)
 {
-	int rcret = 0, change = 0, reloadskin = 0, check = 0;
+	int rcret = 0, change = 0, reloadskin = 0;
 	struct skin* language = getscreen("language");
 	struct skin* listbox = getscreennode(language, "listbox");
 
@@ -37,40 +37,33 @@ int screenlanguage(int flag)
 					initlocale(getconfig("localepath", NULL));
 					addconfig("lang", listbox->select->name);
 					change = 1;
+
 				}
 				else if(setlang(listbox->select->name) == 0 && initlocale(getconfig("localepath", NULL)) == 0)
 				{
 					addconfig("lang", listbox->select->name);
 					change = 1;
 				}
-
-				if(flag == 0 && change == 1)
+				else
 				{
-					check = 1;
-					if(checkbox("UFS910") == 1 || checkbox("UFS922") == 1 || checkbox("UFS912") == 1)
+					if(checkbox("UFS910") == 1 || checkbox("UFS922") == 1 || checkbox("UFC960") == 1)
 					{
 						char* file = ostrcat("/mnt/swapextensions/usr/local/share/titan/", listbox->select->name, 0, 0);
 						printf("file=%s\n",file);
 						if(!file_exist(file))
-							check = 0;
+							textbox(_("Message"), _("Install language Package !"), _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 600, 200, 10, 0);
+
 						free(file), file = NULL;
 					}
-					printf("1check=%d\n",check);
-
-					if(check == 1)
-					{
-						printf("2check=%d\n",check);
-
-						textbox(_("Message"), _("Change language needs reboot"), NULL, 0, NULL, 0, NULL, 0, NULL, 0, 1000, 200, 5, 0);
-						resettvpic();
-						oshutdown(3, 1);
-					}
-					else
-					{
-						printf("3check=%d\n",check);
-						textbox(_("Message"), _("Install language Package"), NULL, 0, NULL, 0, NULL, 0, NULL, 0, 1000, 200, 5, 0);
-					}
 				}
+
+				if(flag == 0 && change == 1)
+				{
+					textbox(_("Message"), _("Change language needs reboot"), NULL, 0, NULL, 0, NULL, 0, NULL, 0, 1000, 200, 5, 0);
+					resettvpic();
+					oshutdown(3, 1);
+				}
+
 				if(flag == 2 && change == 1)
 					reloadskin = 1;
 			}
