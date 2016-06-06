a871fe0eeb6bf6da914c5e01212b5e9537740cef
tt4sk <tt4sk@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-06-06 19:18:37 +0000

    [TiThek] fixed: beeg website changed

--- mediathek/localparser_secret/beeg.sh
+++ mediathek/localparser_secret/beeg.sh
@@ -6,7 +6,7 @@
 SRC=$1
 INPUT=$2
 PARAM=$3
-URL="https://beeg.com/api/v6/1830/"
+URL="https://beeg.com/api/v6/1850/"
 PARSER=`echo $SRC | tr '/' '\n' | tail -n1 | sed 's/.sh//'`
 NAME=`echo -n ${PARSER:0:1} | tr '[a-z]' '[A-Z]'`${PARSER:1}
 
@@ -107,7 +107,7 @@ BEGIN { table = ""
         for (i = 0; i <= 255; i++) {
            ord[sprintf("%c", i)] = i
         }
-        crc = "hO8VdzDLCBeVXd6SMpaCvJumQz9ESo7Uuih"
+        crc = "mxcch6zo7Z0"
       }
 /\"/  { m = 1
         while (m == 1) {
