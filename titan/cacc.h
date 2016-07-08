fda541b69fc27af79e2ff5149fb76f78384fc27e
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-07-08 14:46:32 +0000

    fix set_key

--- titan/cacc.h
+++ titan/cacc.h
@@ -51,8 +51,10 @@ int descrambler_set_key1(struct dvbdev* node, int index, int parity, unsigned ch
 		err("NULL detect");
 		return 1;
 	}
-	
+
 	struct ca_descr_data d;
+
+#ifdef MIPSEL
 
 	d.index = index;
 	d.parity = parity;
@@ -71,6 +73,22 @@ int descrambler_set_key1(struct dvbdev* node, int index, int parity, unsigned ch
 
 	if (ioctl(node->fd, CA_SET_DESCR_DATA, &d))
 		printf("CA_SET_DESCR_DATA\n");
+
+#else
+	if (descrambler_open())
+	{
+		d.index = index;
+		d.parity = parity;
+		d.data_type = CA_DATA_KEY;
+		d.length = 32;
+		d.data = data;
+
+		if (ioctl(desc_fd, CA_SET_DESCR_DATA, &d))
+			printf("CA_SET_DESCR_DATA\n");
+	}
+	descrambler_close();
+
+#endif
 
 	return 0;
 }
