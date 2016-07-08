774213a0124493ad286dc6519ae8bbce0dde9f12
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-07-08 14:26:29 +0000

    fix ca3

--- titan/titan/descr.h
+++ titan/titan/descr.h
@@ -8,6 +8,7 @@ int descrambler_set_key1(struct dvbdev* node, int index, int parity, unsigned ch
 	}
 	
 	struct ca_descr_data d;
+
 	if (descrambler_open())
 	{
 		d.index = index;
@@ -16,7 +17,7 @@ int descrambler_set_key1(struct dvbdev* node, int index, int parity, unsigned ch
 		d.length = 32;
 		d.data = data;
 
-		if (ioctl(node->fd, CA_SET_DESCR_DATA, &d))
+		if (ioctl(desc_fd, CA_SET_DESCR_DATA, &d))
 			printf("CA_SET_DESCR_DATA\n");
 	}
 	descrambler_close();
