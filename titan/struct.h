f8a39e723124b87311d6b6aafc922712d268a8de
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-07-14 23:20:49 +0000

    disable double define

--- titan/struct.h
+++ titan/struct.h
@@ -205,7 +205,7 @@
 #define CUSTOMER_CERT "/etc/pem/customer.pem"
 #define DEVICE_CERT   "/etc/pem/device.pem"
 #define MAX_ELEMENTS    33
-#ifdef MIPSEL
+#ifdef MIPSEL1
 #define CA_SET_DESCR_DATA _IOW('o', 137, struct ca_descr_data)
 #endif
 
