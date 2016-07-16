f82c82d730ec930bc8b44f8dad3e2700131a4543
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-07-16 17:33:03 +0000

    next test

--- titan/titan/ca.h
+++ titan/titan/ca.h
@@ -2098,7 +2098,8 @@ int sendcapmttocam(struct dvbdev* dvbnode, struct service* node, unsigned char* 
 			// cacc
 			dvbnode->caslot->scrambled = 1;
 			debug(620, "set scrambled=%d", dvbnode->caslot->scrambled);
-			if(dvbnode->caslot->ccmgr_ready == 1)
+			debug(620, "scrambled=%d ccmgr_ready=%d ccmanager=%d", dvbnode->caslot->scrambled, dvbnode->caslot->ccmgr_ready, casession[caservicenr].ccmanager);
+			if(dvbnode->caslot->ccmgr_ready == 1 && casession[caservicenr].ccmanager == 1)
 				resendKey(dvbnode);
 
 			return 0;
