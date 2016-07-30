4c5436464c0bae81741e1f745e5748463b185b9d
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-07-30 21:03:28 +0000

    fix

--- plugins/tithek/movshare.h
+++ plugins/tithek/movshare.h
@@ -6,6 +6,7 @@ char* movshare(char* link)
 	debug(99, "link: %s", link);
 	int debuglevel = getconfigint("debuglevel", NULL);
 	char* tmphost = NULL;
+	char* tmpurl = NULL;
 	char* tmppath = NULL;
 	char* tmpstr = NULL;
 	char* send = NULL;
@@ -22,20 +23,25 @@ char* movshare(char* link)
 	char* tmplink = NULL, *pos = NULL, *path = NULL;
 
 	tmplink = ostrcat(link, NULL, 0, 0);
-
+	
+printf("11111111111111111111\n");
 	if(ostrstr(link, "/Out/?s=") != NULL)
 	{
+printf("222222222222222222222\n");
+
 		tmplink = string_replace("/Out/?s=", "", tmplink, 1);
 		debug(99, "remove out string: %s", tmplink);
 	}
 	
-	if(tmplink == NULL || ostrncmp("http://", tmplink, 7))
+	if(tmplink == NULL || ostrncmp("http", tmplink, 4))
 	{
+printf("33333333333333333333333333\n");
+
 		textbox(_("Message"), _("Hoster Url not http://") , _("OK"), getrcconfigint("rcok", NULL), _("EXIT"), getrcconfigint("rcexit", NULL), NULL, 0, NULL, 0, 1200, 200, 0, 0);
 		goto end;
 	}
 
-	tmphost = string_replace("http://", "", tmplink, 0);
+	tmphost = string_replace("https://", "", tmplink, 0);
 	free(tmplink) , tmplink = NULL;
 
 	if(tmphost != NULL)
@@ -55,6 +61,11 @@ char* movshare(char* link)
 	char* host = NULL;
 	host = ostrcat(tmphost, NULL, 0, 0);
 	tmppath = string_replace("/video/", "/embed.php?v=", tmppath, 1);
+//	tmppath = string_replace("/video/", "/embed/?v=", tmppath, 1);
+
+// https://www.wholecloud.net/embed.php?v=e723e5185b818
+// 7481628c80fea HTTP/1.1\r\nHost: www.wholecloud.net\r\nUser-Agent: 
+ 
 	free(tmphost), tmphost = NULL;
 //	tmphost = ostrcat("embed.movshare.net", NULL, 0, 0);
 	tmphost = ostrcat("www.wholecloud.net", NULL, 0, 0);
@@ -71,7 +82,12 @@ char* movshare(char* link)
 	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);
 	debug(99, "send: %s", send);
 
-	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+	tmpurl = ostrcat("www.wholecloud.net", tmppath, 0, 0);
+	tmpstr = gethttps(tmpurl, NULL, NULL, NULL, NULL, NULL, 1);
+	free(tmpurl), tmpurl = NULL;
+
+//	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+
 	debug(99, "tmpstr: %s", tmpstr);
 	titheklog(debuglevel, "/tmp/movshare1_get", NULL, NULL, NULL, tmpstr);
 
@@ -130,10 +146,14 @@ char* movshare(char* link)
 	free(tmppath), tmppath = NULL;
 //	free(tmphost), tmphost = NULL;
 	free(send), send = NULL;
+
+//	tmppath = ostrcat("/api/player.api.php?file=", file, 0, 0);
+//	tmppath = ostrcat(tmppath, "&key=", 1, 0);
+//	tmppath = ostrcat(tmppath, filekey, 1, 0);
 	
-	tmppath = ostrcat("/api/player.api.php?file=", file, 0, 0);
-	tmppath = ostrcat(tmppath, "&key=", 1, 0);
-	tmppath = ostrcat(tmppath, filekey, 1, 0);
+	tmppath = ostrcat("/api/player.api.php?key=", filekey, 0, 0);
+	tmppath = ostrcat(tmppath, "&file=", 1, 0);
+	tmppath = ostrcat(tmppath, file, 1, 0);
 //	tmphost = ostrcat(tmphost, NULL, 0, 0);
 	
 	free(tmphost), tmphost = NULL;
@@ -146,11 +166,18 @@ char* movshare(char* link)
 	send = ostrcat(send, "GET ", 1, 0);
 	send = ostrcat(send, tmppath, 1, 0);
 	send = ostrcat(send, " HTTP/1.1\r\nHost: ", 1, 0);
-	send = ostrcat(send, tmphost, 1, 0);
+//	send = ostrcat(send, tmphost, 1, 0);
+	send = ostrcat(send, "www.wholecloud.net", 1, 0);
 	send = ostrcat(send, "\r\nUser-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.99 Safari/535.1\r\nConnection: close\r\nAccept-Encoding: gzip\r\n\r\n", 1, 0);
 	debug(99, "send: %s", send);
 	free(tmpstr), tmpstr = NULL;
-	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
+
+
+	tmpurl = ostrcat("www.wholecloud.net", tmppath, 0, 0);
+	tmpstr = gethttps(tmpurl, NULL, NULL, NULL, NULL, NULL, 1);
+	free(tmpurl), tmpurl = NULL;
+	
+//	tmpstr = gethttpreal(tmphost, tmppath, 80, NULL, NULL, NULL, 0, send, NULL, 5000, 1);
 	debug(99, "tmpstr: %s", tmpstr);
 	titheklog(debuglevel, "/tmp/movshare2_get", NULL, NULL, NULL, tmpstr);
 
