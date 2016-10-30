053823346ec79ac0bdd2b8f6a717c578d50c41b9
caliban <caliban@dc659be2-726c-4bf2-b84f-ff68d3143153>
2016-10-30 05:30:57 +0000

    next try

--- titan/titan/header.h
+++ titan/titan/header.h
@@ -8,9 +8,6 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#ifndef INLINE
-# define INLINE extern inline
-#endif
 
 // ca.h
 void sendSPDU(struct dvbdev* dvbnode, unsigned char tag, void *data, int len, int sessionnr, void *apdu, int alen);
@@ -736,8 +733,8 @@ int changemask(struct skin* node, char* text);
 int changepicmem(struct skin* node, char* text, int timeout, int del);
 int convertxmlentry(char *value, uint8_t *proz);
 unsigned char* savejpg(char* filename, int width, int height, int channels, int newwidth, int newheight, int quality, unsigned char *buf);
-inline void drawpixelfastfb(struct fb* node, int posx, int posy, unsigned long color);
-inline void drawpixelfb(struct fb* node, int posx, int posy, unsigned long color);
+static inline void drawpixelfastfb(struct fb* node, int posx, int posy, unsigned long color);
+static inline void drawpixelfb(struct fb* node, int posx, int posy, unsigned long color);
 unsigned char * resize(unsigned char *origin, int ox, int oy, int dx, int dy, int type, unsigned char * dst, int flag);
 
 //volume.h
