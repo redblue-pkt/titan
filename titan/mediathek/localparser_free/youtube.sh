e3b1ef97743f43bebf27feab609f1665e53d7261
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2017-02-26 20:59:30 +0000

    tithek update youtube

--- titan/mediathek/localparser_free/youtube.sh
+++ titan/mediathek/localparser_free/youtube.sh
@@ -5,18 +5,18 @@
 
 SRC=$1
 INPUT=$2
-FROM=$3
-PAGE=$4
+PAGE=$3
+NEXT=$4
+PARSER=`echo $SRC | tr '/' '\n' | tail -n1 | sed 's/.sh//'`
 
-FILENAME=`echo $PAGE | tr '/' '.'`
-FILENAME=`echo $FILENAME | tr '&' '.'`
+FILENAME="$PARSER $INPUT $PAGE $NEXT"
+FILENAME=`echo $FILENAME | tr '&' '.' | tr '/' '.' | tr '?' '.' | tr ';' '.' | tr '=' '.' | sed 's/ \+/./g' | sed 's/\.\+/./g'`
 
-if [ -z "$PAGE" ]; then
+if [ -z "$FILENAME" ]; then
 	FILENAME=none
 fi
 
 URL=https://www.googleapis.com
-PARSER=`echo $SRC | tr '/' '\n' | tail -n1 | sed 's/.sh//'`
 NAME=Youtube
 
 mkdir $TMP > /dev/null 2>&1
@@ -39,18 +39,20 @@ init()
 
 mainmenu()
 {
-#	echo "Category#$SRC $SRC category#http://atemio.dyndns.tv/mediathek/menu/category.jpg#category.jpg#$NAME#0" > $TMP/$PARSER.$INPUT.list
-	echo "Search#$SRC $SRC search '/youtube/v3/search?q=%search%&regionCode=US&part=snippet&hl=en_US&key=AIzaSyAd-YEOqZz9nXVzGtn3KWzYLbLaajhqIDA&type=video&maxResults=50'#http://atemio.dyndns.tv/mediathek/menu/search.jpg#search.jpg#$NAME#112" >$TMP/$PARSER.$INPUT.list
-	echo "$TMP/$PARSER.$INPUT.list"
+#	echo "Category#$SRC $SRC category#http://atemio.dyndns.tv/mediathek/menu/category.jpg#category.jpg#$NAME#0" > $TMP/$FILENAME.list
+	echo "Search#$SRC $SRC search '/youtube/v3/search?q=%search%&regionCode=US&part=snippet&hl=en_US&key=AIzaSyAd-YEOqZz9nXVzGtn3KWzYLbLaajhqIDA&type=video&maxResults=50'#http://atemio.dyndns.tv/mediathek/menu/search.jpg#search.jpg#$NAME#112" >$TMP/$FILENAME.list
+	echo "$TMP/$FILENAME.list"
 }
 
 search()
 {
-	rm $TMP/$PARSER.$INPUT.$FILENAME.list > /dev/null 2>&1
-#	if [ ! -e "$TMP/$PARSER.$INPUT.$FROM.$FILENAME.list" ]; then
+	rm $TMP/$FILENAME.list > /dev/null 2>&1
+	rm $TMP/cache.* > /dev/null 2>&1
+
+	if [ ! -e "$TMP/$FILENAME.list" ]; then
 		piccount=0
-		$curlbin "$URL/$FROM" -o "$TMP/cache.$PARSER.$INPUT.$FILENAME.1"
-		cat $TMP/cache.$PARSER.$INPUT.$FILENAME.1 | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!"kind":!\nkind":!g' | grep ^"kind" | grep videoId >$TMP/cache.$PARSER.$INPUT.$FILENAME.2
+		$curlbin "$URL/$FROM" -o "$TMP/cache.$FILENAME.1"
+		cat $TMP/cache.$FILENAME.1 | tr '\n' '\r' |  tr '\r' ' ' | tr '\n' ' ' | tr '\t' ' ' | sed 's/ \+/ /g' | sed 's!"kind":!\nkind":!g' | grep ^"kind" | grep videoId >$TMP/cache.$FILENAME.2
 
 		while read -u 3 ROUND; do
 			ID=`echo $ROUND | sed 's!"videoId": !\nvideoId=!g' | grep ^videoId= | cut -d'"' -f2 | tail -n1`
@@ -58,6 +60,7 @@ search()
 			TITLE=`echo $ROUND | sed 's!"title": !\ntitle=!g' | grep ^title= | cut -d'"' -f2 | tail -n1`
 #			URL="/get_video_info?el=leanback&cplayer=UNIPLAYER&cos=Windows&height=1080&cbr=Chrome&hl=en_US&cver=4&ps=leanback&c=TVHTML5&video_id=$ID&cbrver=40.0.2214.115&width=1920&cosver=6.1&ssl_stream=1"
 			URL="https://www.youtube.com/get_video_info?el=leanback&cplayer=UNIPLAYER&cos=Windows&height=1080&cbr=Chrome&hl=en_US&cver=4&ps=leanback&c=TVHTML5&video_id=$ID&cbrver=40.0.2214.115&width=1920&cosver=6.1&ssl_stream=1"
+			NEWPAGE="https://www.youtube.com/watch?v=$ID"
 
 			if [ -z "$TITLE" ];then
 				TITLE=`echo $ROUND | cut -d"<" -f2 | cut -d">" -f2`
@@ -69,25 +72,34 @@ search()
 
 			TITLE=`echo $TITLE | sed -e 's/&#038;/&/g' -e 's/&amp;/und/g' -e 's/&quot;/"/g' -e 's/&lt;/\</g' -e 's/&#034;/\"/g' -e 's/&#039;/\"/g' -e 's/#034;/\"/g' -e 's/#039;/\"/g' -e 's/&szlig;/Ãx/g' -e 's/&ndash;/-/g' -e 's/&Auml;/Ã/g' -e 's/&Uuml;/ÃS/g' -e 's/&Ouml;/Ã/g' -e 's/&auml;/Ã¤/g' -e 's/&uuml;/Ã¼/g' -e 's/&ouml;/Ã¶/g' -e 's/&eacute;/Ã©/g' -e 's/&egrave;/Ã¨/g' -e 's/%F6/Ã¶/g' -e 's/%FC/Ã¼/g' -e 's/%E4/Ã¤/g' -e 's/%26/&/g' -e 's/%C4/Ã/g' -e 's/%D6/Ã/g' -e 's/%DC/ÃS/g' -e 's/%28/(/g' -e 's/%29/)/g' -e 's/%3A/:/g' -e 's/%40/@/g' -e 's/%2B/&/g' -e 's/%C3/A/g' -e 's/%B1/&/g' -e 's/%5B//g' -e 's/%5D//g' -e 's!%2F!/!g' -e 's/|/ /g' -e 's/(/ /g' -e 's/)/ /g' -e 's/+/ /g' -e 's/\//-/g' -e 's/,/ /g' -e 's/;/ /g' -e 's/:/ /g' -e 's/\.\+/./g'`
 
-			if [ ! -z "$TITLE" ] && [ ! -z "$URL" ];then
-				if [ ! -e $TMP/$PARSER.$INPUT.$FILENAME.list ];then
-					touch $TMP/$PARSER.$INPUT.$FILENAME.list
+			if [ ! -z "$TITLE" ] && [ ! -z "$NEWPAGE" ];then
+				if [ ! -e $TMP/$FILENAME.list ];then
+					touch $TMP/$FILENAME.list
 				fi
 				piccount=$[$piccount+1]
-#				LINE="$TITLE#$SRC $SRC list '$URL'#$PIC#$PARSER_$piccount.jpg#$NAME#0"
-				LINE="$TITLE#$URL#$PIC#$PARSER_$piccount.jpg#$NAME#14"
-
-				echo "$LINE" >> $TMP/$PARSER.$INPUT.$FILENAME.list
+#				LINE="$TITLE#$SRC $SRC list '$URL'#$PIC#$FILENAME_$piccount.jpg#$NAME#0"
+#				LINE="$TITLE#$URL#$PIC#$FILENAME_$piccount.jpg#$NAME#14"
+				LINE="$TITLE#$SRC $SRC hoster '$NEWPAGE'#$PIC#$FILENAME.$piccount.jpg#$NAME#111"
+				echo "$LINE" >> $TMP/$FILENAME.list
 			fi
 
-		done 3<$TMP/cache.$PARSER.$INPUT.$FILENAME.2
-		rm $TMP/cache.* > /dev/null 2>&1
-#	fi
-	echo "$TMP/$PARSER.$INPUT.$FILENAME.list"
+		done 3<$TMP/cache.$FILENAME.2
+#		rm $TMP/cache.* > /dev/null 2>&1
+	fi
+	echo "$TMP/$FILENAME.list"
+}
+
+hoster()
+{
+	rm $TMP/cache.$FILENAME.* > /dev/null 2>&1
+	/tmp/localhoster/hoster.sh youtube_dl $PAGE > $TMP/cache.$FILENAME.1
+	STREAMURL=`cat $TMP/cache.$FILENAME.1 | head -n1`
+	echo $STREAMURL
 }
 
 case $INPUT in
 	init) $INPUT;;
 	mainmenu) $INPUT;;
+	hoster) $INPUT;;
 	search) $INPUT;;
 esac
