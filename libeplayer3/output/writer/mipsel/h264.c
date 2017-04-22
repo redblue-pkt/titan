0fcf44af2df889318181c27e191d56fab767dc5e
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2017-04-22 16:07:20 +0000

    disable debug

--- libeplayer3/output/writer/mipsel/h264.c
+++ libeplayer3/output/writer/mipsel/h264.c
@@ -55,10 +55,10 @@
 /* ***************************** */
 /* Makros/Constants              */
 /* ***************************** */
-#define H264_DEBUG
+//#define H264_DEBUG
 #ifdef H264_DEBUG
 
-static short debug_level = 10;
+static short debug_level = 0;
 
 #define h264_printf(level, fmt, x...) do { \
 if (debug_level >= level) printf("[%s:%s] " fmt, __FILE__, __FUNCTION__, ## x); } while (0)
