22cb5cbbbff3107870bb7cdfe67b98f949d4c20d
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2017-04-24 18:19:30 +0000

    cleanup

--- titan/libeplayer3/makeBRCM.sh
+++ titan/libeplayer3/makeBRCM.sh
@@ -6,7 +6,7 @@ function usage {
    echo "Usage:"
    echo "$0 platform ffmpeg_ver"
    echo "platform:       mipsel | mipsel_softfpu | armv7 | armv5t"
-   echo "ffmpeg_ver:     2.8.5 | 3.0 | 3.1.1"
+   echo "ffmpeg_ver:     2.8.5 | 3.0.5 | 3.1.1 | 3.2.2"
    exit 1
 }
 
@@ -24,9 +24,9 @@ then
     usage
 fi
 
-if [ "$FFMPEG_VERSION" != "2.8.5" -a "$FFMPEG_VERSION" != "3.0" -a "$FFMPEG_VERSION" != "3.1.1" ];
+if [ "$FFMPEG_VERSION" != "2.8.5" -a "$FFMPEG_VERSION" != "3.0.5" -a "$FFMPEG_VERSION" != "3.1.1" -a "$FFMPEG_VERSION" != "3.2.2" ];
 then
-    echo "Please give supported ffmpeg (2.8.5|3.0|3.1.1) version!"
+    echo "Please give supported ffmpeg (2.8.5|3.0.5|3.1.1|3.2.2) version!"
     usage
 fi
 
@@ -37,8 +37,8 @@ case "$EPLATFORM" in
         export PATH=$BASE_PATH"i686-linux/usr/bin/mipsel-oe-linux/":$PATH
         export SYSROOT=$BASE_PATH"et4x00"
         CFLAGS="  -mel -mabi=32 -march=mips32 "
-        FFMPEG_CFLAGS=" -mel -mabi=32 -march=mips32 "
-        FFMPEG_LDFLAGS=" -lrtmp "
+        FFMPEG_CFLAGS=" -mel -mabi=32 -march=mips32 -I$SYSROOT/usr/include/libxml2/ "
+        FFMPEG_LDFLAGS=" -lssl -lcrypto -lxml2 "
         ;;
     mipsel_softfpu)
         BASE_PATH="/mnt/new2/softFPU/openpli/build/tmp/sysroots/"
@@ -49,10 +49,21 @@ case "$EPLATFORM" in
         FFMPEG_CFLAGS=" -mel -mabi=32 -msoft-float -march=mips32 "
         ;;
     armv7)
-        BASE_PATH="/mnt/new2/vusolo4k/openvuplus_3.0/build/vusolo4k/tmp/sysroots/"
-        export TOOLCHAIN_NAME="arm-oe-linux-gnueabi"
-        export PATH=$BASE_PATH"i686-linux/usr/bin/arm-oe-linux-gnueabi/":$PATH
-        export SYSROOT=$BASE_PATH"vusolo4k"
+        if [ 0 -eq 1 ];
+        then 
+            echo "ARMv7 toolchain openvuplus_3.0"
+            BASE_PATH="/mnt/new2/vusolo4k/openvuplus_3.0/build/vusolo4k/tmp/sysroots/"
+            export TOOLCHAIN_NAME="arm-oe-linux-gnueabi"
+            export PATH=$BASE_PATH"i686-linux/usr/bin/arm-oe-linux-gnueabi/":$PATH
+            export SYSROOT=$BASE_PATH"vusolo4k"
+        else
+            echo "ARMv7 toolchain openpli_4.0"
+            BASE_PATH="/mnt/new2/openpli_micro/openpli-oe-core/build/tmp/sysroots/"
+            export TOOLCHAIN_NAME="arm-oe-linux-gnueabi"
+            export PATH=$BASE_PATH"i686-linux/usr/bin/arm-oe-linux-gnueabi/":$PATH
+            export SYSROOT=$BASE_PATH"hd51"
+
+        fi
         CFLAGS=" -march=armv7-a -mfloat-abi=hard -mfpu=neon "
         ;;
     armv5t)
@@ -95,15 +106,20 @@ SOURCE_FILES+=" output/writer/mipsel/dts.c"
 SOURCE_FILES+=" output/writer/mipsel/amr.c"
 SOURCE_FILES+=" output/writer/mipsel/wma.c"
 
+SOURCE_FILES+=" output/writer/mipsel/h265.c"
 SOURCE_FILES+=" output/writer/mipsel/h264.c"
 SOURCE_FILES+=" output/writer/mipsel/h263.c"
 SOURCE_FILES+=" output/writer/mipsel/mpeg2.c"
 SOURCE_FILES+=" output/writer/mipsel/mpeg4.c"
 SOURCE_FILES+=" output/writer/mipsel/divx3.c"
-SOURCE_FILES+=" output/writer/mipsel/vc1.c"  
-#SOURCE_FILES+=" output/writer/mipsel/wmv.c" 
+SOURCE_FILES+=" output/writer/mipsel/vc1.c"
+SOURCE_FILES+=" output/writer/mipsel/vp.c"
+SOURCE_FILES+=" output/writer/mipsel/wmv.c"
 SOURCE_FILES+=" playback/playback.c"
 
+SOURCE_FILES+=" external/ffmpeg/src/bitstream.c"
+SOURCE_FILES+=" external/ffmpeg/src/latmenc.c"
+SOURCE_FILES+=" external/ffmpeg/src/mpeg4audio.c"
 CURR_PATH=$PWD
 
 EXTEPLAYER3_OUT_FILE=$CURR_PATH"/tmp/out/$EPLATFORM/exteplayer3_ffmpeg"$FFMPEG_VERSION
@@ -114,9 +130,12 @@ function buildFFmpeg
     FFMPEG_BASE_PATH=$CURR_PATH"/tmp/ffmpeg/"
     mkdir -p $FFMPEG_BASE_PATH"tmp/$EPLATFORM/"
     FFMPEG_PATH=$FFMPEG_BASE_PATH"tmp/$EPLATFORM/ffmpeg-"$FFMPEG_VERSION
+    FFMPEG_PATCHES_PATH=$FFMPEG_BASE_PATH"patches/"$FFMPEG_VERSION"/"
     
     SOURCE_URL="http://ffmpeg.org/releases/ffmpeg-"$FFMPEG_VERSION".tar.gz"
     OUT_FILE=$FFMPEG_BASE_PATH"tmp/ffmpeg-"$FFMPEG_VERSION".tar.gz"
+    
+    echo "FFMPEG PATH: $FFMPEG_PATH"
     
     if [ "true" == "$2" ] || [ ! -d $FFMPEG_PATH ];
     then
@@ -133,6 +152,14 @@ function buildFFmpeg
         if [ ! -d $FFMPEG_PATH ];
         then
             tar -zxf $OUT_FILE -C $FFMPEG_BASE_PATH"tmp/$EPLATFORM/"
+            
+            if [ -d $FFMPEG_PATCHES_PATH ];
+            then
+                echo "Applay patches for ffmpeg version $FFMPEG_VERSION if any"
+                cd $FFMPEG_PATH
+                for i in "$FFMPEG_PATCHES_PATH"*.patch; do patch -p1 < $i; done
+                cd $CURR_PATH
+            fi
         fi
         
         CONFIGURE_PATH=$FFMPEG_BASE_PATH"/scripts_$EPLATFORM/configure_"$FFMPEG_VERSION".sh"
@@ -146,15 +173,15 @@ function buildFFmpeg
 }
 
 # rebuild ffmpeg libs, force rebuild
-buildFFmpeg $FFMPEG_VERSION "false" "false"
+buildFFmpeg $FFMPEG_VERSION "true" "true"
 
 rm -rf $EXTEPLAYER3_OUT_FILE
 
 echo "FFMPEG_PATH = $FFMPEG_PATH"
-"$CROSS_COMPILE"gcc -fdata-sections -ffunction-sections -Wl,--gc-sections -Os $CFLAGS --sysroot=$SYSROOT $LDFLAGS $CPPFLAGS -I"$CURR_PATH"/include  -I$FFMPEG_PATH/usr/include/ -L$FFMPEG_PATH/usr/lib/ $SOURCE_FILES -o $EXTEPLAYER3_OUT_FILE -Wfatal-errors -lpthread -lavformat -lavcodec -lavutil -lswresample 
+"$CROSS_COMPILE"gcc -Wimplicit-function-declaration -Wimplicit-int -fdata-sections -ffunction-sections -Wl,--gc-sections -Os $CFLAGS --sysroot=$SYSROOT $LDFLAGS $CPPFLAGS -I"$CURR_PATH"/include -I"$CURR_PATH"/external/ -I$FFMPEG_PATH/usr/include/ -L$FFMPEG_PATH/usr/lib/ $SOURCE_FILES -o $EXTEPLAYER3_OUT_FILE -Wfatal-errors -lpthread -lavformat -lavcodec -lavutil -lswresample 
 "$CROSS_COMPILE"strip -s $EXTEPLAYER3_OUT_FILE
 
-exit 0
+#exit 0
 
 FFMPEG_PACK_TMP=tmp/ffmpeg/tmp/ffmpeg"$FFMPEG_VERSION"_$EPLATFORM
 rm -rf $FFMPEG_PACK_TMP 
