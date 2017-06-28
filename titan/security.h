8e59a6c20cdcd635940262f004af0803f8b35470
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2017-06-28 18:37:33 +0000

    schubs

--- titan/security.h
+++ titan/security.h
@@ -2004,14 +2004,20 @@ struct update* createupdatelist(int mode)
 		free(cmd),cmd = NULL;
 
 		newnode->skinname = "systemupdate_flash_online_menu";
-		newnode->filemask = ostrcat(newnode->filemask, "*.img *.nfi *.zip", 1, 0);
+		if(checkrealbox("DM520") == 1)
+			newnode->filemask = ostrcat(newnode->filemask, "*.img *.nfi", 1, 0);
+		else
+			newnode->filemask = ostrcat(newnode->filemask, "*.img *.nfi *.zip", 1, 0);
 		newnode->filepath = ostrcat(newnode->filepath, "/tmp/online", 1, 0);
 		newnode->type = ostrcat(newnode->type, "online", 1, 0);
 	}
 	else if (mode == 1)
 	{
 		newnode->skinname = "systemupdate_flash_tmp_menu";
-		newnode->filemask = ostrcat(newnode->filemask, "*.img *.nfi *.zip", 1, 0);
+		if(checkrealbox("DM520") == 1)
+			newnode->filemask = ostrcat(newnode->filemask, "*.img *.nfi", 1, 0);
+		else
+			newnode->filemask = ostrcat(newnode->filemask, "*.img *.nfi *.zip", 1, 0);
 		if(file_exist("/var/backup"))
 			newnode->filepath = ostrcat(newnode->filepath, "/var/backup", 1, 0);
 		else
