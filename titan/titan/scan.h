eebceb3eb517d1d214c3f92f0e44bde931c8c799
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2017-08-24 17:47:14 +0000

    next fix

--- titan/titan/scan.h
+++ titan/titan/scan.h
@@ -1311,7 +1311,9 @@ void doscan(struct stimerthread* timernode)
 					//scan DVB-T2 as DVB-T
 					tpnode_tmp = createtransponder(tpnode->id, FE_OFDM, tpnode->orbitalpos, tpnode->frequency, tpnode->inversion, tpnode->symbolrate, tpnode->polarization, tpnode->fec, tpnode->modulation, tpnode->rolloff, tpnode->pilot, 1);
 					if(tpnode_tmp == NULL)
+					{
 						err("can't add DVB-T2 Transponder");
+					}
 					else
 					{
 						debug(500, "add transponder like DVB-T");
