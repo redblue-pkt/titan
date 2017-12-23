e2d68da98d0f64e2d6c722957cdb585f1fc41bf7
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2017-12-23 23:04:57 +0000

    reset

--- titan/titan/frontenddev.h
+++ titan/titan/frontenddev.h
@@ -1637,7 +1637,7 @@ uint16_t fereadsignalstrength(struct dvbdev* node)
 	{
 		printf("brutto STRENGTH %d tuner:%s\n", signal, node->feinfo->name );  
 		if(ostrstr(node->feinfo->name, "Si2166B") != NULL || ostrstr(node->feinfo->name, "BCM45208") != NULL)
-			signal = signal * 516;
+			signal = signal * 257;
 		debug(200, "frontend signal = %02x", (signal * 100) / 0xffff);
 	}
 	if(signal == 0) 
