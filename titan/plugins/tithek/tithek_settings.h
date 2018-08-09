a25ecc512cebf25df72920cee795d933ee2f140b
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2018-08-09 23:55:48 +0000

    fix

--- titan/plugins/tithek/tithek_settings.h
+++ titan/plugins/tithek/tithek_settings.h
@@ -32,6 +32,8 @@ void screentithek_settings()
 	addchoicebox(cover, "4", _("show 20 entrys"));
 	addchoicebox(cover, "5", _("show 30 entrys"));
 	addchoicebox(cover, "6", _("show in list"));
+	addchoicebox(cover, "7", _("show description"));
+
 	setchoiceboxselection(cover, getconfig("tithek_cover", NULL));
 
 	addchoicebox(view, "0", _("show auto entrys"));
@@ -41,6 +43,7 @@ void screentithek_settings()
 	addchoicebox(view, "4", _("show 20 entrys"));
 	addchoicebox(view, "5", _("show 30 entrys"));
 	addchoicebox(view, "6", _("show in list"));
+	addchoicebox(view, "7", _("show description"));
 	setchoiceboxselection(view, getconfig("tithek_view", NULL));
 
 	addchoicebox(picratio, "0", _("no"));
