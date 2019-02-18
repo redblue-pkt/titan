7917481e522f05a27601d02cae27b220c2b9634c
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-02-18 17:42:55 +0000

    Fix

--- titan/harddisk.h
+++ titan/harddisk.h
@@ -801,7 +801,7 @@ void hddformat(char* dev, char* filesystem)
 			cmd = ostrcat("/sbin/cmd.sh \"mkfs.ext3 -T largefile -m0 -O dir_index\" /dev/" , dev, 0, 0);
 		else if(ostrcmp(filesystem, "ext4") == 0)
 			cmd = ostrcat("/sbin/cmd.sh \"mkfs.ext4 -T largefile -m0 -O dir_index\" /dev/" , dev, 0, 0);
-endif
+#endif
 
 		if(format == 2) cmd = ostrcat(cmd , "1", 1, 0);
 			
