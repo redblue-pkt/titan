88ffe5888750aa41b072f96c41cf7d0e4ed00d60
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-02-19 00:00:12 +0000

    fix fix

--- titan/titan/player.h
+++ titan/titan/player.h
@@ -295,7 +295,7 @@ int playerstartts(char* file, int flag)
 		close(fd);
 		return 1;
 	}
-	printf("player-> dvrnode: %i:%i\n", dvrnode->devnr, dvrnode->->adapter);
+	printf("player-> dvrnode: %i:%i\n", dvrnode->devnr, dvrnode->adapter);
 	if(flag == 0 || flag == 2)
 	{
 		//TODO: funktion to get tssize from file content
