719648de2eb72bb88baad58051ef7878e9cd990c
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-02-20 00:40:32 +0000

    update kino and fix cast search

--- titan/mediathek/localparser_secret/kinox.sh
+++ titan/mediathek/localparser_secret/kinox.sh
@@ -69,9 +69,19 @@ init()
 mainmenu()
 {
 	echo "Kino#$SRC $SRC kino 'Kino-filme.html'#http://atemio.dyndns.tv/mediathek/menu/Cine-Films.jpg#Cine-Films.jpg#$NAME#0" >$TMP/$FILENAME.list
+
+	echo "Movie (Latest)#$SRC $SRC latest 'Latest-Movies.html'#http://atemio.dyndns.tv/mediathek/menu/Latest-Movies.jpg#Latest-Movies.jpg#$NAME#0" >>$TMP/$FILENAME.list
+	echo "Movie (Popular)#$SRC $SRC latest 'Popular-Movies.html'#http://atemio.dyndns.tv/mediathek/menu/Popular-Movies.jpg#Popular-Movies.jpg#$NAME#0" >>$TMP/$FILENAME.list
 	echo "Movie (A-Z)#$SRC $SRC sorted movie#http://atemio.dyndns.tv/mediathek/menu/a-z.jpg#a-z.jpg#$NAME#0" >>$TMP/$FILENAME.list
+
+	echo "Tv (Latest)#$SRC $SRC latest 'Latest-TVSeries.html'#http://atemio.dyndns.tv/mediathek/menu/Latest-Series.jpg#Latest-Series.jpg#$NAME#0" >$TMP/$FILENAME.list
+	echo "Tv (Popular)#$SRC $SRC latest 'Popular-TVSeries.html'#http://atemio.dyndns.tv/mediathek/menu/Popular-Series.jpg#Popular-Series.jpg#$NAME#0" >$TMP/$FILENAME.list
 	echo "Tv (A-Z)#$SRC $SRC sorted series#http://atemio.dyndns.tv/mediathek/menu/a-z.jpg#a-z.jpg#$NAME#0" >>$TMP/$FILENAME.list
+
+	echo "Doku (Latest)#$SRC $SRC latest 'Latest-Documentations.html'#http://atemio.dyndns.tv/mediathek/menu/Latest-Documentations.jpg#Latest-Documentations.jpg#$NAME#0" >>$TMP/$FILENAME.list
+	echo "Doku (Popular)#$SRC $SRC latest 'Popular-Documentations.html'#http://atemio.dyndns.tv/mediathek/menu/Popular-Documentations.jpg#Popular-Documentations.jpg#$NAME#0" >>$TMP/$FILENAME.list
 	echo "Doku (A-Z)#$SRC $SRC sorted documentation#http://atemio.dyndns.tv/mediathek/menu/a-z.jpg#a-z.jpg#$NAME#0" >>$TMP/$FILENAME.list
+
 	echo "Actor (A-Z)#$SRC $SRC sorted actor#http://atemio.dyndns.tv/mediathek/menu/a-z.jpg#a-z.jpg#$NAME#0" >>$TMP/$FILENAME.list
 	echo "Director (A-Z)#$SRC $SRC sorted director#http://atemio.dyndns.tv/mediathek/menu/a-z.jpg#a-z.jpg#$NAME#0" >>$TMP/$FILENAME.list
 	echo "Search#http://atemio.dyndns.tv/mediathek/youtube/streams/kinox.all-sorted.list#http://atemio.dyndns.tv/mediathek/menu/search.jpg#search.jpg#KinoX#21" >>$TMP/$FILENAME.list
@@ -385,17 +395,133 @@ kino()
 			fi
 
 		done 3<$TMP/cache.$FILENAME.2
-		cat "$TMP/$FILENAME.list"
 		rm $TMP/cache.$FILENAME.* > /dev/null 2>&1
 
 	fi
 	echo "$TMP/$FILENAME.list"
 }
 
+latest()
+{
+#	rm $TMP/cache.$FILENAME.* > /dev/null 2>&1
+	rm $TMP/$FILENAME.list
+	if [ ! -e "$TMP/$FILENAME.list" ]; then
+		piccount=0
+#		$curlbin "$URL/$PAGE" -o "$TMP/cache.$FILENAME.1"
+		$ACTIVEBIN "$URL/$PAGE" > $TMP/cache.$FILENAME.1
+
+		if [ `cat $TMP/cache.$FILENAME.1 | grep "KinoX 404 Not Found" | wc -l` -eq 1 ];then
+			ERRORMSG="KinoX 404 Not Found"
+#			echo "errormsg=$ERRORMSG"
+			LINE="$ERRORMSG#$NEWPAGE#$PIC#kinox_$piccount.jpg#KinoX#22"
+			echo "$LINE" > $TMP/$FILENAME.list
+			exit
+		fi
+
+		if [ `cat $TMP/cache.$FILENAME.1 | grep "KinoX 503 Service Temporarily Unavailable" | wc -l` -eq 1 ];then
+			ERRORMSG="KinoX 503 Service Temporarily Unavailable"
+#			echo "errormsg=$ERRORMSG"
+			LINE="$ERRORMSG#$NEWPAGE#$PIC#kinox_$piccount.jpg#KinoX#22"
+			echo "$LINE" > $TMP/$FILENAME.list
+			exit
+		fi
+
+		cat $TMP/cache.$FILENAME.1 | tr '\n' ' ' | sed 's/<tr>/\n<tr>/g' | grep ^"<tr>" | grep "/Stream/" >$TMP/cache.$FILENAME.2
+
+		while read -u 3 ROUND; do
+			piccount=`expr $piccount + 1`
+			filename=`echo $ROUND | sed -nr 's/.*<a href="\/Stream\/([^\/]+)" .*/\1/p'`
+			picname=`echo $filename | sed 's!.html!.jpg!'`
+			searchname=`echo $filename | sed 's!.html!!'`	
+
+			PAGE=`echo $ROUND | sed -nr 's/.*<a href="([^"]+)" .*/\1/p'`
+			TITLE=`echo $picname | sed 's!.jpg!!' | tr "_" " "`
+			LANG=`echo $ROUND | grep 'src="/gr/sys/lng' | sed 's!src="/gr/sys/lng/!\n!' | tail -n1 |cut -d"." -f1`
+			IMDB=`echo $ROUND | sed -nr 's/.*"Rating">([^>]+)<\/td>.*/\1/p'`
+			YEAR=`echo $ROUND | sed -nr 's/.*"Year">([^>]+)<\/span>.*/\1/p'`
+
+			if [ `cat /mnt/config/titan.cfg | grep tithek_kinox_pic=1 | wc -l` -eq 1 ];then
+		#		$wgetbin --no-check-certificate "$MAINURL/$PAGE" -O cache."$filename".list
+#				echo $BIN /tmp/localhoster/cloudflare.py "$URL/$PAGE"
+				$ACTIVEBIN "$URL/$PAGE" > $TMP/cache."$filename".list
+				if [ $(cat $TMP/cache."$filename".list | wc -l) -eq 0 ];then
+#					echo $BIN2 /tmp/localhoster/cloudflare.py "$URL/$PAGE"
+					$ACTIVEBIN "$URL/$PAGE" > $TMP/cache."$filename".list
+				fi
+				if [ $(cat $TMP/cache."$filename".list | wc -l) -eq 0 ];then
+#					echo $BIN3 /tmp/localhoster/cloudflare.py "$URL/$PAGE"
+					$ACTIVEBIN "$URL/$PAGE" > $TMP/cache."$filename".list
+				fi
+				if [ $(cat $TMP/cache."$filename".list | wc -l) -eq 0 ];then
+#					echo $BIN4 /tmp/localhoster/cloudflare.py "$URL/$PAGE"
+					$ACTIVEBIN "$URL/$PAGE" > $TMP/cache."$filename".list
+				fi
+				if [ $(cat $TMP/cache."$filename".list | wc -l) -eq 0 ];then
+#					echo $BIN5 /tmp/localhoster/cloudflare.py "$URL/$ROUND"
+					$ACTIVEBIN "$URL/$filepath" > $TMP/cache."$filename".list
+				fi
+				if [ $(cat $TMP/cache."$filename".list | wc -l) -eq 0 ];then
+#					echo $BIN6 /tmp/localhoster/cloudflare.py "$URL/$PAGE"
+					$ACTIVEBIN "$URL/$PAGE" > $TMP/cache."$filename".list
+				fi
+				PIC=$URL/`cat $TMP/cache."$filename".list | tr '><' '>\n<' | grep $picname | cut -d '"' -f2 | sort -um`
+				LANG=`cat $TMP/cache."$filename".list | grep 'alt="language" src="/gr/sys/lng' | sed 's!alt="language" src="/gr/sys/lng/!\n!' | tail -n1 |cut -d"." -f1`
+			else
+				PIC="http://atemio.dyndns.tv/mediathek/menu/default.jpg"
+#				LANG=1
+			fi
+
+		 	LANGTXT=" (de)"
+
+			if [ ! -z $LANG ];then
+				if [ $LANG = "1" ];then
+				 	LANGTXT=" (de)"
+				elif [ $LANG = "2" ];then
+				 	LANGTXT=" (en)"
+				else
+				 	LANGTXT=" (??)"
+				fi
+			fi
+
+			if [ ! -z "$IMDB" ];then
+				IMDBTXT=" ($IMDB)"
+			fi
+			if [ ! -z "$YEAR" ];then
+				YEARTXT=" ($YEAR)"
+			fi
+
+			NEWPAGE="$URL$PAGE"
+
+			if [ -z "$PIC" ]; then
+				PIC="http://atemio.dyndns.tv/mediathek/menu/default.jpg"
+			fi
+
+			TITLE=`echo $TITLE | sed -e 's/&#038;/&/g' -e 's/&amp;/und/g' -e 's/&quot;/"/g' -e 's/&lt;/\</g' -e 's/&#034;/\"/g' -e 's/&#039;/\"/g' -e 's/#034;/\"/g' -e 's/#039;/\"/g' -e 's/&szlig;/\C3x/g' -e 's/&ndash;/-/g' -e 's/&Auml;/\C3/g' -e 's/&Uuml;/\C3S/g' -e 's/&Ouml;/\C3/g' -e 's/&auml;/\E4/g' -e 's/&uuml;/\FC/g' -e 's/&ouml;/\F6/g' -e 's/&eacute;/\E9/g' -e 's/&egrave;/\E8/g' -e 's/%F6/\F6/g' -e 's/%FC/\FC/g' -e 's/%E4/\E4/g' -e 's/%26/&/g' -e 's/%C4/\C3/g' -e 's/%D6/\C3/g' -e 's/%DC/\C3S/g' -e 's/%28/(/g' -e 's/%29/)/g' -e 's/%3A/:/g' -e 's/%40/@/g' -e 's/%2B/&/g' -e 's/%C3/A/g' -e 's/%B1/&/g' -e 's/%5B//g' -e 's/%5D//g' -e 's!%2F!/!g' -e 's/|/ /g' -e 's/(/ /g' -e 's/)/ /g' -e 's/+/ /g' -e 's/\//-/g' -e 's/,/ /g' -e 's/;/ /g' -e 's/:/ /g' -e 's/\.\+/./g'`
+
+			if [ ! -z "$TITLE" ] && [ ! -z "$NEWPAGE" ];then
+				if [ ! -e $TMP/$FILENAME.list ];then
+					touch $TMP/$FILENAME.list
+				fi
+				piccount=`expr $piccount + 1`
+				LINE="$TITLE$LANGTXT$YEARTXT$IMDBTXT#$NEWPAGE#$PIC#kinox_$piccount.jpg#KinoX#22aaaa"
+
+				if [ `cat $TMP/$FILENAME.list | grep "$TITLE" | wc -l` -eq 0 ];then
+					echo "$LINE" >> $TMP/$FILENAME.list
+				fi
+			fi
+		done 3<$TMP/cache.$FILENAME.2
+		rm $TMP/cache.$FILENAME.* > /dev/null 2>&1
+
+	fi
+	echo "$TMP/$FILENAME.list"
+}
+
+
 case $INPUT in
 	init) $INPUT;;
 	mainmenu) $INPUT;;
 	search) $INPUT;;
 	kino) $INPUT;;
+	latest) $INPUT;;
 	sorted) $INPUT;;
 esac
