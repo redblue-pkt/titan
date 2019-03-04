9e2375edd82ac8c3ce5777f504261e068b66a036
aafsvn <aafsvn@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-03-04 19:57:16 +0000

    enable changefbresolution on mipsel

--- titan/titan/fb.h
+++ titan/titan/fb.h
@@ -447,10 +447,11 @@ void blitfb1()
 void changefbresolution(char *value, int flag)
 {
 	debug(444, "fb->colbytes: %d", fb->colbytes);
+#ifndef OEBUILD
 #ifdef MIPSEL
 	return;
 #endif
-
+#endif
 	if(ostrcmp("pal", value) == 0)
 	{
 		fb->width = 720;
