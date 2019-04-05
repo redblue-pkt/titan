a9381083b5e48fa43bb7a8a6bc836906f1233d0f
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-04-05 16:33:36 +0000

    fix last

--- titan/global.h
+++ titan/global.h
@@ -2245,7 +2245,7 @@ char* convert_dtimesec(int sec)
 
 int checkdate()
 {
-	time_t dvbtime = 0; 
+	time_t dvbtime = 0;
 	if(timeokw == 0 || time(NULL) < 1072224000 || status.timeupdatecount > 3600) // 01.01.2004
 	{
 		printf("--> check dvbtime");
