f9413768e904fb88bf8437d9a5d273d6c5a88653
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-04-09 16:52:10 +0000

    SF8008 test clear buffer at standby

--- titan/titan/service.h
+++ titan/titan/service.h
@@ -836,6 +836,8 @@ int servicestop(struct service *node, int clear, int flag)
 
 			dmxstop(node->dmxaudiodev);
 			videostop(node->videodev, clear);
+			if(clear == 1 && checkchipset("3798MV200") == 1)
+				videoclearbuffer(status.aktservice->videodev);
 		}
 		int	fastzap = getconfigint("fastzap", NULL);
 
