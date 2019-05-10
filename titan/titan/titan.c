7f69cf722123c8dab533d2d5d3a6201de80efaad
aafsvn <aafsvn@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-05-10 18:03:57 +0000

    update sf8008s

--- titan/titan/titan.c
+++ titan/titan/titan.c
@@ -873,7 +873,7 @@ timeokw = 1;
 	waitvsync();
 	if(checkbox("DM7020HD") == 0 && checkbox("DM7020HDV2") == 0 && checkbox("VUSOLO2") == 0 && checkbox("DM900") == 0 && checkbox("DM920") == 0 && checkbox("DM520") == 0 && checkbox("DM525") == 0)
 		setfbosd();
-	if(checkrealbox("HD51") == 1 || checkrealbox("HD60") == 1 || checkrealbox("HD61") == 1 || checkrealbox("SF8008") == 1)
+	if(checkrealbox("HD51") == 1 || checkrealbox("HD60") == 1 || checkrealbox("HD61") == 1 || checkrealbox("SF8008") == 1 || checkrealbox("SF8008S") == 1 || checkrealbox("SF8008T") == 1)
 		setfbosdnull();
 	status.usedirectfb = 1;
 #endif
