dbb3e76df10c4c6e970b0252d3d2c7e3ce547b9e
aafsvn <aafsvn@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-06-29 19:46:02 +0000

    remove atemio workaround 2 not needed anymore

--- titan/titan.c
+++ titan/titan.c
@@ -732,8 +732,8 @@ timeokw = 1;
 
 	// set pvr 1 = allowed , 0 = disabled
 	status.pvr = 1;	
-	if(file_exist("/etc/.stable") && (checkbox("ATEMIO6000") == 1 || checkbox("ATEMIO6100") == 1 || checkbox("ATEMIO6200") == 1))
-		status.pvr = 0;
+//	if(file_exist("/etc/.stable") && (checkbox("ATEMIO6000") == 1 || checkbox("ATEMIO6100") == 1 || checkbox("ATEMIO6200") == 1))
+//		status.pvr = 0;
 
 	int sos = getconfigint("sos", NULL);
 	sos = 1; //workaround
