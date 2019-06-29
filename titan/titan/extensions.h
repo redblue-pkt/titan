c7d30b089d8fcc481fb850f041e9534fba36ac56
aafsvn <aafsvn@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-06-29 22:45:01 +0000

    fix tpk install dont show swap allways

--- titan/titan/extensions.h
+++ titan/titan/extensions.h
@@ -169,7 +169,7 @@ char* getinstallpath(char* path, char* size)
 	if(path == NULL || path[0] == '*' || ostrstr(path, "swap") != NULL)
 	{
 #ifdef OEBUILD
-		if(file_exist("/media/.swapextensionsdev") == 1 || file_exist("/var/swap"))
+		if(file_exist("/media/.swapextensionsdev") == 1 || (file_exist("/var/swap") && !isdir("/var/swap")))
 #else
 		if(file_exist("/tmp/.swapextensionsdev") == 1 || file_exist("/var/swap"))
 #endif
