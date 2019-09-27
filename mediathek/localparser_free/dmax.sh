861533c4ea785916f659c1ee0102bd509600d336
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-09-27 22:35:52 +0000

    fix tithek

--- mediathek/localparser_free/dmax.sh
+++ mediathek/localparser_free/dmax.sh
@@ -37,10 +37,10 @@ fi
 
 init()
 {
-	if [ "$ARCH" == "arm" ] || [ "$ARCH" == "mipsel" ] || ([ "$ARCH" == "sh4" ] && [ -e "/etc/.beta" ]);then
+#	if [ "$ARCH" == "arm" ] || [ "$ARCH" == "mipsel" ] || ([ "$ARCH" == "sh4" ] && [ -e "/etc/.beta" ]);then
 		rm -rf $TMP > /dev/null 2>&1
 		echo "$NAME ($TYPE)#$SRC $SRC mainmenu#http://atemio.dyndns.tv/mediathek/menu/$PARSER.jpg#$PARSER.jpg#TiThek#0"
-	fi
+#	fi
 }
 
 mainmenu()
