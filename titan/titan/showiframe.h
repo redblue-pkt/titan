000e585dc007f798ef7e13c0df60e288e4bb55e8
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-10-08 04:30:06 +0000

    update showiframe

--- titan/titan/showiframe.h
+++ titan/titan/showiframe.h
@@ -4,16 +4,15 @@
 #define VIDEO_STREAMTYPE_MPEG2 0
 #define VIDEO_STREAMTYPE_MPEG4_H264 1
 
-void singlepicthread(struct stimerthread* self, char* path, int flag)
+void singlepicthread(struct stimerthread* self, struct dvbdev* node , char* filename)
 {
-	printf("singlepicthread start\n");
+	status.singlepicthreadstatus += 1;
+	printf("singlepicthread (%d) start filename: %s\n", status.singlepicthreadstatus, filename);
 
 	if(status.singlepicthread != NULL || self == NULL)
 	{
-		free(path); path = NULL;
 		return;
 	}
-	status.singlepicthreadstatus = 1;
 	status.singlepicthread = self;
 
 
@@ -27,76 +26,75 @@ void singlepicthread(struct stimerthread* self, char* path, int flag)
 */
 	usleep(150000);
 
-	showsinglepic();
+	showsinglepic(node, filename);
 
 	status.singlepicthread = NULL;
-	status.singlepicthreadstatus = 0;
-	printf("singlepicthread end\n");
+	printf("singlepicthread (%d) end filename: %s\n", status.singlepicthreadstatus, filename);
 }
 
-void showsinglepic()
+void showsinglepic(struct dvbdev* node, char* filename)
 {
-	printf("showsinglepic start\n");
+	printf("showsinglepic (%d) start filename: %s\n", status.singlepicthreadstatus, filename);
 
 	if (status.singlepicfd >= 0)
 	{
-		if (ioctl(status.singlepicfd, VIDEO_STOP, 0) < 0)
-			printf("showsinglepic VIDEO_STOP failed: %m\n");
-		if (ioctl(status.singlepicfd, VIDEO_SELECT_SOURCE, VIDEO_SOURCE_DEMUX) < 0)
-				printf("showsinglepic VIDEO_SELECT_SOURCE DEMUX failed: %m\n");
-		close(status.singlepicfd);
-		status.singlepicfd = -1;
+		printf("showsinglepic (%d) videostop filename: %s\n", status.singlepicthreadstatus, filename);
+		videostop(node, 1);
+		videoselectsource(node, VIDEO_SOURCE_DEMUX);
+		videoclose(node, -1);
+		node = NULL;
 	}
 
-	printf("showsinglepic end\n");
+	printf("showsinglepic (%d) end filename: %s\n", status.singlepicthreadstatus, filename);
 }
 
 //flag 0: stop service
 //flag 1: not stop service
 int singlepicstart(const char *filename, int flag)
 {
+	printf("---start----------------------\n");
+	printf("singlepicstart filename: %s\n", filename);
+
 	int ret = 0;
 
-	if(checkchipset("3798MV200") == 0)
+	if(status.aktservice->type != STILLPIC)
 	{
-		if(status.aktservice->type != STILLPIC)
+		if(flag == 0)
 		{
-			if(flag == 0)
-			{
-				ret = servicestop(status.aktservice, 0, 1);
-				if(ret == 1) return 1;
-			}
+			ret = servicestop(status.aktservice, 0, 1);
+			if(ret == 1) return 1;
 		}
 	}
 
 	int fd = open(filename, O_RDONLY);
 	if(fd >= 0)
 	{
+
+
 		struct dvbdev* videonode = NULL;
 		struct stat s;
 		fstat(fd, &s);
 
-		if(checkchipset("3798MV200") == 0)
+		if(checkchipset("3798MV200") == 1 || status.aktservice->type != STILLPIC)
 		{
-			if(status.aktservice->type != STILLPIC)
+			if(status.aktservice->videodev != NULL)
 			{
-				if(status.aktservice->videodev != NULL)
-				{
-					videoclose(status.aktservice->videodev, -1);
-					status.aktservice->videodev = NULL;
-				}
-				videonode = videoopen(0, 0);
-				videoselectsource(videonode, VIDEO_SOURCE_MEMORY);
-	
-				videosetformat(videonode, VIDEO_FORMAT_16_9);
-				videosetstreamtype(videonode, MPEGV);
-				videoplay(videonode);
-			}	
-			else
-				videonode = status.aktservice->videodev;
-		}
+				videoclose(status.aktservice->videodev, -1);
+				status.aktservice->videodev = NULL;
+			}
 
-		if(checkchipset("3798MV200") == 1 || videonode != NULL)
+			videonode = videoopen(0, 0);
+			videoselectsource(videonode, VIDEO_SOURCE_MEMORY);
+
+			videosetformat(videonode, VIDEO_FORMAT_16_9);
+			videosetstreamtype(videonode, MPEGV);
+			videoplay(videonode);
+
+		}
+		else
+			videonode = status.aktservice->videodev;
+
+		if(videonode != NULL)
 		{
 			struct video_still_picture stillpic;
 			char* iframe = NULL;
@@ -109,11 +107,9 @@ int singlepicstart(const char *filename, int flag)
 				return 1;
 			}
 
-			if(checkchipset("3798MV200") == 0)
-			{
-				status.aktservice->type = STILLPIC;
-				status.aktservice->videodev = videonode;
-			}
+			status.aktservice->type = STILLPIC;
+			status.aktservice->videodev = videonode;
+			status.singlepicfd = status.aktservice->videodev->fd;
 
 			TEMP_FAILURE_RETRY(read(fd, iframe, s.st_size));
 			stillpic.iFrame = iframe;
@@ -127,6 +123,7 @@ int singlepicstart(const char *filename, int flag)
 			unsigned char stuffing[8192];
 
 			memset(stuffing, 0, 8192);
+
 			if(checkchipset("3798MV200") == 1)
 			{
 				int streamtype;
@@ -134,51 +131,30 @@ int singlepicstart(const char *filename, int flag)
 					streamtype = VIDEO_STREAMTYPE_MPEG4_H264;
 				else
 					streamtype = VIDEO_STREAMTYPE_MPEG2;
-
-				if (ioctl(status.singlepicfd, VIDEO_SELECT_SOURCE, VIDEO_SOURCE_MEMORY) < 0)
-					printf("[eTSMPEGDecoder] VIDEO_SELECT_SOURCE MEMORY failed: %m\n");
-				if (ioctl(status.singlepicfd, VIDEO_SET_STREAMTYPE, streamtype) < 0)
-					printf("[eTSMPEGDecoder] VIDEO_SET_STREAMTYPE failed: %m\n");
-				if (ioctl(status.singlepicfd, VIDEO_PLAY) < 0)
-					printf("[eTSMPEGDecoder] VIDEO_PLAY failed: %m\n");
-				if (ioctl(status.singlepicfd, VIDEO_CONTINUE) < 0)
-					printf("[eTSMPEGDecoder] VIDEO_CONTINUE: %m\n");
-				if (ioctl(status.singlepicfd, VIDEO_CLEAR_BUFFER) < 0)
-					printf("[eTSMPEGDecoder] VIDEO_CLEAR_BUFFER: %m\n");
-
-				while(pos <= (s.st_size - 4) && !(seq_end_avail = (!iframe[pos] && !iframe[pos + 1] && iframe[pos + 2] == 1 && iframe[pos + 3] == 0xB7)))
-					++pos;
-			
-				if((iframe[3] >> 4) != 0xE) // no pes header
-					write(status.singlepicfd, pes_header, sizeof(pes_header));
-				else
-					iframe[4] = iframe[5] = 0x00;
-			
-				write(status.singlepicfd, iframe, s.st_size);
-			
-				if(!seq_end_avail)
-					write(status.singlepicfd, seq_end, sizeof(seq_end));
-			
-				write(status.singlepicfd, stuffing, 8192);
-				addtimer(&singlepicthread, START, 1000, 1, (void*)ostrcat(filename, NULL, 0, 0), (void*)flag, NULL);
+				videoselectsource(videonode, VIDEO_SOURCE_MEMORY);
+				videosetstreamtype(videonode, streamtype);
+				videosetformat(videonode, VIDEO_FORMAT_16_9);	
+				videoplay(videonode);
+				videocontinue(videonode);
+				videoclearbuffer(videonode);
 			}
+
+			while(pos <= (s.st_size - 4) && !(seq_end_avail = (!iframe[pos] && !iframe[pos + 1] && iframe[pos + 2] == 1 && iframe[pos + 3] == 0xB7)))
+				++pos;
+		
+			if((iframe[3] >> 4) != 0xE) // no pes header
+				write(videonode->fd, pes_header, sizeof(pes_header));
 			else
-			{
-				while(pos <= (s.st_size - 4) && !(seq_end_avail = (!iframe[pos] && !iframe[pos + 1] && iframe[pos + 2] == 1 && iframe[pos + 3] == 0xB7)))
-					++pos;
-			
-				if((iframe[3] >> 4) != 0xE) // no pes header
-					write(videonode->fd, pes_header, sizeof(pes_header));
-				else
-					iframe[4] = iframe[5] = 0x00;
-			
-				write(videonode->fd, iframe, s.st_size);
-			
-				if(!seq_end_avail)
-					write(videonode->fd, seq_end, sizeof(seq_end));
-			
-				write(videonode->fd, stuffing, 8192);
-			}
+				iframe[4] = iframe[5] = 0x00;
+		
+			write(videonode->fd, iframe, s.st_size);
+		
+			if(!seq_end_avail)
+				write(videonode->fd, seq_end, sizeof(seq_end));
+		
+			write(videonode->fd, stuffing, 8192);
+			if(checkchipset("3798MV200") == 1)
+				addtimer(&singlepicthread, START, 150000, 1, (void*)videonode, (void*)ostrcat(filename, NULL, 0, 0), NULL);
 #else
 			videoclearbuffer(videonode);
 			videostillpicture(videonode, &stillpic);
@@ -193,6 +169,8 @@ int singlepicstart(const char *filename, int flag)
 		err("open %s\n", filename);
 		return 1;
 	}
+	printf("---end-----------------------\n");
+
 	return 0;
 }
 
