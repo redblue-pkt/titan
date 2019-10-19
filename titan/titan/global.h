2eba2ec2622d1692dc4357c446e19990a35bac74
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-10-19 18:22:39 +0000

    fix

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -6628,21 +6628,21 @@ char* gethypridtunerchoices(int dev)
 	{
 		tmpstr2 = string_resub("Mode ", ":", tmpstr5, 0);
 		value = ostrcat(tmpstr2, "\n", 0, 0);
-		tmpstr5[0] = '\n'
+		tmpstr5[0] = '\n';
 	}
 	tmpstr5 = ostrstr(tmpstr5, "Mode ");
 	if(tmpstr5 != NULL)
 	{
 		tmpstr3 = string_resub("Mode ", ":", tmpstr5, 0);
 		value = ostrcat(tmpstr3, "\n", 0, 0);
-		tmpstr5[0] = '\n'
+		tmpstr5[0] = '\n';
 	}
 		tmpstr5 = ostrstr(tmpstr5, "Mode ");
 	if(tmpstr5 != NULL)
 	{
 		tmpstr4 = string_resub("Mode ", ":", tmpstr5, 0);
 		value = ostrcat(tmpstr4, "\n", 0, 0);
-		tmpstr5[0] = '\n'
+		tmpstr5[0] = '\n';
 	}
 #else
 
