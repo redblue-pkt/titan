fe461b0ee787fa4a0d48c6899789957f14ed8ac3
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-10-24 22:38:03 +0000

    add kodistarter / e2starter plugin

---
+++ titan/plugins/kodistarter/kodistarter.h
@@ -0,0 +1,61 @@
+#ifndef KODISTARTER_H
+#define KODISTARTER_H
+
+extern struct skin* skin;
+struct fb *fb = NULL;
+
+void screenkodistarter()
+{
+	int rcret = -1, ret = 0, rcsaverwait = 0, count = 0, screensaver_delay = 0, rcwait = 1000;
+	char* cmd = NULL, *buf = NULL, *tmpstr = NULL, *tmpstr1 = NULL;
+	int fifo = -1;
+	struct skin* kodistarter = getscreen("kodistarter");
+	struct skin* guiname = getscreennode(kodistarter, "guiname");
+	struct skin* install = getscreennode(kodistarter, "install");
+	struct skin* loading = getscreen("loading");
+	
+	ret = servicestop(status.aktservice, 1, 1);
+	if(ret == 1) return;
+
+	drawscreen(skin, 0, 0);
+	drawscreen(kodistarter, 0, 0);
+
+	cmd = ostrcat(cmd, "/sbin/kodi.sh", 1, 0);
+
+	//start gui
+	debug(10, "cmd: %s", cmd);
+	system(cmd);
+	free(cmd), cmd = NULL;
+
+	tmpstr = ostrcat(tmpstr, _("Kodi Gui Starter"), 1, 0);
+	changetext(guiname, tmpstr);
+	free(tmpstr), tmpstr = NULL;
+
+	tmpstr = ostrcat(_("Wait for install or press EXIT"), NULL, 0, 0);
+	changetext(install, tmpstr);
+
+	changepic(kodistarter, "%pluginpath%/kodistarter/skin/background.jpg");					
+	drawscreen(kodistarter, 0, 0);
+
+	while(1)
+	{
+		rcret = waitrcext(NULL, rcwait, 0, 0);
+
+ 		if(rcret == getrcconfigint("rcexit", NULL)) break;
+		if(rcret == getrcconfigint("rcok", NULL)) break;
+	}
+
+	//stop renderer
+	system("killall -9 kodi.sh");
+		
+	if(status.lastservice != NULL)
+		servicestart(status.lastservice->channel, NULL, NULL, 0);
+
+	free(tmpstr), tmpstr = NULL;
+
+	delownerrc(kodistarter);
+	clearscreen(loading);
+	clearscreen(kodistarter);
+}
+
+#endif
