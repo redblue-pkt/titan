6667d717acd8cb17281d035682c43a71f3c2d8e9
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2019-11-09 15:05:01 +0000

    fix

--- titan/titan/global.h
+++ titan/titan/global.h
@@ -8185,7 +8185,7 @@ int setzapmode(char* value)
 
 int update_iptv(int file)
 {
-	char* path = NULL, *ip = NULL, *tmpstr = NULL, *link = NULL;
+	char* path = NULL, *ip = NULL, *port = NULL, *tmpstr = NULL, *link = NULL;
 	int ret = 0;
 
 	tmpstr = dirname(file);
@@ -8202,12 +8202,17 @@ int update_iptv(int file)
 		if(file_exist(tmpstr))
 			unlink(tmpstr);
 
-		ip = oregex("://(.*)/.*", link);
+		ip = oregex("://(.*):.*", link);
 		path = oregex("://.*/(.*).*", link);
+		port = oregex("://.*/.*:(.*)/.*", link);
+
+		debug(10, "ip %s", ip);
+		debug(10, "path %s", path);
+		debug(10, "port %s", port);
 
 
 		if(!file_exist(tmpstr))
-			gethttp(ip, path, 80, tmpstr, HTTPAUTH, 5000, NULL, 0);
+			gethttp(ip, path, port, tmpstr, HTTPAUTH, 5000, NULL, 0);
 
 		if(file_exist(tmpstr)) ret = 1;
 
