6f39b97b4ed77a8b3cd7e70ac19f80f63411e5e0
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2020-01-01 20:17:52 +0000

    fix

--- mediathek/localhoster/cloudflare.py
+++ mediathek/localhoster/cloudflare.py
@@ -23,19 +23,15 @@ class CloudflareResolver(object):
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                    'Content-Type': 'text/html; charset=utf-8'}
 
+#        html = self.net.http_GET(web_url, headers=headers).content
+#        #html = self.request(web_url, cookie_file=/mnt/network/cookies, cloudflare=True)
+#        ret = self.net.save_cookies('/mnt/network/cookies')      
+#        print "html", html.encode('utf8')
 
-#        print "cloudflare.CheckIfActive: ", web_url
-
-        try:
-            CF = cloudflare.CloudflareBypass()
-            html = CF.GetHtml(web_url)
-            print CF.GetReponseInfo()
-            print "html", html
-        except:
-            html = self.net.http_GET(web_url, headers=headers).content
-            #html = self.request(web_url, cookie_file=/mnt/network/cookies, cloudflare=True)
-            ret = self.net.save_cookies('/mnt/network/cookies')      
-            print "html", html.encode('utf8')
+        CF = cloudflare.CloudflareBypass()
+        html = CF.GetHtml(web_url)
+        print CF.GetReponseInfo()
+        print "html", html
 
 sys.stdout = CloudflareResolver()
 
