2846d4fa2ade9b677218f63a802ac9b97b040031
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2020-02-17 07:09:28 +0000

    schubs

--- titan/titan/titan.c
+++ titan/titan/titan.c
@@ -1096,7 +1096,7 @@ timeokw = 1;
 	if(getconfigint("ciplusrun", NULL) == 1)
 	{
 		serviceret = servicestart(getchannel(getconfigint("ciplus_serviceid", NULL), getconfigllu("ciplus_transponderid", NULL)), getconfig("ciplus_channellist", NULL), NULL, 0);
-		sleep(2);
+		sleep(2); 
 	}
 
 	//tune to channel
