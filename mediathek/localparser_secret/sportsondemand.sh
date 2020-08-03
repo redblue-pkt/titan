013063f9a5a9c9b9942a8feb4dd7219ece1481a0
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2020-08-03 21:12:20 +0000

    fix tithek

--- mediathek/localparser_secret/sportsondemand.sh
+++ mediathek/localparser_secret/sportsondemand.sh
@@ -991,6 +991,9 @@ hoster()
 #		cat /tmp/localcache/cache.sportsondemand.hoster..1 | tr -d '\r'|  tr -d '\n' | sed 's/<iframe/\n<iframe/g' | grep ^"<iframe" |awk 'BEGIN {} /<iframe/ { i = index($0, "src=\"") + 5;j = index(substr($0, i), "\"") - 1;newpage = substr($0, i, j); print newpage; next}' | grep -v gif | grep -v allowTransparency
 		URL=`cat $TMP/cache.$PARSER.$INPUT.$FROM.1 | tr -d '\r'|  tr -d '\n' | sed 's/<iframe/\n<iframe/g' | grep ^"<iframe" |awk 'BEGIN {} /<iframe/ { i = index($0, "src=\"") + 5;j = index(substr($0, i), "\"") - 1;newpage = substr($0, i, j); print newpage; next}' | grep -v gif | grep -v allowTransparency`
 
+        if [ ! `echo $URL | grep " //" | wc -l` -eq 0 ];then
+            URL=$(echo $URL | sed 's! //!\nhttp://!' | tail -n1)
+        fi
 #		$curlbin --referer ${PAGE} -o $TMP/cache.$PARSER.$INPUT.$FROM.2 ${URL}
 
 	fi
