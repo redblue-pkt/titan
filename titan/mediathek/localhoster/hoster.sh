658eab58e2a5129bdad227c182d9a9893f7efd86
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2020-12-18 00:58:53 +0000

    tithek voe disable proxy

--- titan/mediathek/localhoster/hoster.sh
+++ titan/mediathek/localhoster/hoster.sh
@@ -397,6 +397,7 @@ streamcrypt()
 
 voe() 
 {
+	curlbin=$(echo $curlbin | sed "s!$PROXY!!")
 	if [ "$ARCH" == "sh4" ];then
 		URL=`$curlbin "$INPUT" | sed -nr "s/.*src: '([^']+)'.*/\1/p" | sed 's/https:/http:/g'` 
 	else
