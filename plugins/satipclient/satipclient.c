853cf88e2a62fd8722cd92b7bcd97252f4430cb5
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2021-02-14 14:13:45 +0000

    add satipclient building stuff

---
+++ plugins/satipclient/satipclient.c
@@ -0,0 +1,38 @@
+#include "../titan/struct.h"
+#include "../titan/debug.h"
+#include "../titan/header.h"
+#include "minisatip.h"
+
+char pluginname[] = "SAT-IP Client";
+char plugindesc[] = "SAT-IP Client Settings";
+char pluginpic[] = "%pluginpath%/satipclient/plugin.png";
+
+int pluginaktiv = 0;
+int pluginversion = PLUGINVERSION;
+
+//wird beim laden ausgefuehrt
+void init(void)
+{
+	char* tmpstr = NULL;
+
+	tmpstr = createpluginpath("/satipclient/skin.xml", 0);
+	readscreen(tmpstr, 207, 1);
+	free(tmpstr); tmpstr = NULL;
+
+	pluginaktiv = 1;
+	debug(10, "satipclient Plugin loaded !!!");
+}
+
+//wird beim entladen ausgefuehrt
+void deinit(void)
+{
+	delmarkedscreen(207);
+	pluginaktiv = 0;
+	debug(10, "satipclient Plugin removed !!!");
+}
+
+//wird in der Pluginverwaltung bzw Menue ausfeguehrt
+void start(void)
+{
+	screensatipclient();
+}
---
+++ plugins/satipclient/satipclient.h
@@ -0,0 +1,96 @@
+#ifndef SATIPCLIENT_H
+#define SATIPCLIENT_H
+
+void screensatipclient()
+{
+	int rcret = -1, ret = 0;
+	struct skin* satipclient = getscreen("satipclientsettings");
+	struct skin* listbox = getscreennode(satipclient, "listbox");
+	char* tmpstr = NULL, *satipclientstop = NULL, *satipclientstart = NULL;
+	struct skin* tmp = NULL;
+	struct skin* node = NULL;
+
+	satipclientstop = ostrcat("/etc/init.d/satipclient stop", NULL, 0, 0);
+	satipclientstart = ostrcat("/etc/init.d/satipclient start", NULL, 0, 0);
+
+	addscreenrc(satipclient, listbox);
+	listbox->aktline = 1;
+	listbox->aktpage = 1;
+
+	node = addlistbox(satipclient, listbox, node, 1);
+	if(node != NULL)
+	{
+		node->type = CHOICEBOX;
+		changetext(node, _("autostart")); changename(node, "satipclient");
+		addchoicebox(node, "n", _("no")); addchoicebox(node, "y", _("yes"));
+		setchoiceboxselection(node, getownconfig(node->name));
+	}
+
+	drawscreen(satipclient, 0, 0);
+	tmp = listbox->select;
+
+	while(1)
+	{
+		addscreenrc(satipclient, tmp);
+
+		tmp = listbox->select;
+
+		if(listbox->select != NULL)
+		{
+			addownconfigscreentmp(listbox->select->name, listbox->select);
+		}
+
+		if(rcret == getrcconfigint("rcexit", NULL)) break;
+
+		if(rcret == getrcconfigint("rcok", NULL))
+		{
+			debug(10, "cmd: %s", satipclientstop);
+			system(satipclientstop);
+			debug(10, "cmd: %s", satipclientstart);
+			ret = system(satipclientstart);
+			if(ret == 0)
+				textbox(_("Message"), _("SAT-IP Client started and config saved"), _("OK"), getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, NULL, 0, 600, 200, 5, 0);
+			else
+				textbox(_("Message"), _("SAT-IP Client not started,\nPlease check your config."), _("OK"), getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, NULL, 0, 600, 200, 0, 0);
+			drawscreen(satipclient, 0, 0);			
+			writeownconfigtmp();
+			writeallconfig(1);
+			break;
+		}
+
+		if(rcret == getrcconfigint("rcred", NULL))
+		{
+			debug(10, "cmd: %s", satipclientstop);
+			system(satipclientstop);
+			textbox(_("Message"), _("SAT-IP Client now stopped"), _("OK"), getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, NULL, 0, 600, 200, 5, 0);
+			drawscreen(satipclient, 0, 0);
+		}
+
+		if(rcret == getrcconfigint("rcgreen", NULL))
+		{
+			debug(10, "cmd: %s", satipclientstop);
+			system(satipclientstop);
+			debug(10, "cmd: %s", satipclientstart);
+			ret = system(satipclientstart);
+			if(ret == 0)
+				textbox(_("Message"), _("SAT-IP Client started."), _("OK"), getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, NULL, 0, 600, 200, 5, 0);
+			else
+				textbox(_("Message"), _("SAT-IP Client not started,\nPlease check your config."), _("OK"), getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, NULL, 0, 600, 200, 0, 0);
+			drawscreen(satipclient, 0, 0);
+		}
+
+		if(rcret == getrcconfigint("rcyellow", NULL))
+		{
+			screentunerconfig();
+			writeallconfig(1);
+			drawscreen(satipclient, 0, 0);
+		}
+	}
+
+	delownconfigtmpall();
+	delmarkedscreennodes(satipclient, 1);
+	delownerrc(satipclient);
+	clearscreen(satipclient);
+}
+
+#endif
