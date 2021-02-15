c0668a45033ef15992d022b19a37cbe729f04796
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2021-02-15 03:11:36 +0000

    fix

--- titan/plugins/satipclient/satipclient.h
+++ titan/plugins/satipclient/satipclient.h
@@ -12,8 +12,8 @@ void screensatipclient()
 
 	satipclientstop = ostrcat("/etc/init.d/satipclient stop", NULL, 0, 0);
 	satipclientstart = ostrcat("/etc/init.d/satipclient start", NULL, 0, 0);
-	satipclientscan = ostrcat(createpluginpath("/satipclient/files/scan.py", 0), NULL, 0, 0);
-	satipclientrun = ostrcat(createpluginpath("/satipclient/files/run.sh", 0), NULL, 0, 0);
+	satipclientscan = createpluginpath("/satipclient/files/scan.py", 0);
+	satipclientrun = createpluginpath("/satipclient/files/run.sh", 0);
 
 	addscreenrc(satipclient, listbox);
 	listbox->aktline = 1;
