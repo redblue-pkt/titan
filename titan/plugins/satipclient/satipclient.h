dd515955c9a815634181c40b9cd200333a9364a5
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2021-02-17 02:47:55 +0000

    update satipclient and fix last pos if not press ok/exit

--- titan/plugins/satipclient/satipclient.h
+++ titan/plugins/satipclient/satipclient.h
@@ -3,10 +3,12 @@
 
 void screensatipclient()
 {
-	int rcret = -1, ret = 0;
+	int rcret = -1, ret = 0, i = 0, j = 0, count1 = 0, count2 = 0;
 	struct skin* satipclient = getscreen("satipclientsettings");
 	struct skin* listbox = getscreennode(satipclient, "listbox");
-	char* tmpstr = NULL, *satipclientstop = NULL, *satipclientstart = NULL, *satipclientscan = NULL, *satipclientrun = NULL, *satipclientconfig = NULL, *cmd = NULL;
+	char* tmpstr = NULL,*tmpstr1 = NULL, *tmpstr2 = NULL, *satipclientstop = NULL, *satipclientstart = NULL, *satipclientscan = NULL, *satipclientrun = NULL, *satipclientconfig = NULL, *cmd = NULL;
+	char* name = NULL, *text = NULL, *type = NULL, *ip = NULL, *showname = NULL, *desc = NULL, *vtunerconf = NULL;
+	
 	struct skin* tmp = NULL;
 	struct skin* node = NULL;
 
@@ -20,14 +22,77 @@ void screensatipclient()
 	listbox->aktline = 1;
 	listbox->aktpage = 1;
 
-	node = addlistbox(satipclient, listbox, node, 1);
-	if(node != NULL)
-	{
-		node->type = CHOICEBOX;
-		changetext(node, _("autostart")); changename(node, "satipclient");
-		addchoicebox(node, "n", _("no")); addchoicebox(node, "y", _("yes"));
-		setchoiceboxselection(node, getownconfig(node->name));
+	vtunerconf = ostrcat("/mnt/network/vtuner.conf", NULL, 0, 0);
+
+	struct clist *myconfig[LISTHASHSIZE] = {NULL};
+	readconfig(vtunerconf, myconfig);
+
+	cmd = ostrcat("cat ", vtunerconf, 0, 0);
+	cmd = ostrcat(cmd, " | cut -d'=' -f2", 1, 0);
+
+	struct splitstr* ret1 = NULL;
+	tmpstr1 = string_newline(command(cmd));
+
+	ret1 = strsplit(tmpstr1, "\n", &count1);
+	if(ret1 != NULL && count1 > 0)
+	{	
+		for(i = 0; i < count1; i++)
+		{
+			printf("round i-count1 (%d/%d)\n", i, count1);
+			count2 = 0;
+			tmpstr2 = string_newline(command(cmd));
+			struct splitstr* ret2 = NULL;
+
+			node = addlistbox(satipclient, listbox, node, 1);
+			if(node != NULL)
+			{
+				node->type = CHOICEBOX;
+				text = ostrcat(_("VTUNER"), " (", 0, 0);
+				text = ostrcat(text, oitoa(i), 1, 1);
+				text = ostrcat(text, ")", 1, 0);
+				name = ostrcat(oitoa(i), NULL, 1, 0);
+				changetext(node, text);
+				changename(node, name);
+
+				ret2 = strsplit(tmpstr2, "\n", &count2);
+				if(ret2 != NULL && count2 > 0)
+				{
+					for(j = 0; j < count2; j++)
+					{
+						desc = oregex(".*desc:(.*),ipaddr:.*", ret2[j].part);
+						if(desc != NULL)
+						{
+							showname = ostrcat(showname, desc, 1, 0);
+							showname = ostrcat(showname, " (", 1, 0);
+							ip = oregex(".*,ipaddr:(.*),tuner_type:.*", ret2[j].part);
+							showname = ostrcat(showname, ip, 1, 0);
+							showname = ostrcat(showname, ") ", 1, 0);
+							type = oregex(".*,tuner_type:(.*),uuid:.*", ret2[j].part);
+							showname = ostrcat(showname, type, 1, 0);
+						}
+						else
+						{
+							showname = ostrcat(showname, _("is not used"), 1, 0);
+						}
+
+						addchoicebox(node, ret2[j].part, showname);
+						printf("addchoicebox: showname=%s entry=%s\n", showname, ret2[j].part);
+
+						free(type), type = NULL;
+						free(ip), ip = NULL;
+						free(desc), desc = NULL;
+						free(showname), showname = NULL;
+					}
+				}
+				free(ret2), ret2 = NULL;
+				setchoiceboxselection(node, getlist(myconfig, node->name, NULL));
+				free(text), text = NULL;
+				free(name), name = NULL;
+			}
+		}
 	}
+	free(ret1), ret1 = NULL;
+	free(cmd), cmd = NULL;
 
 	drawscreen(satipclient, 0, 0);
 	tmp = listbox->select;
@@ -39,13 +104,13 @@ void screensatipclient()
 		tmp = listbox->select;
 
 		if(listbox->select != NULL)
-		{
-			addownconfigscreentmp(listbox->select->name, listbox->select);
-		}
+			addlist(myconfig, listbox->select->name, listbox->select->ret);
 
 		if(rcret == getrcconfigint("rcexit", NULL)) break;
 		if(rcret == getrcconfigint("rcok", NULL))
 		{
+			writelist(myconfig, vtunerconf);
+
 			debug(10, "cmd: %s", satipclientconfig);
 			system(satipclientconfig);
 
@@ -58,8 +123,7 @@ void screensatipclient()
 			else
 				textbox(_("Message"), _("SAT-IP Client not started,\nPlease check your config."), _("OK"), getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, NULL, 0, 600, 200, 0, 0);
 			drawscreen(satipclient, 0, 0);			
-			writeownconfigtmp();
-			writeallconfig(1);
+
 			break;
 		}
 		else if(rcret == getrcconfigint("rcred", NULL))
@@ -71,6 +135,8 @@ void screensatipclient()
 		}
 		else if(rcret == getrcconfigint("rcgreen", NULL))
 		{
+			writelist(myconfig, vtunerconf);
+
 			debug(10, "cmd: %s", satipclientconfig);
 			system(satipclientconfig);
 
@@ -107,6 +173,7 @@ void screensatipclient()
 		}
 	}
 
+	free(vtunerconf), vtunerconf = NULL;
 	free(satipclientstart), satipclientstart = NULL;
 	free(satipclientstop), satipclientstop = NULL;
 	free(satipclientscan), satipclientscan = NULL;
