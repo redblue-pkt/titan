cf2d4363c35dcea00ff98ed864fbd31d33bddb8f
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-02-23 02:07:13 +0000

    test

--- titan/plugins/mc/mc_videoplayer.h
+++ titan/plugins/mc/mc_videoplayer.h
@@ -336,8 +336,17 @@ void screenmc_videoplayer()
 //				recthumbfirstthread = addtimer(&createrecthumbfirstthread, START, 1000, 1, NULL, NULL, NULL);
 
 //            filename = createpath(filelistpath->text, filelist->select->name);
+            drawscreen(apskin, 0, 0);
         	struct stimerthread *recthumbfirstthread = NULL;
 		    recthumbfirstthread = addtimer(&createrecthumbfirstthread, START, 1000, 1, (void*)ostrcat(filelistpath->text, NULL, 0, 0), (void*)ostrcat(filelist->select->name, NULL, 0, 0), NULL);
+
+			//wait for recthumblastthread end before shutdown
+			int count = 0;
+			while(gettimer(recthumbfirstthread) != NULL && count < 60)
+			{
+				sleep(1);
+				count++;
+			}
 
             char* cmd = NULL;
 	        if(checkchipset("3798MV200") == 1)
