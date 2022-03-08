47ed4af50fe92786bd5d934d3bec56e366290cd0
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-03-08 17:03:43 +0000

    test

--- titan/titan/header.h
+++ titan/titan/header.h
@@ -757,11 +757,11 @@ int changemask(struct skin* node, char* text);
 int changepicmem(struct skin* node, char* text, int timeout, int del);
 int convertxmlentry(char *value, uint8_t *proz);
 unsigned char* savejpg(char* filename, int width, int height, int channels, int newwidth, int newheight, int quality, unsigned char *buf);
-inline void drawpixelfastfb(struct fb* node, int posx, int posy, unsigned long color);
-inline void drawpixelfb(struct fb* node, int posx, int posy, unsigned long color);
+//static inline void drawpixelfastfb(struct fb* node, int posx, int posy, unsigned long color);
+//static inline void drawpixelfb(struct fb* node, int posx, int posy, unsigned long color);
 unsigned char * resize(unsigned char *origin, int ox, int oy, int dx, int dy, int type, unsigned char * dst, int flag);
-inline void drawpixelfast(int posx, int posy, unsigned long color);
-inline void drawpixel(int posx, int posy, unsigned long color);
+//static inline void drawpixelfast(int posx, int posy, unsigned long color);
+//static inline void drawpixel(int posx, int posy, unsigned long color);
 static inline unsigned long getpixelbuf(char* buf, int posx, int posy, int bufwidth);
 
 //volume.h
