983e1e4b04eaf2f4bab1a154a3d4dded26804725
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-08-04 22:52:23 +0000

    fix

--- titan/channel.h
+++ titan/channel.h
@@ -328,7 +328,6 @@ struct channel* addchannel(char *line, int count, struct channel* last)
         if(epgurl != NULL && ostrcmp("(null)", epgurl) != 0)
         {
         	newnode->epgurl = ostrcat(streamurl, NULL, 0, 0);
-            printf("set newnode->epgurl: %s\n", epgurl);
     		debug(202, "set newnode->epgurl:", epgurl);
         }
         else
