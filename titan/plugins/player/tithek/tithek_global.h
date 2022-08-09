47634de1f779c8e8d3f39928e5617156821bdb5a
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-08-09 12:25:10 +0000

    test

--- titan/plugins/player/tithek/tithek_global.h
+++ titan/plugins/player/tithek/tithek_global.h
@@ -1989,6 +1989,23 @@ void servicebouquets_update(int flag)
 			free(tmpstr2), tmpstr2 = NULL;
 		}
 		free(ret1), ret1 = NULL;
+
+        if(getconfigint("tithek_servicebouquets_autoupdate_msg", NULL) == 1)
+    		textbox(_("Message"), _("Titan will be reloaded Channellist!"), _("OK"), getrcconfigint("rcok", NULL), NULL, 0, NULL, 0, NULL, 0, 1000, 200, 0, 0);
+	    debug(202, "Titan will be reloaded channellist!");
+        freesat();
+        freeallbouquet();
+        freemainbouquet(0);
+        freechannel(0);
+        freetransponder();
+        freeprovider();
+        ret = readsat(getconfig("satfile", NULL));
+        ret = readtransponder(getconfig("transponderfile", NULL));
+        ret = readprovider(getconfig("providerfile", NULL));
+        ret = readchannel(getconfig("channelfile", NULL));
+        ret = readtransponderencoding(getconfig("transponderencodingfile", NULL));
+        ret = readmainbouquet(getconfig("bouquetfile", NULL));
+        ret = readallbouquet();
     }
 }
 
