203273647ac8bc8f2d2e283cc7b8009e23e773b4
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-08-11 11:26:20 +0000

    fix

--- titan/mediathek/localparser_secret/vavoo.sh
+++ titan/mediathek/localparser_secret/vavoo.sh
@@ -215,13 +215,7 @@ search()
         if [ ! -e /tmp/settings ];then mkdir /tmp/settings; fi
         if [ -e /mnt/settings/bouquets.tithek.autoupdate."$NAME"."$NEXT".tv ];then
             echo "LIST=$(cat /mnt/settings/bouquets.tithek.autoupdate."$NAME"."$NEXT".tv | sed 's/0#/#/g' | tr '\n' '#' | sed 's/##/#\\|#/g' | sed "s/^#/'/" | sed -e "s/#$/'/g")" >$TMP/$FILENAME.cmd.list
-            #LIST=$(cat /mnt/settings/bouquets.tithek.autoupdate.VaVoo.Germany.tv | sed 's/0#/#/g' | tr '\n' '#' | sed 's/##/#\\|#/g' | sed "s/^#/'/" | sed -e "s/#$/'/g")
-            #cat /mnt/settings/channel | grep -v $LIST| wc -l
-            if [ ! -z "$LIST" ];then
-                echo "cat /mnt/settings/channel | grep -v $LIST > /tmp/settings/channel.tmp" >> $TMP/$FILENAME.cmd.list
-            else
-                echo "cp /mnt/settings/channel /tmp/settings/channel.tmp" > $TMP/$FILENAME.cmd.list
-            fi
+            echo "cat /mnt/settings/channel | grep -v \$LIST >> /tmp/settings/channel.tmp" >> $TMP/$FILENAME.cmd.list
             echo "cp /mnt/settings/bouquets.tithek.autoupdate."$NAME"."$NEXT".tv /mnt/settings/bouquets.tithek.autoupdate."$NAME"."$NEXT".tv.org" >> $TMP/$FILENAME.cmd.list
         else
             echo "cp /mnt/settings/channel /tmp/settings/channel.tmp" > $TMP/$FILENAME.cmd.list
