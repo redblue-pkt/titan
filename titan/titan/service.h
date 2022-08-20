020a4a849c49caba3b338580b75ef8e3f381f6c7
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-08-20 08:35:23 +0000

    add service stop before iptv playback

--- titan/titan/service.h
+++ titan/titan/service.h
@@ -160,6 +160,7 @@ int servicestartreal(struct channel* chnode, char* channellist, char* pin, int f
         printf("playerstart1 epgurl: %s\n", chnode->epgurl);
         addconfigtmp("playerbuffersize", "0");
         addconfigtmp("playerbufferseektime", "0");
+        servicestop(status.aktservice, 1, 1);
         playerstart(chnode->streamurl);
         status.play = 2;
 		delconfigtmp("playerbuffersize");
@@ -545,6 +546,7 @@ int servicestartreal(struct channel* chnode, char* channellist, char* pin, int f
             printf("playerstart2 epgurl: %s\n", chnode->epgurl);
             addconfigtmp("playerbuffersize", "0");
             addconfigtmp("playerbufferseektime", "0");
+            servicestop(status.aktservice, 1, 1);
             playerstart(chnode->streamurl);
             status.play = 2;
 			delconfigtmp("playerbuffersize");
