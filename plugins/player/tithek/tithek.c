04917ab09e3bfb08f9f40bd9c7ccee3cd1fa041f
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-10-18 15:12:13 +0000

    fix kino url

--- plugins/player/tithek/tithek.c
+++ plugins/player/tithek/tithek.c
@@ -125,7 +125,7 @@ void start(void)
 	status.listboxselectcol = convertcol("tithek_selectcol");
 
 	if(getconfig("tithek_kinox_url", NULL) == NULL)
-		addconfigtmp("tithek_kinox_url", "https://kinoz.to");
+		addconfigtmp("tithek_kinox_url", "https://kinos.to");
 
 	if(getconfig("tithek_kinox_localhoster", NULL) == NULL)
 		addconfigtmp("tithek_kinox_localhoster", "1");
