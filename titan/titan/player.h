bce5b310880a6e1ae6c98cf5744d8979bbaa8505
gost <gost@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-11-11 22:41:59 +0000

    fix Start at last position

--- titan/titan/player.h
+++ titan/titan/player.h
@@ -1432,6 +1432,8 @@ int playerstart(char* file)
 						off64_t seekpos = atoll(skip1);
 						sec = seekpos / 90000;
 						printf("++++ seek sec: %d\n", sec);
+						usleep(100000);
+						playerseek(sec);
 					}
 					free(skip1); skip1 = NULL;
 				}
