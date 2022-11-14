5b5fbfeb10c0039e540eb3e518935ff578a617aa
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-11-14 19:21:46 +0000

    hid secret feed and cleanup struct

--- titan/titan/security.h
+++ titan/titan/security.h
@@ -1320,6 +1320,9 @@ void setskinnodeslocked(int flag)
                 if(ostrcmp("extensions_mediatpkinstall", child->name) == 0) child->locked = 0;
             }
 
+			// disable unused menu on oebuild
+			if(ostrcmp("feed_ipk", child->name) == 0) child->locked = 1;
+
 			// hide pvr / timeshift
 			if(status.pvr == 0)
 			{
