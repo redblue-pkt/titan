18b6e21dd4e11cdb8d2b258dc8cb16212e74c15b
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2022-11-25 12:53:28 +0000

    fix

---
+++ titan/plugins/tools/lcdpearl1/files/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+PWD=`echo $0 | sed -e s/start.sh//g`
+#PWD=`pwd`
+export LD_LIBRARY_PATH=$PWD/libs
+cd $PWD
+#./lcd4linux -f $PWD/lcd4linux.conf $1
+./lcd4linux -f /var/etc/lcd4linux.conf $1
+exit
