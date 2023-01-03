d5fec861a59d367dfbb08cff07ea490b96da6b10
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2023-01-03 19:29:20 +0000

    enable networkbrowser.sh

---
+++ titan/plugins/network/networkbrowser/networkbrowser.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+
+RANGE=$1
+portlist="21 445"
+
+rm /tmp/netbrowser.portscan.log > /dev/null 2>&1
+
+for PORT in $portlist; do
+	portscan 5 $RANGE $PORT 0 255 >> /tmp/netbrowser.portscan.log
+done
+
+arp -a > /tmp/netbrowser.arp.log
+
+cat /tmp/netbrowser.portscan.log | cut -d" " -f1 | sort -u >/tmp/netbrowser.portscan.ip.log
+
+while read -u 3 ROUND; do
+	echo ROUND $ROUND
+	while read -u 3 ROUND2; do
+		IP=$(echo $ROUND2 | awk '{ print $2 }'  | cut -d"(" -f2 | cut -d")" -f1)
+		DNS=$(echo $ROUND2 | awk '{ print $1 }')
+		if [ "$IP" == "$ROUND" ];then
+			echo "found $DNS ($IP)"
+		fi
+	done 3</tmp/netbrowser.arp.log
+	
+done 3</tmp/netbrowser.portscan.ip.log
