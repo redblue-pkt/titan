46523e2e7efe2e8cbb4147b358525543e4b00194
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2023-01-09 23:00:54 +0000

    fix

--- plugins/network/networkbrowser/networkbrowser.h
+++ plugins/network/networkbrowser/networkbrowser.h
@@ -1096,15 +1096,15 @@ void getnetworkbrowser_cifs(struct menulist** mlist, char* s, char* r, char* u, 
 			tmpstr2 = string_resub("---------      ----      -------", "This machine has a browse list:", tmpstr1, 0);
 		else
 			tmpstr2 = string_resub("---------       ----      -------", "Server               Comment", tmpstr1, 0);
-printf("tmpstr1 1%s", tmpstr2);
+printf("tmpstr1 1 %s\n", tmpstr2);
 
 		if(tmpstr2 == NULL)
 			tmpstr2 = oregex("---------       ----      -------(.*)", tmpstr1);
-printf("tmpstr1 2%s", tmpstr2);
+printf("tmpstr1 2 %s\n", tmpstr2);
 
-//		if(tmpstr2 == NULL)
+		if(tmpstr2 == NULL)
 			tmpstr2 = string_resub("---------       ----      -------", "SMB1 disabled -- no workgroup available", tmpstr1, 0);
-printf("tmpstr1 3%s", tmpstr2);
+printf("tmpstr1 3 %s\n", tmpstr2);
 
 		debug(70, "------ result 2 ------");
 		debug(70, "%s", tmpstr2);
