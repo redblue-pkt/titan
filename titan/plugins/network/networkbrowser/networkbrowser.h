b91114f529cd4894c70775c0aeb26678f3084059
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2023-01-09 22:37:21 +0000

    test

--- titan/plugins/network/networkbrowser/networkbrowser.h
+++ titan/plugins/network/networkbrowser/networkbrowser.h
@@ -1102,7 +1102,7 @@ printf("tmpstr1 1%s", tmpstr2);
 			tmpstr2 = oregex("---------       ----      -------(.*)", tmpstr1);
 printf("tmpstr1 2%s", tmpstr2);
 
-		if(tmpstr2 == NULL)
+//		if(tmpstr2 == NULL)
 			tmpstr2 = string_resub("---------       ----      -------", "SMB1 disabled -- no workgroup available", tmpstr1, 0);
 printf("tmpstr1 3%s", tmpstr2);
 
