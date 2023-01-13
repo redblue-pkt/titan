3239efe7df00da4dfe07bc486e3ff3fd88d86bd0
obi <obi@dc659be2-726c-4bf2-b84f-ff68d3143153>
2023-01-13 13:15:41 +0000

    fix gmedia

--- titan/plugins/player/gmediarender/gmediarender.sh
+++ titan/plugins/player/gmediarender/gmediarender.sh
@@ -3,14 +3,20 @@
 
 echo start gmediarender
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/var/swap/lib:/mnt/swapextensions/lib:/var/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/var/swap/usr/lib:/mnt/swapextensions/usr/lib:/var/usr/lib
 export PATH=$PATH:/var/swap/bin:/mnt/swapextensions/bin:/var/bin
-export PATH=$PATH:/var/swap/usr/bin:/mnt/swapextensions/usr/bin:/usr/bin
+export PATH=$PATH:/var/swap/usr/bin:/mnt/swapextensions/usr/bin:/var/usr/bin
 
 if [ -e /etc/.oebuild ] || [ -e /etc/.ovbuild ];then
 	check=`opkg list_installed titan-gmediarender | wc -l`
 	echo "check: $check"
 	if [ "$check" == 0 ];then
 		opkg install titan-gmediarender --force-reinstall
+	fi
+	check=`opkg list_installed libupnp | wc -l`
+	echo "check: $check"
+	if [ "$check" == 0 ];then
+		opkg install libupnp --force-reinstall
 	fi
 fi
 
