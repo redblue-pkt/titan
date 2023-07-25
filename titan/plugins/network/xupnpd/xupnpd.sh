e58573906571e0c759424499f74e164cecfe62c4
aafsvn <aafsvn@dc659be2-726c-4bf2-b84f-ff68d3143153>
2023-07-25 00:07:28 +0000

    update xupnpd

--- titan/plugins/network/xupnpd/xupnpd.sh
+++ titan/plugins/network/xupnpd/xupnpd.sh
@@ -1,3 +1,4 @@
+
 #! /bin/sh
 #
 ### BEGIN INIT INFO
@@ -7,14 +8,21 @@
 # Short-Description:    xupnpd Server
 ### END INIT INFO
 
-export PATH=$PATH:/var/swap/bin:/mnt/swapextensions/bin:/var/bin
+if [ -e /etc/.ddtbuild ];then
+    export PATH=$PATH:/var/swap/bin:/mnt/swapextensions/bin:/var/bin
 
-if [ -e /mnt/swapextensions/bin/xupnpd ];then
-	INSTDIR=/mnt/swapextensions
+    if [ -e /mnt/swapextensions/bin/xupnpd ];then
+	    INSTDIR=/mnt/swapextensions
+    else
+	    INSTDIR=/var/swap
+    fi	
+    export XUPNPDROOTDIR=$INSTDIR/etc/xupnpd
 else
-	INSTDIR=/var/swap
-fi	
-export XUPNPDROOTDIR=$INSTDIR/etc/xupnpd
+    INSTDIR=`echo $0 | sed -e s/xupnpd.sh//g`
+
+    export XUPNPDROOTDIR=$INSTDIR/files
+    export PATH=$PATH:/var/swap/bin:/mnt/swapextensions/bin:/var/bin:$INSTDIR
+fi
 
 model=`cat /etc/model`
 NAME=xupnpd
@@ -23,10 +31,15 @@ DAEMON=xupnpd
 USER=root
 GROUP=root
 IP=`ifconfig | grep inet | grep Bcast | awk '{print $2}' | cut -d":" -f2`
+PORT=`cat /mnt/config/titan.cfg | grep stream_port | cut -d"=" -f2` 
  
 case $1 in
   start)
-	bouquet2m3u http://$IP:22222/ $INSTDIR/etc/xupnpd/playlists
+    if [ -e /etc/.ddtbuild ];then
+    	bouquet2m3u $IP $PORT $INSTDIR/etc/xupnpd/playlists
+    else
+    	bouquet2m3u $IP $PORT $INSTDIR/files/playlists
+    fi
     start-stop-daemon --verbose --start --exec xupnpd
   ;;
   stop)
