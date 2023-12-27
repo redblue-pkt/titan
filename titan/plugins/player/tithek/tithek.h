1962d465cd22c0532f02b39d070b4ff023335b87
aafsvn <aafsvn@dc659be2-726c-4bf2-b84f-ff68d3143153>
2023-12-27 19:08:21 +0000

    fix

--- titan/plugins/player/tithek/tithek.h
+++ titan/plugins/player/tithek/tithek.h
@@ -2049,7 +2049,7 @@ void submenu(struct skin* listbox, struct skin* load, char* title)
 		filename = createfilename(title, ((struct tithek*)listbox->select->handle)->title, 0);
 
 printf("tithek>submenu free status.actplaypts: %s\n", status.actplaypts);
-		free(status.actplaypts);
+//		free(status.actplaypts);
 		status.actplaypts = ostrcat(filename, ".pts", 0, 0);
 printf("tithek>submenu set status.actplaypts: %s\n", status.actplaypts);
 		if(!file_exist(filename))
